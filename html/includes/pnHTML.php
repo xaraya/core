d4004a3cbad612e5503df061148026525fdcb7ba
John.Cox <John.Cox@d38yrl11.>
2002-10-27 20:02:10 +0000

    html/includes/pnBLCompiler.php: 	#  changed standard headers html/includes/pnBlocks.php: 	#  changed standard headers html/includes/pnConfig.php: 	#  changed standard headers html/includes/pnCore.php: 	#  changed standard headers html/includes/pnDB.php: 	#  changed standard headers html/includes/pnEvt.php: 	#  changed standard headers html/includes/pnException.php: 	#  changed standard headers html/includes/pnHTML.php: 	#  changed standard headers html/includes/pnLegacy.php: 	#  changed standard headers html/includes/pnLog.php: 	#  changed standard headers html/includes/pnMLS.php: 	#  changed standard headers html/includes/pnMod.php: 	#  changed standard headers html/includes/pnSecurity.php: 	#  changed standard headers html/includes/pnServer.php: 	#  changed standard headers html/includes/pnSession.php: 	#  changed standard headers html/includes/pnTableDDL.php: 	#  changed standard headers html/includes/pnTemplate.php: 	#  changed standard headers html/includes/pnUser.php: 	#  changed standard headers html/includes/pnVar.php: 	#  changed standard headers

--- html/includes/pnHTML.php
+++ html/includes/pnHTML.php
@@ -1,22 +1,9 @@
-<?php // File: $Id$
+<?php 
+// File: $Id$
 // ----------------------------------------------------------------------
-// PostNuke Content Management System
-// Copyright (C) 2001 by the PostNuke Development Team.
-// http://www.postnuke.com/
-// ----------------------------------------------------------------------
-// LICENSE
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License (GPL)
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// Xaraya eXtensible Management System
+// Copyright (C) 2002 by the Xaraya Development Team.
+// http://www.xaraya.org
 // ----------------------------------------------------------------------
 // Original Author of file: Jim McDonald
 // Purpose of file: HTML helpers
