86268a4748078b0995db276b7267d23e089b616c
andyv <andyv@andyv.local>
2002-12-03 20:02:12 +0000

    html/modules/adminpanels/xarblocks/adminmenu.php: 	#  templating adminmenu block html/modules/adminpanels/xartemplates/blocks/sidemenu.xd: 	#  BitKeeper file /Users/andyv/Sites/xaraya/html/modules/adminpanels/xartemplates/blocks/sidemenu.xd

--- html/modules/adminpanels/xarblocks/adminmenu.php
+++ html/modules/adminpanels/xarblocks/adminmenu.php
@@ -56,68 +56,51 @@ function adminpanels_adminmenublock_display($blockinfo){
 	    return;
 	}
     
-    // we need to hide the adminmenu block 
-    // if the current module is not an admin_capable and if we are not in the admin part..
-    // we also want to hide other centre blocks
-    // hack atm, because I couldn't find proper API functions for this situation
-    // NOTE_TO_SELF: prolly need to move this to adminapi
-//    list($dbconn) = xarDBGetConn();
-//    $xartable = xarDBGetTables();
-//    $modulestable = $xartable['modules'];
-//
-//    $query = "SELECT xar_admin_capable
-//              FROM $modulestable
-//              WHERE xar_name ='". xarVarPrepForStore(xarModGetName()) ."'";
-//
-//    $result = $dbconn->Execute($query);
-//
-//    if($dbconn->ErrorNo() != 0) {
-//        return;
-//    }
-//
-//    if ($result->EOF) {
-//        return false;
-//    }
-//    list($state) = $result->fields;
-//    $result->Close();
-// 
-    // are we in the admin part of the module?
-    // NOTE_TO_SELF: will it hold water with new php versions?
-//    $isadmin = preg_match("/admin/i", xarServerGetVar("REQUEST_URI"));
-
-    // removed lots of commented out stuff below
-
-    // Get variables from content block
-//    $vars = unserialize($blockinfo['content']);
-
-    // which module is currently loaded?
-//    $thismod = xarModGetName(); // moved to xaradminapi
-    
-    
     // TODO: display content sensitive link to the manual-online_help
-    
-    // prepare the show
-    xarModAPILoad('adminpanels', 'admin');
-    
-    // do we need to update the menu modules and categories in db table?
-    if(!xarModAPIFunc('adminpanels', 'admin', 'updatemenudb')){
-        return; // we're outa luck
+    $marker = xarModGetVar('adminpanels', 'marker');
+    if(!isset($marker)){
+        xarModSetVar('adminpanels' ,'marker', '[x]');
+        $marker = '[x]';
     }
+    
+    if(!xarModGetVar('adminpanels', 'showold')){
+        $marker = '';
+    }
+    
+    // which module is loaded atm?
+    // not sure why this function should be private, how else can we do it?
+    // TODO: check with Marco about it
+    list($modName) = xarRequestGetInfo();
     
     // Sort Order Status and Links Display.
     $menustyle = xarModGetVar('adminpanels','menustyle');
     if($menustyle == 'byname'){
         // sort by name
-        $data = xarModAPIFunc('adminpanels', 'admin', 'buildbyname');
+//        $data = xarModAPIFunc('adminpanels', 'admin', 'buildbyname'); // obsolete
+        foreach($mods as $mod){
+            $label = $mod['name'];
+            $link = xarModURL($mod['name'] ,'admin', 'main', array());
+            // depending on which module is currently loaded we display accordingly
+            if($label == $modName){
+                $adminmods[] = array('label' => $label, 'link' => '', 'marker' => $marker);
+            }else{
+                $adminmods[] = array('label' => $label, 'link' => $link, 'marker' => '');
+            }
+        }
+        // prepare the data for template(s)
+        $data = xarTplBlock('adminpanels','sidemenu', array('adminmods' => $adminmods));
     }else if ($menustyle == 'bycat'){
         // sort by categories
+        xarModAPILoad('adminpanels', 'admin');
         $data = xarModAPIFunc('adminpanels', 'admin', 'buildbycat');
     }else if ($menustyle == 'byweight'){
         // sort by weight
-        $data = xarModAPIFunc('adminpanels', 'admin', 'buildbyname');
+        xarModAPILoad('adminpanels', 'admin');
+        $data = xarModAPIFunc('adminpanels', 'admin', 'buildbyweight');
     }else if ($menustyle == 'bygroup'){
         // sort by group
-        $data = xarModAPIFunc('adminpanels', 'admin', 'buildbyname');
+        xarModAPILoad('adminpanels', 'admin');
+        $data = xarModAPIFunc('adminpanels', 'admin', 'buildbygroup');
     } else {
         // default view by categories
         $data = xarModAPIFunc('adminpanels', 'admin', 'buildbycat');
@@ -146,25 +129,4 @@ function adminpanels_adminmenublock_update($blockinfo)
 
     // Return - nothing to update
     return $blockinfo;
-}
-
-/**
- * utility function to add module links
- */
-function addMenuStyledUrl($name, $url, $comment)
-{
-        // Bullet list
-        if (empty($url)) {
-            // Separator
-            if (empty($name)) {
-                $content = "<br />";
-            } else {
-                $content = "<br /><b>$name</b><br />";
-            }
-        } else {
-	    // End Bracket Linking
-            $content = "<strong><big>&middot;</big></strong>&nbsp;<a class=\"xar-normal\" href=\"$url\" title=\"$comment\">$name</a><br />";
-        }
-
-    return $content;
 }
