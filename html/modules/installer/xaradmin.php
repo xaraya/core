b06c3f7f014d99ceb2e28dd8c63864b75c564f9f
John.Cox <John.Cox@mcnabb.>
2002-12-14 20:02:12 +0000

    html/includes/xarCore.php: 	#  Fix the installer. html/install.php: 	#  Use installer theme in installer without having to change it inside the config file. html/includes/xarCore.php: 	#  Fix the installer. html/install.php: 	#  Use installer theme in installer without having to change it inside the config file. html/modules/installer/xaradmin.php: 	#  Fixed spelling error.

--- html/modules/installer/xaradmin.php
+++ html/modules/installer/xaradmin.php
@@ -357,7 +357,7 @@ function installer_admin_finish()
 
     $now = time();
 
-    $varshtml['html_content'] = 'Please deleate the install.php from your webroot .';
+    $varshtml['html_content'] = 'Please delete the install.php from your webroot .';
     $varshtml['expire'] = $now + 24000;
     $msg = serialize($varshtml);
 
