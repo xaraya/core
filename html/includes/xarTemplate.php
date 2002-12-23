06315780c534f82022369a3e4882027c34945e18
marcel <marcel@hsdev.com>
2002-12-23 20:02:12 +0000

    html/modules/dynamicdata/xaradmin.php: 	#  Consistant Exceptions for xarSecAuthAction. html/modules/dynamicdata/xaradminapi.php: 	#  Consistant Exceptions for xarSecAuthAction. html/modules/dynamicdata/xaruser.php: 	#  Consistant Exceptions for xarSecAuthAction. html/modules/dynamicdata/xaruserapi.php: 	#  Consistant Exceptions for xarSecAuthAction. html/modules/installer/xaradmin.php: 	#  Consistant Exceptions for xarSecAuthAction. html/modules/adminpanels/xarblocks/adminmenu.php: 	#  Implemented tree view for admin panels. html/modules/adminpanels/xartemplates/blocks/sidemenu.xd: 	#  Implemented tree view for admin panels. html/modules/adminpanels/xartemplates/admin-confirmlogout.xd: 	#  More work on the tree views. html/modules/adminpanels/xartemplates/admin-modifyconfig.xd: 	#  More work on the tree views. html/modules/base/xartemplates/admin-main.xd: 	#  More Tree Views for admin panels. html/modules/base/xartemplates/admin-sysinfo.xd: 	#  More Tree Views for admin panels. html/modules/blocks/xartemplates/admin-delete_group.xd: 	#  More Tree Views for admin panels. html/modules/blocks/xartemplates/admin-delete_instance.xd: 	#  More Tree Views for admin panels. html/modules/blocks/xartemplates/admin-main.xd: 	#  More Tree Views for admin panels. html/modules/blocks/xartemplates/admin-modify_group.xd: 	#  More Tree Views for admin panels. html/modules/blocks/xartemplates/admin-modify_instance.xd: 	#  More Tree Views for admin panels. html/modules/blocks/xartemplates/admin-new_group.xd: 	#  More Tree Views for admin panels. html/modules/blocks/xartemplates/admin-new_instance.xd: 	#  More Tree Views for admin panels. html/modules/blocks/xartemplates/admin-view_groups.xd: 	#  More Tree Views for admin panels. html/modules/blocks/xartemplates/admin-view_instances.xd: 	#  More Tree Views for admin panels. html/modules/blocks/xartemplates/admin-view_types.xd: 	#  More Tree Views for admin panels. html/modules/modules/xartemplates/admin-list.xd: 	#  Finishing touches on the tree view. html/modules/modules/xartemplates/admin-main.xd: 	#  Finishing touches on the tree view. html/modules/themes/xartemplates/admin-list.xd: 	#  Finishing touches on the tree view. html/modules/themes/xartemplates/admin-main.xd: 	#  Finishing touches on the tree view. html/modules/themes/xartemplates/admin-modify.xd: 	#  Finishing touches on the tree view. html/modules/themes/xartemplates/admin-modifyconfig.xd: 	#  Finishing touches on the tree view. html/modules/users/xartemplates/admin-createmail.xd: 	#  Finishing touches on the tree view. html/modules/users/xartemplates/admin-createuser.xd: 	#  Finishing touches on the tree view. html/modules/users/xartemplates/admin-main.xd: 	#  Finishing touches on the tree view. html/modules/users/xartemplates/admin-modifyconfig.xd: 	#  Finishing touches on the tree view. html/modules/adminpanels/xarversion.php: 	#  Added description to version file. html/includes/xarTemplate.php: 	#  On marcos request.

--- html/includes/xarTemplate.php
+++ html/includes/xarTemplate.php
@@ -227,10 +227,10 @@ function xarTpl_renderPage($mainModuleOutput, $otherModulesOutput = NULL, $templ
     $templateName = xarVarPrepForOS($templateName);
     $sourceFileName = "$xarTpl_themeDir/pages/$templateName.xt";
 
-    if ($xarTpl_headJavaScript) {
+    if ($xarTpl_headJavaScript !='') {
         $xarTpl_headJavaScript = "<script type=\"text/javascript\">\n<!--\n{$xarTpl_headJavaScript}\n// -->\n</script>";
     }
-    if ($xarTpl_bodyJavaScript) {
+    if ($xarTpl_bodyJavaScript !='') {
         $xarTpl_bodyJavaScript = "<script type=\"text/javascript\">\n<!--\n{$xarTpl_bodyJavaScript}\n// -->\n</script>";
     }
     
