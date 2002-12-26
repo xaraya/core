a903ad6f73fe93a0463b58e4404344451a56d782
John.Cox <John.Cox@mcnabb.>
2002-12-26 20:02:12 +0000

    html/modules/mail/class/class.phpmailer.php: 	#  BitKeeper file C:/documents and settings/john cox/my documents/development/www/dev/xaraya/html/modules/mail/class/class.phpmailer.php html/modules/mail/class/class.smtp.php: 	#  BitKeeper file C:/documents and settings/john cox/my documents/development/www/dev/xaraya/html/modules/mail/class/class.smtp.php html/modules/mail/xardocs/ChangeLog.txt: 	#  BitKeeper file C:/documents and settings/john cox/my documents/development/www/dev/xaraya/html/modules/mail/xardocs/ChangeLog.txt html/modules/mail/xardocs/LICENSE: 	#  BitKeeper file C:/documents and settings/john cox/my documents/development/www/dev/xaraya/html/modules/mail/xardocs/LICENSE html/modules/mail/xardocs/README: 	#  BitKeeper file C:/documents and settings/john cox/my documents/development/www/dev/xaraya/html/modules/mail/xardocs/README html/modules/mail/xarinit.php: 	#  BitKeeper file C:/documents and settings/john cox/my documents/development/www/dev/xaraya/html/modules/mail/xarinit.php html/modules/mail/xartemplates/admin-main.xd: 	#  BitKeeper file C:/documents and settings/john cox/my documents/development/www/dev/xaraya/html/modules/mail/xartemplates/admin-main.xd html/modules/mail/xartemplates/admin-modifyconfig.xd: 	#  BitKeeper file C:/documents and settings/john cox/my documents/development/www/dev/xaraya/html/modules/mail/xartemplates/admin-modifyconfig.xd html/modules/mail/xarversion.php: 	#  BitKeeper file C:/documents and settings/john cox/my documents/development/www/dev/xaraya/html/modules/mail/xarversion.php

---
+++ html/modules/mail/xarinit.php
@@ -0,0 +1,51 @@
+<?php
+// File: $Id$
+// ----------------------------------------------------------------------
+// Xaraya eXtensible Management System
+// Copyright (C) 2002 by the Xaraya Development Team.
+// http://www.xaraya.org
+// ----------------------------------------------------------------------
+// Original Author of file: John Cox via phpMailer Team 
+// Purpose of file:  Initialisation functions for the Mail Hook
+// ----------------------------------------------------------------------
+
+/**
+ * initialise the mail module
+ */
+function mail_init()
+{
+    xarModSetVar('mail', 'server', 'mail');
+    xarModSetVar('mail', 'adminname', 'Your Name');
+    xarModSetVar('mail', 'adminmail', 'Your Email');
+    xarModSetVar('mail', 'wordwrap', '50');
+    xarModSetVar('mail', 'priority', '3');
+    xarModSetVar('mail', 'smtpPort', '25');
+    xarModSetVar('mail', 'smtpHost', 'Your SMTP Host');
+    return true;
+}
+
+/**
+ * upgrade the mail module from an old version
+ */
+function mail_upgrade($oldversion)
+{
+    return false;
+}
+
+/**
+ * delete the mail module
+ */
+function mail_delete()
+{
+    xarModDelVar('mail', 'server');
+    xarModDelVar('mail', 'adminname');
+    xarModDelVar('mail', 'adminmail');
+    xarModDelVar('mail', 'wordwrap');
+    xarModDelVar('mail', 'priority');
+    xarModDelVar('mail', 'replytoname');
+    xarModDelVar('mail', 'replytoemail');
+
+    return true;
+}
+
+?>
---
+++ html/modules/mail/xarversion.php
@@ -0,0 +1,17 @@
+<?php 
+$modversion['name'] = 'Mail';
+$modversion['id'] = '771';
+$modversion['version'] = '0.01';
+$modversion['description'] = 'Mail handling utility module';
+$modversion['credits'] = 'xardocs/credits.txt';
+$modversion['help'] = 'xardocs/help.txt';
+$modversion['changelog'] = 'xardocs/changelog.txt';
+$modversion['license'] = 'xardocs/license.txt';
+$modversion['official'] = 1;
+$modversion['author'] = 'John Cox via phpMailer';
+$modversion['contact'] = 'niceguyeddie@xaraya.com';
+$modversion['admin'] = 1;
+$modversion['securityschema'] = array('mail::' => '::');
+$modversion['class'] = 'Core Complete';
+$modversion['category'] = 'Global';
+?>
