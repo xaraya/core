9c2ccb716031e1b685308071914e0c134fb14f4f
John.Cox <John.Cox@mcnabb.>
2003-02-16 20:03:02 +0000

    html/modules/themes/xarblocks/meta.php: 	#  More work on meta block.  Just need to get the block group made and installed, and allowed in the f html/modules/themes/xartemplates/blocks/meta.xd: 	#  BitKeeper file C:/documents and settings/john cox/my documents/development/www/dev/xaraya/html/modules/themes/xartemplates/blocks/meta.xd html/modules/themes/xartemplates/blocks/metaAdmin.xd: 	#  BitKeeper file C:/documents and settings/john cox/my documents/development/www/dev/xaraya/html/modules/themes/xartemplates/blocks/metaAdmin.xd

--- html/modules/themes/xarblocks/meta.php
+++ html/modules/themes/xarblocks/meta.php
@@ -47,17 +47,25 @@ function themes_metablock_display($blockinfo)
         return;
     }
 
-    //$incoming = xarVarGetCached('Blocks.articles','summary');
-    //$incoming .= ' ';  
-    $incoming = xarVarGetCached('Blocks.articles','body');
+    // Get current content
+    $vars = @unserialize($blockinfo['content']);
 
-    if (!empty($incoming)){
+    $incomingdesc = xarVarGetCached('Blocks.articles','summary');
 
-        // The following is from PostNuke's Header.php.  I believe the original function
-        // Was written by Tim Litwiller.
-        
+    if (!empty($incomingdesc)){
         // Strip -all- html
-        $htmlless = strip_tags($incoming);
+        $htmlless = strip_tags($incomingdesc);
+        $meta['description'] = $htmlless;
+    } else {
+        $meta['description'] = $vars['metadescription'];
+    }
+
+    $incomingkey = xarVarGetCached('Blocks.articles','body');
+
+    if (!empty($incomingkey)){
+       
+        // Strip -all- html
+        $htmlless = strip_tags($incomingkey);
         
         // Strip anything that isn't alphanumeric or _ - 
         $symbolLess = trim(ereg_replace('([^a-zA-Z0-9_-])+',' ',$htmlless));
@@ -77,13 +85,24 @@ function themes_metablock_display($blockinfo)
         sort($keywords, SORT_STRING);
         
         // Merge the list of words into a single, comma delimited string of keywords
-        $text = implode(",",$keywords);
+        $meta['keywords'] = implode(",",$keywords);
     
     } else {
-        $text = '';
+        $meta['keywords'] = $vars['metakeywords'];
     }
+
+    $meta['generator'] = xarConfigGetVar('System.Core.VersionNum');
+    $meta['generator'] .= ' :: ';
+    $meta['generator'] .= xarConfigGetVar('System.Core.VersionID');
+
+    if (!empty($args['geourl'])){
+        $meta['longitude'] = $args['longitude'];
+        $meta['latitude'] = $args['latitude'];
+    }
+
+    $meta['activepage'] = xarServerGetCurrentURL();
     
-    $blockinfo['content'] = $text;
+    $blockinfo['content'] = $meta;
     return $blockinfo;
  
 }
@@ -105,6 +124,18 @@ function themes_metablock_modify($blockinfo)
     if (empty($vars['metadescription'])) {
         $vars['metadescription'] = '';
     }
+    // Defaults
+    if (empty($vars['usegeo'])) {
+        $vars['usegeo'] = '';
+    }
+    // Defaults
+    if (empty($vars['longitude'])) {
+        $vars['longitude'] = '';
+    }
+    // Defaults
+    if (empty($vars['latitude'])) {
+        $vars['latitude'] = '';
+    }
     
     $content = xarTplBlock('themes', 'metaAdmin', $vars);
 
@@ -118,8 +149,15 @@ function themes_metablock_modify($blockinfo)
 function themes_metablock_update($blockinfo)
 {
     list($vars['metakeywords'],
-         $vars['metadescription']) = xarVarCleanFromInput('metakeywords',
-                                                          'metadescription');
+         $vars['metadescription'],
+         $vars['usegeo'],
+         $vars['longitude'],
+         $vars['latitude']) = xarVarCleanFromInput('metakeywords',
+                                                   'metadescription',
+                                                   'usegeo',
+                                                   'longitude',
+                                                   'latitude');
+
     // Defaults
     if (empty($vars['metakeywords'])) {
         $vars['metakeywords'] = '';
@@ -127,6 +165,18 @@ function themes_metablock_update($blockinfo)
     
     if (empty($vars['metadescription'])) {
         $vars['metadescription'] = '';
+    }
+
+    if (empty($vars['usegeo'])) {
+        $vars['usegeo'] = '';
+    }
+
+    if (empty($vars['longitude'])) {
+        $vars['longitude'] = '';
+    }
+
+    if (empty($vars['latitude'])) {
+        $vars['latitude'] = '';
     }
 
     $blockinfo['content'] = serialize($vars);
