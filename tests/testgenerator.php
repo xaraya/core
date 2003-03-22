6a633fa70fb0921a9fc3050bc8b7ccb46ce4b964
besfred <besfred@chimera.medusa>
2003-03-22 20:03:03 +0000

    tests/testgenerator.php: 	#  this one tries to use regular expressions 	#  doesnt yet write a file

--- tests/testgenerator.php
+++ tests/testgenerator.php
@@ -1,28 +1,89 @@
 <?php
-// call me on the commandline like this:
-//
-// shell> php -n testgenerator.php xarMyFileToBeTested.php
-//
+/**
+ * call me on the commandline like this:
+ *
+ * shell> php -n testgenerator.php xarMyFileToBeTested.php
+ */
 
-// specify file
-$file = $argv[1];
-if (empty($file)) {
-	$file = 'xarEvt.php';
+// define function
+if (!function_exists('file_get_contents')) {
+	// you are using php < 4.3.0 => you should consider an upgrade
+	function file_get_contents($_file) {
+		if (file_exists($_file)) {
+			ob_start();
+			$retval = readfile($_file);
+			// no readfile error
+			if (false !== $retval) {
+				$retval = ob_get_contents();
+				ob_end_clean();
+				return($retval);
+			}
+		}
+		return(false);
+	}
 }
 
+function parse_php($_filecontent) {
+	$functionPattern = '°function .*?\)°';
+	$classesPattern  = '°class .*? °';
+//	$classesPattern  = '°class .*?\\}\\}°';
+
+	preg_match_all($classesPattern, $_filecontent, $classes);
+	preg_match_all($functionPattern, $_filecontent, $functions);
+
+	$returnval = array('classes' => $classes[0], 'functions' => $functions[0]);
+	return($returnval);
+}
+
+// main
+
+// no file specified
+if (empty($argv[1])) {
+	echo("usage: php -n {$argv[0]} file.php\r\n");
+	exit();
+}
+
+$filecontent = file_get_contents($argv[1]);
+
+if (false == $filecontent) {
+	echo("file not found or not readable");
+	exit();
+}
+
+
+if (empty($filecontent)) {
+	echo("usage: php -n {$argv[0]} file.php\r\n");
+	exit();
+}
+
+if(!$result = parse_php($filecontent)) {
+	echo("parser failed\r\n");
+	exit();
+}
+
+print_r($result);
+echo("parser finished\r\n");
+
+
 // get functions and names
+/*
 $testFileName = 'test'.substr($file,3);
 $testClassName = 'test'.substr($file,3,-4);
-$oldState = get_defined_functions();
-include($file);
-$newState = get_defined_functions();
-$diff = array_diff($newState['user'], $oldState['user']);
+$oldFuncs = get_defined_functions();
+$oldClasses = get_declared_classes();
+@include($file);
+$newFuncs = get_defined_functions();
+$newClasses = get_declared_classes();
+$diffFuncs = array_diff($newFuncs['user'], $oldFuncs['user']);
+
+print_r($oldClasses);
+print_r($newClasses);
 
 // initialise skeleton
 $skel = array();
 
 // build skeleton
-$skel[] = '<?php';
+$skel[] = '<?p'.'hp'; // split because editor doesnt like it otherwise
 $skel[] = ' ';
 $skel[] = 'class '.$testClassName.' extends xarTestCase {';
 $skel[] = ' ';
@@ -34,14 +95,14 @@ $skel[] = '    function precondition() {';
 $skel[] = '        return true;';
 $skel[] = '    }';
 $skel[] = ' ';
-$skel[] = '    function teardown () {';
+$skel[] = '    function teardown() {';
 $skel[] = ' ';
 $skel[] = '    }';
 $skel[] = ' ';
 $skel[] = '    // following function are proposals. please change according to your needs';
 $skel[] = ' ';
-foreach($diff as $func) {
-	$skel[] = '    function test'.substr($func,3).' {';
+foreach($diffFuncs as $func) {
+	$skel[] = '    function test'.substr($func,3).'() {';
 	$skel[] = '        return $this->assertTrue('.$func.'([enter arguments]),"[Test name]");';
 	$skel[] = '    }';
 	$skel[] = ' ';
@@ -54,22 +115,24 @@ $skel[] = '}';
 $skel[] = ' ';
 $skel[] = '$suite->AddTestCase(\''.$testClassName.'\',\'Testing [enter your testcase name]\');';
 $skel[] = ' ';
-$skel[] = '?>';
+$skel[] = '?'.'>'; // split because editor doesnt like it otherwise
+
+$content = implode("\r\n",$skel);
 
 // write file
 if (!$handle = fopen($testFileName, 'w+')) {
-	print "Cannot open file ($testFileName)";
+	echo("Cannot open file ($testFileName)");
     exit;
 }
-foreach($skel as $line) {
-    if (!fwrite($handle, $line."\r\n")) {
-        print "Cannot write to file ($testFileName)";
-        exit;
-    }
+
+if (!fwrite($handle, $content)) {
+	echo("Cannot write to file ($testFileName)");
+	exit;
 }
 
 // echo message     
-echo "File ($testFileName) successfully generated.\r\n";
+echo("File ($testFileName) successfully generated.\r\n");
     
 fclose($handle);
+*/
 ?>
