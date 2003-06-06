02301c25a6b424c8e3336e26bdff167f376bbb06
Jason.Judge <Jason.Judge@acad-ajp.>
2003-06-06 20:03:06 +0000

    html/modules/installer/xaradmin.php: 	#  Feature Request for password validation in the install. html/modules/installer/xartemplates/admin-create_administrator.xd: 	#  Feature Request for password validation in the install. html/modules/dynamicdata/users.xml: 	#  bug fixes and feature requests. html/modules/mail/class/class.phpmailer.php: 	#  bug fixes and feature requests. html/modules/mail/class/class.smtp.php: 	#  bug fixes and feature requests. html/modules/roles/xartemplates/user-user_menu_form.xd: 	#  bug fixes and feature requests. html/modules/roles/xartemplates/user-user_menu_icon.xd: 	#  bug fixes and feature requests. html/modules/base/xarversionsapi/compare.php: 	#  Changes suggested by Rabbitt: 	#  1. Return -1/0/+1 as result of comparison. 	#  2. Allow any arbitrary character as the field/level separator. 	#  3. Update the tests with further examples and notes (the test script will form the documentation). tests/base/version_compare.php: 	#  Changes suggested by Rabbitt: 	#  1. Return -1/0/+1 as result of comparison. 	#  2. Allow any arbitrary character as the field/level separator. 	#  3. Update the tests with further examples and notes (the test script will form the documentation).

--- html/modules/base/xarversionsapi/compare.php
+++ html/modules/base/xarversionsapi/compare.php
@@ -1,6 +1,20 @@
 <?php
 
 /**
+ * File: $Id: s.xaruser.php 1.16 03/04/07 04:30:01-04:00 johnny@falling.local.lan $
+ *
+ * Base User version management functions
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @link http://www.xaraya.com
+ *
+ * @subpackage base
+ * @author Jason Judge
+ * @todo none
+ */
+
+/**
  * Compare two legal-style versions supplied as strings or arrays, to an arbitrary number of levels
  * Usage : $which = xarModAPIFunc('base', 'versions', 'compare', array('ver1'=>$version1, 'ver2'=>$version2));
  *
@@ -9,12 +23,13 @@
  * @param $args['ver2'] version number 2 (string or array)
  * @param $args['levels'] maxiumum levels to compare (default: all levels)
  * @param $args['strict'] indicates strict numeric-only comparisons (default: true)
+ * @param $args['sep'] level separator character (default: .)
  * @returns number
  * @return number indicating which parameter is the latest version
  */
 function base_versionsapi_compare($args)
 {
-    // Indicates which is the latest version: 1, 2 or 0 (neither).
+    // Indicates which is the latest version: -1, +1 or 0 (neither).
     // Can be limited to a certain number of levels. The defaul 0 levels
     // is limited only by the versions passed in.
     // Versions can be strings ('1.2.3') or arrays (array(1, 2, 3)).
@@ -32,6 +47,7 @@ function base_versionsapi_compare($args)
         $strict = true;
     }
 
+    // Default either version to '0' if not pass in at all.
     if (!isset($ver1)) {
         $ver1 = '0';
     }
@@ -40,6 +56,14 @@ function base_versionsapi_compare($args)
         $ver2 = '0';
     }
 
+    // Default the level separator to '.' if none valid passed in.
+    if (!isset($sep) || strlen($sep) <> 1) {
+        $sep = '.';
+    }
+
+    // Quote the separator for use in the preg cleanup.
+    $sep2 = preg_quote($sep);
+
     // Get the number of levels to check.
     if (!settype($levels, 'integer') || $levels < 0) {
         $levels = 0;
@@ -47,29 +71,29 @@ function base_versionsapi_compare($args)
 
     // If arrays have been passed in, convert them to a legal-format string.
     if (is_array($ver1)) {
-        $ver1 = implode('.', $ver1);
+        $ver1 = implode($sep, $ver1);
     }
 
     if (is_array($ver2)) {
-        $ver2 = implode('.', $ver2);
+        $ver2 = implode($sep, $ver2);
     }
 
     // Clean up the input strings.
     list($ver1, $ver2) = preg_replace(
         array(
-           '/(\s'. ($strict ? '|[^\d.]' : '') .')*/',
-           '/^\./',
-           '/\.$/',
-           '/\.\./',
+           '/(\s'. ($strict ? '|[^\d'.$sep2.']' : '') .')*/',
+           '/^'.$sep2.'/',
+           '/'.$sep2.'$/',
+           '/'.$sep2.$sep2.'/',
            '/^$/'
         ),
-        array('', '0.', '.0', '.0.', '0'),
+        array('', '0'.$sep, $sep.'0', $sep.'0'.$sep, '0'),
         array($ver1, $ver2)
     );
 
     // Explode the strings into arrays for comparing.
-    $ver1 = explode('.', $ver1);
-    $ver2 = explode('.', $ver2);
+    $ver1 = explode($sep, $ver1);
+    $ver2 = explode($sep, $ver2);
 
     // Get the highest number of levels in a version.
     $limitlevels = max(count($ver1), count($ver2));
@@ -96,8 +120,8 @@ function base_versionsapi_compare($args)
         // Note, we are comparing strings, BUT if both values happen
         // to be numeric, then PHP will do a numeric comparison.
         // PHP's behaviour saves us some work type-casting.
-        if ($ver1[$i] > $ver2[$i]) {$latest = 1; break;}
-        if ($ver2[$i] > $ver1[$i]) {$latest = 2; break;}
+        if ($ver1[$i] > $ver2[$i]) {$latest = (-1); break;}
+        if ($ver2[$i] > $ver1[$i]) {$latest = (+1); break;}
     }
     
     return $latest;
--- tests/base/version_compare.php
+++ tests/base/version_compare.php
@@ -1,8 +1,20 @@
 <?php
+/**
+ * File: $Id: s.xaruser.php 1.16 03/04/07 04:30:01-04:00 johnny@falling.local.lan $
+ *
+ * Tests for base API function versions/compare
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @link http://www.xaraya.com
+ *
+ * @subpackage base
+ * @author Jason Judge
+ * @todo none
+ */
+
 // Script tests API function version compare in the base module.
 // Run this script from the site root directory.
-// Author: Jason Judge
-// Created: 4 June 2003
 
 // initialize the Xaraya core
 include 'includes/xarCore.php';
@@ -13,30 +25,48 @@ xarCoreInit(XARCORE_SYSTEM_ALL);
 // ver2 = second version to compare
 // levels = number of levels to limit the comparison (null = default = 0 = unlimited)
 // strict = flag to indicate strict numeric comparisons (null = default = true)
+// sep = separator character
+// notes = notes on the test
 // result = expected result
 
 $test_array = array(
-   array('ver1'=>' .-1.2.sdf.3.  f6', 'ver2'=>'4.5.6', 'levels'=>4, 'strict'=>null, 'result'=>2),
-   array('ver1'=>'1.2', 'ver2'=>'1.3', 'levels'=>null, 'strict'=>null, 'result'=>2),
-   array('ver1'=>'1.3', 'ver2'=>'1.2', 'levels'=>null, 'strict'=>null, 'result'=>1),
-   array('ver1'=>'1.2', 'ver2'=>'1.2', 'levels'=>null, 'strict'=>null, 'result'=>0),
-   array('ver1'=>'1.2.1', 'ver2'=>'1.3.2', 'levels'=>null, 'strict'=>null, 'result'=>2),
-   array('ver1'=>'1.2', 'ver2'=>'1.2', 'levels'=>null, 'strict'=>null, 'result'=>0),
-   array('ver1'=>'1.3.1', 'ver2'=>'1.3.2', 'levels'=>null, 'strict'=>null, 'result'=>2),
-   array('ver1'=>'1.3.1', 'ver2'=>'1.3.2', 'levels'=>0, 'strict'=>null, 'result'=>2),
-   array('ver1'=>'1.3.1', 'ver2'=>'1.3.2', 'levels'=>3, 'strict'=>null, 'result'=>2),
-   array('ver1'=>'1.3.1', 'ver2'=>'1.3.2', 'levels'=>2, 'strict'=>null, 'result'=>0),
-   array('ver1'=>'1.3.1', 'ver2'=>'1.3.2', 'levels'=>1, 'strict'=>null, 'result'=>0),
-   array('ver1'=>'', 'ver2'=>'1.3.2', 'levels'=>null, 'strict'=>null, 'result'=>2),
-   array('ver1'=>'1.3.2', 'ver2'=>'   ', 'levels'=>null, 'strict'=>null, 'result'=>1),
-   array('ver1'=>'', 'ver2'=>'   ', 'levels'=>null, 'strict'=>null, 'result'=>0),
-   array('ver1'=>'1.10', 'ver2'=>'1.2', 'levels'=>null, 'strict'=>true, 'result'=>1),
-   array('ver1'=>'1.10', 'ver2'=>'1.2', 'levels'=>null, 'strict'=>false, 'result'=>1),
-   array('ver1'=>'1.10', 'ver2'=>'1.2g', 'levels'=>null, 'strict'=>true, 'result'=>1),
-   array('ver1'=>'1.10', 'ver2'=>'1.2g', 'levels'=>null, 'strict'=>false, 'result'=>2),
-   array('ver1'=>'1.10', 'ver2'=>array(1,'2g'), 'levels'=>null, 'strict'=>null, 'result'=>1),
-   array('ver1'=>array(1,10), 'ver2'=>'1.11', 'levels'=>null, 'strict'=>null, 'result'=>2),
-   array('ver1'=>array(1,10), 'ver2'=>'1.9', 'levels'=>null, 'strict'=>null, 'result'=>1)
+   array('ver1'=>' .-1.2.sdf.3.  f6', 'ver2'=>'0.1.2.0.3.7', 'levels'=>6, 'strict'=>null, 'result'=>1, 'sep'=>null,
+      'notes'=>'" .-1.2.sdf.3.  f6" is cleaned up to become 0.1.2.0.3.6'),
+   array('ver1'=>' .-1.2.sdf.3.  f6', 'ver2'=>'0.1.2.0.3.7', 'levels'=>5, 'strict'=>null, 'result'=>0, 'sep'=>null,
+      'notes'=>'" .-1.2.sdf.3.  f6" is cleaned up to become 0.1.2.0.3.6'),
+   array('ver1'=>'1.2', 'ver2'=>'1.3', 'levels'=>null, 'strict'=>null, 'result'=>1, 'sep'=>null),
+   array('ver1'=>'1.3', 'ver2'=>'1.2', 'levels'=>null, 'strict'=>null, 'result'=>-1, 'sep'=>null),
+   array('ver1'=>'1.2', 'ver2'=>'1.2', 'levels'=>null, 'strict'=>null, 'result'=>0, 'sep'=>null),
+   array('ver1'=>'1.2.1', 'ver2'=>'1.3.2', 'levels'=>null, 'strict'=>null, 'result'=>1, 'sep'=>null),
+   array('ver1'=>'1.2', 'ver2'=>'1.2', 'levels'=>null, 'strict'=>null, 'result'=>0, 'sep'=>null),
+   array('ver1'=>'1.3.1', 'ver2'=>'1.3.2', 'levels'=>null, 'strict'=>null, 'result'=>1, 'sep'=>null),
+   array('ver1'=>'0.0.0.0.0.0.0.0.0.0.0.2', 'ver2'=>'0.0.0.0.0.0.0.0.0.0.0.1', 'levels'=>null, 'strict'=>null, 'result'=>-1, 'sep'=>null),
+   array('ver1'=>'1.3.1', 'ver2'=>'1.3.2', 'levels'=>0, 'strict'=>null, 'result'=>1, 'sep'=>null,
+      'notes'=>'0 means all levels'),
+   array('ver1'=>'1.3.1', 'ver2'=>'1.3.2', 'levels'=>3, 'strict'=>null, 'result'=>1, 'sep'=>null),
+   array('ver1'=>'1.3.1', 'ver2'=>'1.3.2', 'levels'=>2, 'strict'=>null, 'result'=>0, 'sep'=>null,
+      'notes'=>'At two levels or fewer these versions look identical'),
+   array('ver1'=>'1.3.1', 'ver2'=>'1.3.2', 'levels'=>1, 'strict'=>null, 'result'=>0, 'sep'=>null),
+   array('ver1'=>'', 'ver2'=>'1.3.2', 'levels'=>null, 'strict'=>null, 'result'=>1, 'sep'=>null),
+   array('ver1'=>'1.3.2', 'ver2'=>'   ', 'levels'=>null, 'strict'=>null, 'result'=>-1, 'sep'=>null),
+   array('ver1'=>'', 'ver2'=>'   ', 'levels'=>null, 'strict'=>null, 'result'=>0, 'sep'=>null),
+   array('ver1'=>'1.10', 'ver2'=>'1.2', 'levels'=>null, 'strict'=>true, 'result'=>-1, 'sep'=>null),
+   array('ver1'=>'1.10', 'ver2'=>'1.2', 'levels'=>null, 'strict'=>false, 'result'=>-1, 'sep'=>null),
+   array('ver1'=>'1.10', 'ver2'=>'1.2g', 'levels'=>null, 'strict'=>true, 'result'=>-1, 'sep'=>null,
+       'notes'=>'Number 10 is numerically greater than 2 (stripped of the "g")'),
+   array('ver1'=>'1.10', 'ver2'=>'1.2g', 'levels'=>null, 'strict'=>false, 'result'=>1, 'sep'=>null,
+      'notes'=>'String "2g" is greater than string "10"'),
+   array('ver1'=>'1.10', 'ver2'=>array(1,'2g'), 'levels'=>null, 'strict'=>null, 'result'=>-1, 'sep'=>null),
+   array('ver1'=>array(1,10), 'ver2'=>'1.11', 'levels'=>null, 'strict'=>null, 'result'=>1, 'sep'=>null),
+   array('ver1'=>array(1,10), 'ver2'=>'1.9', 'levels'=>null, 'strict'=>null, 'result'=>-1, 'sep'=>null),
+   array('ver1'=>array(1,10), 'ver2'=>'1:11', 'levels'=>null, 'strict'=>null, 'result'=>1, 'sep'=>':'),
+   array('ver1'=>'4-5-6-7', 'ver2'=>'5-6-7-8-9-0', 'levels'=>null, 'strict'=>null, 'result'=>1, 'sep'=>'-'),
+   array('ver1'=>'5-6-7', 'ver2'=>'4-5-6-7-8-9-0', 'levels'=>null, 'strict'=>null, 'result'=>-1, 'sep'=>'-',
+       'notes'=>'As a version 5.6.7 is greater than 4.5.6...'),
+   array('ver1'=>'5-6-7', 'ver2'=>'4-5-6-7-8-9-0', 'levels'=>null, 'strict'=>null, 'result'=>1, 'sep'=>'x',
+       'notes'=>'As a number 4567890 is greater than 567'),
+   array('ver1'=>'5-6-7', 'ver2'=>'4-5-6-7-8-9-0', 'levels'=>null, 'strict'=>false, 'result'=>-1, 'sep'=>'x',
+      'notes' => 'As a string "5-6-7" is alpha-numerically greater than "4-5-6..."')
 );
 ?>
 
@@ -50,15 +80,18 @@ $test_array = array(
       <th>Version 2</th>
       <th>Levels</th>
       <th>Strict Numeric</th>
+      <th>Separator</th>
       <th>Expected Result</th>
       <th>Actual Result</th>
       <th>Pass/Fail</th>
+      <th>Notes</th>
    </tr>
    <?
       foreach($test_array as $test) {
          $params = array('ver1'=>$test['ver1'], 'ver2'=>$test['ver2']);
          if (isset($test['levels'])) {$params['levels'] = $test['levels'];}
          if (isset($test['strict'])) {$params['strict'] = $test['strict'];}
+         if (isset($test['sep'])) {$params['sep'] = $test['sep'];}
          $actual = xarModAPIFunc('base', 'versions', 'compare', $params);
          if ($actual == $test['result']) {$status = 'Pass';} else {$status = '<b>Fail</b>';}
          if (!is_array($test['ver1'])) {$ver1 = '&quot;'.$test['ver1'].'&quot;';}
@@ -71,9 +104,11 @@ $test_array = array(
       <td><?echo $ver2;?></td>
       <td><?echo $test['levels'];?></td>
       <td><?if (isset($test['strict'])) echo $test['strict'] ? 'True' : 'False';?></td>
+      <td><?echo $test['sep'];?></td>
       <td><?echo $test['result'];?></td>
       <td><?echo $actual;?></td>
       <td><?echo $status;?></td>
+      <td><?echo $test['notes'];?></td>
       </tr>
    <?
        }
