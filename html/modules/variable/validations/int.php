dd4f4f79cf533788c9f7f010595bb698a6d8e030
nuncanada <nuncanada@h0ly.>
2003-06-08 20:03:06 +0000

    html/modules/variable/validations/int.php: 	#  More one USER EXCEPTION i didnt comment out... Not prepared yet for them. 	#  They will come back soon. Need to rework exceptions before...

--- html/modules/variable/validations/int.php
+++ html/modules/variable/validations/int.php
@@ -9,7 +9,7 @@ function variable_validations_int (&$subject, $parameters) {
 
     if ("$subject" != "$value") {
         $msg = xarML('Not a Integer Type: "#(1)"', $subject);
-        xarExceptionSet(XAR_USER_EXCEPTION, 'BAD_PARAM', new SystemException($msg));
+//        xarExceptionSet(XAR_USER_EXCEPTION, 'BAD_PARAM', new SystemException($msg));
         return false;
     }
 
