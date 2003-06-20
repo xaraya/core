f429a4942eb7080d2cd5f456514d4358f8faf425
BRUMDall <BRUMDall@brumdall2m.bru.fsc.net>
2003-06-20 20:03:06 +0000

    tests/import/postnuke/import_pn.php: 	#  break import script into parts to reveal the (lack of) magic tests/import/postnuke/README: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/README tests/import/postnuke/import_pn_cleanup.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_cleanup.php tests/import/postnuke/import_pn_comments.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_comments.php tests/import/postnuke/import_pn_faqanswer.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_faqanswer.php tests/import/postnuke/import_pn_faqcategories.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_faqcategories.php tests/import/postnuke/import_pn_links_categories.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_links_categories.php tests/import/postnuke/import_pn_links_links.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_links_links.php tests/import/postnuke/import_pn_poll_data.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_poll_data.php tests/import/postnuke/import_pn_poll_desc.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_poll_desc.php tests/import/postnuke/import_pn_pollcomments.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_pollcomments.php tests/import/postnuke/import_pn_queue.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_queue.php tests/import/postnuke/import_pn_seccont.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_seccont.php tests/import/postnuke/import_pn_sections.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_sections.php tests/import/postnuke/import_pn_stories.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_stories.php tests/import/postnuke/import_pn_topicscat.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_topicscat.php tests/import/postnuke/import_pn_users.php: 	#  BitKeeper file D:/wintools/cygwin/home/xaraya-stable/tests/import/postnuke/import_pn_users.php

---
+++ tests/import/postnuke/import_pn_poll_desc.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * File: $Id$
+ *
+ * Import PostNuke .71+ poll descriptions into your Xaraya test site
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @link http://www.xaraya.com
+ * 
+ * @subpackage import
+ * @author mikespub <mikespub@xaraya.com>
+ */
+
+/**
+ * Note : this file is part of import_pn.php and cannot be run separately
+ */
+
+    echo "<strong>$step. Importing old poll descriptions</strong><br>\n";
+
+    if (!xarModIsAvailable('polls')) {
+        echo "The polls module is not activated in Xaraya<br>\n";
+        $step++;
+        return;
+    }
+
+    $query = 'SELECT COUNT(*) FROM ' . $oldprefix . '_poll_desc';
+    $result =& $dbconn->Execute($query);
+    if (!$result) {
+        die("Oops, count polls failed : " . $dbconn->ErrorMsg());
+    }
+    $count = $result->fields[0];
+    $result->Close();
+
+    $query = 'SELECT pdesc.pn_pollid,pn_title,pn_timestamp,pn_voters,SUM(pn_optioncount)
+              FROM ' . $oldprefix . '_poll_desc as pdesc
+              LEFT JOIN ' . $oldprefix . '_poll_data as pdata
+                  ON pdesc.pn_pollid = pdata.pn_pollid
+              GROUP BY pdata.pn_pollid
+              ORDER BY pdesc.pn_pollid ASC';
+    $result =& $dbconn->Execute($query);
+    if (!$result) {
+        die("Oops, select polls failed : " . $dbconn->ErrorMsg());
+    }
+    $pollid = array();
+    $num = 1;
+    while (!$result->EOF) {
+        list($pid,$title,$time,$wrongvotes,$realvotes) = $result->fields;
+        if (empty($title)) {
+            $title = xarML('[none]');
+        }
+        $newpid = xarModAPIFunc('polls','admin','create',
+                                array('title' => $title,
+                                      'polltype' => 'single', // does PN support any other kind ?
+                                      'private' => 0,
+                                      'time' => $time,
+                                      'votes' => $realvotes));
+        if (empty($newpid)) {
+            echo "Insert poll ($pid) $title failed : " . xarExceptionRender('text') . "<br>\n";
+        } elseif ($count < 200) {
+            echo "Inserted poll ($pid) $title<br>\n";
+        } elseif ($num % 100 == 0) {
+            echo "Inserted poll $num<br>\n";
+            flush();
+        }
+        if (!empty($newpid)) {
+            $pollid[$pid] = $newpid;
+        }
+        $num++;
+        $result->MoveNext();
+    }
+    $result->Close();
+
+?>
