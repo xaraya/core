bf84ef13c05fb729753013bb7ed3af7c5499e83a
marcel <marcel@hsdev.com>
2003-06-30 20:03:06 +0000

    html/modules/installer/xarconfigurations/core.conf.php: 	#  BitKeeper file /var/www/xar/stable/main/html/modules/installer/xarconfigurations/core.conf.php

---
+++ html/modules/installer/xarconfigurations/core.conf.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * File: core.conf.php
+ *
+ * Configuration file for a core xaraya installation
+ *
+ * @package installer
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @link http://www.xaraya.com
+ *
+ * @author Marcel van der Boom <marcel@xaraya.com>
+ */
+
+$configuration_name = 'Core Xaraya install (aka as minimal)';
+
+$options  = array(
+    array(
+        'item' => '1',
+        'option' => 'true',
+        'comment' => xarML('Registered users have read access to all modules of the site.')
+    ),
+    array(
+        'item' => '2',
+        'option' => 'false',
+        'comment' => xarML('Unregistered users have read access to the non-core modules of the site. If this option is not chosen unregistered users see only the first page.')
+    ),
+
+);
+
+$configuration_options = $options;
+
+/**
+ * Load the configuration
+ *
+ * @access public
+ * @return boolean
+ */
+function installer_core_configuration_load($args)
+{
+// disable caching of module state in xarMod.php
+    $GLOBALS['xarMod_noCacheState'] = true;
+
+// load the modules chosen
+    xarModAPIFunc('modules','admin','regenerate');
+
+    $content['marker'] = '[x]';                                           // create the user menu
+    $content['displaymodules'] = 1;
+    $content['content'] = '';
+
+    // Load up database
+    list($dbconn) = xarDBGetConn();
+    $tables = xarDBGetTables();
+
+    $blockGroupsTable = $tables['block_groups'];
+
+    $query = "SELECT    xar_id as id
+              FROM      $blockGroupsTable
+              WHERE     xar_name = 'left'";
+
+    $result =& $dbconn->Execute($query);
+    if (!$result) return;
+
+    // Freak if we don't get one and only one result
+    if ($result->PO_RecordCount() != 1) {
+        $msg = xarML("Group 'left' not found.");
+        xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM',
+                       new SystemException(__FILE__.'('.__LINE__.'): '.$msg));
+        return;
+    }
+
+    list ($leftBlockGroup) = $result->fields;
+
+    $adminBlockId= xarModAPIFunc('blocks',
+                                 'admin',
+                                 'block_type_exists',
+                                 array('modName'  => 'base',
+                                       'blockType'=> 'menu'));
+
+    if (!isset($adminBlockId) && xarExceptionMajor() != XAR_NO_EXCEPTION) {
+        return;
+    }
+
+    xarModAPIFunc('blocks','admin','create_instance',array('title' => 'Main Menu',
+                                                           'type' => $adminBlockId,
+                                                           'group' => $leftBlockGroup,
+                                                           'template' => '',
+                                                           'content' => serialize($content),
+                                                           'state' => 2));
+
+// load the privileges chosen
+
+    if(in_array(1,$args)) {
+        installer_core_readaccess();
+        xarAssignPrivilege('ReadAccess','Users');
+    }
+    else {
+        installer_core_casualaccess();
+        xarAssignPrivilege('CasualAccess','Users');
+    }
+
+    if(in_array(2,$args)) {
+        installer_core_readnoncore();
+        xarAssignPrivilege('ReadNonCore','Everybody');
+   }
+    else {
+        if(in_array(1,$args)) installer_core_casualaccess();
+        xarAssignPrivilege('CasualAccess','Everybody');
+    }
+
+    return true;
+}
+
+function installer_core_casualaccess()
+{
+    xarRegisterPrivilege('CasualAccess','All','themes','Block','All','ACCESS_OVERVIEW','Minimal access to a site');
+    xarRegisterPrivilege('ViewLogin','All','roles','Block','login:Login:All','ACCESS_OVERVIEW','View the Login block');
+    xarRegisterPrivilege('ViewBlocks','All','base','Block','All','ACCESS_OVERVIEW','View blocks of the Base module');
+    xarRegisterPrivilege('ViewLoginItems','All','dynamicdata','Item','All','ACCESS_OVERVIEW','View some Dynamic Data items');
+    xarMakePrivilegeRoot('CasualAccess');
+    xarMakePrivilegeRoot('ViewLogin');
+    xarMakePrivilegeRoot('ViewBlocks');
+    xarMakePrivilegeRoot('ViewLoginItems');
+    xarMakePrivilegeMember('ViewLogin','CasualAccess');
+    xarMakePrivilegeMember('ViewBlocks','CasualAccess');
+    xarMakePrivilegeMember('ViewLoginItems','CasualAccess');
+}
+
+function installer_core_readnoncore()
+{
+    xarRegisterPrivilege('ReadNonCore','All','empty','All','All','ACCESS_NONE','Read access only to none-core modules');
+    xarRegisterPrivilege('DenyPrivileges','All','privileges','All','All','ACCESS_NONE','Deny access to the Privileges module');
+    xarRegisterPrivilege('DenyAdminPanels','All','adminpanels','All','All','ACCESS_NONE','Deny access to the AdminPanels module');
+    xarRegisterPrivilege('DenyBlocks','All','blocks','All','All','ACCESS_NONE','Deny access to the Blocks module');
+    xarRegisterPrivilege('DenyMail','All','mail','All','All','ACCESS_NONE','Deny access to the Mail module');
+    xarRegisterPrivilege('DenyModules','All','modules','All','All','ACCESS_NONE','Deny access to the Modules module');
+    xarRegisterPrivilege('DenyThemes','All','themes','All','All','ACCESS_NONE','Deny access to the Themes module');
+    xarMakePrivilegeRoot('ReadNonCore');
+    xarMakePrivilegeRoot('DenyPrivileges');
+    xarMakePrivilegeRoot('DenyAdminPanels');
+    xarMakePrivilegeRoot('DenyBlocks');
+    xarMakePrivilegeRoot('DenyMail');
+    xarMakePrivilegeRoot('DenyModules');
+    xarMakePrivilegeRoot('DenyThemes');
+    xarMakePrivilegeMember('ReadAccess','ReadNonCore');
+    xarMakePrivilegeMember('DenyPrivileges','ReadNonCore');
+    xarMakePrivilegeMember('DenyAdminPanels','ReadNonCore');
+    xarMakePrivilegeMember('DenyBlocks','ReadNonCore');
+    xarMakePrivilegeMember('DenyMail','ReadNonCore');
+    xarMakePrivilegeMember('DenyModules','ReadNonCore');
+    xarMakePrivilegeMember('DenyThemes','ReadNonCore');
+}
+
+function installer_core_readaccess()
+{
+        xarRegisterPrivilege('ReadAccess','All','All','All','All','ACCESS_READ','Read access to all modules');
+        xarMakePrivilegeRoot('ReadAccess');
+}
+?>
