6330dad0e1c11436ae63c6f7bc24bcf19ee1ca31
nuncanada <nuncanada@h0ly.(none)>
2003-06-30 20:03:06 +0000

    html/includes/pnLegacy.php: 	#  added depreciated calls to the legacy file. html/includes/xarBlocks.php: 	#  reworked xarBlocks.  Now mainly loads from the blocks module. html/includes/xarSecurity.php: 	#  added depreciated calls to the legacy file. html/includes/xarXML.php: 	#  minor error html/modules/blocks/xaradmin/delete_group.php: 	#  reworked xarBlocks.  Now mainly loads from the blocks module. html/modules/blocks/xaradmin/delete_instance.php: 	#  reworked xarBlocks.  Now mainly loads from the blocks module. html/modules/blocks/xaradmin/modify_group.php: 	#  reworked xarBlocks.  Now mainly loads from the blocks module. html/modules/blocks/xaradmin/modify_instance.php: 	#  reworked xarBlocks.  Now mainly loads from the blocks module. html/modules/blocks/xaradmin/update_instance.php: 	#  reworked xarBlocks.  Now mainly loads from the blocks module. html/modules/blocks/xaradmin/view_groups.php: 	#  reworked xarBlocks.  Now mainly loads from the blocks module. html/modules/installer/xaradmin.php: 	#  reworked xarBlocks.  Now mainly loads from the blocks module. html/modules/themes/xarinit.php: 	#  reworked xarBlocks.  Now mainly loads from the blocks module. html/modules/blocks/xaradminapi/getinfo.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/xaraya-stable/html/modules/blocks/xaradminapi/getinfo.php html/modules/blocks/xaradminapi/groupgetinfo.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/xaraya-stable/html/modules/blocks/xaradminapi/groupgetinfo.php html/modules/blocks/xaradminapi/load.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/xaraya-stable/html/modules/blocks/xaradminapi/load.php html/modules/modules/xaradminapi/activatedependency.php: 	#  Going further with this in a new repository now... 	#  Realized this should be only to initialize/unitialize, no need to actually activate the module. (Maybe there can be? If so we need 	#  more options in the dependency Appendix to the Modules RFC) 	# 	#  I didnt change as much as it seems, gnu diff couldn't figure this out :( 	#  We need better differencing mechanism, ones that understand the language syntax... 	# 	#  citool is really nifty in X Windows :-) BitKeeper/etc/logging_ok: 	#  Logging to logging@openlogging.org accepted

--- html/modules/modules/xaradminapi/activatedependency.php
+++ html/modules/modules/xaradminapi/activatedependency.php
@@ -1,100 +1,69 @@
 <?php
 
 /**
- * Activate all of the modules dependencies if possible.
+ * Initialize all of the modules dependencies if possible.
  *
  * @param $maindId int ID of the module to look dependents for
  * @returns bool
  * @return true on dependencies activated, false for not
  * @raise NO_PERMISSION
  */
-function modules_adminapi_activatedependency($mainId)
-{
-    // Security Check
-    // need to specify the module because this function is called by the installer module
-    if(!xarSecurityCheck('AdminModules',1,'All','All','modules')) return;
+function modules_adminapi_initializedependency($mainId) {
+	// Security Check
+	// need to specify the module because this function is called by the installer module
+	if (!xarSecurityCheck('AdminModules', 1, 'All', 'All', 'modules'))
+		return;
 
-    // Argument check
-    if (!isset($mainId)) {
-       $msg = xarML('Missing module regid (#(1)).', $mainId);
-        xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM',
-                       new SystemException(__FILE__.'('.__LINE__.'): '.$msg));return;
-    }
+	// Argument check
+	if (!isset($mainId)) {
+		$msg = xarML('Missing module regid (#(1)).', $mainId);
+		xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM', new SystemException(__FILE__.'('.__LINE__.'): '.$msg));
+		return;
+	}
 
-    // Get module information
-    $modInfo = xarModGetInfo($mainId);
-    if (!isset($modInfo)) {
-        xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'MODULE_NOT_EXIST',
-                       new SystemException(__FILE__."(".__LINE__."): Module (regid: $regid) does not exist."));
-                       return;
-    }
-    
-    $dependency = $modInfo['denpendency'];
+	// See if we have lost any modules since last generation
+	if (!xarModAPIFunc('modules', 'admin', 'checkmissing')) {
+		return;
+	}
 
-    // See if we have lost any modules since last generation
-    if (!xarModAPIFunc('modules','admin','checkmissing')) {return;}
+	// Check if all dependencies are ok
+	if (!xarModAPIFunc('modules', 'admin', 'verifydependencies', $mainId)) {
+		return;
+	}
 
-    // Get all modules in DB
-    // A module is dependent only if it was already initialised at least.
-    // So db modules should be a safe start to go looking for them
-    $fileModules = xarModAPIFunc('modules','admin','getfilemodules');
-    if (!isset($fileModules)) return;
+	// Get module information
+	$modInfo = xarModGetInfo($mainId);
+	if (!isset($modInfo)) {
+		xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'MODULE_NOT_EXIST', new SystemException(__FILE__."(".__LINE__."): Module (regid: $regid) does not exist."));
+		return;
+	}
 
-    foreach ($fileModules as $name => $fileInfo) {
-        $fileMods[$fileInfo['regid']] = $fileInfo;
-    }
+	$dependency = $modInfo['denpendency'];
 
-    foreach ($dependecy as $module_id => $conditions) {
-    
-        if (is_array($conditions)) {
-        
-            //The module id is in $modId
-            $modId = $module_id;
+	//The dependencies are ok, they shouldnt change in the middle of the 
+	//script execution, so let's assume this.
+	foreach ($dependecy as $module_id => $conditions) {
+		if (is_array($conditions)) {
+			//The module id is in $modId
+			$modId = $module_id;
+		} else {
+			//The module id is in $conditions
+			$modId = $conditions;
+		}
 
-            //Required module inexistent
-            if (!isset($fileMods[$modId])) {
-                //Need to add some info for the user
-                return false;
-            }
+		if (!xarModAPIFunc('modules', 'admin', 'activatedependency', $modId)) {
+			$msg = xarML('Unable to activate module with ID (#(1)).', $modId);
+			xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM', new SystemException(__FILE__.'('.__LINE__.'): '.$msg));
+			return;
+		}
+	}
 
-            //Checks if the dependency version is bigger than the min version
-            if (xarModAPIFunc('base','versions','compare',array(
-                'ver1' => $conditions['minversion'],
-                'ver2' => $fileMods[$modId]['version'])) < 0) {
-                //Need to add some info for the user
-                return false; // 1st version is bigger
-            }
+	// Finally, now that dependencies are dealt with, initialize the module
+	if (!xarModAPIFunc('modules', 'admin', 'initialize', array('regid' => $mainId))) {
+		return;
+	}
 
-            //Checks if the dependency version is smaller than the max version
-            if (xarModAPIFunc('base','versions','compare',array(
-                'ver1' => $conditions['maxversion'],
-                'ver2' => $fileMods[$modId]['version'])) > 0) {
-                //Need to add some info for the user
-                return false; // 1st version is smaller
-            }
-
-        } else {
-
-            //The module id is in $conditions
-            $modId = $conditions;
-
-            //Required module inexistent
-            if (!isset($fileMods[$modId])) {
-                //Need to add some info for the user
-                return false;
-            }
-            
-            //We just have the ID, so no conditions to check for.
-        }
-        
-        //So far so good, lets start the dependecy's dependecies
-
-        //TODO: Add check for loops here..
-        // Modules shouldnt depend on one that depend on itself... Still a check for this case is a good idea.
-        if (!xarModAPIFunc('modules','admin','activatedependency',$module_id)) {return;}
-    }
-
-    return true;
+	return true;
 }
 
 ?>
