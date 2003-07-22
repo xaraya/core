993d7e5cd9656e833c19e3a90df28e19507e993a
besfred <besfred@chimera.medusa>
2003-07-22 20:03:07 +0000

    html/modules/modules/xaradminapi/getfilemodules.php: 	#  fix for bug #955 html/modules/themes/xaradminapi/getfilethemes.php: 	#  fix for bug #955

--- html/modules/modules/xaradminapi/getfilemodules.php
+++ html/modules/modules/xaradminapi/getfilemodules.php
@@ -21,87 +21,90 @@ function modules_adminapi_getfilemodules()
     $dh = opendir('modules');
 
     while ($modOsDir = readdir($dh)) {
-        if ((is_dir("modules/$modOsDir")) &&
-                ($modOsDir != '.') &&
-                ($modOsDir != '..') &&
-                ($modOsDir != 'CVS') &&
-                ($modOsDir != 'SCCS') &&
-                ($modOsDir != 'PENDING')) {
+        switch ($modOsDir) {
+            case '.':
+            case '..':
+            case 'CVS':
+            case 'SCCS':
+            case 'PENDING':
+                break;
+            default:
+                if (is_dir("modules/$modOsDir")) {
 
-            // no xarversion.php, no module
-            $modFileInfo = xarMod_getFileInfo($modOsDir);
-            if (!isset($modFileInfo)) {
-                continue;
-            }
+                    // no xarversion.php, no module
+                    $modFileInfo = xarMod_getFileInfo($modOsDir);
+                    if (!isset($modFileInfo)) {
+                        continue;
+                    }
 
-            // Found a directory
-            $name         = $modOsDir;
-            $regId        = $modFileInfo['id'];
-            $version      = $modFileInfo['version'];
-            $mode         = XARMOD_MODE_SHARED;
-            $class        = $modFileInfo['class'];
-            $category     = $modFileInfo['category'];
-            $adminCapable = $modFileInfo['admin_capable'];
-            $userCapable  = $modFileInfo['user_capable'];
-            $dependency   = $modFileInfo['dependency'];
+                    // Found a directory
+                    $name         = $modOsDir;
+                    $regId        = $modFileInfo['id'];
+                    $version      = $modFileInfo['version'];
+                    $mode         = XARMOD_MODE_SHARED;
+                    $class        = $modFileInfo['class'];
+                    $category     = $modFileInfo['category'];
+                    $adminCapable = $modFileInfo['admin_capable'];
+                    $userCapable  = $modFileInfo['user_capable'];
+                    $dependency   = $modFileInfo['dependency'];
             
+                    // TODO: beautify :-)
+                    if (!isset($regId)) {
+                        xarSessionSetVar('errormsg', "Module '$name' doesn't seem to have a registered module ID defined in xarversion.php - skipping...\nPlease register your module at http://www.xaraya.com");
+                        continue;
+                    }
 
-            // TODO: beautify :-)
-            if (!isset($regId)) {
-                xarSessionSetVar('errormsg', "Module '$name' doesn't seem to have a registered module ID defined in xarversion.php - skipping...\nPlease register your module at http://www.xaraya.com");
-                continue;
-            }
+                    //Defaults
+                    if (!isset($version)) {
+                        $version = 0;
+                    }
 
-            //Defaults
-            if (!isset($version)) {
-                $version = 0;
-            }
+                    //FIXME: <johnny> add class and category checking
+                    if (!isset($class)) {
+                        $class = 'Miscellaneous';
+                    }
 
-            //FIXME: <johnny> add class and category checking
-            if (!isset($class)) {
-                $class = 'Miscellaneous';
-            }
+                    if (!isset($category)) {
+                        $category = 'Miscellaneous';
+                    }
 
-            if (!isset($category)) {
-                $category = 'Miscellaneous';
-            }
+                    // Work out if admin-capable
+                    if (!isset($adminCapable)) {
+                        $adminCapable = 0;
+                    }
 
-            // Work out if admin-capable
-            if (!isset($adminCapable)) {
-                $adminCapable = 0;
-            }
+                    //FIXME: <johnny> remove this when xarversion.php contains the user setting
+                    if (file_exists('modules/' . $modOsDir .'/xaruser.php')) {
+                        $userCapable = 1;
+                    }
 
-            //FIXME: <johnny> remove this when xarversion.php contains the user setting
-            if (file_exists('modules/' . $modOsDir .'/xaruser.php')) {
-                $userCapable = 1;
-            }
+                    // No dependency information = ok
+                    if (!isset($dependency)) {
+                        $dependency = array();
+                    }
 
-            // No dependency information = ok
-            if (!isset($dependency)) {
-                $dependency = array();
-            }
+                    //FIXME: <johnny> this detection isn't finished yet... we should be checking
+                    //for xaruser.php and then overriding with if $modFileInfo['user_capable'] is 1
+                    // Work out if user-capable
+                    if (1 == $modFileInfo['user_capable']) {
+                        $userCapable = 1;
+                    } else {
+                        $userCapable = 0;
+                    }
 
-            //FIXME: <johnny> this detection isn't finished yet... we should be checking
-            //for xaruser.php and then overriding with if $modFileInfo['user_capable'] is 1
-            // Work out if user-capable
-            if (1 == $modFileInfo['user_capable']) {
-                $userCapable = 1;
-            } else {
-                $userCapable = 0;
-            }
-
-            $fileModules[$name] = array('directory'     => $modOsDir,
-                                        'name'          => $name,
-                                        'regid'         => $regId,
-                                        'version'       => $version,
-                                        'mode'          => $mode,
-                                        'class'         => $class,
-                                        'category'      => $category,
-                                        'admin_capable' => $adminCapable,
-                                        'user_capable'  => $userCapable,
-                                        'dependency'    => $dependency);
-        }
-    }
+                    $fileModules[$name] = array('directory'     => $modOsDir,
+                                                'name'          => $name,
+                                                'regid'         => $regId,
+                                                'version'       => $version,
+                                                'mode'          => $mode,
+                                                'class'         => $class,
+                                                'category'      => $category,
+                                                'admin_capable' => $adminCapable,
+                                                'user_capable'  => $userCapable,
+                                                'dependency'    => $dependency);
+                } // if
+        } // switch
+    } // while
     closedir($dh);
 
     return $fileModules;
--- html/modules/themes/xaradminapi/getfilethemes.php
+++ html/modules/themes/xaradminapi/getfilethemes.php
@@ -12,81 +12,85 @@ function themes_adminapi_getfilethemes()
     $fileThemes = array();
     $dh = opendir('themes');
 
-    //SCCS is Bitkeeper's special directory , should we add cvs back into the mix too?
     while ($themeOsDir = readdir($dh)) {
-        if ((is_dir("themes/$themeOsDir")) &&
-                ($themeOsDir != '.') &&
-                ($themeOsDir != '..') &&
-                ($themeOsDir != 'SCCS')) {
+        switch ($themeOsDir) {
+            case '.':
+            case '..':
+            case 'CVS':
+            case 'SCCS':
+            case 'PENDING':
+                break;
+            default:
+                if (is_dir("themes/$themeOsDir")) {
 
-            // no xartheme.php, no theme
-            $themeFileInfo = xarTheme_getFileInfo($themeOsDir);
-            if (!isset($themeFileInfo)) {
-                continue;
-            }
+                    // no xartheme.php, no theme
+                    $themeFileInfo = xarTheme_getFileInfo($themeOsDir);
+                    if (!isset($themeFileInfo)) {
+                        continue;
+                    }
 
-            // Found a directory
-	        $name         = $themeFileInfo['name'];
-            $regId        = $themeFileInfo['id'];
-            $directory    = $themeFileInfo['directory'];
-            $author       = $themeFileInfo['author'];
-            $homepage     = $themeFileInfo['homepage'];
-            $email        = $themeFileInfo['email'];
-            $description  = $themeFileInfo['description'];
-            $contact_info = $themeFileInfo['contact_info'];
-            $publish_date = $themeFileInfo['publish_date'];
-            $license      = $themeFileInfo['license'];
-            $version      = $themeFileInfo['version'];
-            $xar_version  = $themeFileInfo['xar_version'];
-            $bl_version   = $themeFileInfo['bl_version'];
-            $class        = $themeFileInfo['class'];
+                    // Found a directory
+                    $name         = $themeFileInfo['name'];
+                    $regId        = $themeFileInfo['id'];
+                    $directory    = $themeFileInfo['directory'];
+                    $author       = $themeFileInfo['author'];
+                    $homepage     = $themeFileInfo['homepage'];
+                    $email        = $themeFileInfo['email'];
+                    $description  = $themeFileInfo['description'];
+                    $contact_info = $themeFileInfo['contact_info'];
+                    $publish_date = $themeFileInfo['publish_date'];
+                    $license      = $themeFileInfo['license'];
+                    $version      = $themeFileInfo['version'];
+                    $xar_version  = $themeFileInfo['xar_version'];
+                    $bl_version   = $themeFileInfo['bl_version'];
+                    $class        = $themeFileInfo['class'];
 
-            // TODO: beautify :-)
-            if (!isset($regId)) {
-                xarSessionSetVar('errormsg', "Theme '$name' doesn't seem to have a registered theme ID defined in xarversion.php - skipping...\nPlease register your theme at http://www.xaraya.com/index.php?module=release&func=addid if you haven't done so yet, and add \$themeversion['id'] = 'your ID'; in xarversion.php");
-                continue;
-            }
+                    // TODO: beautify :-)
+                    if (!isset($regId)) {
+                        xarSessionSetVar('errormsg', "Theme '$name' doesn't seem to have a registered theme ID defined in xarversion.php - skipping...\nPlease register your theme at http://www.xaraya.com/index.php?module=release&func=addid if you haven't done so yet, and add \$themeversion['id'] = 'your ID'; in xarversion.php");
+                        continue;
+                    }
 
-            // TODO: beautify :-)
-            if (!isset($regId) || xarVarPrepForOS($directory) != $themeOsDir) {
-                xarSessionSetVar('errormsg', "Theme '$name' exists in themes/$themeOsDir but should be in themes/$directory according to themes/$themeOsDir/xartheme.php... Skipping this theme until resolved.");
-                continue;
-            }
-            //Defaults
-            if (!isset($version)) {
-                $version = 1.0;
-            }
+                    // TODO: beautify :-)
+                    if (!isset($regId) || xarVarPrepForOS($directory) != $themeOsDir) {
+                        xarSessionSetVar('errormsg', "Theme '$name' exists in themes/$themeOsDir but should be in themes/$directory according to themes/$themeOsDir/xartheme.php... Skipping this theme until resolved.");
+                        continue;
+                    }
+                    //Defaults
+                    if (!isset($version)) {
+                        $version = 1.0;
+                    }
 
-            if (!isset($xar_version)) {
-                $xar_version = 1.0;
-            }
+                    if (!isset($xar_version)) {
+                        $xar_version = 1.0;
+                    }
 
-            if (!isset($bl_version)) {
-                $bl_version = 1.0;
-            }
+                    if (!isset($bl_version)) {
+                        $bl_version = 1.0;
+                    }
 
-            //FIXME: <johnny> add class and category checking
-            if (!isset($class)) {
-                $class = '0';
-            }
+                    //FIXME: <johnny> add class and category checking
+                    if (!isset($class)) {
+                        $class = '0';
+                    }
 
-
-            $fileThemes[$name] = array('name'             => $name,
-                                        'regid'           => $regId,
-                                        'directory'       => $directory,
-                                        'author'          => $author,
-                                        'homepage'        => $homepage,
-                                        'email'           => $email,
-                                        'description'     => $description,
-                                        'contact_info'    => $contact_info,
-                                        'publish_date'    => $publish_date,
-                                        'license'         => $license,
-                                        'version'         => $version,
-                                        'xar_version'     => $xar_version,
-                                        'bl_version'      => $bl_version,
-                                        'class'           => $class);
-        }
-    }
+                    $fileThemes[$name] = array('name'             => $name,
+                                               'regid'            => $regId,
+                                               'directory'        => $directory,
+                                               'author'           => $author,
+                                               'homepage'         => $homepage,
+                                               'email'            => $email,
+                                               'description'      => $description,
+                                               'contact_info'     => $contact_info,
+                                               'publish_date'     => $publish_date,
+                                               'license'          => $license,
+                                               'version'          => $version,
+                                               'xar_version'      => $xar_version,
+                                               'bl_version'       => $bl_version,
+                                               'class'            => $class);
+                } // if
+        } // switch
+    } // while
     closedir($dh);
     return $fileThemes;
 }
