e5fae81aed72b296572da55aec8bd6219437734b
John.Cox <John.Cox@mcnabb.>
2003-07-27 20:03:07 +0000

    html/includes/validations/array.php: 	#  Supressing 'Soft' Erros html/includes/validations/bool.php: 	#  Supressing 'Soft' Errors html/includes/validations/checkbox.php: 	#  Supressing 'Soft' Errors html/includes/validations/email.php: 	#  Supressing 'Soft' Errors html/includes/validations/enum.php: 	#  Supressing 'Soft' Errors html/includes/validations/float.php: 	#  Supressing 'Soft' Errors html/includes/validations/fullemail.php: 	#  Supressing 'Soft' Errors html/includes/validations/html.php: 	#  Supressing 'Soft' Errors html/includes/validations/id.php: 	#  Supressing 'Soft' Errors html/includes/validations/int.php: 	#  Supressing 'Soft' Errors html/includes/validations/isset.php: 	#  Supressing 'Soft' Errors html/includes/validations/list.php: 	#  Supressing 'Soft' Errors html/includes/validations/mxcheck.php: 	#  Supressing 'Soft' Errors html/includes/validations/notempty.php: 	#  Supressing 'Soft' Errors html/includes/validations/regexp.php: 	#  Supressing 'Soft' Errors html/includes/validations/str.php: 	#  Supressing 'Soft' Errors html/includes/xarVar.php: 	#  Supressing 'Soft' Errors 	# 	#  -> Changed flags (3->4) so they can be used idenpendently (And from a switch to ifs) 	#  -> Added $supress parameter to xarVarValidate to supress 'soft' errors 	#  -> Adapted xarVarFetch to use it 	#  -> Adapted validations to have such a parameter. tests/import/phpnuke/README: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/README tests/import/phpnuke/import_nuke.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke.php tests/import/phpnuke/import_nuke_cleanup.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_cleanup.php tests/import/phpnuke/import_nuke_comments.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_comments.php tests/import/phpnuke/import_nuke_faqanswer.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_faqanswer.php tests/import/phpnuke/import_nuke_faqcategories.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_faqcategories.php tests/import/phpnuke/import_nuke_links_categories.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_links_categories.php tests/import/phpnuke/import_nuke_links_links.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_links_links.php tests/import/phpnuke/import_nuke_poll_data.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_poll_data.php tests/import/phpnuke/import_nuke_poll_desc.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_poll_desc.php tests/import/phpnuke/import_nuke_pollcomments.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_pollcomments.php tests/import/phpnuke/import_nuke_queue.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_queue.php tests/import/phpnuke/import_nuke_seccont.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_seccont.php tests/import/phpnuke/import_nuke_sections.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_sections.php tests/import/phpnuke/import_nuke_stories.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_stories.php tests/import/phpnuke/import_nuke_topicscat.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_topicscat.php tests/import/phpnuke/import_nuke_users.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/stable/tests/import/phpnuke/import_nuke_users.php

---
+++ tests/import/phpnuke/import_nuke_links_categories.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * File: $Id$
+ *
+ * Import PostNuke .71+ web link categories into your Xaraya test site
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @link http://www.xaraya.com
+ * 
+ * @subpackage import
+ * @author mikespub <mikespub@xaraya.com>
+ */
+
+/**
+ * Note : this file is part of import_pn.php and cannot be run separately
+ */
+
+    echo "<strong>$step. Importing old web link categories</strong><br>\n";
+
+    $weblinks[0] = xarModAPIFunc('categories', 'admin', 'create', array(
+                                'name' => 'Web Links',
+                                'description' => 'Web Link Categories (.7x style)',
+                                'parent_id' => 0));
+    $query = 'SELECT cid, parentid, title, cdescription
+              FROM ' . $oldprefix . '_links_categories
+              ORDER BY parentid ASC, cid ASC';
+    $result =& $dbconn->Execute($query);
+    if (!$result) {
+        die("Oops, select links_categories failed : " . $dbconn->ErrorMsg());
+    }
+    while (!$result->EOF) {
+        list($id, $parent, $title, $descr) = $result->fields;
+        if (!isset($weblinks[$parent])) {
+            echo "Oops, missing parent $parent for category ($id) $title<br>\n";
+            $result->MoveNext();
+            continue;
+        }
+        $weblinks[$id] = xarModAPIFunc('categories', 'admin', 'create', array(
+                                      'name' => $title,
+                                      'description' => $descr,
+                                 //     'image' => "$imgurl/topics/$image",
+                                      'parent_id' => $weblinks[$parent]));
+        echo "Creating web link category ($id) $title - $descr<br>\n";
+        $result->MoveNext();
+    }
+    $result->Close();
+    xarModSetVar('installer','weblinks',serialize($weblinks));
+
+    $settings = unserialize(xarModGetVar('articles', 'settings.6'));
+    $settings['number_of_categories'] = 1;
+    $settings['cids'] = array($weblinks[0]);
+    xarModSetVar('articles', 'settings.6', serialize($settings));
+    xarModSetVar('articles', 'number_of_categories.6', 1);
+    xarModSetVar('articles', 'mastercids.6', $weblinks[0]);
+
+    echo '<a href="import_nuke.php">Return to start</a>&nbsp;&nbsp;&nbsp;
+          <a href="import_nuke.php?step=' . ($step+1) . '&module=articles">Go to step ' . ($step+1) . '</a><br>';
+    $dbconn->Execute('OPTIMIZE TABLE ' . $tables['categories']);
+
+?>
---
+++ tests/import/phpnuke/import_nuke_links_links.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * File: $Id$
+ *
+ * Import PostNuke .71+ web links into your Xaraya test site
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @link http://www.xaraya.com
+ * 
+ * @subpackage import
+ * @author mikespub <mikespub@xaraya.com>
+ */
+
+/**
+ * Note : this file is part of import_pn.php and cannot be run separately
+ */
+
+    echo "<strong>$step. Importing old web links</strong><br>\n";
+
+    $userid = unserialize(xarModGetVar('installer','userid'));
+    if (xarModIsAvailable('hitcount') && xarModAPILoad('hitcount','admin')) {
+        $docounter = 1;
+    }
+    $weblinks = unserialize(xarModGetVar('installer','weblinks'));
+    $regid = xarModGetIDFromName('articles');
+    $query = 'SELECT lid, cid, title, ' . $oldprefix . '_links_links.url, description,
+                     UNIX_TIMESTAMP(date), ' . $oldprefix . '_links_links.name, ' . $oldprefix . '_links_links.email, hits,
+                     submitter, linkratingsummary, totalvotes, uid
+              FROM ' . $oldprefix . '_links_links
+              LEFT JOIN ' . $oldprefix . '_users
+              ON ' . $oldprefix . '_users.uname = ' . $oldprefix . '_links_links.submitter
+              ORDER BY lid ASC';
+    $result =& $dbconn->Execute($query);
+    if (!$result) {
+        die("Oops, select links failed : " . $dbconn->ErrorMsg());
+    }
+    while (!$result->EOF) {
+        list($lid, $catid, $title, $url, $descr, $date, $name,
+            $email, $hits, $submitter, $rating, $votes, $uid) = $result->fields;
+        $status = 2;
+        $language = '';
+        if (isset($userid[$uid])) {
+            $uid = $userid[$uid];
+        } // else we're lost :)
+        if (empty($uid) || $uid < 2) {
+            $uid = _XAR_ID_UNREGISTERED;
+        }
+        if (!empty($email)) {
+            $email = ' <' . $email . '>';
+        }
+        $cids = array();
+        if (isset($weblinks[$catid])) {
+            $cids[] = $weblinks[$catid];
+        }
+        if (empty($title)) {
+            $title = xarML('[none]');
+        }
+        $newaid = xarModAPIFunc('articles',
+                                'admin',
+                                'create',
+                                array('title' => $title,
+                                      'summary' => $descr,
+                                      'body' => $url,
+                                      'notes' => $name . $email,
+                                      'status' => $status,
+                                      'ptid' => 6,
+                                      'pubdate' => $date,
+                                      'authorid' => $uid,
+                                      'language' => $language,
+                                      'cids' => $cids,
+                                      'hits' => $hits
+                                     )
+                               );
+        if (!isset($newaid)) {
+            echo "Insert web link ($lid) $title failed : " . xarExceptionRender('text') . "<br>\n";
+        } else {
+            echo "Inserted web link ($lid) $title<br>\n";
+        }
+// TODO: ratings
+        $result->MoveNext();
+    }
+    $result->Close();
+    echo "<strong>TODO : import ratings, editorials, new links and modifications etc.</strong><br><br>\n";
+    echo '<a href="import_nuke.php">Return to start</a>&nbsp;&nbsp;&nbsp;
+          <a href="import_nuke.php?step=' . ($step+1) . '">Go to step ' . ($step+1) . '</a><br>';
+    $dbconn->Execute('OPTIMIZE TABLE ' . $tables['articles']);
+    $dbconn->Execute('OPTIMIZE TABLE ' . $tables['categories_linkage']);
+    if (!empty($docounter)) {
+        $dbconn->Execute('OPTIMIZE TABLE ' . $tables['hitcount']);
+    }
+
+?>
