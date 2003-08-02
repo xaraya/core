b0c19ddbbd2851386690d01dc2f886c0282cfbc6
rcave <rcave@lxwdev-1.schwabfoundation.org>
2003-08-02 20:03:08 +0000

    html/modules/blocks/xaradminapi/unregister_block_type.php: 	#  Fix for bug #1005 - Remove the block ids from xar_block_instances 	#  and xar_block_group_instances so errors aren't thrown when a 	#  module is remove.

--- html/modules/blocks/xaradminapi/unregister_block_type.php
+++ html/modules/blocks/xaradminapi/unregister_block_type.php
@@ -22,7 +22,35 @@ function blocks_adminapi_unregister_block_type($args)
     $xartable = xarDBGetTables();
 
     $block_types_table = $xartable['block_types'];
+    $block_instances_table = $xartable['block_instances'];
 
+    // First we need to retrieve the block ids and remove 
+    // the corresponding id's from the xar_block_instances
+    // and xar_block_group_instances tables
+    $query = "SELECT    inst.xar_id as id
+              FROM      $block_instances_table as inst,
+                        $block_types_table as btypes
+              WHERE     btypes.xar_id = inst.xar_type_id
+              AND       btypes.xar_module = '$modName'
+              AND       btypes.xar_type = '$blockType'";
+
+    $result =& $dbconn->Execute($query);
+    if (!$result) return;
+
+    while (!$result->EOF) {
+        list($bid) = $result->fields;
+
+        // Pass ids to API
+        xarModAPIFunc('blocks',
+                      'admin',
+                      'delete_instance', array('bid' => $bid));
+
+        $result->MoveNext();
+    }
+
+    $result->Close();
+
+    // Delete the block type
     $query = "DELETE FROM $block_types_table WHERE xar_module = '$modName' AND xar_type = '$blockType';";
     $result =& $dbconn->Execute($query);
     if (!$result) return;
