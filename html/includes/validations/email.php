dccc01780485f4b75b9d46c9d4159f109aee14f9
iansym <iansym@howler.asphyxia.com>
2003-08-16 20:03:08 +0000

    html/includes/validations/email.php: 	#  quick path to allow .info, .museum, and other TLDs with up to 6 characters.

--- html/includes/validations/email.php
+++ html/includes/validations/email.php
@@ -15,7 +15,7 @@
  */
 function variable_validations_email (&$subject, $parameters=null, $supress_soft_exc)
 {
-    if (!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $subject)) {
+    if (!eregi('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,6})$', $subject)) {
 
         $msg = xarML('Invalid Variable #(1), does not match an e-mail type.', $subject);
         if (!$supress_soft_exc) xarExceptionSet(XAR_USER_EXCEPTION, 'BAD_PARAM', new SystemException($msg));
