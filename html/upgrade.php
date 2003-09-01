3c57836af0cd8ab0e5d30c48caed8588524a0036
John.Cox <John.Cox@mcnabb.>
2003-09-01 20:03:09 +0000

    html/modules/installer/xaradmin.php: 	#  only show core install when core is the package selected. html/includes/xarCore.php: 	#  Upped version for release. html/modules/base/xarinit.php: 	#  Upped version for release. html/upgrade.php: 	#  Upped version for release.

--- html/upgrade.php
+++ html/upgrade.php
@@ -430,7 +430,7 @@ if (empty($step)) {
     $configvars[] = array(array('name'    =>  'Site.User.AnonymousUID',
                                 'set'     =>  $roleanon->getID()),
                           array('name'    =>  'System.Core.VersionNum',
-                                'set'     =>  '.9.0.7'));
+                                'set'     =>  '.9.1.0'));
  
     foreach($configvars as $configvar){
         foreach($configvar as $var){
