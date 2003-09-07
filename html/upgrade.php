f5d08ecc199db6b169f8dc59b7c8e21b93bc7208
John.Cox <John.Cox@mcnabb.>
2003-09-07 20:03:09 +0000

    html/modules/base/xarinit.php: 	#  upped version to 911 html/modules/base/xartemplates/admin-modifyconfig.xd: 	#  upped version to 911 html/upgrade.php: 	#  upped version to 911

--- html/upgrade.php
+++ html/upgrade.php
@@ -721,7 +721,7 @@ if (empty($step)) {
     $configvars[] = array(array('name'    =>  'Site.User.AnonymousUID',
                                 'set'     =>  $roleanon->getID()),
                           array('name'    =>  'System.Core.VersionNum',
-                                'set'     =>  '.9.1.0'));
+                                'set'     =>  '.9.1.1'));
  
     foreach($configvars as $configvar){
         foreach($configvar as $var){
