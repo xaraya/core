e76ebc438e7b3c6aa5fbeb4b4ed47e2cca7ae804
BRUMDall <BRUMDall@brumdall2m.bru.fsc.net>
2003-09-20 20:03:09 +0000

    html/includes/xarBLCompiler.php: 	#  - For custom tags, both the tag in template and the handler should be present at compile time, except otherwise, this should 	#  fix bug #262 html/modules/dynamicdata/xaradmin/query.php: 	#  fix query update + optionally join DD object with main module table html/modules/dynamicdata/xartemplates/admin-query.xd: 	#  fix query update + optionally join DD object with main module table html/modules/dynamicdata/xartemplates/util-main.xd: 	#  fix query update + optionally join DD object with main module table

--- html/modules/dynamicdata/xaradmin/query.php
+++ html/modules/dynamicdata/xaradmin/query.php
@@ -17,6 +17,8 @@ function dynamicdata_admin_query($args)
     if(!xarVarFetch('oldtable', 'str', $oldtable, '', XARVAR_NOT_REQUIRED)) {return;}
     if(!xarVarFetch('itemid', 'int', $itemid, 0, XARVAR_NOT_REQUIRED)) {return;}
     if(!xarVarFetch('olditemid', 'int', $olditemid, 0, XARVAR_NOT_REQUIRED)) {return;}
+    if(!xarVarFetch('join', 'str', $join, '', XARVAR_NOT_REQUIRED)) {return;}
+    if(!xarVarFetch('oldjoin', 'str', $oldjoin, '', XARVAR_NOT_REQUIRED)) {return;}
 
     if(!xarVarFetch('field', 'isset', $field, NULL, XARVAR_DONT_SET)) {return;}
     if(!xarVarFetch('where', 'isset', $where, NULL, XARVAR_DONT_SET)) {return;}
@@ -25,6 +27,7 @@ function dynamicdata_admin_query($args)
     if(!xarVarFetch('numitems', 'isset', $numitems, NULL, XARVAR_DONT_SET)) {return;}
     if(!xarVarFetch('startnum', 'isset', $startnum, NULL, XARVAR_DONT_SET)) {return;}
 
+    $reset = false;
     // changed selected object
     if ($itemid != $olditemid) {
         $table = '';
@@ -32,12 +35,9 @@ function dynamicdata_admin_query($args)
         $query = '';
         $oldquery = '';
         $newquery = '';
-        $field = array();
-        $where = array();
-        $value = array();
-        $sort = array();
-        $numitems = 20;
-        $startnum = 1;
+        $join = '';
+        $oldjoin = '';
+        $reset = true;
     // changed selected table
     } elseif ($table != $oldtable) {
         $itemid = 0;
@@ -45,31 +45,35 @@ function dynamicdata_admin_query($args)
         $query = '';
         $oldquery = '';
         $newquery = '';
-        $field = array();
-        $where = array();
-        $value = array();
-        $sort = array();
-        $numitems = 20;
-        $startnum = 1;
+        $join = '';
+        $oldjoin = '';
+        $reset = true;
     // changed selected query
     } elseif ($query != $oldquery) {
         $itemid = 0;
         $olditemid = 0;
         $table = '';
         $oldtable = '';
+        $join = '';
+        $oldjoin = '';
         $newquery = $query;
-        $field = array();
-        $where = array();
-        $value = array();
-        $sort = array();
-        $numitems = 20;
-        $startnum = 1;
+        $reset = true;
+    // changed selected join table
+    } elseif ($join != $oldjoin) {
+        $table = '';
+        $oldtable = '';
+        $query = '';
+        $oldquery = '';
+        $newquery = '';
+        $reset = true;
     // used the pager, so we retrieve the current query from session variables
     } elseif (!empty($startnum) && is_numeric($startnum)
               && empty($itemid) && empty($table) && empty($query)) {
         $query = xarSessionGetVar('DynamicData.LastQuery');
         if (!empty($query)) {
             $newquery = $query;
+            $startpager = $startnum;
+            $reset = true;
         }
     // used the header sort, so we retrieve the current query from session variables
     } elseif (!empty($sort) && is_string($sort)
@@ -78,15 +82,33 @@ function dynamicdata_admin_query($args)
         if (!empty($query)) {
             $newquery = $query;
             $sorthead = $sort;
-            $sort = null;
+            $reset = true;
         }
+    }
+
+    if ($reset) {
+        $field = array();
+        $where = array();
+        $value = array();
+        $sort = array();
+        $numitems = 20;
+        $startnum = 1;
     }
 
     if (!empty($query) && $query == $newquery) {
         $queryinfo = xarModGetVar('dynamicdata','query.'.$query);
         if (!empty($queryinfo)) {
             $queryvars = unserialize($queryinfo);
-            extract($queryvars);
+            if ($reset) {
+                extract($queryvars);
+            } else {
+                $itemid = $queryvars['itemid'];
+                $olditemid = $queryvars['olditemid'];
+                $table = $queryvars['table'];
+                $oldtable = $queryvars['oldtable'];
+                $query = $queryvars['query'];
+                $oldquery = $queryvars['oldquery'];
+            }
         }
     }
 
@@ -109,12 +131,49 @@ function dynamicdata_admin_query($args)
     $data['oldtable'] = $table;
     $data['tables'] = $dbconn->MetaTables();
 
+    $data['join'] = $join;
+    $data['oldjoin'] = $join;
+    $data['jointables'] = '';
+
     if (!empty($itemid)) {
-        $data['object'] =& xarModAPIFunc('dynamicdata','user','getobject',
-                                         array('objectid' => $itemid));
+        $data['object'] = new Dynamic_Object_List(array('objectid' => $itemid));
         if (isset($data['object']) && !empty($data['object']->objectid)) {
             $data['itemid'] = $data['object']->objectid;
             $data['label'] = $data['object']->label;
+            if (empty($data['object']->primary)) {
+            // (try to) show the "static" properties, corresponding to fields in dedicated
+            // tables for this module
+                $static = xarModAPIFunc('dynamicdata','util','getstatic',
+                                        array('modid' => $data['object']->moduleid,
+                                              'itemtype' => $data['object']->itemtype));
+                $data['jointables'] = array();
+                if (!empty($static)) {
+                    $count = count($data['object']->properties);
+                    foreach ($static as $name => $propinfo) {
+                        if (preg_match('/^(\w+)\.(\w+)$/', $propinfo['source'], $matches)) {
+                            $jointable = $matches[1];
+                            $data['jointables'][$jointable] = $jointable;
+                        }
+                    }
+                }
+                if (!empty($join) && isset($data['jointables'][$join])) {
+                    $meta = xarModAPIFunc('dynamicdata','util','getmeta',
+                                          array('table' => $join));
+                    if (!isset($meta) || !isset($meta[$join])) {
+                        return xarML('Invalid table #(1)',xarVarPrepForDisplay($join));
+                    }
+                    $count = count($data['object']->properties);
+                    foreach ($meta[$join] as $name => $propinfo) {
+                        $data['object']->addProperty($propinfo);
+                        $data['object']->properties[$name]->items = & $data['object']->items;
+                    }
+                    if (count($data['object']->properties) > $count) {
+                        // put join properties in front
+                        $joinprops = array_splice($data['object']->properties,$count);
+                        $data['object']->properties = array_merge($joinprops,$data['object']->properties);
+                    }
+                }
+            }
             $data['properties'] =& $data['object']->properties;
         } else {
             return;
@@ -123,13 +182,14 @@ function dynamicdata_admin_query($args)
         $meta = xarModAPIFunc('dynamicdata','util','getmeta',
                               array('table' => $table));
         if (!isset($meta) || !isset($meta[$table])) {
-           return xarML('Invalid table');
+            return xarML('Invalid table #(1)',xarVarPrepForDisplay($table));
         }
-        $data['object'] =& xarModAPIFunc('dynamicdata','user','getobject',
-                                         array('objectid' => -1, // dummy object
-                                               'name' => $table));
+        $data['object'] = new Dynamic_Object_List(array('objectid' => -1, // dummy object
+                                                        'moduleid' => 182, // needed for showlist check in template
+                                                        'name' => $table));
         foreach ($meta[$table] as $name => $propinfo) {
             $data['object']->addProperty($propinfo);
+            $data['object']->properties[$name]->items = & $data['object']->items;
         }
         $data['label'] = xarML('Table #(1)',$table);
         $data['properties'] =& $data['object']->properties;
@@ -159,6 +219,9 @@ function dynamicdata_admin_query($args)
     if (empty($numitems)) {
         $numitems = 20;
     }
+    if (!empty($startpager)) {
+        $startnum = $startpager;
+    }
     if (empty($startnum)) {
         $startnum = 1;
     }
@@ -173,23 +236,23 @@ function dynamicdata_admin_query($args)
 
 // TODO: clean up passing of where clauses
     $whereclause = '';
-    $join = '';
+    $and = '';
     if (!empty($where) && count($where) > 0 && !empty($value) && count($value) > 0) {
         foreach ($where as $name => $what) {
             if (empty($what)) continue;
             if (!isset($value[$name])) continue;
             if ($what == 'like') {
-                $whereclause .= $join . $name . " LIKE '%" . xarVarPrepForStore($value[$name]) . "%'";
+                $whereclause .= $and . $name . " LIKE '%" . xarVarPrepForStore($value[$name]) . "%'";
             } elseif ($what == 'start') {
-                $whereclause .= $join . $name . " LIKE '" . xarVarPrepForStore($value[$name]) . "%'";
+                $whereclause .= $and . $name . " LIKE '" . xarVarPrepForStore($value[$name]) . "%'";
             } elseif ($what == 'end') {
-                $whereclause .= $join . $name . " LIKE '%" . xarVarPrepForStore($value[$name]) . "'";
+                $whereclause .= $and . $name . " LIKE '%" . xarVarPrepForStore($value[$name]) . "'";
             } elseif ($what == 'in') {
-                $whereclause .= $join . $name . " IN (" . xarVarPrepForStore($value[$name]) . ")";
+                $whereclause .= $and . $name . " IN (" . xarVarPrepForStore($value[$name]) . ")";
             } else {
-                $whereclause .= $join . $name . " $what '" . xarVarPrepForStore($value[$name]) . "'";
+                $whereclause .= $and . $name . " $what '" . xarVarPrepForStore($value[$name]) . "'";
             }
-            $join = ' and ';
+            $and = ' and ';
         }
     }
 
@@ -213,41 +276,65 @@ function dynamicdata_admin_query($args)
         $sortlist = null;
     }
 
-    if (!empty($itemid) && $itemid == $olditemid) {
-        $mylist = new Dynamic_Object_List(array('objectid' => $itemid,
-                                                'fieldlist' => $fieldlist,
-                                                'where' => $whereclause,
-                                                'sort' => $sortlist,
-                                                'startnum' => $startnum,
-                                                'numitems' => $numitems));
-        $mylist->getItems();
-        $data['mylist'] = & $mylist;
-        if (empty($newquery)) {
-            $newquery = xarML('Last Query');
-        }
-        xarSessionSetVar('DynamicData.LastQuery',$newquery);
-    } elseif (!empty($table) && $table == $oldtable) {
     // TODO: clean up generation of dummy object
-        $mylist = new Dynamic_Object_List(array('objectid' => -1, // dummy object
-                                                'moduleid' => 182, // needed for showlist check
-                                                'name' => $table,
-                                                'startnum' => $startnum,
-                                                'numitems' => $numitems));
-        foreach ($meta[$table] as $name => $propinfo) {
-            $mylist->addProperty($propinfo);
-            $mylist->properties[$name]->items = & $mylist->items;
+    if ( (!empty($itemid) && $itemid == $olditemid) ||
+         (!empty($table) && $table == $oldtable) ) {
+        $data['object']->fieldlist = $fieldlist;
+        $data['object']->startnum = $startnum;
+        $data['object']->numitems = $numitems;
+        // regenerate the data stores
+        $data['object']->datastores = null;
+        $data['object']->getDataStores();
+        foreach (array_keys($data['object']->datastores) as $name) {
+            $data['object']->datastores[$name]->itemids = & $data['object']->itemids;
         }
-        $mylist->fieldlist = $fieldlist;
-        $mylist->getDataStores();
-        $mylist->getItems(array('where' => $whereclause,
-                                'sort' => $sortlist));
-        $data['mylist'] = & $mylist;
+        $data['object']->getItems(array('where' => $whereclause,
+                                        'sort' => $sortlist));
+        $data['mylist'] =& $data['object'];
         if (empty($newquery)) {
             $newquery = xarML('Last Query');
         }
         xarSessionSetVar('DynamicData.LastQuery',$newquery);
     } else {
         xarSessionSetVar('DynamicData.LastQuery','');
+    }
+
+    if (!empty($newquery)) {
+        $data['newquery'] = $newquery;
+        $queryvars = array();
+        $queryvars['itemid'] = $itemid;
+        $queryvars['olditemid'] = $olditemid;
+        $queryvars['table'] = $table;
+        $queryvars['oldtable'] = $oldtable;
+        $queryvars['query'] = $newquery;
+        $queryvars['oldquery'] = $newquery;
+        $queryvars['newquery'] = $newquery;
+        $queryvars['join'] = $join;
+        $queryvars['oldjoin'] = $oldjoin;
+        $queryvars['field'] = $field;
+        $queryvars['where'] = $where;
+        $queryvars['value'] = $value;
+        $queryvars['sort'] = $sort;
+        $queryvars['numitems'] = $numitems;
+    // don't save the start number here
+    //   $queryvars['startnum'] = $startnum;
+    // TODO: clean up query cleaning
+        if (count($data['queries']) >= 20) {
+            $dropquery = array_pop($data['queries']);
+            if (!empty($dropquery)) {
+                xarModDelVar('dynamicdata','query.'.$dropquery);
+            }
+            xarModSetVar('dynamicdata','querylist',serialize($data['queries']));
+        }
+        xarModSetVar('dynamicdata','query.'.$newquery, serialize($queryvars));
+        if (count($data['queries']) == 0 || !in_array($newquery,$data['queries'])) {
+            array_unshift($data['queries'],$newquery);
+            xarModSetVar('dynamicdata','querylist',serialize($data['queries']));
+        }
+        $data['query'] = $newquery;
+        $data['oldquery'] = $newquery;
+    } else {
+        $data['newquery'] = '';
     }
 
     $data['numfields'] = count($data['properties']);
@@ -276,42 +363,6 @@ function dynamicdata_admin_query($args)
                                  array('id' => '-3', 'name' => xarML('sort #(1) - down', 3)),
                                 );
     $data['submit'] = xarML('Update Query');
-
-    if (!empty($newquery)) {
-        $data['newquery'] = $newquery;
-        $queryvars = array();
-        $queryvars['itemid'] = $itemid;
-        $queryvars['olditemid'] = $olditemid;
-        $queryvars['table'] = $table;
-        $queryvars['oldtable'] = $oldtable;
-        $queryvars['query'] = $newquery;
-        $queryvars['oldquery'] = $newquery;
-        $queryvars['newquery'] = $newquery;
-        $queryvars['field'] = $field;
-        $queryvars['where'] = $where;
-        $queryvars['value'] = $value;
-        $queryvars['sort'] = $sort;
-        $queryvars['numitems'] = $numitems;
-    // don't save the start number here
-    //   $queryvars['startnum'] = $startnum;
-    // TODO: clean up query cleaning
-        if (count($data['queries']) >= 20) {
-            $dropquery = array_pop($data['queries']);
-            if (!empty($dropquery)) {
-                xarModDelVar('dynamicdata','query.'.$dropquery);
-            }
-            xarModSetVar('dynamicdata','querylist',serialize($data['queries']));
-        }
-        xarModSetVar('dynamicdata','query.'.$newquery, serialize($queryvars));
-        if (count($data['queries']) == 0 || !in_array($newquery,$data['queries'])) {
-            array_unshift($data['queries'],$newquery);
-            xarModSetVar('dynamicdata','querylist',serialize($data['queries']));
-        }
-        $data['query'] = $newquery;
-        $data['oldquery'] = $newquery;
-    } else {
-        $data['newquery'] = '';
-    }
 
     // Return the template variables defined in this function
     return $data;
