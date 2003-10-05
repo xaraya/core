cf85f41b26e58c3b4720acc18664d9aae33d19aa
andyv <andyv@andyv.plus.com>
2003-10-05 20:03:10 +0000

    html/includes/xarCore.php: 	#  Upped version number html/modules/base/xarinit.php: 	#  Upped version number html/upgrade.php: 	#  Upped version number html/modules/themes/xaradmin/themesinfo.php: 	#  some cleanup - mod -> to -> theme html/modules/themes/xaradmin/themesinfo.php: 	#  Rename: html/modules/themes/xaradmin/themeinfo.php -> html/modules/themes/xaradmin/themesinfo.php html/modules/themes/xaradmin/themesinfo.php: 	#  Rename: html/modules/themes/xaradmin/modinfo.php -> html/modules/themes/xaradmin/themeinfo.php html/modules/themes/xartemplates/admin-themesinfo.xd: 	#  cleaned up template, added theme preview selector.. if it happens to be popular, may add some config options html/modules/themes/xartemplates/admin-themesinfo.xd: 	#  Rename: html/modules/themes/xartemplates/admin-themeinfo.xd -> html/modules/themes/xartemplates/admin-themesinfo.xd html/modules/themes/xartemplates/admin-themesinfo.xd: 	#  Rename: html/modules/themes/xartemplates/admin-modinfo.xd -> html/modules/themes/xartemplates/admin-themeinfo.xd

---
+++ html/modules/themes/xaradmin/themesinfo.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * View complete module information/details
+ * function passes the data to the template
+ *
+ * @access public
+ * @param none
+ * @returns array
+ * @todo some facelift
+ */
+function themes_admin_themesinfo(){
+    
+    // Security check - not needed here, imo 
+    // we just show some info here, not changing anything
+    /* if (!xarSecConfirmAuthKey()) return; */
+
+    $data = array();
+    
+    if (!xarVarFetch('id', 'int:1:', $id)) return; 
+    // obtain maximum information about module
+    $info = xarThemeGetInfo($id);
+    // data vars for template
+    $data['themeid']              = xarVarPrepForDisplay($id);
+    $data['themename']            = xarVarPrepForDisplay($info['name']);
+    $data['themedescr']           = xarVarPrepForDisplay($info['description']);
+    //$data['themedispname']        = xarVarPrepForDisplay($themeinfo['displayname']);
+    $data['themelisturl']         = xarModURL('themes', 'admin', 'list');
+
+    $data['themedir']             = xarVarPrepForDisplay($info['directory']);
+    $data['themeclass']           = xarVarPrepForDisplay($info['class']);
+    $data['themever']             = xarVarPrepForDisplay($info['version']);
+    $data['themeauthor']          = preg_replace('/,/', '<br />', xarVarPrepForDisplay($info['author']));
+    if(!empty($info['dependency'])){
+        $dependency             = xarML('Working on it...');
+    } else {
+        $dependency             = xarML('None');
+    }
+    $data['themedependency']      = xarVarPrepForDisplay($dependency);
+    
+    // Redirect
+    return $data;
+}
+?>
