a0e512359b946b9232aab5d7408d2c1f5025a35a
John.Cox <John.Cox@mcnabb.>
2003-10-31 20:03:10 +0000

    html/modules/modules/xaradmin/release.php: 	#  use the base functionality to grab the release data.  Good stuff Mike;) html/modules/themes/xaradmin/release.php: 	#  use the base functionality to grab the release data.  Good stuff Mike;)

--- html/modules/themes/xaradmin/release.php
+++ html/modules/themes/xaradmin/release.php
@@ -12,48 +12,27 @@ function themes_admin_release()
 {
     // Security Check
 	if(!xarSecurityCheck('EditModules')) return;
-
     // allow fopen
     ini_set('allow_url_fopen', 1);
-
     // Require the xmlParser class
     require_once('modules/base/xarclass/xmlParser.php');
-
     // Require the feedParser class
     require_once('modules/base/xarclass/feedParser.php');
-
-
     // Check and see if a feed has been supplied to us.
     // Need to change the url once release module is moved to 
     $feedfile = "http://www.xaraya.com/index.php/articles/rnid/c69/?theme=rss";
-
-    // Create Cache File for 7 days.
-    $refresh = (time() - 604800);
-    $varDir = xarCoreGetVarDirPath();
-    $cacheKey = md5($feedfile);
-    $cachedFileName = $varDir . '/cache/rss/' . $cacheKey . '.xml';
-    if ((file_exists($cachedFileName)) && (filemtime($cachedFileName) > $refresh)) {
-        $fp = @fopen($cachedFileName, 'r');
-        // Create a need feedParser object
-        $p = new feedParser();
-        // Read From Our Cache
-        $feeddata = fread($fp, filesize($cachedFileName));
-        // Tell feedParser to parse the data
-        $info = $p->parseFeed($feeddata);
-    } else {
-        // Create a need feedParser object
-        $p = new feedParser();
-
-        // Read in our sample feed file
-        $feeddata = @implode("",@file($feedfile));
-
-        // Tell feedParser to parse the data
-        $info = $p->parseFeed($feeddata);
-        $fp = fopen("$cachedFileName", "wt");
-        fwrite($fp, $feeddata);
-        fclose($fp);    
-    }
-
+    // Get the feed file (from cache or from the remote site)
+    $feeddata = xarModAPIFunc('base', 'user', 'getfile',
+                              array('url' => $feedfile,
+                                    'cached' => true,
+                                    'cachedir' => 'cache/rss',
+                                    'refresh' => 604800,
+                                    'extension' => '.xml'));
+    if (!$feeddata) return;
+    // Create a need feedParser object
+    $p = new feedParser();
+    // Tell feedParser to parse the data
+    $info = $p->parseFeed($feeddata);
     if (empty($info['warning'])){
         foreach ($info as $content){
              foreach ($content as $newline){
@@ -78,7 +57,6 @@ function themes_admin_release()
                 }
             }
         }
-
         $data['chantitle']  =   $info['channel']['title'];
         $data['chanlink']   =   $info['channel']['link'];
         $data['chandesc']   =   $info['channel']['description'];
@@ -87,10 +65,7 @@ function themes_admin_release()
         xarExceptionSet(XAR_USER_EXCEPTION, 'MISSING_DATA', new DefaultUserException($msg));
         return;
     }
-    
     $data['feedcontent'] = $feedcontent;
-
     return $data;
 }
-
 ?>
