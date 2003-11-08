255f62d7e7bd00bc5c5aca74dd22e060d05920cc
judgej <judgej@acad-ajp.>
2003-11-08 20:03:11 +0000

    html/modules/base/xarjavascriptapi/handlemodulejavascript.php: 	#  Bug 1542: JavaScript include - default the included JS from the module the 	#  template calling the tag is running from.

--- html/modules/base/xarjavascriptapi/handlemodulejavascript.php
+++ html/modules/base/xarjavascriptapi/handlemodulejavascript.php
@@ -4,7 +4,14 @@
  * Handle <xar:base-include-javascript ...> form field tags
  * Format : <xar:base-include-javascript definition="$definition" /> with $definition an array
  *       or <xar:base-include-javascript filename="thisname.js" module="modulename" position="head|body|whatever"/>
- * Default module is 'base' and default position is 'head'; filename is mandatory.
+ * Default module is the module in which the tag is called and default position is 'head'; filename is mandatory.
+ *
+ * Example:
+ * The following tag is included in an 'articles' template. The file 'myfile.js'
+ * can be located in either themes/<current>/modules/articles/includes or
+ * modules/articles/xartemplates/includes:
+ *
+ *    <xar:base-include-javascript filename="myfile.js"/>
  *
  * @author Jason Judge
  * @param $args array containing the form field definition or the type, name, value, ...
@@ -21,7 +28,14 @@ function base_javascriptapi_handlemodulejavascript($args)
     }
 
     // Set some defaults - only attribute 'filename' is mandatory.
-    if (empty($module)) {$module = 'base';}
+    if (empty($module)) {
+        // No module name is supplied, default the module from the
+        // current template module (not the current executing module).
+        $module = '$_bl_module_name';
+    } else {
+        // The module name is supplied.
+        $module = '\'' . addslashes($module) . '\'';
+    }
     if (empty($position)) {$position = 'head';}
 
     // Return the code to call up the javascript file.
@@ -32,8 +46,8 @@ function base_javascriptapi_handlemodulejavascript($args)
     if (!empty($filename)) {
         $out = "xarModAPIFunc("
             . "'base', 'javascript', 'modulefile', "
-            ."array('module'=>'" . addslashes($module)
-            . "', 'filename'=>'" . addslashes($filename)
+            ."array('module'=>" . $module
+            . ", 'filename'=>'" . addslashes($filename)
             . "', 'position'=>'" . addslashes($position) . "')); ";
     } else {
         $out = '';
