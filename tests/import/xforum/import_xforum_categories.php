7579f0256379706158ef71ff89e0f953d26f7d7c
jojodee <jojodee@hbwdev.net>
2003-11-30 20:03:11 +0000

    tests/import/xforum/README: 	#  BitKeeper file C:/servroot/home/jojodee/bk/stable/tests/import/xforum/README tests/import/xforum/import_xforum.php: 	#  BitKeeper file C:/servroot/home/jojodee/bk/stable/tests/import/xforum/import_xforum.php tests/import/xforum/import_xforum_categories.php: 	#  BitKeeper file C:/servroot/home/jojodee/bk/stable/tests/import/xforum/import_xforum_categories.php tests/import/xforum/import_xforum_cleanup.php: 	#  BitKeeper file C:/servroot/home/jojodee/bk/stable/tests/import/xforum/import_xforum_cleanup.php tests/import/xforum/import_xforum_forums.php: 	#  BitKeeper file C:/servroot/home/jojodee/bk/stable/tests/import/xforum/import_xforum_forums.php tests/import/xforum/import_xforum_posts.php: 	#  BitKeeper file C:/servroot/home/jojodee/bk/stable/tests/import/xforum/import_xforum_posts.php tests/import/xforum/import_xforum_topics.php: 	#  BitKeeper file C:/servroot/home/jojodee/bk/stable/tests/import/xforum/import_xforum_topics.php tests/import/xforum/import_xforum_users.php: 	#  BitKeeper file C:/servroot/home/jojodee/bk/stable/tests/import/xforum/import_xforum_users.php

---
+++ tests/import/xforum/import_xforum_categories.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ /**
+ * File: $Id$
+ *
+ * Quick & dirty import of xForum data into Xaraya test sites
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @link http://www.xaraya.com
+ * 
+ * @subpackage import
+ * @original author mikespub <mikespub@xaraya.com>
+ * @author jojodee <jojodee@xaraya.com>
+*/
+
+/**
+ * Note : this file is part of import_xforum.php and cannot be run separately
+ */
+
+    echo "<strong>$step. Importing xforum categories into categories</strong><br/>\n";
+
+    $regid = xarModGetIDFromName('xarbb');
+    $categories = xarModAPIFunc('categories', 'admin', 'create',
+                                array('name' => 'xarBB Forum Index',
+                                      'description' => 'xarBB Forum Index',
+                                      'parent_id' => 0));
+// set this as base category for xarbb
+  //    $ptid = xarModGetVar('installer','ptid');
+ //     if (!empty($ptid)) {
+   //      $settings = unserialize(xarModGetVar('xarbb', 'settings.'.$ptid));
+   //   $settings['defaultview'] = 'c' . $categories;
+ //      xarModSetVar('xarbb', 'settings.'.$ptid, serialize($settings));
+    xarModSetVar('xarbb', 'number_of_categories.1', 1);
+        xarModSetVar('xarbb', 'mastercids', $categories);
+ //  }
+
+    $query = 'SELECT type , fid, name, displayorder,fup
+              FROM `'.$oldprefix.'_xforum_forums`
+              WHERE TYPE = \'group\' OR TYPE = \'forum\'';
+    $result =& $dbconn->Execute($query);
+    if (!$result) {
+        die("Oops, select categories failed : " . $dbconn->ErrorMsg());
+    }
+    $catid = array();
+    while (!$result->EOF) {
+        list($ctype, $id, $title, $order,$fup) = $result->fields;
+        if ($fup==0) {
+        $catid[$id] = xarModAPIFunc('categories', 'admin', 'create',
+                                    array('name' => $title,
+                                          'description' => $title,
+                                          'parent_id' => $categories));
+        echo "Creating category ($id) $title<br/>\n";
+        }else {
+        $catid[$id] = xarModAPIFunc('categories', 'admin', 'create',
+                                    array('name' => $title,
+                                          'description' => $title,
+                                          'parent_id' => $catid[$fup]));
+        }
+        $result->MoveNext();
+    }
+    $result->Close();
+    xarModSetVar('installer','categories',$categories);
+    xarModSetVar('installer','catid',serialize($catid));
+
+?>
---
+++ tests/import/xforum/import_xforum_forums.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * File: $Id$
+ *
+ * Quick & dirty import of xForum data into Xaraya test sites
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @link http://www.xaraya.com
+ *
+ * @subpackage import
+ * @ based on phpBB import by author mikespub <mikespub@xaraya.com>
+ * @ author jojodee <jojodee@xaraya.com>
+*/
+
+/**
+ * Note : this file is part of import_xForum.php and cannot be run separately
+ */
+ //
+    echo "<strong>$step. Creating xForum forums and allocating to categories</strong><br/>\n";
+
+   $forumusers= xarModGetVar('installer','userid');
+    if (!isset($forumusers)) {
+        $forumusers = array();
+    } else {
+        $forumusers = unserialize($forumusers);
+    }
+
+    $catids= unserialize(xarModGetVar('installer', 'catid'));
+    $query = 'SELECT fid, name, description, displayorder, posts, threads, fup
+              FROM `'.$oldprefix.'_xforum_forums`
+              WHERE type=\'forum\' OR type=\'sub\'
+              ORDER BY fup ASC, fid ASC';
+
+    $result =& $dbconn->Execute($query);
+    if (!$result) {
+        die("Oops, select forums failed : " . $dbconn->ErrorMsg());
+    }
+
+    $forumid = array();
+    $catinfo = array();
+    while (!$result->EOF) {
+        list($id, $name, $descr, $order, $fposts, $fthreads, $fup) = $result->fields;
+        if (!isset($catids[$fup])) {
+            echo "Oops - no category id for $id<br />\n";
+       }
+
+       if ((empty($fup)) || ($fup==0)){
+           $cids[$id] = xarModGetVar('xarbb', 'mastercids.1');
+       } else {
+           $cids[$id]=array($catids[$fup]);
+       }
+       // The API function is called
+       // Hmmm - can't directly create all new data in forum here with current xarBB function
+          $forumid[$id]=xarModAPIFunc('xarbb',
+                               'admin',
+                               'create',
+                               array('fname'    => $name,
+                                     'fdesc'    => $descr,
+                                     'cids'     => $cids[$id],
+                                     'fposter'  => 3)); //Set all forum creation to Admin
+
+
+        if ($forumid[$id]) {
+           echo "Creating forum ($fup) ->$forumid[$id] - $name - $descr<br/>\n";
+        }else {
+           echo "Problem with forum ". $forumid[$id];
+        }
+
+       $result->MoveNext();
+    }
+    $result->Close();
+    xarModSetVar('installer','forumid',serialize($forumid));
+    //Reset mastercid again - seems to be getting lost
+    $cats=xarModGetVar('installer','categories');
+    xarModSetVar('xarbb', 'mastercids', $cats);
+
+    echo '<a href="import_xforum.php">Return to start</a>&nbsp;&nbsp;&nbsp;
+          <a href="import_xforum.php?step=' . ($step+1) . '&module=articles">Go to step ' . ($step+1) . '</a><br/>';
+    $dbconn->Execute('OPTIMIZE TABLE ' . $tables['categories']);
+    if (!empty($docounter)) {
+        $dbconn->Execute('OPTIMIZE TABLE ' . $tables['hitcount']);
+    }
+
+?>
