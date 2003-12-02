fa89acbb3683e75a8b15b2d851e7f4fad9a01fe4
mikespub <mikespub@fully.qualified.hostname>
2003-12-02 20:03:12 +0000

    html/includes/properties/Dynamic_GroupList_Property.php: 	#  adapt grouplist property to work with groups

--- html/includes/properties/Dynamic_GroupList_Property.php
+++ html/includes/properties/Dynamic_GroupList_Property.php
@@ -10,7 +10,7 @@
  * Include the base class
  *
  */
-include_once "includes/properties/Dynamic_UserList_Property.php";
+include_once "includes/properties/Dynamic_Select_Property.php";
 
 /**
  * handle the grouplist property
@@ -18,13 +18,112 @@ include_once "includes/properties/Dynamic_UserList_Property.php";
  * @package dynamicdata
  *
  */
-class Dynamic_GroupList_Property extends Dynamic_UserList_Property
+class Dynamic_GroupList_Property extends Dynamic_Select_Property
 {
 
     function Dynamic_GroupList_Property($args)
     {
         $this->Dynamic_Select_Property($args);
-        $this->roles = xarModAPIFunc('roles', 'user', 'getallgroups');
+    }
+
+    function validateValue($value = null)
+    {
+        if (!isset($value)) {
+            $value = $this->value;
+        }
+        if (!empty($value)) {
+            // check if this is a valid group id
+            $group = xarModAPIFunc('roles','user','get',
+                                   array('uid' => $value,
+                                         'type' => 1)); // we're looking for a group here
+            if (!empty($group)) {
+                $this->value = $value;
+                return true;
+            }
+        } elseif (empty($value)) {
+            $this->value = $value;
+            return true;
+        }
+        $this->invalid = xarML('selection');
+        $this->value = null;
+        return false;
+    }
+
+//    function showInput($name = '', $value = null, $options = array(), $id = '', $tabindex = '')
+    function showInput($args = array())
+    {
+        extract($args);
+        if (!isset($value)) {
+            $value = $this->value;
+        }
+        if (!isset($options) || count($options) == 0) {
+            $options = $this->options;
+        }
+        if (count($options) == 0) {
+
+// TODO: handle large # of groups too (optional - less urgent than for users)
+
+            $groups = xarModAPIFunc('roles', 'user', 'getallgroups');
+            foreach ($groups as $group) {
+                $options[] = array('id' => $group['uid'], 'name' => $group['name']);
+            }
+        }
+        if (empty($name)) {
+            $name = 'dd_' . $this->id;
+        }
+        if (empty($id)) {
+            $id = $name;
+        }
+        $out = '<select' .
+               ' name="' . $name . '"' .
+               ' id="'. $id . '"' .
+               (!empty($tabindex) ? ' tabindex="'.$tabindex.'" ' : '') .
+               '>';
+        foreach ($options as $option) {
+            $out .= '<option';
+            if (empty($option['id']) || $option['id'] != $option['name']) {
+                $out .= ' value="'.$option['id'].'"';
+            }
+            if ($option['id'] == $value) {
+                $out .= ' selected="selected">'.$option['name'].'</option>';
+            } else {
+                $out .= '>'.$option['name'].'</option>';
+            }
+        }
+        $out .= '</select>' .
+               (!empty($this->invalid) ? ' <span class="xar-error">'.xarML('Invalid #(1)', $this->invalid) .'</span>' : '');
+        return $out;
+    }
+
+    function showOutput($args = array())
+    {
+        extract($args);
+        if (!isset($value)) {
+            $value = $this->value;
+        }
+        if (empty($value)) {
+            $groupname = '';
+        } else {
+            $group = xarModAPIFunc('roles','user','get',
+                                   array('uid' => $value,
+                                         'type' => 1)); // we're looking for a group here
+            if (empty($group) || empty($group['name'])) {
+                $groupname = '';
+            } else {
+                $groupname = $group['name'];
+            }
+        }
+        if ($value > 1) {
+
+// TODO: have some meaningful user GUI in roles to show group info ?
+//       + adapt the URL below to point there :-)
+
+            return '<a href="'.xarModURL('roles','user','display',
+                                         array('uid' => $value))
+                    . '">'.xarVarPrepForDisplay($groupname).'</a>';
+        } else {
+            return xarVarPrepForDisplay($groupname);
+        }
     }
 
 }
