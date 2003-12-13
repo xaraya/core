ae29e632b2dd4953af7e5490f0b6086e5eb99acd
marcel <marcel@hsdev.com>
2003-12-13 20:03:12 +0000

    html/modules/modules/xaradmin/activate.php: 	#  2002->2003 html/modules/modules/xaradmin/deactivate.php: 	#  2002->2003 html/modules/modules/xaradmin/hooks.php: 	#  2002->2003 html/modules/modules/xaradmin/install.php: 	#  2002->2003 html/modules/modules/xaradmin/installall.php: 	#  2002->2003 html/modules/modules/xaradmin/list.php: 	#  2002->2003 html/modules/modules/xaradmin/main.php: 	#  2002->2003 html/modules/modules/xaradmin/modify.php: 	#  2002->2003 html/modules/modules/xaradmin/modinfo.php: 	#  2002->2003 html/modules/modules/xaradmin/modinfonew.php: 	#  2002->2003 html/modules/modules/xaradmin/prefs.php: 	#  2002->2003 html/modules/modules/xaradmin/regenerate.php: 	#  2002->2003 html/modules/modules/xaradmin/release.php: 	#  2002->2003 html/modules/modules/xaradmin/remove.php: 	#  2002->2003 html/modules/modules/xaradmin/settings.php: 	#  2002->2003 html/modules/modules/xaradmin/tools.php: 	#  2002->2003 html/modules/modules/xaradmin/update.php: 	#  2002->2003 html/modules/modules/xaradmin/updatehooks.php: 	#  2002->2003 html/modules/modules/xaradmin/upgrade.php: 	#  2002->2003 html/modules/modules/xaradminapi/activate.php: 	#  2002->2003 html/modules/modules/xaradminapi/add_module_alias.php: 	#  2002->2003 html/modules/modules/xaradminapi/checkmissing.php: 	#  2002->2003 html/modules/modules/xaradminapi/checkversion.php: 	#  2002->2003 html/modules/modules/xaradminapi/deactivate.php: 	#  2002->2003 html/modules/modules/xaradminapi/deactivatewithdependents.php: 	#  2002->2003 html/modules/modules/xaradminapi/delete_module_alias.php: 	#  2002->2003 html/modules/modules/xaradminapi/disablehooks.php: 	#  2002->2003 html/modules/modules/xaradminapi/enablehooks.php: 	#  2002->2003 html/modules/modules/xaradminapi/executeinitfunction.php: 	#  2002->2003 html/modules/modules/xaradminapi/getalldependencies.php: 	#  2002->2003 html/modules/modules/xaradminapi/getalldependents.php: 	#  2002->2003 html/modules/modules/xaradminapi/getdbmodules.php: 	#  2002->2003 html/modules/modules/xaradminapi/getfilemodules.php: 	#  2002->2003 html/modules/modules/xaradminapi/gethookedmodules.php: 	#  2002->2003 html/modules/modules/xaradminapi/gethooklist.php: 	#  2002->2003 html/modules/modules/xaradminapi/getlist.php: 	#  2002->2003 html/modules/modules/xaradminapi/getmenulinks.php: 	#  2002->2003 html/modules/modules/xaradminapi/initialise.php: 	#  2002->2003 html/modules/modules/xaradminapi/installwithdependencies.php: 	#  2002->2003 html/modules/modules/xaradminapi/list.php: 	#  2002->2003 html/modules/modules/xaradminapi/regenerate.php: 	#  2002->2003 html/modules/modules/xaradminapi/remove.php: 	#  2002->2003 html/modules/modules/xaradminapi/removewithdependents.php: 	#  2002->2003 html/modules/modules/xaradminapi/set_pref_defaults.php: 	#  2002->2003 html/modules/modules/xaradminapi/setstate.php: 	#  2002->2003 html/modules/modules/xaradminapi/update.php: 	#  2002->2003 html/modules/modules/xaradminapi/updatehooks.php: 	#  2002->2003 html/modules/modules/xaradminapi/upgrade.php: 	#  2002->2003 html/modules/modules/xaradminapi/verifydependency.php: 	#  2002->2003 html/modules/modules/xarinit.php: 	#  2002->2003 	#  keyword expansion html/modules/modules/xarversion.php: 	#  2002->2003 	#  keyword expansion

--- html/modules/modules/xaradmin/settings.php
+++ html/modules/modules/xaradmin/settings.php
@@ -5,7 +5,7 @@
  * List modules and current settings
  *
  * @package Xaraya eXtensible Management System
- * @copyright (C) 2002 by the Xaraya Development Team.
+ * @copyright (C) 2003 by the Xaraya Development Team.
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  * @subpackage modules module
--- html/modules/modules/xaradminapi/getlist.php
+++ html/modules/modules/xaradminapi/getlist.php
@@ -5,7 +5,7 @@
  * Get a list of modules that matches required criteria
  *
  * @package Xaraya eXtensible Management System
- * @copyright (C) 2002 by the Xaraya Development Team.
+ * @copyright (C) 2003 by the Xaraya Development Team.
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  * @subpackage modules module
