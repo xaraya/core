56d86b34bb0c019345a2d2a83d3048e9efe67334
jojodee <jojodee@hbwdev.net>
2003-12-13 20:03:12 +0000

    html/modules/mail/xarinit.php: 	#  Update standard file header with date and GPL licence html/modules/mail/xarschedulerapi/sendmail.php: 	#  Update with standard file header html/modules/mail/xartemplates/admin-compose.xd: 	#  Add standard template header with GPL license html/modules/mail/xartemplates/admin-main.xd: 	#  Add standard template header with GPL license html/modules/mail/xartemplates/admin-modifyconfig.xd: 	#  Add standard template header with GPL license 	#  Correct typo in form searchstring and replacestring labels html/modules/mail/xarversion.php: 	#  Update standard file header with date and GPL licence html/modules/mail/class/index.html: 	#  BitKeeper file C:/servroot/home/jojodee/bk/stable/html/modules/mail/class/index.html html/modules/mail/class/language/index.html: 	#  BitKeeper file C:/servroot/home/jojodee/bk/stable/html/modules/mail/class/language/index.html html/modules/mail/xardocs/index.html: 	#  BitKeeper file C:/servroot/home/jojodee/bk/stable/html/modules/mail/xardocs/index.html html/modules/mail/xartemplates/index.html: 	#  BitKeeper file C:/servroot/home/jojodee/bk/stable/html/modules/mail/xartemplates/index.html

--- html/modules/mail/xarschedulerapi/sendmail.php
+++ html/modules/mail/xarschedulerapi/sendmail.php
@@ -1,5 +1,17 @@
 <?php
-
+/**
+ * File: $Id
+ * 
+ * Send queued/scheduled mails
+ * 
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ * @subpackage mail module
+ * @author John Cox <admin@dinerminor.com> 
+ * @author mikespub 
+ */
 /**
  * send queued/scheduled mails (executed by the scheduler module)
  * 
