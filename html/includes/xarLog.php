717e4516796c2e735b79a80d610c49317a93fa5b
marcel <marcel@hsdev.com>
2003-12-14 20:03:12 +0000

    html/includes/xarLog.php: 	#  There was a mod api function in xarLog, that's no good when the module subsystem isn't running yet, 	#  copied the dumpvariable function from the base module in here.

--- html/includes/xarLog.php
+++ html/includes/xarLog.php
@@ -84,6 +84,111 @@ function xarLogMessage($message, $level = XARLOG_LEVEL_DEBUG) {
 function xarLogVariable($name, $var, $level = XARLOG_LEVEL_DEBUG)
 {
     $args = array('name'=>$name, 'var'=>$var, 'format'=>'text');
-    xarLogMessage(xarModAPIFunc('base','log','dumpvariable', $args), $level);
+    // no go for the xarModAPIFunc, that subsystem is not there yet
+    
+    xarLogMessage(xarLog__dumpVariable($args,$level));
 }
+
+
+/**
+ *  Helper function for variable logging
+ *
+ * @todo This function came from logger api in base module, which is
+ *       a no go in this subsystem, as the module subsystem may not exist
+ *       I copied the literal function in here.
+ * FIXME: this needs to be done by the logger classes in the loggers subdirectory
+ *
+ */
+function xarLog__dumpVariable ($array)
+{
+
+    static $depth = 0;
+
+    // $var, $name, $classname and $format
+    extract($array);
+
+    if ($depth > 32) {
+        return 'Recursive Depth Exceeded';
+    }
+    
+    if ($depth == 0) {
+        $blank = '';
+    } else {
+        $blank = str_repeat(' ', $depth);
+    }
+    $depth += 1;
+    
+    $TYPE_COLOR = "#FF0000";
+    $NAME_COLOR = "#0000FF";
+    $VALUE_COLOR = "#999900";
+    
+    $str = '';
+    
+    if (isset($name)) {
+        if ($format == 'html') {
+            $str = "<span style=\"color: $NAME_COLOR;\">".$blank.'Variable name: <b>'.
+                htmlspecialchars($name).'</b></span><br/>';
+        } else {
+            $str = $blank."Variable name: $name\n";
+        }
+    }
+    
+    $type = gettype($var);
+    if (is_object($var)) {
+        $args = array('name'=>$name, 'var'=>get_object_vars($var), 'classname'=>get_class($var), 'format'=>$format);
+        // RECURSIVE CALL
+        $str = xarLog__dumpVariable($args);
+    } elseif (is_array($var)) {
+        
+        if (isset($classname)) {
+            $type = 'class';
+        } else {
+            $type = 'array';
+        }
+        
+        if ($format == 'html') {
+            $str .= "<span style=\"color: $TYPE_COLOR;\">".$blank."Variable type: $type</span><br/>";
+        } else {
+            $str .= $blank."Variable type: $type\n";
+        }
+        
+        if ($format == 'html') {
+            $str .= '{<br/><ul>';
+        } else {
+            $str .= $blank."{\n";
+        }
+        
+        foreach($var as $key => $val) {
+            $args = array('name'=>$key, 'var'=>$val, 'format'=>$format);
+            // RECURSIVE CALL
+            $str .= xarLog__dumpVariable($args);
+        }
+        
+        if ($format == 'html') {
+            $str .= '</ul>}<br/><br/>';
+        } else {
+            $str .= $blank."}\n\n";
+        }
+    } else {
+        if ($var === NULL) {
+            $var = 'NULL';
+        } else if ($var === false) {
+            $var = 'false';
+        } else if ($var === true) {
+            $var = 'true';
+        }
+        if ($format == 'html') {
+            $str .= "<span style=\"color: $TYPE_COLOR;\">".$blank."Variable type: $type</span><br/>";
+            $str .= "<span style=\"color: $VALUE_COLOR;\">".$blank.'Variable value: "'.
+                htmlspecialchars($var).'"</span><br/><br/>';
+        } else {
+            $str .= $blank."Variable type: $type\n";
+            $str .= $blank."Variable value: \"$var\"\n\n";
+        }
+    }
+    
+    $depth -= 1;
+    return $str;
+}
+
 ?>
