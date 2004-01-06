1b31bda427ff028d2a2692395e98a7ccfeb84848
marc <marc@marclaptop.>
2004-01-06 20:04:01 +0000

    html/modules/roles/xartemplates/admin-main.xd: 	#  cosmetics html/modules/privileges/xartemplates/admin-main.xd: 	#  fix bug 1879 html/modules/privileges/xarversion.php: 	#  fix bug 1878

--- html/modules/privileges/xarversion.php
+++ html/modules/privileges/xarversion.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * $Id: s.xarversion.php 1.8 02/11/28 18:19:02-05:00 John.Cox@mcnabb. $
- * 
+ *
  * Privileges Adminstration
- * 
+ *
  * @package Xaraya eXtensible Management System
  * @copyright (C) 2003 by the Xaraya Development Team.
- * @license GPL {@link http://www.gnu.org/licenses/gpl.html} 
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
- * 
+ *
  * @subpackage Privileges Module
  * @author Marc Lutolf <marcinmilan@xaraya.com>
  */
@@ -17,10 +17,6 @@ $modversion['name'] = 'Privileges Adminstration';
 $modversion['id'] = '1098';
 $modversion['version'] = '0.1.0';
 $modversion['description'] = 'Modify privileges security';
-$modversion['credits'] = 'xardocs/credits.txt';
-$modversion['help'] = 'xardocs/help.txt';
-$modversion['changelog'] = 'xardocs/changelog.txt';
-$modversion['license'] = 'xardocs/license.txt';
 $modversion['official'] = 1;
 $modversion['author'] = 'Marc Lutolf';
 $modversion['contact'] = 'http://www.xaraya.com/';
