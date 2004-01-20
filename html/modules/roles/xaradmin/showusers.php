d0e892817d2c008d38b96497aed3c56fe664e9ec
kams <kams@webudev.(none)>
2004-01-20 20:04:01 +0000

    html/modules/installer/xaradmin.php: 	#  Auto merged html/modules/installer/xaradmin.php: 	#  Exclude html/modules/installer/xaradmin.php: 	#  fix bug 1935 html/includes/datastores/Dynamic_VariableTable_DataStore.php: 	#  assign db tables by reference 	#  bug #1116 html/modules/installer/xaradmin.php: 	#  Auto merged html/modules/installer/xaradmin.php: 	#  Auto merged html/modules/installer/xaradmin.php: 	#  making core modules pass QA test 2.18 and 2.22 html/modules/installer/xaradmin.php: 	#  potential fix for bug 1935 html/modules/roles/xaradmin/showusers.php: 	#  Small fix Roles UI (Assertion error with session)

--- html/modules/roles/xaradmin/showusers.php
+++ html/modules/roles/xaradmin/showusers.php
@@ -28,22 +28,17 @@ function roles_admin_showusers()
                                                        'type'   => 1));
     }
     xarVarSetCached('roles', 'defaultgroupuid', $defaultgroupuid);
+    $defaultstyle = xarSessionGetVar('rolesdisplay');
+    if (isset($defaultstyle)) $defaultstyle = 0;
 
     if (!xarVarFetch('uid', 'int:0:', $uid, $defaultgroupuid['uid'], XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('startnum', 'int:1:', $startnum, 1, XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('state', 'int:0:', $data['state'], 0, XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('selstyle', 'isset', $data['selstyle'], xarSessionGetVar('rolesdisplay'), XARVAR_DONT_SET)) return;
+    if (!xarVarFetch('selstyle', 'isset', $data['selstyle'], $defaultstyle, XARVAR_DONT_SET)) return;
     if (!xarVarFetch('invalid', 'str:0:', $data['invalid'], NULL, XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('order', 'str:0:', $data['order'], 'name', XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('search', 'str:0:', $data['search'], NULL, XARVAR_NOT_REQUIRED)) return;
     
-    /*if (!isset($data['selstyle'])) {
-        if (isset($userdisplay)) {
-            $data['selstyle'] = $userdisplay;
-        } else {
-            $data['selstyle'] ="tabbed";
-        }
-    }*/
     xarSessionSetVar('rolesdisplay', $data['selstyle']);
 
     //Create the role tree
