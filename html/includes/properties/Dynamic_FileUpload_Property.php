b491ce60d844726eed81f475b32eafc227848cd9
mikespub <mikespub@fully.qualified.hostname>
2004-02-22 20:04:02 +0000

    html/modules/dynamicdata/xartemplates/user-showoutput-urlicon.xd: 	#  Fix for bug #2258. Change $value to $link. 	#  Remove redundant validation checking. html/modules/blocks/xaradminapi/create_instance.php: 	#  Bug 2311: allow block content to be supplied on creating a bock through the API. 	#  Allows content to be supplied as a string or an array. 	#  Also: validate and pre-process the block 'name' to ensure it uses the same 	#  rules that are enforced through the GUI. We probably need more of this type 	#  of thing. html/modules/blocks/xaradminapi/update_instance.php: 	#  Bug 2311: allow block content to be supplied on creating a bock through the API. 	#  Allows content to be supplied as a string or an array. 	#  Also: validate and pre-process the block 'name' to ensure it uses the same 	#  rules that are enforced through the GUI. We probably need more of this type 	#  of thing. html/modules/base/xarjavascriptapi/geteventattributes.php: 	#  Exclude html/modules/base/xarjavascriptapi/geteventattributes.php: 	#  BitKeeper file F:/bk/core/stable/html/modules/base/xarjavascriptapi/geteventattributes.php html/modules/base/xarjavascriptapi/geteventattributes.php: 	#  BitKeeper file F:/bk/core/stable/html/modules/base/xarjavascriptapi/geteventattributes.php html/modules/base/xarjavascriptapi/geteventjs.php: 	#  Exclude html/modules/base/xarjavascriptapi/geteventjs.php: 	#  BitKeeper file F:/bk/core/stable/html/modules/base/xarjavascriptapi/geteventjs.php html/modules/base/xarjavascriptapi/geteventjs.php: 	#  BitKeeper file F:/bk/core/stable/html/modules/base/xarjavascriptapi/geteventjs.php html/modules/base/xarjavascriptapi/handleeventjavascript.php: 	#  Exclude html/modules/base/xarjavascriptapi/handleeventjavascript.php: 	#  BitKeeper file F:/bk/core/stable/html/modules/base/xarjavascriptapi/handleeventjavascript.php html/modules/base/xarjavascriptapi/handleeventjavascript.php: 	#  BitKeeper file F:/bk/core/stable/html/modules/base/xarjavascriptapi/handleeventjavascript.php html/modules/base/xarjavascriptapi/handlemodulejavascript.php: 	#  Exclude html/modules/base/xarjavascriptapi/handlemodulejavascript.php: 	#  Bug 2309: support 'code' attribute so code fragments can be passed in. html/includes/properties/Dynamic_FileUpload_Property.php: 	#  bugs 2239 2162 etc. - fix file upload with uploads module

--- html/includes/properties/Dynamic_FileUpload_Property.php
+++ html/includes/properties/Dynamic_FileUpload_Property.php
@@ -38,11 +38,16 @@ class Dynamic_FileUpload_Property extends Dynamic_Property
         }     
         // Determine if the uploads module is hooked to the calling module
         // if so, we will use the uploads modules functionality 
-        $list = xarModGetHookList(xarModGetName(), 'item', 'transform');
-        foreach ($list as $hook) {
-            if ($hook['module'] == 'uploads') {
-                $this->UploadsModule_isHooked = TRUE;
-                break;
+        if (xarVarGetCached('Hooks.uploads','ishooked')) {
+            $this->UploadsModule_isHooked = TRUE;
+        } else {
+        // FIXME: this doesn't take into account the itemtype or non-main module objects
+            $list = xarModGetHookList(xarModGetName(), 'item', 'transform');
+            foreach ($list as $hook) {
+                if ($hook['module'] == 'uploads') {
+                    $this->UploadsModule_isHooked = TRUE;
+                    break;
+                }
             }
         }
         
@@ -85,12 +90,6 @@ class Dynamic_FileUpload_Property extends Dynamic_Property
 
     function validateValue($value = null)
     {
-        
-        // if the uploads module is hooked in, use it's functionality instead
-        if ($this->UploadsModule_isHooked == TRUE) {
-            return $this->_UploadModule_validateValue($value);
-        }
-        
         // the variable corresponding to the file upload field is no longer set in PHP 4.2.1+
         // but we're using a hidden field to keep track of any previously uploaded file here
         if (!isset($value)) {
@@ -101,6 +100,43 @@ class Dynamic_FileUpload_Property extends Dynamic_Property
         } else {
             $name = 'dd_'.$this->id;
         }
+
+        // retrieve new value for preview + new/modify combinations
+        if (xarVarIsCached('DynamicData.FileUpload',$name)) {
+            $this->value = xarVarGetCached('DynamicData.FileUpload',$name);
+            return true;
+        }
+
+        // if the uploads module is hooked in, use it's functionality instead
+        if ($this->UploadsModule_isHooked == TRUE) {
+            $return = xarModAPIFunc('uploads','admin','validatevalue',
+                                    array('id' => $name, // not $this->id
+                                          'value' => $value,
+                                          'multiple' => $this->multiple,
+                                          'format' => 'fileupload',
+                                          'maxsize' => $this->maxSize));
+            if (!isset($return) || !is_array($return) || count($return) < 2) {
+                $this->value = null;
+            // CHECKME: copied from autolinks :)
+                // 'text' rendering will return an array
+                $errorstack = xarErrorGet();
+                $errorstack = array_shift($errorstack);
+                $this->invalid = $errorstack['short'];
+                xarErrorHandled();
+                return false;
+            }
+            if (empty($return[0])) {
+                $this->value = null;
+                $this->invalid = xarML('value');
+                return false;
+            } else {
+                $this->value = $return[1];
+                // save new value for preview + new/modify combinations
+                xarVarSetCached('DynamicData.FileUpload',$name,$this->value);
+                return true;
+            }
+        }
+
         $upname = $name .'_upload';
         $filetype = $this->filetype;
         
@@ -125,7 +161,7 @@ class Dynamic_FileUpload_Property extends Dynamic_Property
                 }
                 $this->value = $fileName;
                 // save new value for preview + new/modify combinations
-                xarVarSetCached('DynamicData.FileUpload',$name,$file);
+                xarVarSetCached('DynamicData.FileUpload',$name,$fileName);
             } else {
             // TODO: assign random name + figure out mime type to add the right extension ?
                 $this->invalid = xarML('file name for upload');
@@ -172,28 +208,37 @@ class Dynamic_FileUpload_Property extends Dynamic_Property
         xarVarSetCached('Hooks.dynamicdata','withupload',1);
 
         if ($this->UploadsModule_isHooked == TRUE) {
-            return $this->_UploadModule_showInput($value);
-        } else {
-            // user must have unhooked the uploads module
-            // remove any left over values
-            if (!empty($value) && (is_numeric($value) || stristr($value, ';'))) {
+            // user must have hooked the uploads module after uploading files directly
+            // CHECKME: remove any left over values - or migrate entries to uploads table ?
+            if (!empty($value) && !is_numeric($value) && !stristr($value, ';')) {
                 $value = '';
             }
-            
-            if (!empty($this->filetype)) {
-                $extensions = $this->filetype;
-                $allowed = '<br />' . xarML('Allowed file types : #(1)',$extensions);
-            } else {
-                $allowed = '';
-            }
+            return xarModAPIFunc('uploads','admin','showinput',
+                                 array('id' => $name, // not $this->id
+                                       'value' => $value,
+                                       'multiple' => $this->multiple,
+                                       'format' => 'fileupload'));
         }
-        
+
+        // user must have unhooked the uploads module
+        // remove any left over values
+        if (!empty($value) && (is_numeric($value) || stristr($value, ';'))) {
+            $value = '';
+        }
+
+        if (!empty($this->filetype)) {
+            $extensions = $this->filetype;
+            $allowed = '<br />' . xarML('Allowed file types : #(1)',$extensions);
+        } else {
+            $allowed = '';
+        }
+
         $size       = !empty($size) ? $size : $this->size;
         $maxSize    = !empty($maxSize) ? $maxSize : $this->maxSize;
         $tabindex   = !empty($tabindex) ? ' tabindex="' . $tabindex . '" ' : '';
         $value      = !empty($value) ? xarML('Uploaded file: #(1)',$value) . '<br /> <input type="hidden" name="' . $name . '" value="' . $value .'" />' : '';
         $invalid    = !empty($this->invalid) ? '<span class="xar-error">' . xarML('Invalid #(1)',  $this->invalid) . '</span>' : '';
-        
+
         // we're using a hidden field to keep track of any previously uploaded file here
         return ($value .
                '<input type="hidden" name="MAX_FILE_SIZE" value="'. $maxSize  .'" />' .
@@ -210,7 +255,9 @@ class Dynamic_FileUpload_Property extends Dynamic_Property
         }
         
         if ($this->UploadsModule_isHooked) {
-            return $this->_UploadModule_showOutput($value);
+            return xarModAPIFunc('uploads','user','showoutput',
+                                 array('value' => $value,
+                                       'format' => 'fileupload'));
         } 
         
         // Note: you can't access files directly in the document root here
@@ -220,7 +267,6 @@ class Dynamic_FileUpload_Property extends Dynamic_Property
                 // remove any left over values
                 return '';
             } 
-            
             // if the uploads module is hooked (to be verified and set by the calling module)
             if (!empty($this->basedir) && file_exists($this->basedir . '/'. $value) && is_file($this->basedir . '/'. $value)) {
                 $value = xarVarPrepForDisplay($value);
@@ -232,199 +278,6 @@ class Dynamic_FileUpload_Property extends Dynamic_Property
         } else {
             return '';
         }
-    }
-        
-    function _UploadModule_validateValue($value = NULL) 
-    {
-        
-        // If we've just been previewed, then use the value that was passed in :)
-        if (!empty($value)) {
-            if (strlen($value) && stristr($value, ';') && ';' == $value{0}) {
-                $this->value = $value;
-                return true;
-            }                 
-        } elseif (!isset($value) || empty($value)) {
-            xarModAPILoad('uploads', 'user');
-
-            $typeCheck = 'enum:0:' . _UPLOADS_GET_STORED;
-            $typeCheck .= (xarModGetVar('uploads', 'dd.fileupload.external') == TRUE) ? ':' . _UPLOADS_GET_EXTERNAL : '';
-            $typeCheck .= (xarModGetVar('uploads', 'dd.fileupload.trusted') == TRUE) ? ':' . _UPLOADS_GET_LOCAL : '';
-            $typeCheck .= (xarModGetVar('uploads', 'dd.fileupload.upload') == TRUE) ? ':' . _UPLOADS_GET_UPLOAD : '';
-            $typeCheck .= ':';
-
-
-            if (!xarVarFetch($this->id . '_attach_type', $typeCheck, $action, NULL, XARVAR_NOT_REQUIRED)) return;
-            
-            if (!isset($action)) {
-                $value = $this->value;
-                $action = -2;
-            }
-        }  
-
-        $args['action']    = $action;
-
-        switch ($action) {
-            case _UPLOADS_GET_UPLOAD:
-                
-                $file_maxsize = xarModGetVar('uploads', 'file.maxsize');
-                $file_maxsize = $file_maxsize > 0 ? $file_maxsize : $this->maxSize;
-                
-
-                if (!xarVarFetch('MAX_FILE_SIZE', "int::$file_maxsize", $maxSize)) return;
-
-                if (!xarVarFetch('', 'array:1:', $_FILES[$this->id . '_attach_upload'])) return;
-
-                $upload         =& $_FILES[$this->id . '_attach_upload'];
-                $args['upload'] =& $_FILES[$this->id . '_attach_upload'];
-                break;
-            case _UPLOADS_GET_EXTERNAL:
-                // minimum external import link must be: ftp://a.ws  <-- 10 characters total
-
-                if (!xarVarFetch($this->id . '_attach_external', 'regexp:/^([a-z]*).\/\/(.{7,})/', $import, '', XARVAR_NOT_REQUIRED)) return;
-
-                $args['import'] = $import;
-                break;
-            case _UPLOADS_GET_LOCAL:
-
-                if (!xarVarFetch($this->id . '_attach_trusted', 'list:regexp:/(?<!\.{2,2}\/)[\w\d]*/', $fileList)) return;
-
-                $importDir = xarmodGetVar('uploads', 'path.imports-directory');
-                foreach ($fileList as $file) {
-                    $file = str_replace('/trusted', $importDir, $file);
-                    $args['fileList']["$file"] = xarModAPIFunc('uploads', 'user', 'file_get_metadata',
-                                                                array('fileLocation' => "$file"));
-                    $args['fileList']["$file"]['fileSize'] = $args['fileList']["$file"]['fileSize']['long'];
-                }
-                break;
-            case _UPLOADS_GET_STORED:
-
-                if (!xarVarFetch($this->id . '_attach_stored', 'list:str:1:', $fileList, XARVAR_NOT_REQUIRED)) return;
-                
-                // If we've made it this far, then fileList was empty to start, 
-                // so don't complain about it being empty now
-                if (empty($fileList) || !is_array($fileList)) {
-                    $this->value = NULL;
-                    return true;
-                }
-                // We prepend a semicolon onto the list of fileId's so that
-                // we can tell, in the future, that this is a list of fileIds 
-                // and not just a filename
-                $this->value = ';' . implode(';', $fileList);
-
-                return true;
-                break;
-            case '-1':
-                $this->value = NULL;
-                return true;
-            default: 
-                if (isset($this->value)) {
-                    if (strlen($this->value) && $this->value{0} == ';') {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                } else {
-                    // If we have managed to get here then we have a NULL value
-                    // and $action was most likely either null or something unexpected
-                    // So let's keep things that way :-)
-                    return true;
-                }
-                break;
-        }
-
-        if (!empty($action)) { 
-            
-            if (isset($storeType)) {
-                $args['storeType'] = $storeType;
-            }
-
-            $list = xarModAPIFunc('uploads','user','process_files', $args);
-            $storeList = array();
-            foreach ($list as $file => $fileInfo) {
-                if (!isset($fileInfo['errors'])) {
-                    $storeList[] = $fileInfo['fileId'];
-                } else {
-                    $msg = xarML('Error Found: #(1)', $fileInfo['errors'][0]['errorMesg']);
-                    xarExceptionSet(XAR_SYSTEM_EXCEPTION, 'UNKNOWN_ERROR', new SystemException($msg));
-
-                    return;
-                }
-            } 
-            if (is_array($storeList) && count($storeList)) {
-                // We prepend a semicolon onto the list of fileId's so that
-                // we can tell, in the future, that this is a list of fileIds 
-                // and not just a filename
-                $this->value = ';' . implode(';', $storeList);
-            } else {
-                $this->value = NULL;
-
-                return false;
-            }
-        } else {
-            return false;
-        }
-
-
-        return true;
-    }
-
-    function _UploadModule_showInput($value = NULL) 
-    {
-            
-        $trusted_dir = xarModGetVar('uploads', 'path.imports-directory');
-        $descend = TRUE;
-
-        xarModAPILoad('uploads', 'user');        
-        
-        $data['getAction']['LOCAL']       = _UPLOADS_GET_LOCAL;
-        $data['getAction']['EXTERNAL']    = _UPLOADS_GET_EXTERNAL;
-        $data['getAction']['UPLOAD']      = _UPLOADS_GET_UPLOAD;
-        $data['getAction']['STORED']      = _UPLOADS_GET_STORED;
-        $data['getAction']['REFRESH']     = _UPLOADS_GET_REFRESH_LOCAL;
-        $data['id']                       = $this->id;
-        $data['file_maxsize'] = xarModGetVar('uploads', 'file.maxsize');;
-        $data['fileList']     = xarModAPIFunc('uploads', 'user', 'import_get_filelist', 
-                                               array('descend' => $descend, 'fileLocation' => $trusted_dir));
-        $data['storedList']   = xarModAPIFunc('uploads', 'user', 'db_getall_files');
-
-        // used to allow selection of multiple files
-        $data['multiple_' . $this->id] = $this->multiple;
-
-        if (!empty($value)) {
-            // We use array_filter to remove any values from 
-            // the array that are empty, null, or false
-            $aList = array_filter(explode(';', $value));
-            
-            if (is_array($aList) && count($aList)) {
-                $data['inodeType']['DIRECTORY']   = _INODE_TYPE_DIRECTORY;
-                $data['inodeType']['FILE']        = _INODE_TYPE_FILE;
-                $data['Attachments'] = xarModAPIFunc('uploads', 'user', 'db_get_file', 
-                                                      array('fileId' => $aList));
-                $list = $this->showOutput(array('value' => $value));
-                
-                foreach ($aList as $fileId) {
-                    if (isset($data['storedList'][$fileId])) {
-                        $data['storedList'][$fileId]['selected'] = TRUE;
-                    }
-                }
-            }
-        }    
-        
-        return (isset($list) ? $list : '') . xarTplModule('uploads', 'user', 'attach_files', $data, NULL);
-        
-    }
-    
-    function _UploadModule_showOutput($value) 
-    {
-        $value = array_filter(explode(';', $value));
-        
-        if (is_array($value) && count($value)) {
-            $data['Attachments'] = xarModAPIFunc('uploads', 'user', 'db_get_file', array('fileId' => $value));    
-        } else {
-            $data['Attachments'] = '';
-        } 
-                
-        return xarTplModule('uploads', 'user', 'attachment-list', $data, NULL);        
     }
 
 }
