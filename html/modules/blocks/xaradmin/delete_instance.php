31e42f44d7904d0385fe05aac533a502fd54e3e6
judgej <judgej@acad-ajp.>
2004-03-03 20:04:03 +0000

    html/modules/blocks/xaradmin/delete_instance.php: 	#  Bug 2453: allow delete of a block instance when JavaScript is not available. html/modules/blocks/xartemplates/admin-delete_instance.xd: 	#  Bug 2453: allow delete of a block instance when JavaScript is not available.

--- html/modules/blocks/xaradmin/delete_instance.php
+++ html/modules/blocks/xaradmin/delete_instance.php
@@ -18,33 +18,36 @@
 function blocks_admin_delete_instance()
 {
     // Get parameters
-    if (!xarVarFetch('bid','int:1:',$bid)) return;
-    if (!xarVarFetch('confirm','str:1:',$confirm,'',XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('bid', 'id', $bid)) return;
+    if (!xarVarFetch('confirm', 'str:1:', $confirm, '', XARVAR_NOT_REQUIRED)) {return;}
 
     // Security Check
-	if(!xarSecurityCheck('DeleteBlock',0,'Instance')) return;
+	if (!xarSecurityCheck('DeleteBlock', 0, 'Instance')) {return;}
 
     // Check for confirmation
     if (empty($confirm)) {
         // No confirmation yet - get one
 
         // Get details on current block
-        $blockinfo = xarModAPIFunc('blocks', 
-                                   'admin', 
-                                   'getinfo', array('blockId' => $bid));
+        $blockinfo = xarModAPIFunc(
+            'blocks', 'user', 'get', array('bid' => $bid)
+        );
 
-        return array('instance' => $blockinfo,
-                     'authid' => xarSecGenAuthKey(),
-                     'deletelabel' => xarML('Delete'));
+        return array(
+            'instance' => $blockinfo,
+            'authid' => xarSecGenAuthKey(),
+            'deletelabel' => xarML('Delete')
+        );
     }
 
     // Confirm Auth Key
-    if (!xarSecConfirmAuthKey()) return;
+    if (!xarSecConfirmAuthKey()) {return;}
 
     // Pass to API
-    xarModAPIFunc('blocks',
-                  'admin',
-                  'delete_instance', array('bid' => $bid));
+    xarModAPIFunc(
+        'blocks', 'admin', 'delete_instance',
+        array('bid' => $bid)
+    );
 
     xarResponseRedirect(xarModURL('blocks', 'admin', 'view_instances'));
 
