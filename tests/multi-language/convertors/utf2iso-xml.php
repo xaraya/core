62951944a10d9432c9b8b5a7aba38c8626972e2e
voll <voll@yahoo.com>
2004-03-09 20:04:03 +0000

    tests/multi-language/convertors/utf2iso-php.php: 	#  BitKeeper file C:/bk/xaraya/core/stable/tests/multi-language/convertors/utf2iso-php.php tests/multi-language/convertors/utf2iso-xml.php: 	#  BitKeeper file C:/bk/xaraya/core/stable/tests/multi-language/convertors/utf2iso-xml.php tests/multi-language/convertors/xml2php-utf.php: 	#  BitKeeper file C:/bk/xaraya/core/stable/tests/multi-language/convertors/xml2php-utf.php tests/multi-language/convertors/utf2iso-php.php: 	#  Convertors for locales files tests/multi-language/convertors/utf2iso-xml.php: 	#  Convertors for locales files tests/multi-language/convertors/xml2php-utf.php: 	#  Convertors for locales files

--- tests/multi-language/convertors/utf2iso-xml.php
+++ tests/multi-language/convertors/utf2iso-xml.php
@@ -8,42 +8,43 @@ $inputEncoding = "utf-8";
 $outputEncoding = "windows-1251";
 
 set_time_limit(360);
-function searchDir($path) {
-  $fileModules = array();
-  $dh = opendir($path);
-  while ($entry = readdir($dh)) {
-    if (is_dir("$path/$entry")) {
-      if (($entry != '.') &&
-        ($entry != '..')) {
-        //Recurse
-        $outpath = str_replace($inputLocale,  $outputLocale,  "$path/$entry" );
-        if (!file_exists($outpath)) {
-            mkdir($outpath);
-        }
-        searchDir("$path/$entry");
-      }
-    } else { // -> is file
-      if (substr($entry, strlen($entry)-4) == '.xml') {
-        echo "<br>Reading file $path/$entry\r\n";
-        $file = file ("$path/$entry");
-        $file_contents = implode ('', $file);
-        $new_file_contents1 = str_replace(
-           "locale=\"$inputLocale\"", 
-           "locale=\"$outputLocale\"", 
-           $file_contents );
-        $new_file_contents2 = iconv($inputEncoding, $outputEncoding, $new_file_contents1 );
-        //$new_file_contents2 = mb_convert_encoding($file_contents, $outputEncoding, $inputEncoding);
-        $outpath = str_replace($inputLocale, $outputLocale,  $path );
-        if (!file_exists($outpath)) {
-            mkdir($outpath);
+function searchDir($path)
+{
+    $fileModules = array();
+    $dh = opendir($path);
+    while ($entry = readdir($dh)) {
+        if (is_dir("$path/$entry")) {
+            if (($entry != '.') &&
+                ($entry != '..')) {
+                //Recurse
+                $outpath = str_replace($inputLocale,    $outputLocale,    "$path/$entry" );
+                if (!file_exists($outpath)) {
+                        mkdir($outpath);
+                }
+                searchDir("$path/$entry");
+            }
+        } else { // -> is file
+            if (substr($entry, strlen($entry)-4) == '.xml') {
+                echo "<br>Reading file $path/$entry\r\n";
+                $file = file ("$path/$entry");
+                $file_contents = implode ('', $file);
+                $new_file_contents1 = str_replace(
+                     "locale=\"$inputLocale\"", 
+                     "locale=\"$outputLocale\"", 
+                     $file_contents );
+                $new_file_contents2 = iconv($inputEncoding, $outputEncoding, $new_file_contents1 );
+                //$new_file_contents2 = mb_convert_encoding($file_contents, $outputEncoding, $inputEncoding);
+                $outpath = str_replace($inputLocale, $outputLocale,    $path );
+                if (!file_exists($outpath)) {
+                        mkdir($outpath);
 	}
-        $fp = fopen ( "$outpath/$entry", "w+" );
-        fwrite($fp, $new_file_contents2);
-        fclose($fp);
-      }
+                $fp = fopen ( "$outpath/$entry", "w+" );
+                fwrite($fp, $new_file_contents2);
+                fclose($fp);
+            }
+        }
     }
-  }
-  closedir($dh);
+    closedir($dh);
 }
 $path = $inputLocale.'/xml';
 searchDir ($path);
