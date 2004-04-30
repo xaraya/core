f1a0a16a6ce5485c5ec7b612c07283b743938906
mcortez <mcortez@ariel2.fullcoll.edu>
2004-04-30 20:04:04 +0000

    html/modules/dynamicdata/xaradmin/importpropertytypes.php: 	#  BitKeeper file C:/cygwin/home/mcortez/ddnoedit/html/modules/dynamicdata/xaradmin/importpropertytypes.php html/modules/dynamicdata/xaradminapi/importpropertytypes.php: 	#  BitKeeper file C:/cygwin/home/mcortez/ddnoedit/html/modules/dynamicdata/xaradminapi/importpropertytypes.php

---
+++ html/modules/dynamicdata/xaradmin/importpropertytypes.php
@@ -0,0 +1,10 @@
+<?php
+function dynamicdata_admin_importpropertytypes ($args)
+{
+	
+	$args['flush'] = 'false';
+	$success = xarModAPIFunc('dynamicdata','admin','importpropertytypes', $args);
+	
+	return array();
+}
+?>
---
+++ html/modules/dynamicdata/xaradminapi/importpropertytypes.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+ * File: $Id$
+ *
+ * Check the properties directory for properties and import them into the Property Type table.
+ *
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2003 by the Xaraya Development Team.
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.xaraya.com
+ *
+ * @subpackage dynamicdata module
+ * @author Michael Cortez <mcortez@fullcoll.edu>
+*/
+/**
+ * Check the properties directory for properties and import them into the Property Type table.
+ *
+ * @author the DynamicData module development team
+ * @param $args['flush'] flush the property type table before import true/false (optional)
+ * @returns array
+ * @return an array of the property types currently available
+ * @raise BAD_PARAM, NO_PERMISSION
+ */
+
+function dynamicdata_adminapi_importpropertytypes( $args )
+{
+	extract( $args );
+	
+
+	// Get list of properties in properties directory.
+	$PropertiesDir = 'includes/properties/';
+
+	// Open Properties Directory
+	if ($pdh = opendir($PropertiesDir)) 
+	{
+		$proptypes = array();
+
+		$numLoaded = 0;
+		
+		// Loop through properties directory
+		while (($propertyfile = readdir($pdh)) !== false) 
+		{
+			$propertyfilepath = $PropertiesDir . $propertyfile;
+
+			// Only Process files, not directories
+			if( is_file($propertyfilepath) )
+			{
+				// Get the name of each file, assumed to be the name of the property
+				// Get file type
+				list($propertyClass, $type) = explode('.',$propertyfile);
+				
+				// Only worry about php files, not security place holder .html files or other garbage that might be present
+				if( $type == 'php' )
+				{
+					// Include the file into the environment
+					require_once $propertyfilepath;
+					
+					// Tell the property to skip initialization, this is only really needed for Dynamic_FieldType_Property
+					// because it causes this function to recurse.
+					$args['skipInit'] = true;
+					
+					// Instantiate a copy of this class
+					$property = new $propertyClass($args);
+					
+					// Get the base information that used to be hardcoded into /modules/dynamicdata/class/properties.php
+					$baseInfo = $property->getBasePropertyInfo();
+					
+
+					// Check if there is any reason why we shouldn't include this property in the property list					
+					$skipProperty = false;
+					
+					// If the property needs specific files to exist, check for them
+					// Example: HTML Area property needs to check to see if HTMLArea javascript files are present
+					if( isset($baseInfo['dependancies']) && ($baseInfo['dependancies'] != '') )
+					{
+						$dependancies = explode(';', $baseInfo['dependancies']);
+						foreach( $dependancies as $dependancy )
+						{
+							if( !file_exists($dependancy) )
+							{
+								$skipProperty = true;
+								break;
+							}
+						}
+					}
+
+					// Check if any Modules are required
+					// For Example: Categories, Ratings, Hitcount properties all require their respective modules to be enabled
+					if( isset($baseInfo['requiresmodule']) && ($baseInfo['requiresmodule'] != '') )
+					{
+						$modulesNeeded = explode(';', $baseInfo['requiresmodule']);
+						foreach( $modulesNeeded as $moduleName )
+						{
+							if( !xarModIsAvailable($moduleName) )
+							{
+								$skipProperty = true;
+								break;
+							}
+						}
+					}
+
+					// If we're still going to add this property to the list
+					if( !$skipProperty )
+					{
+						// Save the name of the property
+						$baseInfo['propertyClass'] = $propertyClass;
+						
+						// Check for aliases
+						if( !isset($baseInfo['aliases']) || ($baseInfo['aliases'] == '') || !is_array($baseInfo['aliases']) )
+						{
+							// Add the property to the property type list
+							$proptypes[$baseInfo['id']] = $baseInfo;
+							
+						} else if ( is_array($baseInfo['aliases']) && (count($baseInfo['aliases']) > 0) ) {
+							// if aliases are present include them as seperate entries
+							$aliasList = '';
+							foreach( $baseInfo['aliases'] as $aliasInfo )
+							{
+								// Save the name of the property, for the alias
+								$aliasInfo['propertyClass'] = $propertyClass;
+								
+								// Add the alias to the property type list
+								$proptypes[$aliasInfo['id']] = $aliasInfo;
+								$aliasList .= $aliasInfo['id'].',';
+							}
+							
+							// Store a list of reference ID's from the base property it's aliases
+							$baseInfo['aliases'] = $aliasList;
+
+							// Add the base property to the property type list
+							$proptypes[$baseInfo['id']] = $baseInfo;
+						}
+					}
+				}				
+			}
+		}
+		closedir($pdh);
+
+		// Sort the property types
+		ksort( $proptypes );
+		
+	}
+
+	
+	return $proptypes;
+}
+
+?>
