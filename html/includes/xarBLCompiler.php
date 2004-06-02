d74e8c5c9e897cb4627acac724bd948294ce63b7
marc <marc@marclaptop.>
2004-06-02 20:04:06 +0000

    html/includes/xarBLCompiler.php: 	#  small change to rearrange code so the module attribute doesn' come into play in the template tag when the type is theme

--- html/includes/xarBLCompiler.php
+++ html/includes/xarBLCompiler.php
@@ -2948,40 +2948,18 @@ class xarTpl__XarTemplateNode extends xarTpl__TplTagNode
         $subdata = '$_bl_data';  // Subdata defaults to the data of the current template
         $type = 'module';        // Default type is module included template.
         extract($this->attributes);
-    
+
         // File attribute is mandatory
         if (!isset($file)) {
             $this->raiseError(XAR_BL_MISSING_ATTRIBUTE,'Missing \'file\' attribute in <xar:template> tag.', $this);
             return;
         }
-        
-        // Module attribute is optional
-        if(!isset($module)) {
-            // No module attribute specified, determine it
-            // The module which needs to be passed in needs to come from the location of the
-            // template which holds the tag, not the active module although they will be the same
-            // in most cases. If the active module would be passed in, this would break when
-            // calling API functions from other modules which in turn use a template (rare, but possible,
-            // like generating xml with blocklayout). By passing in the modulename which holds the
-            // template, we make sure that the include resolves to the right file.
-            $patharray = explode('/',dirname($this->fileName));
-            // We need the value after 'modules' always, whether the container is overridden
-            foreach($patharray as $patharrayid => $patharrayname) {
-                if ($patharrayname == 'modules') {
-                    $module = $patharray[$patharrayid+1];
-                    break;
-                }
-            }
-        }
-        
-        // Resolve the file attribute
+
+                // Resolve the file attribute
         $file = xarTpl__ExpressionTransformer::transformPHPExpression($file);
         if (!isset($file)) {
             return;
         }
-        
-        // Resolve the module attribute
-        $module = xarTpl__ExpressionTransformer::transformPHPExpression($module);
 
         // Resolve subdata attribute
         $subdata = xarTpl__ExpressionTransformer::transformPHPExpression($subdata);
@@ -2991,6 +2969,27 @@ class xarTpl__XarTemplateNode extends xarTpl__TplTagNode
             return "xarTpl_includeThemeTemplate(\"$file\", $subdata)";
             break;
         case 'module':
+            // Module attribute is optional
+            if(!isset($module)) {
+                // No module attribute specified, determine it
+                // The module which needs to be passed in needs to come from the location of the
+                // template which holds the tag, not the active module although they will be the same
+                // in most cases. If the active module would be passed in, this would break when
+                // calling API functions from other modules which in turn use a template (rare, but possible,
+                // like generating xml with blocklayout). By passing in the modulename which holds the
+                // template, we make sure that the include resolves to the right file.
+                $patharray = explode('/',dirname($this->fileName));
+                // We need the value after 'modules' always, whether the container is overridden
+                foreach($patharray as $patharrayid => $patharrayname) {
+                    if ($patharrayname == 'modules') {
+                        $module = $patharray[$patharrayid+1];
+                        break;
+                    }
+                }
+            }
+            // Resolve the module attribute
+            $module = xarTpl__ExpressionTransformer::transformPHPExpression($module);
+
             return "xarTpl_includeModuleTemplate(\"$module\", \"$file\", $subdata)";
             break;
         default:
