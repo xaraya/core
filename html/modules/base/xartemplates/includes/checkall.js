c46e1616569da956c5a157cc8a0e583d35be5001
mikespub <mikespub@fully.qualified.hostname>
2004-06-27 20:04:06 +0000

    html/modules/base/xartemplates/includes/checkall.js: 	#  bug 1210 check/uncheck all checkboxes in a form html/modules/base/xartemplates/includes/checkall.js: 	#  BitKeeper file D:/wintools/cygwin/home/stable/html/modules/base/xartemplates/includes/checkall.js

---
+++ html/modules/base/xartemplates/includes/checkall.js
@@ -0,0 +1,14 @@
+/**
+ * check or uncheck all checkboxes in a form
+ * Example :
+ * <a href="javascript:xar_base_checkall(document.forms['thisform'],true)">Check All</a>
+ * <a href="javascript:xar_base_checkall(document.forms['thisform'],false)">Uncheck All</a>
+ */
+function xar_base_checkall(formobject, value) {
+	for (i = 0; i < formobject.length; i++) {
+		if (formobject.elements[i].type == 'checkbox') {
+			formobject.elements[i].checked = value;
+		}
+	}
+}
+
