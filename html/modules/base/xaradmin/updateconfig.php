46f3fd1536e80763bc20e48055796bd999a54eab
voll <voll@yahoo.com>
2004-07-10 20:04:07 +0000

    html/modules/base/xartemplates/includes/modifyconfig-locales.xd: 	#  BitKeeper file C:/bk/xaraya/core/stable/html/modules/base/xartemplates/includes/modifyconfig-locales.xd html/modules/base/xaradmin/updateconfig.php: 	#  copy locales configuration to base module html/modules/base/xartemplates/admin-modifyconfig.xd: 	#  copy locales configuration to base module html/modules/base/xaradmin/updateconfig.php: 	#  bug 1662 deny UNBOXED mode for non-UTF8 default locale

--- html/modules/base/xaradmin/updateconfig.php
+++ html/modules/base/xaradmin/updateconfig.php
@@ -73,6 +73,12 @@ function base_admin_updateconfig()
             if (!in_array($defaultLocale,$localesList)) $localesList[] = $defaultLocale;
             sort($localesList);
 
+            if (($MLSMode == 'UNBOXED') && (xarMLSGetCharsetFromLocale($defaultLocale) != 'utf-8')) {
+                $msg = xarML('You should select utf-8 locale as default before selecting UNBOXED mode');
+                xarErrorSet(XAR_USER_EXCEPTION, 'BAD_DATA', new DefaultUserException($msg));
+                break;
+            }
+
             // Locales
             xarConfigSetVar('Site.MLS.MLSMode', $MLSMode);
             xarConfigSetVar('Site.MLS.DefaultLocale', $defaultLocale);
