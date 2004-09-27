e558ddd2690c31a590309e3b96cf33b4072bcc72
miko <miko@chris-dudleys-computer.local>
2004-09-27 20:04:09 +0000

    html/includes/xarSession.php: 	#  Changing from 13 xarFuncIsDisabled to 1 	#  Code readability help html/xaradodb/lang/adodb-sv.inc.php: 	#  oops - this should be sv not en, since these is the swedish file not english 	#  thanks to Claus dofor spotting this html/xaradodb/lang/adodb-da.inc.php: 	#  BitKeeper file /Users/miko/xaraya/xaraya-stable/html/xaradodb/lang/adodb-da.inc.php

---
+++ html/xaradodb/lang/adodb-da.inc.php
@@ -0,0 +1,33 @@
+<?php
+// Christian Tiberg" christian@commsoft.nu
+$ADODB_LANG_ARRAY = array (
+			'LANG'                      => 'da',
+            DB_ERROR                    => 'Ukendt fejl',
+            DB_ERROR_ALREADY_EXISTS     => 'findes allerede',
+            DB_ERROR_CANNOT_CREATE      => 'kan ikke oprette',
+            DB_ERROR_CANNOT_DELETE      => 'kan ikke slette',
+            DB_ERROR_CANNOT_DROP        => 'kan ikke droppe',
+            DB_ERROR_CONSTRAINT         => 'begrænsning overskredet',
+            DB_ERROR_DIVZERO            => 'division med nul',
+            DB_ERROR_INVALID            => 'ugyldig',
+            DB_ERROR_INVALID_DATE       => 'ugyldig dato eller tid',
+            DB_ERROR_INVALID_NUMBER     => 'ugyldigt tal',
+            DB_ERROR_MISMATCH           => 'ingen match',
+            DB_ERROR_NODBSELECTED       => 'ingen database valgt',
+            DB_ERROR_NOSUCHFIELD        => 'intet sådant felt',
+            DB_ERROR_NOSUCHTABLE        => 'ingen sådan tabel',
+            DB_ERROR_NOT_CAPABLE        => 'DB-program kan ikke gøre dette',
+            DB_ERROR_NOT_FOUND          => 'ikke fundet',
+            DB_ERROR_NOT_LOCKED         => 'ikke låst',
+            DB_ERROR_SYNTAX             => 'syntaksfejl',
+            DB_ERROR_UNSUPPORTED        => 'understøttes ikke',
+            DB_ERROR_VALUE_COUNT_ON_ROW => 'værdiantal i række',
+            DB_ERROR_INVALID_DSN        => 'ugylidigt DSN',
+            DB_ERROR_CONNECT_FAILED     => 'tilslutning mislykkedes',
+            0	                       => 'ingen fejl', // DB_OK
+            DB_ERROR_NEED_MORE_DATA     => 'utilstrækkelige data angivet',
+            DB_ERROR_EXTENSION_NOT_FOUND=> 'udvidelse ikke fundet',
+            DB_ERROR_NOSUCHDB           => 'ingen sådan database',
+            DB_ERROR_ACCESS_VIOLATION   => 'utilstrækkelige rettigheder'
+);
+?>
