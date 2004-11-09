e751a27e37d1172096df8bbc9aa2c0ea0c26a27c
marcel <marcel@hsdev.com>
2004-11-09 20:04:11 +0000

    html/xaradodb/datadict/datadict-sqlite.inc.php: 	#  brace

--- html/xaradodb/datadict/datadict-sqlite.inc.php
+++ html/xaradodb/datadict/datadict-sqlite.inc.php
@@ -1,16 +1,13 @@
 <?php
 
 /**
-V4.20 22 Feb 2004  (c) 2000-2004 John Lim (jlim@natsoft.com.my). All rights reserved.
- Released under both BSD license and Lesser GPL library license. 
- Whenever there is any discrepancy between the two licenses, 
- the BSD license will take precedence.
-	
+
  Set tabs to 4 for best viewing.
  
  */
 
-class ADODB2_sqlite extends ADODB_DataDict {
+class ADODB2_sqlite extends ADODB_DataDict 
+{
 	
 	var $databaseType = 'sqlite';
 	var $seqField = false;
