b7c898337ddfdc25ba282d137edb2a38c4137e42
marcel <marcel@hsdev.com>
2004-11-28 20:04:11 +0000

    html/includes/properties/Dynamic_StateList_Property.php: 	#  xml compliance dd statelist html/modules/dynamicdata/xartemplates/admin-showinput-statelist.xd: 	#  xml compliance dd statelist

--- html/includes/properties/Dynamic_StateList_Property.php
+++ html/includes/properties/Dynamic_StateList_Property.php
@@ -165,8 +165,8 @@ class Dynamic_StateList_Property extends Dynamic_Select_Property
         */
 
         $data['soptions'] = $soptions;
-        $data['invalid']  = !empty($this->invalid) ? ' <span class="xar-error">'.xarML('Invalid #(1)', $this->invalid) .'</span>' : '';
-        $data['tabindex'] =! empty($tabindex) ? ' tabindex="'.$tabindex.'" ' : '';
+        $data['invalid']  = !empty($this->invalid) ? xarML('Invalid #(1)', $this->invalid) : '';
+        $data['tabindex'] =! empty($tabindex) ? $tabindex : 0;
 
 
         $template="statelist";
