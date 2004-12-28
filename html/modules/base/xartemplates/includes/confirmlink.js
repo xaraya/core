48ca7baba05408f44ac2f9bb80fe5065f5eb8b96
mikespub <mikespub@fully.qualified.hostname>
2004-12-28 20:04:12 +0000

    html/modules/base/xartemplates/includes/confirmlink.js: 	#  bug 2835 - remove untranslated text from javascript

--- html/modules/base/xartemplates/includes/confirmlink.js
+++ html/modules/base/xartemplates/includes/confirmlink.js
@@ -1,13 +1,13 @@
 function xar_base_confirmLink(theLink, xarConfirmMsg)
 {
-    var confirmMsg  = 'Do you really want to';
     // Confirmation is not required in the configuration file
     // or browser is Opera (crappy js implementation)
-    if (confirmMsg == '' || typeof(window.opera) != 'undefined') {
+// CHECKME: is this still valid for Opera ?
+    if (typeof(window.opera) != 'undefined') {
         return true;
     }
 
-    var is_confirmed = confirm(confirmMsg + ' :\n' + xarConfirmMsg);
+    var is_confirmed = confirm(xarConfirmMsg);
     if (is_confirmed) {
         theLink.href += '&confirm=1';
     }
