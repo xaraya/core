2f49894c7679b36d61bb370f2cebab1b33d0a13b
mikespub <mikespub@fully.qualified.hostname>
2005-01-22 20:05:01 +0000

    html/includes/properties/Dynamic_Array_Property.php: 	#  BitKeeper file D:/wintools/cygwin/home/stable/html/includes/properties/Dynamic_Array_Property.php html/modules/dynamicdata/xartemplates/admin-showinput-array.xd: 	#  BitKeeper file D:/wintools/cygwin/home/stable/html/modules/dynamicdata/xartemplates/admin-showinput-array.xd html/modules/dynamicdata/xartemplates/user-showoutput-array.xd: 	#  BitKeeper file D:/wintools/cygwin/home/stable/html/modules/dynamicdata/xartemplates/user-showoutput-array.xd

---
+++ html/includes/properties/Dynamic_Array_Property.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * Dynamic Array Property
+ *
+ * @package Xaraya eXtensible Management System
+ * @subpackage dynamicdata module
+ */
+class Dynamic_Array_Property extends Dynamic_Property
+{
+    var $fields = array();
+    var $size = 40;
+
+    function Dynamic_Array_Property($args)
+    {
+        $this->Dynamic_Property($args);
+        // check validation for list of fields (optional)
+        if (!empty($this->validation) && strchr($this->validation,';')) {
+            $this->fields = explode(';',$this->validation);
+        }
+    }
+
+    function validateValue($value = null)
+    {
+        if (!isset($value)) {
+            $value = $this->value;
+        }
+        if (empty($value)) {
+            $value = array('');
+        } elseif (!is_array($value)) {
+            $out = @unserialize($value);
+            if ($out !== false) {
+                $value = $out;
+            } else {
+                $value = array($value);
+            }
+        }
+        if (count($this->fields) > 0) {
+        // TODO: do something with field list ?
+        }
+        $this->value = serialize($value);
+        return true;
+    }
+
+    function showInput($args = array())
+    {
+        extract($args);
+        if (empty($name)) {
+            $name = 'dd_' . $this->id;
+        }
+        if (empty($id)) {
+            $id = $name;
+        }
+        if (!isset($value)) {
+            $value = $this->value;
+        }
+        if (isset($fields)) {
+            $this->fields = $fields;
+        }
+        if (empty($value)) {
+            $value = array('');
+        } elseif (!is_array($value)) {
+            $out = @unserialize($value);
+            if ($out !== false) {
+                $value = $out;
+            } else {
+                $value = array($value);
+            }
+        }
+        if (count($this->fields) > 0) {
+            $fieldlist = $this->fields;
+        } else {
+            $fieldlist = array_keys($value);
+        }
+        $data = array();
+        $data['name']     = $name;
+        $data['id']       = $id;
+        $data['value'] = array();
+        foreach ($fieldlist as $field) {
+            if (!isset($value[$field])) {
+                $data['value'][$field] = '';
+            } else {
+                $data['value'][$field] = xarVarPrepForDisplay($value[$field]);
+            }
+        }
+        $data['tabindex'] = !empty($tabindex) ? $tabindex : 0;
+        $data['invalid']  = !empty($this->invalid) ? xarML('Invalid #(1)', $this->invalid) :'';
+        $data['size']     = !empty($size) ? $size : $this->size;
+
+        $template = "array";
+        return xarTplModule('dynamicdata', 'admin', 'showinput', $data ,$template);
+    }
+
+    function showOutput($args = array())
+    {
+        extract($args);
+        if (!isset($value)) {
+            $value = $this->value;
+        }
+        if (empty($value)) {
+            $value = array('');
+        } elseif (!is_array($value)) {
+            $out = @unserialize($value);
+            if ($out !== false) {
+                $value = $out;
+            } else {
+                $value = array($value);
+            }
+        }
+        if (count($this->fields) > 0) {
+            $fieldlist = $this->fields;
+        } else {
+            $fieldlist = array_keys($value);
+        }
+        $data = array();
+        $data['value'] = array();
+        foreach ($fieldlist as $field) {
+            if (!isset($value[$field])) {
+                $data['value'][$field] = '';
+            } else {
+                $data['value'][$field] = xarVarPrepForDisplay($value[$field]);
+            }
+        }
+
+        $template = "array";
+        return xarTplModule('dynamicdata', 'user', 'showoutput', $data ,$template);
+    }
+
+    /**
+     * Get the base information for this property.
+     *
+     * @returns array
+     * @return base information for this property
+     **/
+     function getBasePropertyInfo()
+     {
+         $baseInfo = array(
+                           'id'         => 999,
+                           'name'       => 'array',
+                           'label'      => 'Array',
+                           'format'     => '999',
+                           'validation' => '',
+                           'source'     => '',
+                           'dependancies' => '',
+                           'requiresmodule' => '',
+                           'aliases' => '',
+                           'args'         => '',
+                           // ...
+                          );
+        return $baseInfo;
+     }
+}
+
+?>
