1b6bd709908ab091b520a960c4f6e4aeec592a80
judgej <judgej@acadweb.co.uk>
2005-03-01 20:05:03 +0000

    html/xaradodb/drivers/adodb-xarpostgres64.inc.php: 	#  Parent class typo.

--- html/xaradodb/drivers/adodb-xarpostgres64.inc.php
+++ html/xaradodb/drivers/adodb-xarpostgres64.inc.php
@@ -39,7 +39,7 @@ class ADODB_xarpostgres64 extends ADODB_postgres64
         }
 
         // Execute the standard driver.
-        $result = ADODB_mysql::_Execute($sql, $inputar);
+        $result = ADODB_postgres64::_Execute($sql, $inputar);
 
         if (xarCoreIsDebugFlagSet(XARDBG_SQL)) {
             $lmtime = explode(' ', microtime());
