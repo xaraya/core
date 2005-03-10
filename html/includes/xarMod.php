37b8236423c2e39b894a9f66055b1bffd4ced58b
judgej <judgej@acadweb.co.uk>
2005-03-10 20:05:03 +0000

    html/includes/xarMod.php: 	#  Restore a DB fix accidently overwritten

--- html/includes/xarMod.php
+++ html/includes/xarMod.php
@@ -1807,8 +1807,8 @@ function xarMod_getBaseInfo($modName, $type = 'module')
 
     $query = 'SELECT mods.xar_regid, mods.xar_directory, mods.xar_mode,'
         . ' mods.xar_id, modstates.xar_state, mods.xar_name'
-        . ' FROM '.$modulestable.' AS mods'
-        . ' LEFT JOIN '.$modules_statesTable.' AS modstates'
+        . ' FROM '.$modulestable.' mods'
+        . ' LEFT JOIN '.$modules_statesTable.' modstates'
         . ' ON modstates.xar_regid = mods.xar_regid'
         . ' WHERE mods.xar_name = ? OR mods.xar_directory = ?';
     $bindvars = array($modName, $modName);
@@ -1977,7 +1977,7 @@ function xarMod_getVarsByName($varName, $type = 'module')
         $module_table = $tables['modules'];
 
         $query = "SELECT mods.xar_name, vars.xar_value
-                      FROM $module_table as mods , $module_varstable as vars
+                      FROM $module_table mods , $module_varstable as vars
                       WHERE mods.xar_id = vars.xar_modid AND
                             vars.xar_name = ?";
         break;
