03aa094cd7ef7a246105cd680289bdc590cf774a
marcel <marcel@hsdev.com>
2005-03-24 20:05:03 +0000

    html/modules/base/xartemplates/includes/xmlhttprequest.js: 	#  - eliminate a global 	#  - registering a handler is something different than calling a function, this was the bug 	#  - add paranoid try/catch clauses 	#  - add some TODO's

--- html/modules/base/xartemplates/includes/xmlhttprequest.js
+++ html/modules/base/xartemplates/includes/xmlhttprequest.js
@@ -5,61 +5,74 @@
 // Copyright 2004 Marcel van der Boom <marcel@hsdev.com>
 // License: GPL <http://www.gnu.org/licenses/gpl.html>
 
+// TODO: can this global be eliminated?
 var req;
-var postfix = "&pageName=module";
 
 function loadContent(url,tagid) {
-    if (window.XMLHttpRequest) {
-        document.body.style.cursor='wait';
-        req = new XMLHttpRequest();
-        req.onreadystatechange = processReqChange(tagid);
-        req.open("GET", url + postfix, true);
-        req.send(null);
-        return false;
-    } else if (window.ActiveXObject) {
-        document.body.style.cursor='wait';
-        req = new ActiveXObject("Microsoft.XMLHTTP");
-        if (req) {
+    // TODO: this doesnt belong here
+    var postfix = "&pageName=module";
+    // Set the tag id as property of the processReqChange function 
+    // instead of a global variable
+    processReqChange.tagid = tagid;
+    try {
+        if (window.XMLHttpRequest) {
+            document.body.style.cursor='wait';
+            req = new XMLHttpRequest();
             req.onreadystatechange = processReqChange;
             req.open("GET", url + postfix, true);
-            req.send();
-        }
-        return false;
-    } else {
-        return true; // do the normal action
-    }   
+            req.send(null);
+            return false;
+        } else if (window.ActiveXObject) {
+            document.body.style.cursor='wait';
+            req = new ActiveXObject("Microsoft.XMLHTTP");
+            if (req) {
+                req.onreadystatechange = processReqChange;
+                req.open("GET", url + postfix, true);
+                req.send();
+            }
+            return false;
+        } else {
+            return true; // do the normal action
+        }   
+    } catch (e) {
+        alert(e);
+    }
 }
 
 // Handler called on state changes of the request object
-function processReqChange(tagid)
-{
-    if (req.readyState == 4) {
-        if (req.status == 200) {
-            // Do whatever we need with the content returned
-            tag = document.getElementById(tagid);
-            if(tag == null) {
-                // not found, fallback
-                document.location = req.url;
-                return true;
-            }
-            // Make sure we replace the right tag, and dont leave anything behind
-            myparent = tag.parentNode;
-            tag.id ='dummytogetridoftheoriginal';
-            tag.innerHTML = req.responseText;
-            newtag = document.getElementById(tagid);
-            if(newtag == null) {
-                alert('cant find the new tag [' + tagid + ']');
+function processReqChange()
+{   
+    try {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                // Do whatever we need with the content returned
+                tag = document.getElementById(this.tagid);
+                if(tag == null) {
+                    // not found, fallback
+                    document.location = req.url;
+                    return true;
+                }
+                // Make sure we replace the right tag, and dont leave anything behind
+                myparent = tag.parentNode;
+                tag.id ='dummytogetridoftheoriginal';
                 tag.innerHTML = req.responseText;
-                document.body.style.cursor='default';
-                return false;
+                newtag = document.getElementById(this.tagid);
+                if(newtag == null) {
+                    alert('cant find the new tag [' + this.tagid + ']');
+                    tag.innerHTML = req.responseText;
+                    document.body.style.cursor='default';
+                    return false;
+                } else {
+                    copyofnew = newtag.cloneNode(true);
+                    myparent.replaceChild(copyofnew,tag);
+                    document.body.style.cursor='default';
+                    return false; // cancel the normal action
+                }
             } else {
-                copyofnew = newtag.cloneNode(true);
-                myparent.replaceChild(copyofnew,tag);
-                document.body.style.cursor='default';
-                return false; // cancel the normal action
+                alert("There was a problem retrieving the XML data:\n" +  req.statusText);
             }
-        } else {
-            alert("There was a problem retrieving the XML data:\n" +  req.statusText);
         }
+    } catch(e) {
+        alert(e);
     }
 }
