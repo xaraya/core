25b0252d58487e558dd529c10a047d2466a1b2a9
marc <marc@marclaptop.>
2005-04-16 20:05:04 +0000

    html/modules/themes/xarcssapi/registercsstags.php: 	#  From 8 tages make 1 html/modules/themes/xartemplates/user-additionalstyles.xd: 	#  damn QA tabs

--- html/modules/themes/xarcssapi/registercsstags.php
+++ html/modules/themes/xarcssapi/registercsstags.php
@@ -20,90 +20,30 @@
  * @author Andy Varganov
  * @param none
  * @returns bool
- */ 
+ */
 function themes_cssapi_registercsstags($args)
 {
-    
+
     // just resetting default tags here, nothing else
-    
+
+   // just resetting default tags here, nothing else
+
     // unregister all - just in case they got corrupted or fiddled with via gui
     xarTplUnregisterTag('additional-styles');
-    
-    xarTplUnregisterTag('link-module-stylesheet');
-    xarTplUnregisterTag('import-module-stylesheet');
-    xarTplUnregisterTag('embed-module-styles');
-    
-    xarTplUnregisterTag('link-theme-stylesheet');
-    xarTplUnregisterTag('import-theme-stylesheet');
-    xarTplUnregisterTag('embed-theme-styles');
-    
-    xarTplUnregisterTag('link-common-stylesheet');
-    xarTplUnregisterTag('import-common-stylesheet');
-    
+    xarTplUnregisterTag('style');
+
     // use in theme to render all extra styles tags
-    xarTplRegisterTag( 'themes', 'additional-styles', array(), 'themes_cssapi_rendercss');
-    
-    // use in module templates to add styles by using different linking and embedding methods
-    
-    // LINK FROM MODULE
-    $cssTagAttributes = array(  new xarTemplateAttribute('modname',     XAR_TPL_REQUIRED | XAR_TPL_STRING),
-                                new xarTemplateAttribute('filename',    XAR_TPL_OPTIONAL | XAR_TPL_STRING),
-                                new xarTemplateAttribute('themedir',    XAR_TPL_OPTIONAL | XAR_TPL_STRING),
-                                new xarTemplateAttribute('alternate',   XAR_TPL_OPTIONAL | XAR_TPL_BOOLEAN),
-                                new xarTemplateAttribute('media',       XAR_TPL_OPTIONAL | XAR_TPL_STRING),
-                                new xarTemplateAttribute('title',       XAR_TPL_OPTIONAL | XAR_TPL_STRING));
-    
-    xarTplRegisterTag( 'themes', 'link-module-stylesheet', $cssTagAttributes ,'themes_cssapi_linkmodule');
-    
-    // IMPORT FROM MODULE
-    $cssTagAttributes = array(  new xarTemplateAttribute('modname',     XAR_TPL_REQUIRED | XAR_TPL_STRING),
-                                new xarTemplateAttribute('filename',    XAR_TPL_OPTIONAL | XAR_TPL_STRING),
-                                new xarTemplateAttribute('themedir',    XAR_TPL_OPTIONAL | XAR_TPL_STRING),
-                                new xarTemplateAttribute('media',       XAR_TPL_OPTIONAL | XAR_TPL_STRING));
-    
-    xarTplRegisterTag( 'themes', 'import-module-stylesheet', $cssTagAttributes ,'themes_cssapi_importmodule');
-    
-    // EMBED FROM MODULE
-    $cssTagAttributes = array(  new xarTemplateAttribute('source',      XAR_TPL_REQUIRED | XAR_TPL_STRING),
-                                new xarTemplateAttribute('media',       XAR_TPL_OPTIONAL | XAR_TPL_STRING));
-    
-    xarTplRegisterTag( 'themes', 'embed-module-styles', $cssTagAttributes , 'themes_cssapi_embedmodule');
-    
-    // use in theme templates to add styles by using different linking and embedding methods
-    
-    // LINK FROM THEME
-    $cssTagAttributes = array(  new xarTemplateAttribute('filename',    XAR_TPL_OPTIONAL | XAR_TPL_STRING),
-                                new xarTemplateAttribute('alternate',   XAR_TPL_OPTIONAL | XAR_TPL_BOOLEAN),
-                                new xarTemplateAttribute('media',       XAR_TPL_OPTIONAL | XAR_TPL_STRING),
-                                new xarTemplateAttribute('title',       XAR_TPL_OPTIONAL | XAR_TPL_STRING));
-    
-    xarTplRegisterTag( 'themes', 'link-theme-stylesheet', $cssTagAttributes ,'themes_cssapi_linktheme');
-    
-    // IMPORT FROM THEME
-    $cssTagAttributes = array(  new xarTemplateAttribute('filename',    XAR_TPL_OPTIONAL | XAR_TPL_STRING),
-                                new xarTemplateAttribute('media',       XAR_TPL_OPTIONAL | XAR_TPL_STRING));
-    
-    xarTplRegisterTag( 'themes', 'import-theme-stylesheet', $cssTagAttributes , 'themes_cssapi_importtheme');
-    
-    // EMBED FROM THEME
-    $cssTagAttributes = array(  new xarTemplateAttribute('source',      XAR_TPL_REQUIRED | XAR_TPL_STRING),
-                                new xarTemplateAttribute('media',       XAR_TPL_OPTIONAL | XAR_TPL_STRING));
-    
-    xarTplRegisterTag( 'themes', 'embed-theme-styles', $cssTagAttributes , 'themes_cssapi_embedtheme');
-    
-    
-    // LINK COMMON FROM ANY TEMPLATE
-    $cssTagAttributes = array(  new xarTemplateAttribute('filename',    XAR_TPL_OPTIONAL | XAR_TPL_STRING),
-                                new xarTemplateAttribute('media',       XAR_TPL_OPTIONAL | XAR_TPL_STRING));
-    
-    xarTplRegisterTag( 'themes', 'link-common-stylesheet', $cssTagAttributes ,'themes_cssapi_linkcommon');
-    
-    // IMPORT COMMON FROM ANY TEMPLATE
-    $cssTagAttributes = array(  new xarTemplateAttribute('filename',    XAR_TPL_OPTIONAL | XAR_TPL_STRING),
-                                new xarTemplateAttribute('media',       XAR_TPL_OPTIONAL | XAR_TPL_STRING));
-    
-    xarTplRegisterTag( 'themes', 'import-common-stylesheet', $cssTagAttributes , 'themes_cssapi_importcommon');
-    
+    xarTplRegisterTag( 'themes', 'additional-styles', array(), 'themes_cssapi_styledeliver');
+    // Register the tag which is used to include style information
+    $cssTagAttributes = array(  new xarTemplateAttribute('file'     , XAR_TPL_REQUIRED | XAR_TPL_STRING),
+                                new xarTemplateAttribute('scope'    , XAR_TPL_OPTIONAL | XAR_TPL_STRING),
+                                new xarTemplateAttribute('method'   , XAR_TPL_OPTIONAL | XAR_TPL_STRING),
+                                new xarTemplateAttribute('type'     , XAR_TPL_OPTIONAL | XAR_TPL_STRING),
+                                new xarTemplateAttribute('alternate', XAR_TPL_OPTIONAL | XAR_TPL_BOOLEAN),
+                                new xarTemplateAttribute('media'    , XAR_TPL_OPTIONAL | XAR_TPL_STRING),
+                                new xarTemplateAttribute('title'    , XAR_TPL_OPTIONAL | XAR_TPL_STRING));
+    xarTplRegisterTag( 'themes', 'style', $cssTagAttributes ,'themes_cssapi_styletag');
+
    // return
     return true;
 }
