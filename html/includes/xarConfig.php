416e4a9138dca95c9fa18889ecb5af7c5522cf58
scmadmin <scmadmin@xaraya.com>
2005-09-04 17:41:50 +0000

    html/includes/pnHTML.php: 	#  add @license tag to phpdoc html/includes/pnLegacy.php: 	#  add @license tag to phpdoc html/includes/xarBLCompiler.php: 	#  add @license tag to phpdoc html/includes/xarBlocks.php: 	#  add @license tag to phpdoc html/includes/xarConfig.php: 	#  add @license tag to phpdoc html/includes/xarCore.php: 	#  add @license tag to phpdoc html/includes/xarDB.php: 	#  add @license tag to phpdoc html/includes/xarEvt.php: 	#  add @license tag to phpdoc html/includes/xarException.php: 	#  add @license tag to phpdoc html/includes/xarLog.php: 	#  add @license tag to phpdoc html/includes/xarMLS.php: 	#  add @license tag to phpdoc html/includes/xarMod.php: 	#  add @license tag to phpdoc html/includes/xarSecurity.php: 	#  add @license tag to phpdoc html/includes/xarServer.php: 	#  add @license tag to phpdoc html/includes/xarSession.php: 	#  add @license tag to phpdoc html/includes/xarTableDDL.php: 	#  add @license tag to phpdoc html/includes/xarTemplate.php: 	#  add @license tag to phpdoc html/includes/xarUser.php: 	#  add @license tag to phpdoc html/includes/xarVar.php: 	#  add @license tag to phpdoc

--- html/includes/xarConfig.php
+++ html/includes/xarConfig.php
@@ -6,6 +6,7 @@
  * 
  * @package Xaraya eXtensible Management System
  * @copyright (C) 2002 by the Xaraya Development Team.
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
  * @link http://www.xaraya.org
  * @author Marco Canini
 */
