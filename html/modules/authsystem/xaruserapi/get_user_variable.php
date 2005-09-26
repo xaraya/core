49ee36974dfde2e923cefa74e2f029aa9ca02640
jojodee <jojodee@xaraya.com>
2005-09-26 04:28:09 +0000

    File headers in authsystem

--- html/modules/authsystem/xaruserapi/get_user_variable.php
+++ html/modules/authsystem/xaruserapi/get_user_variable.php
@@ -1,17 +1,12 @@
 <?php
 /**
- * File: $Id$
- *
- * Get a user variable
- *
- * @package authentication
- * @copyright (C) 2003 by the Xaraya Development Team.
+ * @package Xaraya eXtensible Management System
+ * @copyright (C) 2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
  * @subpackage authsystem
- * @author Marco Canini
-*/
+ */
 /**
  * get a user variable (currently unused)
  * @public
