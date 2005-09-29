b794dd1f56a36de4714dcf04859ee09d3a32297f
jojodee <jojodee@xaraya.com>
2005-09-29 15:02:23 +0000

    Adminpanels - might as well do the package header as well, no consolidation of headers, only new copyright header info.

--- html/modules/adminpanels/xaradminapi/create.php
+++ html/modules/adminpanels/xaradminapi/create.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Create a new admin item
+ *
  * @package Xaraya eXtensible Management System
  * @copyright (C) 2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
--- html/modules/adminpanels/xaradminapi/getmenulinks.php
+++ html/modules/adminpanels/xaradminapi/getmenulinks.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Utility functions for menu links
+ *
  * @package Xaraya eXtensible Management System
  * @copyright (C) 2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
