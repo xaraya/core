1a5b5aa181018d5ebc51a8eea651c069ebf34f7c
jojodee <jojodee@xaraya.com>
2005-10-15 09:29:20 +0000

    Bug 4785 fix for object return

--- html/modules/dynamicdata/xaruserapi/getitem.php
+++ html/modules/dynamicdata/xaruserapi/getitem.php
@@ -3,7 +3,7 @@
  * Get all data fields for an item
  *
  * @package Xaraya eXtensible Management System
- * @copyright (C) 2005 The Digital Development Foundation
+ * @copyright (C) 2002-2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
@@ -119,9 +119,8 @@ function &dynamicdata_userapi_getitem($args)
     if (!empty($getobject)) {
         return $object;
     }
-
-    return $object->getFieldValues();
-
+    $objectData = $object->getFieldValues();
+    return $objectData;
 }
 
 ?>
