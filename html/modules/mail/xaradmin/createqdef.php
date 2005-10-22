3dcceed203f7a523977696b8180ad3d32cb40901
marcel <marcel@hsdev.com>
2005-10-22 13:23:59 +0000

    Basic queue def creation should work now 	* html/modules/mail/xaradmin/createqdef.php: implement creating the qdef object 	* html/modules/mail/xaradmin/viewqueues.php: basic checks and actions for making sure we have a qdef 	* html/modules/mail/xardata/qdef.xml: let object be owned by mail module, more work but also more control 	* html/modules/mail/xartemplates/admin-queue-newdef.xd: gonna try something later with the error stuff

--- html/modules/mail/xaradmin/createqdef.php
+++ html/modules/mail/xaradmin/createqdef.php
@@ -16,25 +16,33 @@ function mail_admin_createqdef($args)
         $qdefNew = false;
         if(!xarVarFetch('qdef_name_choose','id',$qdefObjectId)) return;
         // Get the name of the object from dd
-        $qdefObject = xarModApiFunc('dynamicdata','user','getobjectinfo',array('objectid' => $qdefObjectId));
+        $qdefObject = xarModApiFunc('dynamicdata','user','getobject',array('objectid' => $qdefObjectId));
         if(!isset($qdefObject)) return;
-        $qdefName = $qdefObject['name'];
+        $qdefName = $qdefObject->name;
         break;
     default:
         // Wrong value, raise exception
     }
-    xarModSetVar('mail','queue-definition',$qdefName);
  
-    // k, name known now, create it if needed
+    $saneDef = '1d82ec410e6a4f63254a0f16533b6f7247e7903f'; // sha1 has on std definition, update when def changes.
     if($qdefNew) {
-        $sane = '3de31ebcd985f169f4d6948340b601672e138ac8'; // let's honour monotone :-)
-        $xmlDef = file_get_contents('modules/mail/xardata/qdef.xml');
-        if(sha1($xmlDef) != $sane) {
-            // TODO: raise 
-            die('XML definition not proper');
-        }
-        
+        $xmlDef = @file_get_contents('modules/mail/xardata/qdef.xml'); // if it fails, sane check will catch it.
+    } else {
+        // Set the name to the default, so we can validate it against the same hash
+        $qdefObject->name = 'mailqueues'; // $qdefName still holds the original
+        $xmlDef = xarModApiFunc('dynamicdata','util','export', array('objectref' => $qdefObject));
     }
+    if(sha1($xmlDef) != $saneDef) 
+        throw new Exception('XML definition not proper');
+
+    if($qdefNew) {
+        // Take the xml and the objectname and try to create the object
+        $qdefObjectId = xarModApiFunc('dynamicdata','util','import',array('objectname' => $qdefName, 'xml' => $xmlDef));
+        if(!isset($qdefObjectId)) 
+            throw new Exception('object creation failed');
+    }
+    // All went well, we can set the modvar now
+    xarModSetVar('mail','queue-definition',$qdefName);
     xarResponseRedirect(xarModUrl('mail','admin','viewqueues'));
 }
 ?>
--- html/modules/mail/xaradmin/viewqueues.php
+++ html/modules/mail/xaradmin/viewqueues.php
@@ -10,27 +10,34 @@ function mail_admin_viewqueues($args)
 {
      // Security Check
     if (!xarSecurityCheck('AdminMail')) return;
-    /*
-     * What this should do:
-     * 1. Is there a definition to manage queues in?
-     * 2. If so, retrieve the queues.
-     * 3. If not, offer to create a queue definition
-     */
      
     // Retrieve the object which holds our queue definition
     $qDef = xarModGetVar('mail','queue-definition');
-    //if($qDef != NULL) {
-        // Object was found, try to fetch it.
-        //$qDefObject = xarModApiFunc('mail','admin','getqdef',array('name' => $qDef));
-        //die('object found');
-        //} else {
+    if($qDef != NULL) {
+        // Modvar has a value, try fetching the object
+        $qdefObjectInfo = xarModApiFunc('dynamicdata','user','getobjectinfo',array('name' => $qDef));
+        if(!isset($qdefObjectInfo)) {
+            // Mod var exists but object doesnt
+            return OfferCreate($qDef);
+        } else {
+            // Object found
+            // TODO: validate here as well?
+            return array('qdef' => $qdefObjectInfo);
+        }
+    } else {
         // Nothing found for sure, offer to create one.
-        $data['authid'] = xarSecGenAuthKey();
-        $data['qdef_name'] = isset($qDef) ? $qDef : 'mailqueues';
-        $data['qdef_method'] = 1;
-        $data['qdef_create'] = array(array('id' => 1,'name' => xarML('Create a queue with name')));
-        $data['qdef_choose'] = array(array('id' => 2,'name' => xarML('Use object')));
-        return xarTplModule('mail','admin','queue-newdef',$data);
-        //}
+        return OfferCreate();
+    }
+    // 
+}
+
+function OfferCreate($qDef = null)
+{
+    $data['authid'] = xarSecGenAuthKey();
+    $data['qdef_name'] = isset($qDef) ? $qDef : 'mailqueues';
+    $data['qdef_method'] = 1;
+    $data['qdef_create'] = array(array('id' => 1,'name' => xarML('Create new object with name')));
+    $data['qdef_choose'] = array(array('id' => 2,'name' => xarML('Use an existing object')));
+    return xarTplModule('mail','admin','queue-newdef',$data);
 }
 ?>
