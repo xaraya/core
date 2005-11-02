ba7b3c27469a9dee44f877f168f7177050319e5c
jojodee <jojodee@xaraya.com>
2005-11-02 16:39:20 +0000

    Update roles overview

--- html/modules/roles/xaradminapi/getmenulinks.php
+++ html/modules/roles/xaradminapi/getmenulinks.php
@@ -2,12 +2,13 @@
 /**
  * Utility function pass individual menu items to the main menu
  *
- * @package Xaraya eXtensible Management System
- * @copyright (C) 2005 The Digital Development Foundation
+* @package modules
+ * @copyright (C) 2002-2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
  * @subpackage Roles module
+ * @link http://xaraya.com/index.php/release/27.html
  */
 /**
  * utility function pass individual menu items to the main menu
@@ -29,6 +30,10 @@ function roles_adminapi_getmenulinks()
     }*/
 
     if (xarSecurityCheck('EditRole',0)) {
+        $menulinks[] = Array('url' => xarModURL('roles','admin','overview'),
+                               'title' => xarML('Roles Overview'),
+                              'label' => xarML('Overview'));
+
         $menulinks[] = Array('url'   => xarModURL('roles',
                                                   'admin',
                                                   'showusers'),
