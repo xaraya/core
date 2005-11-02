854deb519a09ab9ff721bd0d97744daa44d345c7
jojodee <jojodee@xaraya.com>
2005-11-02 15:23:03 +0000

    Add the overview files

---
+++ html/modules/modules/xaradmin/overview.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Overview displays standard Overview page
+ *
+ * @package modules
+ * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Modules Module
+ * @link http://xaraya.com/index.php/release/1.html
+ */
+/**
+ * Overview displays standard Overview page
+ *
+ * Used to call the template that provides display of the overview
+ *
+ * @author jojodee
+ * @returns array xarTplModule with $data containing template data
+ * @return array containing the menulinks for the overview item on the main manu
+ * @since 2 Nov 2005
+ */
+function modules_admin_overview()
+{
+   /* Security Check */
+    if (!xarSecurityCheck('AdminModules',0)) return;
+
+    $data=array();
+    
+    /* if there is a separate overview function return data to it
+     * else just call the main function that usually displays the overview 
+     */
+
+    return xarTplModule('modules', 'admin', 'main', $data,'main');
+}
+
+?>
