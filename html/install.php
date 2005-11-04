9894a2a906234d1df1db5c000aca6e8f5419e308
jojodee <jojodee@xaraya.com>
2005-11-04 19:23:33 +0000

    Bug 2204 Admin name is changed and causes error in upgrade

--- html/install.php
+++ html/install.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * File: $Id$
- *
  * Xaraya Installer
  *
- * @package Xaraya eXtensible Management System
- * @copyright (C) 2002 by the Xaraya Development Team.
- * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * package installer
+ * @copyright (C) 2002-2005 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
+ *
  * @subpackage Installer
- * @author Johnny Robeson 
+ * @author Johnny Robeson
  * @author Paul Rosania
  * @author Marc Lutolf
  * @author Marcel van der Boom
