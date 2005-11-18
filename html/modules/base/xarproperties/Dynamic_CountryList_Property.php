27411667e16b246cfdc8fa562cef5a1159fd3d0e
mfl <mfl@netspan.ch>
2005-11-18 21:13:04 +0000

    align bad countrylist property with stable

--- html/modules/base/xarproperties/Dynamic_CountryList_Property.php
+++ html/modules/base/xarproperties/Dynamic_CountryList_Property.php
@@ -1,12 +1,14 @@
-
+<?php
 /**
+ * Dynamic Country List Property
+ *
  * @package Xaraya eXtensible Management System
  * @copyright (C) 2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
  * @subpackage Base module
- * @author mikespub <mikespub@xaraya.com>
+ * @author John Cox
  */
 /**
  * Include the base class
