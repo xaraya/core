672bbe85fbbaafdfb4fbe1bc51c0e2424cdd6f92
marcel <marcel@hsdev.com>
2005-12-14 17:58:24 +0000

    Taking advantage of the maturing sequence implementation, Xaraya can receive emails now and put them away in DD! Dont go rushing in to put this in production though :-) 	* html/modules/mail/xaradminapi/maptoqueue.php: fill in the test queue 	* html/modules/mail/xarcliapi/process.php: process input, push it to the test queue 	* html/modules/mail/xardocs/NOTES: dunno, see diffs 	* html/modules/mail/xartemplates/admin-view.xd: likewise

--- html/modules/mail/xaradminapi/maptoqueue.php
+++ html/modules/mail/xaradminapi/maptoqueue.php
@@ -1,10 +1,18 @@
 <?php
-
+/*
+ * Map a mail item to a queue based on defined rules
+ *
+ * @param object msg_structure parsed out result from the mailparser class
+ * @returns array of queue idents
+ */
 function mail_adminapi_maptoqueue($args)
 {
     extract($args);
     if(!isset($msg_structure)) return;
 
-    return 0;
+    include_once('includes/structures/sequences/queue.php');
+    // Test mapping, map em all to the masterq
+    $q = new Queue('dd',array('name' => 'masterq'));
+    return array($q);
 }
 ?>
--- html/modules/mail/xarcliapi/process.php
+++ html/modules/mail/xarcliapi/process.php
@@ -3,17 +3,27 @@
  * Process a raw email supplied to use by some gateway (ws.php for example)
  *
  * This function is now simple, but not smart. Ideally we want to do what we
- * do below very quickly. In other words, we assume we dont get many mails :-)
+ * do below very quickly to prevent real-time lock-ups. 
+ * In other words, the current code assumes we dont get many mails :-)
  *
  * @returns int exitcode to gateway script
  * @todo what do we do with security here?
  */
 function mail_cliapi_process($args)
 {
+    xarLogMessage("MAIL: processing incoming message");
     extract($args);
     assert('$argc > 0 && $argv[1] == "mail"; /* Wrong call to mail_cli_process handler */');
 
+    // TODO: Guess ;-)
+    if(isset($argv[2]) && $argv[2]=='-u') $user = $argv[3];
+    if(isset($argv[4]) && $argv[4]=='-p') $pass = $argv[5];
+    if(!xarUserLogin($user,$pass)) {
+        echo "Authentication failed\n";
+        return 1;
+    }
     // 1. Read stdin for the mail contents (raw)
+    // TODO: what to do when there is silence?
     $input = file_get_contents('php://stdin');
     if(!isset($input)) return _fatal("Could not read from php://stdin");
     if(strlen($input) == 0) return 0; // ok, but nothing to do here
@@ -25,14 +35,18 @@ function mail_cliapi_process($args)
     if($parser->isError($structure)) return _fatal("Could not parse input");
 
     // 3. Based on parse results determine the queue
-    // This would typically be something we want to postpone.
-    $queue = xarModApiFunc('mail','admin','maptoqueue',array('msg_structure' => $structure));
-    if(!isset($queue)) return _fatal("Could not map input to a queue.");
+    // This would typically be something we want to postpone, that is, put it in a default queue quickly
+    // and revisit this later on.
+    $destination = xarModApiFunc('mail','admin','maptoqueue',array('msg_structure' => $structure));
+    if(!isset($destination)) return _fatal("Could not map input to a queue.");
 
-    // 4. Put the message ($raw) into the queue
+    // 4. Put the message ($raw) into the queues
     // This would typically be something we want to postpone.
-    //$result = $queue->push($input);
-    //if(!$result) return _fatal("Could not push intput into determined queue");
+    // TODO: do we want a master queue and push the references instead, thus having only one raw source ?
+    
+    foreach ($destination as $q) {
+        $result = $q->push($input);
+    }
 
     // 5. Generate create hook calls?
     // This would typically be something we want to postpone.
