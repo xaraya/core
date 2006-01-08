6f6721cf10372d81e1d564a899a1ec7bdb439b2c
marcel <marcel@hsdev.com>
2006-01-08 15:36:25 +0000

    Make the whole update for hooks atomic. 	* html/modules/modules/xaradminapi/updatehooks.php: ...

--- html/modules/modules/xaradminapi/updatehooks.php
+++ html/modules/modules/xaradminapi/updatehooks.php
@@ -37,51 +37,56 @@ function modules_adminapi_updatehooks($args)
         throw new ModuleNotFoundException($regid,'Invalid module name found while updating hooks for module with regid #(1)');
     }
 
-    // Delete all entries of modules using this hook (but don't delete the '' module)
-    // signaling there *is* a hook, we want to keep that knowledge in.
-    $sql = "DELETE FROM $xartable[hooks] WHERE xar_tmodid = ? AND xar_smodid <> ?";
-    $dbconn->Execute($sql,array($modinfo['systemid'],0));
-
-    // get the list of all (active) modules
-    $modList = xarModAPIFunc('modules', 'admin', 'getlist');
-
-    // see for which one(s) we need to enable this hook
-    $todo = array();
-    foreach ($modList as $mod) {
-        // Get selected value of hook (which is an array of all the itemtypes selected)
-        // hooked_$mod['name'][0] contains the global setting ( 0 -> not, 1 -> all, 2 -> some)
-        xarVarFetch("hooked_" . $mod['name'],'isset',$ishooked,'',XARVAR_DONT_REUSE);
-        // No setting or explicit NOT, skip it (note: empty shouldn't occur anymore
-        if (!empty($ishooked) && $ishooked[0] != 0) {
-            // There is something in there, either for all itemtypes or for some
-            $todo[$mod['systemid']] = $ishooked;
+    // Make the whole thing atomic
+    try {
+        $dbconn->begin();
+        
+        // Delete all entries of modules using this hook (but don't delete the '' module)
+        // signaling there *is* a hook, we want to keep that knowledge in.
+        $sql = "DELETE FROM $xartable[hooks] WHERE xar_tmodid = ? AND xar_smodid <> ?";
+        $dbconn->Execute($sql,array($modinfo['systemid'],0));
+        
+        // get the list of all (active) modules
+        $modList = xarModAPIFunc('modules', 'admin', 'getlist');
+        
+        // see for which one(s) we need to enable this hook
+        $todo = array();
+        foreach ($modList as $mod) {
+            // Get selected value of hook (which is an array of all the itemtypes selected)
+            // hooked_$mod['name'][0] contains the global setting ( 0 -> not, 1 -> all, 2 -> some)
+            xarVarFetch("hooked_" . $mod['name'],'isset',$ishooked,'',XARVAR_DONT_REUSE);
+            // No setting or explicit NOT, skip it (note: empty shouldn't occur anymore
+            if (!empty($ishooked) && $ishooked[0] != 0) {
+                // There is something in there, either for all itemtypes or for some
+                $todo[$mod['systemid']] = $ishooked;
+            }
         }
-    }
-    // nothing more to do here
-    if (empty($todo)) return true;
-
-    // get the list of individual hooks offered by this module
-    $sql = "SELECT DISTINCT xar_id, xar_smodid, xar_stype, xar_object,
+        // nothing more to do here
+        if (empty($todo)) {
+            $dbconn->commit();
+            return true;
+        }
+        
+        // get the list of individual hooks offered by this module
+        $sql = "SELECT DISTINCT xar_id, xar_smodid, xar_stype, xar_object,
                             xar_action, xar_tarea, xar_tmodid, xar_ttype,
                             xar_tfunc
-            FROM $xartable[hooks]
-            WHERE xar_tmodid = ?";
-    $stmt = $dbconn->prepareStatement($sql);
-    $result = $stmt->executeQuery(array($modinfo['systemid']));
-
-    // Prepare the insert statement outside the loops
-    $sql = "INSERT INTO $xartable[hooks] 
+                FROM $xartable[hooks]
+                WHERE xar_tmodid = ?";
+        $stmt = $dbconn->prepareStatement($sql);
+        $result = $stmt->executeQuery(array($modinfo['systemid']));
+        
+        // Prepare the insert statement outside the loops
+        $sql = "INSERT INTO $xartable[hooks] 
             (xar_id,xar_object,xar_action,xar_smodid,xar_stype,xar_tarea,xar_tmodid,xar_ttype,xar_tfunc)
             VALUES (?,?,?,?,?,?,?,?,?)";
-    $stmt = $dbconn->prepareStatement($sql);
-    while($result->next()) {
-        list($hookid, $hooksmodid, $hookstype, $hookobject, $hookaction,
-             $hooktarea, $hooktmodid, $hookttype, $hooktfunc) = $result->fields;
-
-        // See if this is checked and isn't in the database
-        if (empty($hooksmodid)) {
-            try {
-                $dbconn->begin();
+        $stmt2 = $dbconn->prepareStatement($sql);
+        while($result->next()) {
+            list($hookid, $hooksmodid, $hookstype, $hookobject, $hookaction,
+                 $hooktarea, $hooktmodid, $hookttype, $hooktfunc) = $result->fields;
+            
+            // See if this is checked and isn't in the database
+            if (empty($hooksmodid)) {
                 foreach ($todo as $modId => $hookvalue) {
                     // Insert hook if required
                     xarLogMessage('Value: ' . $hookvalue[0] . ' for ' . $modId);
@@ -93,7 +98,7 @@ function modules_adminapi_updatehooks($args)
                                           $hookobject, $hookaction, $modId,
                                           $itemtype, $hooktarea, $hooktmodid,
                                           $hookttype,$hooktfunc);
-                        $stmt->executeUpdate($bindvars);
+                        $stmt2->executeUpdate($bindvars);
                         // we're done for this module
                         continue;
                     }
@@ -106,17 +111,17 @@ function modules_adminapi_updatehooks($args)
                                           $hookobject, $hookaction, $modId,
                                           $itemtype, $hooktarea, $hooktmodid,
                                           $hookttype,$hooktfunc);
-                        $stmt->executeUpdate($bindvars);
+                        $stmt2->executeUpdate($bindvars);
                     }
                 }
-                $dbconn->commit();
-                $stmt->close();
-            } catch (SQLException $e) {
-                $dbconn->rollback();
-                throw $e;
             }
         }
-    }
+        $dbconn->commit();
+    } catch(Exception $e) {
+        // Catch *any* exception now, split out better, later
+        $dbconn->rollback();
+        throw $e;
+    } 
     $result->close();
 
     return true;
