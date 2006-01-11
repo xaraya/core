86b82b523511779c9e21fae606d0c45ba8e0e4aa
marco.canini <marco.canini@fastwebnet.it>
2006-01-11 08:39:36 +0000

    Small glitch 	* html/index.php: missing </head> in error html

--- html/index.php
+++ html/index.php
@@ -192,7 +192,7 @@ if (!xarMain()) {
             $msg .= '<span style="color: #FF0000;">The following exception is instead the exception caught from the main catch clause (Please note that they could be the same if they were raised inside BL or inside the template):</span>';
             $msg .= '<br /><br />';
             $msg .= $text;
-            echo "<?xml version=\"1.0\"?>\n<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n<head><title>Error</title><body>$msg</body></html>";
+            echo "<?xml version=\"1.0\"?>\n<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n<head><title>Error</title></head><body>$msg</body></html>";
         } else {
             echo $pageOutput;
         }
