3b365e5d5207896a606ac12caffa7f73c862ac3f
mfl <mfl@netspan.ch>
2006-01-15 23:49:42 +0000

    minor corrections

--- html/modules/roles/xaradmin/deleterole.php
+++ html/modules/roles/xaradmin/deleterole.php
@@ -55,7 +55,7 @@ function roles_admin_deleterole()
                      new SystemException($msg));
         return false;
     }
-    if(strtolower($role->getName()) == strtolower(xarModAPIFunc('roles','user','get_defaultgroup'))) {
+    if(strtolower($role->getName()) == strtolower(xarModAPIFunc('roles','user','getdefaultgroup'))) {
         $msg = xarML('The group #(1) is the default group for new users. If you want to remove this group change the roles configuration setting first.', $role->getName());
         xarErrorSet(XAR_USER_EXCEPTION,
                     'CANNOT_CONTINUE',
