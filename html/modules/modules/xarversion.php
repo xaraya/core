e50655f18c762ad7120b7c987e43e191debf41eb
jojodee <jojodee@xaraya.com>
2006-01-26 09:18:26 +0000

    phpdoc file headers and line endings

--- html/modules/modules/xarversion.php
+++ html/modules/modules/xarversion.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * Module initialization
+ * Module initialization 
+ *
  * @package Xaraya eXtensible Management System
  * @copyright (C) 2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
