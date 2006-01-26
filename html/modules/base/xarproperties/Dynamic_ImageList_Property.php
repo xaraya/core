acdeea8ee24239bfd54f4395fa774c0d5fcb6eb4
jojodee <jojodee@xaraya.com>
2006-01-26 14:49:21 +0000

    Update some property templates with latest content, tidy some phpdoc headers

--- html/modules/base/xarproperties/Dynamic_ImageList_Property.php
+++ html/modules/base/xarproperties/Dynamic_ImageList_Property.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Imagelist property
+ *
  * @package Xaraya eXtensible Management System
  * @copyright (C) 2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
