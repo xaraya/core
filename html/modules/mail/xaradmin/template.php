621f51e87263c463e56dae0f844ae3f080538e61
jojodee <jojodee@xaraya.com>
2006-01-28 01:07:19 +0000

    Mail module review, line endings, restore phpdoc header text or spacing

--- html/modules/mail/xaradmin/template.php
+++ html/modules/mail/xaradmin/template.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Modify the email templates for hooked notifications
+ *
  * @package Xaraya eXtensible Management System
  * @copyright (C) 2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
--- html/modules/mail/xaradminapi/replace.php
+++ html/modules/mail/xaradminapi/replace.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Utility function to replace %%calls%%
+ *
  * @package Xaraya eXtensible Management System
  * @copyright (C) 2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
