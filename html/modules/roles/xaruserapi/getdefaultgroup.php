f6729eb2fb760fae8c13649bd11b08ff14fc52d1
jojodee <jojodee@xaraya.com>
2006-02-03 21:48:13 +0000

    Return the default group modvar to roles module, make some changes in xarinit.php for upgrade and activation on install and upgrade of the module, tidy up some blocks and functions to remove traces of the registration, TODO - clean distinction between roles, authsystem and registration further. Still further work here to finalize it

--- html/modules/roles/xaruserapi/getdefaultgroup.php
+++ html/modules/roles/xaruserapi/getdefaultgroup.php
@@ -17,18 +17,10 @@
 function roles_userapi_getdefaultgroup()
 {
 	$authmodule = xarModGetVar('roles','defaultauthmodule');
-    //if (!empty($authmodule)) {
-    if (xarModIsAvailable('registration')) {
-        $registrationgroup= xarModGetVar('registration', 'defaultgroup');
-    }
-    if (isset($registrationgroup)) {
-      /** TODO: jojodee - We really want 'registration' module here not authmodule
-	    * Maybe need to review how the vars are being used now that the modules are
-        * separated out for roles, registration and authentication. For now HARDWIRED to Registration module
-        */
-      //$defaultgroup = xarModGetVar(xarModGetNameFromID($authmodule), 'defaultgroup');
-        $defaultgroup = $registrationgroup;
-    
+    $defaultrole = xarModGetVar('roles', 'defaultgroup');
+    if (isset($defaultrole) && !empty($defaultrole)) {
+        $defaultgroup = $defaultrole;
+
     } else {
 	// TODO: improve on this hardwiring
 		$defaultgroup = 'Users';
