15a8cd8fdddfcf29bef4eaf0cf3b928a74f3aad9
johnny <johnny@localmomentum.net>
2006-02-08 23:19:12 +0000

    from merge: adding back the dd install and make it use exceptions 	* html/modules/installer/xaradmin.php:

--- html/modules/installer/xaradmin.php
+++ html/modules/installer/xaradmin.php
@@ -436,6 +436,20 @@ function installer_admin_bootstrap()
     // load modules into *_modules table
     if (!xarModAPIFunc('modules', 'admin', 'regenerate')) throw new Exception("regenerating module list failed");//return;
 
+     // Initialise and activate dynamic data
+    $modlist = array('dynamicdata');
+    foreach ($modlist as $mod) {
+        // Initialise the module
+        $regid = xarModGetIDFromName($mod);
+        if (isset($regid)) {
+            if (!xarModAPIFunc('modules', 'admin', 'initialise', array('regid' => $regid)))
+                 throw new Exception("Initalising module with regid : $regid failed");
+            // Activate the module
+            if (!xarModAPIFunc('modules', 'admin', 'activate', array('regid' => $regid)))
+                throw new Exception("Activating module with regid: $regid failed");
+        }
+    }
+
     // create the default roles and privileges setup
     include 'modules/privileges/xarsetup.php';
     initializeSetup();
@@ -477,17 +491,13 @@ function installer_admin_bootstrap()
         }
     }
 
-    // Initialise and activate mail, dynamic data
-    $modlist = array('mail');
-    foreach ($modlist as $mod) {
-        // Initialise the module
-        $regid = xarModGetIDFromName($mod);
-        if (!xarModAPIFunc('modules', 'admin', 'initialise', array('regid' => $regid))) 
-            throw new Exception("Initalising module with regid : $regid failed");
-        // Activate the module
-        if (!xarModAPIFunc('modules', 'admin', 'activate', array('regid' => $regid))) 
-            throw new Exception("Activating module with regid: $regid failed");
-    }
+    // Initialise and activate mail
+    $regid = xarModGetIDFromName('mail');
+    if (!xarModAPIFunc('modules', 'admin', 'initialise', array('regid' => $regid))) 
+         throw new Exception("Initalising m with regid : $regid failed");
+    // Activate the module
+    if (!xarModAPIFunc('modules', 'admin', 'activate', array('regid' => $regid))) 
+         throw new Exception("Activating module with regid: $regid failed");
 
     //initialise and activate base module by setting the states
     $baseId = xarModGetIDFromName('base');
