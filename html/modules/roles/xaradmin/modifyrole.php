e515ac6da0bcf70231fcc9642d60dbfac22a14c3
mfl <mfl@netspan.ch>
2006-02-17 21:44:48 +0000

    change security check for frozen roles

--- html/modules/roles/xaradmin/modifyrole.php
+++ html/modules/roles/xaradmin/modifyrole.php
@@ -62,7 +62,7 @@ function roles_admin_modifyrole()
     $data['pname'] = $name;
 
 // Security Check
-    if (!xarSecurityCheck('EditRole',1,'Roles',$name)) return;
+//    if (!xarSecurityCheck('EditRole',1,'Roles',$name)) return;
     $data['frozen'] = !xarSecurityCheck('EditRole',0,'Roles',$name);
 
     if (isset($type)) {
