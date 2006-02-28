f7e0955a80accd93ef53199a53ca02f035f40403
marcel <marcel@hsdev.com>
2006-02-28 09:46:43 +0000

    Doing the showinput inheritanc dance for roles props. Postponed the ones which inherit from base, which johnny is doing, except for urlicon
    	* html/modules/base/xarproperties/Dynamic_TextArea_Property.php: ...
    	* html/modules/base/xarproperties/Dynamic_URLIcon_Property.php: ...
    	* html/modules/roles/xarproperties/Dynamic_AIM_Property.php: ...
    	* html/modules/roles/xarproperties/Dynamic_Affero_Property.php: ...
    	* html/modules/roles/xarproperties/Dynamic_Email_Property.php: ...
    	* html/modules/roles/xarproperties/Dynamic_ICQ_Property.php: ...
    	* html/modules/roles/xarproperties/Dynamic_MSN_Property.php: ...
    	* html/modules/roles/xarproperties/Dynamic_PassBox_Property.php: ...
    	* html/modules/roles/xarproperties/Dynamic_Username_Property.php: ...
    	* html/modules/roles/xarproperties/Dynamic_Yahoo_Property.php: ...

--- html/modules/roles/xarproperties/Dynamic_AIM_Property.php
+++ html/modules/roles/xarproperties/Dynamic_AIM_Property.php
@@ -63,44 +63,15 @@ class Dynamic_AIM_Property extends Dynamic_URLIcon_Property
         return true;
     }
 
-    function showInput($args = array())
+    function showInput($data = array())
     {
-        extract($args);
-        if (!isset($value)) {
-            $value = $this->value;
-        }
-        $data = array();
+        if(!isset($data['value'])) $data['value'] = $this->value;
 
-        if (!empty($value)) {
-            $link = 'aim:goim?screenname='.$value.'&message='.xarML('Hello+Are+you+there?');
-        } else {
-            $link = '';
+        $data['link'] ='';
+        if(!empty($data['value'])) {
+            $data['link'] = 'aim:goim?screenname='.$data['value'].'&message='.xarML('Hello+Are+you+there?');
         }
-        if (empty($name)) {
-            $name = 'dd_' . $this->id;
-        }
-        if (empty($id)) {
-            $id = $name;
-        }
-
-        $data['name']     = $name;
-        $data['id']       = $id;
-        $data['value']    = isset($value) ? xarVarPrepForDisplay($value) : xarVarPrepForDisplay($this->value);
-        $data['tabindex'] = !empty($tabindex) ? ' tabindex="'.$tabindex.'"': '';
-        $data['invalid']  = !empty($this->invalid) ? xarML('Invalid #(1)', $this->invalid) :'';
-        $data['maxlength']= !empty($maxlength) ? $maxlength : $this->maxlength;
-        $data['size']     = !empty($size) ? $size : $this->size;
-        $data['link']     = xarVarPrepForDisplay($link);
-
-        if (empty($module)) {
-            $module = $this->getModule();
-        }
-        if (empty($template)) {
-            $template = $this->getTemplate();
-        }
-
-        return xarTplProperty($module, $template, 'showinput', $data);
-
+        return parent::showInput($data);
     }
 
     function showOutput($args = array())
