9ec272396243577707e2928dcbefdbff73eff2c1
jojodee <jojodee@xaraya.com>
2006-03-24 09:39:04 +0000

    Some header consistency for files

--- html/modules/authsystem/xaruserapi/authenticate_user.php
+++ html/modules/authsystem/xaruserapi/authenticate_user.php
@@ -2,12 +2,13 @@
 /**
  * Authenticate a user
  *
- * @package Xaraya eXtensible Management System
- * @copyright (C) 2005 The Digital Development Foundation
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
- * @subpackage authsystem
+ * @subpackage Authsystem module
+ * @link http://xaraya.com/index.php/release/42.html
  */
 /**
  * authenticate a user
--- html/modules/authsystem/xaruserapi/has_capability.php
+++ html/modules/authsystem/xaruserapi/has_capability.php
@@ -2,12 +2,13 @@
 /**
  * Check for module capability
  *
- * @package Xaraya eXtensible Management System
- * @copyright (C) 2005 The Digital Development Foundation
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
- * @subpackage authsystem
+ * @subpackage Authsystem module
+ * @link http://xaraya.com/index.php/release/42.html
  */
 /**
  * check whether this module has a certain capability
