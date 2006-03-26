3d16807611067b1cb8ff5c65f645d3dadefbe2a3
curtis <curtis@farnham.com>
2006-03-26 21:24:05 +0000

    Make security check in Roles adminmenu use DeleteRole mask, not DeleteExample

--- html/modules/modules/xaradminapi/activate.php
+++ html/modules/modules/xaradminapi/activate.php
@@ -41,10 +41,15 @@ function modules_adminapi_activate ($args)
                            'executeinitfunction',
                            array('regid'    => $regid,
                                  'function' => 'activate'))) {
-        $msg = xarML('Unable to execute "activate" function in the xarinit.php file of module (#(1))', $modInfo['displayname']);
+        // INSERT STARTS
+        return;
+        // INSERT ENDS
+        $msg = xarML('Unable to execute "activate" function in the xarinit.php file of module (#(1))',
+$modInfo['displayname']);
         xarErrorSet(XAR_SYSTEM_EXCEPTION, 'BAD_PARAM', new SystemException($msg));
         return;
     }
+
 
     // Update state of module
     $res = xarModAPIFunc('modules',
