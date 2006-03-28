ffa5b11afffa06971fda174c2abdce7bd83a74e6
curtis <curtis@farnham.com>
2006-03-28 04:05:58 +0000

    Finish moving "Icons" style to separate checkbox
    	* html/modules/themes/xaradmin/settings.php: ...
    	* html/modules/themes/xartemplates/admin-list.xd: ...

--- html/modules/themes/xaradmin/settings.php
+++ html/modules/themes/xaradmin/settings.php
@@ -19,18 +19,20 @@ function themes_admin_settings()
 {
     // Security Check
     if(!xarSecurityCheck('AdminTheme')) return;
-    
+
     // form parameters
-    if (!xarVarFetch('hidecore',  'str:1:', $hidecore,  '0',                  XARVAR_NOT_REQUIRED)) return; 
+    if (!xarVarFetch('hidecore',  'str:1:', $hidecore,  '0',                  XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('selstyle',  'str:1:', $selstyle,  'plain',              XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('selfilter', 'str:1:', $selfilter, 'XARTHEME_STATE_ANY', XARVAR_NOT_REQUIRED)) return; 
+    if (!xarVarFetch('selfilter', 'str:1:', $selfilter, 'XARTHEME_STATE_ANY', XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('selsort',   'str:1:', $selsort,   'namedesc',           XARVAR_NOT_REQUIRED)) return;
     if (!xarVarFetch('regen',     'str:1:', $regen,     false,                XARVAR_NOT_REQUIRED)) return;
-    
+    if (!xarVarFetch('useicons',  'checkbox', $useicons, 'XARTHEME_STATE_ANY', XARVAR_NOT_REQUIRED)) return;
+
     if (!xarModSetUserVar('themes', 'hidecore', $hidecore)) return;
     if (!xarModSetUserVar('themes', 'selstyle', $selstyle)) return;
     if (!xarModSetUserVar('themes', 'selfilter', $selfilter)) return;
     if (!xarModSetUserVar('themes', 'selsort', $selsort)) return;
+    if (!xarModSetUserVar('themes', 'useicons', $useicons)) return;
 
     xarResponseRedirect(xarModURL('themes', 'admin', 'list', array('regen' => $regen = 1)));
 }
