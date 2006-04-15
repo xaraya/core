a92c2ae49f2c461f1dca402189be9cd169958225
marcel <marcel@hsdev.com>
2006-04-15 06:22:25 +0000

    Cherrypicking the xmlhttp POST support from com.xaraya.core 	* html/modules/base/xartemplates/includes/xmlhttprequest.js: ...

--- html/modules/base/xartemplates/includes/xmlhttprequest.js
+++ html/modules/base/xartemplates/includes/xmlhttprequest.js
@@ -1,52 +1,114 @@
-// TODO: some docs
-// Global vars for the request object, what we should add to the url and which
-// id is in the tag, which subtree we should replace with the result of the
-// request.
-// Copyright 2004 Marcel van der Boom <marcel@hsdev.com>
-// License: GPL <http://www.gnu.org/licenses/gpl.html>
+/**
+ * Xaraya XML HTTP Requests
+ *
+ * @package modules
+ * @copyright (C) 2004-2006 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Base module
+ * @link http://xaraya.com/index.php/release/68.html
+ * @author Marcel van der Boom <marcel@hsdev.com>
+ */
 
 // TODO: can this global be eliminated?
 var req;
 var tagToGo; // Apparently at least safari needs this
+var debug = 0;
 
-function loadContent(url,tagid) {
-    // TODO: this doesnt belong here
-    var postfix = "";
-    // Prevent propagating postfixes
-    if(url.search(/\&pageName\=module/) == -1) {
-        postfix = "&pageName=module";
+
+/**
+* Load Content
+*
+* Perform XML HTTP request
+*
+* @author  Marcel van der Boom <marcel@hsdev.com>
+* @access  public
+* @param   string url The URL to request
+* @param   string tagid The ID of the form element to put the .innerHTML into
+* @param   string method (Optional) 'GET' or 'POST' (default=GET)
+* @param   object formobj (Optional) Form object reference (Only needed if method=POST)
+* @return  boolean local_debug (Optional) Whether to include debug output (default=false)
+* @throws  no exceptions
+*/
+function loadContent(url, tagid, method, formobj, local_debug)
+{
+    // validate inputs and set defaults
+    if (local_debug != null) var debug = local_debug
+    if (method == null || method == '') method = 'GET'
+    method = method.toUpperCase();
+    var argstr = null;
+    var join = '';
+
+    // prepare strings according to method
+    if (method == 'POST') {
+        try {
+            argstr = '';
+            for ( i = 0; i < formobj.elements.length; i++ ) {
+                if (formobj.elements[i].name.length > 0) {
+                    argstr = argstr + join + formobj.elements[i].name + "="+formobj.elements[i].value;
+                    join = '&';
+                }
+            }
+        } catch(e) {
+            alert(e);
+            return false;
+        }
+        if (argstr.search(/\&pageName\=module/) == -1) argstr=argstr+"&pageName=module";
+    } else if (method == 'GET') {
+        if (url.search(/\&pageName\=module/) == -1) url=url+"&pageName=module";
     }
-    //alert('URL: ' + url + postfix +', TagId: ' + tagid);
-    // Make the tag id global in this scope, required for some implementations
-    tagToGo = tagid;
+    if (debug) alert("Method: "+method+"\nURL: "+url+"\nArgs: "+argstr);
+
+    tagToGo = tagid; // required for some implementations
     try {
         if (window.XMLHttpRequest) {
             document.body.style.cursor='wait';
             req = new XMLHttpRequest();
             req.onreadystatechange = processReqChange;
-            req.open("GET", url + postfix, true);
-            req.send(null);
+            req.open(method, url, true);
+            if (method == 'POST') {
+                req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                req.setRequestHeader("Content-length", argstr.length);
+                req.setRequestHeader("Connection", "close");
+            }
+            req.send(argstr);
             return false;
         } else if (window.ActiveXObject) {
             document.body.style.cursor='wait';
             req = new ActiveXObject("Microsoft.XMLHTTP");
             if (req) {
                 req.onreadystatechange = processReqChange;
-                req.open("GET", url + postfix, true);
-                req.send();
+                req.open(method, url, true);
+                if (method == 'POST') {
+                    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                    req.setRequestHeader("Content-length", argstr.length);
+                    req.setRequestHeader("Connection", "close");
+                }
+                req.send(argstr);
             }
             return false;
         } else {
-            return true; // do the normal action
-        }  
+            return true; // do the normal action -- should we have an "incompatible browser" error here?
+        }
     } catch(e) {
-        alert("CATCH: " + e);
+        alert(e);
+        return false
     }
 }
 
-// Handler called on state changes of the request object
+/**
+* Process request
+*
+* Handler called on state changes of the request object
+*
+* @author  Marcel van der Boom <marcel@hsdev.com>
+* @access  public
+* @return  nothing
+* @throws  no exceptions
+*/
 function processReqChange()
-{   
+{
     try {
         if (req.readyState == 4) {
             if (req.status == 200) {
@@ -54,13 +116,14 @@ function processReqChange()
                 tag = document.getElementById(tagToGo);
                 if(tag == null) {
                     // not found, fallback
-                    document.location = req.url;
+                    document.location = req.url; // TODO: take out 'pageName' var here
                     return true;
                 }
                 // Make sure we replace the right tag, and dont leave anything behind
                 myparent = tag.parentNode;
                 tag.id ='dummytogetridoftheoriginal';
                 tag.innerHTML = req.responseText;
+                if (debug) alert(req.responseText);
                 newtag = document.getElementById(tagToGo);
                 if(newtag == null) {
                     alert('cant find the new tag [' + tagToGo + ']');
