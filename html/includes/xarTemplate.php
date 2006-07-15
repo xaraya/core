86d95ab70a85411f94c3dcd8ea8d37da16c5ed2d
jojodee <jojodee@xaraya.com>
2006-07-15 11:17:30 +0000

    Remove duplicated code, again, from the xarTemmplate xarTpl__executeFromFile function. This was done previously, not sure how it got back in. The other binary string compare code at the start of this function needs to be reviewed as well. Taking the code duplication with the reference to this new binary compared file for now.

--- html/includes/xarTemplate.php
+++ html/includes/xarTemplate.php
@@ -1129,29 +1129,13 @@ function xarTpl__executeFromFile($sourceFileName, $tplData)
     for ($i = 2; $i<($tplPathCount-1); $i++) array_push($stack, $tplpath[$i]);
     $ctxType = $tplpath[0].':'.implode("/", $stack);
     $ctxName = substr($tplpath[$tplPathCount - 1], 0, -3);
+    /* This $dnType check is a workaround for non-standard templates like we need in workflows etc. */
     /* Temporary partial fix for Bug 5156. This is a temporary workaround and
        while here, themes cannot be translated. This should be fixed as soon as possible */
     if(isset($dnType)) {
         if (xarMLS_loadTranslations($dnType, $dnName, $ctxType, $ctxName) === NULL) return;
     }
-    // Load translations for the template
-    $tplpath = explode("/", $sourceFileName);
-    $tplPathCount = count($tplpath);
-    switch ($tplpath[0]) {
-        case 'modules': $dnType = XARMLS_DNTYPE_MODULE; break;
-        case 'themes':  $dnType = XARMLS_DNTYPE_THEME; break;
-    }
-    $dnName = $tplpath[1];
-    $stack = array();
-    if ($tplpath[2] == 'xartemplates') $tplpath[2] = 'templates';
-    for ($i = 2; $i<($tplPathCount-1); $i++) array_push($stack, $tplpath[$i]);
-    $ctxType = $tplpath[0].':'.implode("/", $stack);
-    $ctxName = substr($tplpath[$tplPathCount - 1], 0, -3);
-    /* This $dnType check is a workaround for non-standard templates like we need in workflows etc. */
-    if(isset($dnType)) {
-        if (xarMLS_loadTranslations($dnType, $dnName, $ctxType, $ctxName) === NULL) return;
-    }
-
+ 
     $needCompilation = true;
     $cachedFileName = null;
     if ($GLOBALS['xarTpl_cacheTemplates']) {
