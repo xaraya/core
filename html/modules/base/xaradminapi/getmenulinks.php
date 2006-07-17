c08b15ccb00966417735a4db386dbc1e68c96fba
marcel <marcel@hsdev.com>
2006-07-17 18:25:56 +0000

    Thanks to marc for giving me test material on the QA check (tabs and braces) 	* html/modules/authsystem/xaradminapi/getmenulinks.php: ... 	* html/modules/authsystem/xardataapi/adminmenu.php: ... 	* html/modules/base/xaradminapi/getmenulinks.php: ... 	* html/modules/base/xaradminapi/menuarray.php: ... 	* html/modules/base/xardataapi/adminmenu.php: ... 	* html/modules/blocks/xaradminapi/getmenulinks.php: ... 	* html/modules/blocks/xardataapi/adminmenu.php: ... 	* html/modules/dynamicdata/xaradminapi/getmenulinks.php: ... 	* html/modules/dynamicdata/xardataapi/adminmenu.php: ... 	* html/modules/mail/xaradminapi/getmenulinks.php: ... 	* html/modules/mail/xardataapi/adminmenu.php: ... 	* html/modules/modules/xaradminapi/getmenulinks.php: ... 	* html/modules/modules/xardataapi/adminmenu.php: ... 	* html/modules/privileges/xaradminapi/getmenulinks.php: ... 	* html/modules/privileges/xardataapi/adminmenu.php: ... 	* html/modules/roles/xaradminapi/getmenulinks.php: ... 	* html/modules/roles/xardataapi/adminmenu.php: ... 	* html/modules/themes/xaradminapi/getmenulinks.php: ... 	* html/modules/themes/xardataapi/adminmenu.php: ...

--- html/modules/base/xaradminapi/getmenulinks.php
+++ html/modules/base/xaradminapi/getmenulinks.php
@@ -17,10 +17,10 @@
  */
 function base_adminapi_getmenulinks()
 {
-	/*
-	This menu gets its data from the adminmenu.php file in the module's xardataapi folder.
-	You can add or change menu items by changing the data there.
-	Or you can create your own menu items here. They should have the form of this example:
+    /*
+    This menu gets its data from the adminmenu.php file in the module's xardataapi folder.
+    You can add or change menu items by changing the data there.
+    Or you can create your own menu items here. They should have the form of this example:
 
     $menulinks = array();
     .....
