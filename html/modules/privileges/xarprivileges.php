1c557f9ff57658ecacb039d0b31daaf42e1afb71
jojodee <jojodee@xaraya.com>
2006-07-21 18:08:38 +0000

    A few file header updates while here Add support for domains as well as subdomains in realms

--- html/modules/privileges/xarprivileges.php
+++ html/modules/privileges/xarprivileges.php
@@ -2,12 +2,13 @@
 /**
  * Privileges administration API
  *
- * @package Xaraya eXtensible Management System
- * @copyright (C) 2005 The Digital Development Foundation
+ * @package modules
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
  * @subpackage Privileges module
+ * @link http://xaraya.com/index.php/release/1098.html 
  */
 
 /*
@@ -418,7 +419,7 @@ class xarMasks
             case "domain":
                 $host = xarServerGetHost();
                 $parts = explode('.',$host);
-                if (count($parts) < 3) {
+                if (count($parts) < 2) {
                     $mask->setRealm('All');
                 } else {
                     $mask->setRealm($parts[0]);
