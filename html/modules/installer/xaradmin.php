fbbe2056eb2cfd08aab6437c6b95e33d5ebc7b1d
michelv <michelv@xarayahosting.nl>
2006-07-21 13:32:20 +0000

    Extend warning on invalid characters in data for the db connection. Slightly related to bug 5825

--- html/modules/installer/xaradmin.php
+++ html/modules/installer/xaradmin.php
@@ -279,7 +279,7 @@ function installer_admin_phase5()
 
     // allow only a-z 0-9 and _ in table prefix
     if (!preg_match('/^\w*$/',$dbPrefix)) {
-        $msg = xarML('Invalid character in table prefix');
+        $msg = xarML('Invalid character in table prefix. Only use a-z, a _ and/or 0-9 in the prefix.');
         xarCore_die($msg);
         return;
     }
