592c168c526bf6d2dab5b22a23592b72ebc231b8
michelv <michelv@xarayahosting.nl>
2006-07-28 14:50:55 +0000

    Clean up some more headers What to do with all the deprecated functions in xarCache? @throws added

--- html/includes/xarUser.php
+++ html/includes/xarUser.php
@@ -2,10 +2,12 @@
 /**
  * User System
  *
- * @package user
+ * @package core
  * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
+ *
+ * @subpackage user
  * @author Jim McDonald
  * @todo <marco> user status field
  * @todo <johnny> look over xarUserComparePasswords
