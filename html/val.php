7f52cdaf9cd56bb0b2d9bd05a062a88b840aa42b
jojodee <jojodee@xaraya.com>
2006-08-01 04:11:33 +0000

    Just a little formatting for function code while i am here

--- html/val.php
+++ html/val.php
@@ -21,14 +21,10 @@ xarCoreInit(XARCORE_SYSTEM_ALL);
 if (!xarVarFetch('v', 'str:1', $v)) return;
 if (!xarVarFetch('u', 'str:1', $u)) return;
 
-$user = xarModAPIFunc('roles',
-                      'user',
-                      'get',
+$user = xarModAPIFunc('roles', 'user', 'get',
                        array('uid' => $u));
 
-xarResponseRedirect(xarModURL('roles',
-                              'user',
-                              'getvalidation',
+xarResponseRedirect(xarModURL('roles', 'user', 'getvalidation',
                               array('stage'   => 'getvalidate',
                                     'valcode' => $v,
                                     'uname'   => $user['uname'],
