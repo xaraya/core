dd79c43ec422d74dc74b6dc71a9f910fe68e2dae
ckraeft <ckraeft@xaraya.com>
2006-08-07 23:01:34 +0000

    Fix redone - merged with head - for xmlhttprequest.js for handling both POSTs and GETs

--- html/modules/base/xartemplates/includes/xmlhttprequest.js
+++ html/modules/base/xartemplates/includes/xmlhttprequest.js
@@ -37,37 +37,47 @@ function loadContent(url, tagid, method, form_obj, local_debug)
     if (local_debug != null) var debug = local_debug
     if (method == null || method == '') method = 'GET'
     method = method.toUpperCase();
-    var argstr = null;
+    var argstr = '';
     var join = '';
+    var pagepat = /\&pageName\=module/;
+    var postfix ='&pageName=module';
     
+    /**
+     * @todo the url handling needs to be documented, a bit strange to do this here
+     */
     url = url.replace(/&amp;/g,'&');
 
     // prepare strings according to method
     if (method == 'POST') {
         if (form_obj != null) formobj = document.getElementById(form_obj);
         try {
-            argstr = '';
             for ( i = 0; i < formobj.elements.length; i++ ) {
                 if (formobj.elements[i].name.length > 0) {
-                    argstr = argstr + join + formobj.elements[i].name + "="+formobj.elements[i].value;
+                    argstr = argstr + join + formobj.elements[i].name + "=" + formobj.elements[i].value;
                     join = '&';
                 }
             }
         } catch(e) {
-            alert(e);
+            if (debug) alert(e);
             return false;
         }
-        if (argstr.search(/\&pageName\=module/) == -1) argstr=argstr+"&pageName=module";
+        if (argstr.search(pagepat) == -1) argstr = argstr + postfix;
     } else if (method == 'GET') {
-        if (url.search(/\&pageName\=module/) == -1) url=url+"&pageName=module";
+        if (url.search(pagepat) == -1) url = url + postfix;
     }
-    if (debug) alert("Method: "+method+"\nURL: "+url+"\nArgs: "+argstr);
+    if (debug) alert("Method: " + method + "\nURL: " + url + "\nArgs: " + argstr);
 
     tagToGo = tagid; // required for some implementations
     try {
         if (window.XMLHttpRequest) {
-            document.body.style.cursor='wait';
             req = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            req = new ActiveXObject("Microsoft.XMLHTTP");
+        } else {
+            return true; // do the normal action -- should we have an "incompatible browser" error here?
+        }
+        if (req) {
+            document.body.style.cursor = 'wait';
             req.onreadystatechange = processReqChange;
             req.open(method, url, true);
             if (method == 'POST') {
@@ -78,26 +88,9 @@ function loadContent(url, tagid, method, form_obj, local_debug)
             req.send(argstr);
             if(method != 'POST')
                 return false;
-        } else if (window.ActiveXObject) {
-            document.body.style.cursor='wait';
-            req = new ActiveXObject("Microsoft.XMLHTTP");
-            if (req) {
-                req.onreadystatechange = processReqChange;
-                req.open(method, url, true);
-                if (method == 'POST') {
-                    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                    req.setRequestHeader("Content-length", argstr.length);
-                    req.setRequestHeader("Connection", "close");
-                }
-                req.send(argstr);
-            }
-            if(method != 'POST')
-                return false;
-        } else {
-            return true; // do the normal action -- should we have an "incompatible browser" error here?
         }
     } catch(e) {
-        alert(e);
+        if(debug) alert(e);
         return false
     }
 }
@@ -109,8 +102,7 @@ function loadContent(url, tagid, method, form_obj, local_debug)
 *
 * @author  Marcel van der Boom <marcel@hsdev.com>
 * @access  public
-* @return  nothing
-* @throws  no exceptions
+* @return  void
 */
 function processReqChange()
 {
@@ -121,6 +113,7 @@ function processReqChange()
                 tag = document.getElementById(tagToGo);
                 if(tag == null) {
                     // not found, fallback
+                    document.body.style.cursor='default'; // set back to be sure
                     document.location = req.url; // TODO: take out 'pageName' var here
                     return true;
                 }
@@ -134,14 +127,12 @@ function processReqChange()
                 if(newtag == null) {
                     if (debug) alert('cant find the new tag [' + tagToGo + ']');
                     tag.id = tagToGo;
-                    document.body.style.cursor='default';
-                    return false;
                 } else {
                     copyofnew = newtag.cloneNode(true);
                     myparent.replaceChild(copyofnew,tag);
-                    document.body.style.cursor='default';
-                    return false; // cancel the normal action
                 }
+                document.body.style.cursor='default';
+                return false; // cancel the normal action
             } else {
                 // CHECKME: as we are in a looping thingie, displaying an alert here
                 // might be premature, but if it really fails how would we know? Just
