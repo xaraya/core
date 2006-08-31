e8b40fffd3f0563e329af7c9a1720c99d751e51f
marcel <marcel@hsdev.com>
2006-08-31 19:03:23 +0000

    Round it off with some formatting
    	* html/modules/dynamicdata/class/interface.php: ...
    	* html/modules/dynamicdata/class/properties.php: ...

--- html/modules/dynamicdata/class/properties.php
+++ html/modules/dynamicdata/class/properties.php
@@ -12,6 +12,13 @@
  * @author mikespub <mikespub@xaraya.com>
  */
 
+ /*
+     As this file was very long in the 1.x series i've split it up into 3 parts
+     in such a way that is not the nicest in coding practices, but makes merges
+     if we receive changes from upwards easy to do (i.e. reject all)
+     Over time this will probably change. For now i just made sure that everyone
+     can still reach the same stuff as before.
+ */
 sys::import('modules.dynamicdata.class.properties.master');
 sys::import('modules.dynamicdata.class.properties.base');
 sys::import('modules.dynamicdata.class.properties.registration');
