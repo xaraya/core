eb6f36357b11446ce0ee8dd180a74a190e14d64b
jojodee <jojodee@xaraya.com>
2006-09-23 12:39:30 +0000

    Bug 5723 - no District of Columbia in state list, take out strange white space in the ID values, add todo to fix this with proper Id, possibly abbreviation rather than repeat state names

--- html/modules/base/xarproperties/Dynamic_StateList_Property.php
+++ html/modules/base/xarproperties/Dynamic_StateList_Property.php
@@ -25,7 +25,6 @@ include_once "modules/base/xarproperties/Dynamic_Select_Property.php";
  */
 class Dynamic_StateList_Property extends Dynamic_Select_Property
 {
-
     function Dynamic_StateList_Property($args)
     {
         $this->Dynamic_Select_Property($args);
@@ -50,7 +49,6 @@ class Dynamic_StateList_Property extends Dynamic_Select_Property
         return true;
     }
 
-//    function showInput($name = '', $value = null, $options = array(), $id = '', $tabindex = '')
     function showInput($args = array())
     {
         extract($args);
@@ -68,13 +66,7 @@ class Dynamic_StateList_Property extends Dynamic_Select_Property
         $data['name']  = $name;
         $data['id']    = $id;
 
-       /*
-        $out = '<select' .
-       ' name="' . $name . '"' .
-       ' id="'. $id . '"' .
-       (!empty($tabindex) ? ' tabindex="'.$tabindex.'" ' : '') .
-       '>';
-       */
+       //todo - this should be fixed with id as an abbreviation and update template accordingly
         $soptions = array();
         $soptions[] = array('id' =>'Please select', 'name' =>'Please select' );
         $soptions[] = array('id' =>'Alabama', 'name' =>'Alabama');
@@ -85,83 +77,71 @@ class Dynamic_StateList_Property extends Dynamic_Select_Property
         $soptions[] = array('id' =>'Colorado', 'name' =>'Colorado');
         $soptions[] = array('id' =>'Connecticut', 'name' =>'Connecticut');
         $soptions[] = array('id' =>'Delaware', 'name' =>'Delaware');
+        $soptions[] = array('id' =>'District of Columbia', 'name' =>'District of Columbia');
         $soptions[] = array('id' =>'Florida', 'name' =>'Florida');
         $soptions[] = array('id' =>'Georgia', 'name' =>'Georgia');
         $soptions[] = array('id' =>'Hawaii', 'name' =>'Hawaii');
         $soptions[] = array('id' =>'Idaho', 'name' =>'Idaho');
         $soptions[] = array('id' =>'Illinois', 'name' =>'Illinois');
         $soptions[] = array('id' =>'Indiana', 'name' =>'Indiana');
-        $soptions[] = array('id' =>'Iowa     ', 'name' =>'Iowa');
-        $soptions[] = array('id' =>'Kansas     ', 'name' =>'Kansas');
-        $soptions[] = array('id' =>'Kentucky     ', 'name' =>'Kentucky');
-        $soptions[] = array('id' =>'Louisiana     ', 'name' =>'Louisiana');
-        $soptions[] = array('id' =>'Maine     ', 'name' =>'Maine');
-        $soptions[] = array('id' =>'Maryland     ', 'name' =>'Maryland');
-        $soptions[] = array('id' =>'Massachusetts     ', 'name' =>'Massachusetts');
-        $soptions[] = array('id' =>'Michigan     ', 'name' =>'Michigan');
-        $soptions[] = array('id' =>'Minnesota     ', 'name' =>'Minnesota');
-        $soptions[] = array('id' =>'Mississippi     ', 'name' =>'Mississippi');
-        $soptions[] = array('id' =>'Missouri     ', 'name' =>'Missouri');
-        $soptions[] = array('id' =>'Montana     ', 'name' =>'Montana');
-        $soptions[] = array('id' =>'Nebraska     ', 'name' =>'Nebraska');
-        $soptions[] = array('id' =>'Nevada     ', 'name' =>'Nevada');
-        $soptions[] = array('id' =>'New Hampshire     ', 'name' =>'New Hampshire');
-        $soptions[] = array('id' =>'New Jersey     ', 'name' =>'New Jersey');
-        $soptions[] = array('id' =>'New Mexico     ', 'name' =>'New Mexico');
-        $soptions[] = array('id' =>'New York     ', 'name' =>'New York');
-        $soptions[] = array('id' =>'North Carolina     ', 'name' =>'North Carolina');
-        $soptions[] = array('id' =>'North Dakota     ', 'name' =>'North Dakota');
-        $soptions[] = array('id' =>'Ohio     ', 'name' =>'Ohio');
-        $soptions[] = array('id' =>'Oklahoma     ', 'name' =>'Oklahoma');
-        $soptions[] = array('id' =>'Oregon     ', 'name' =>'Oregon');
-        $soptions[] = array('id' =>'Pennsylvania     ', 'name' =>'Pennsylvania');
-        $soptions[] = array('id' =>'Rhode Island     ', 'name' =>'Rhode Island');
-        $soptions[] = array('id' =>'South Carolina     ', 'name' =>'South Carolina');
-        $soptions[] = array('id' =>'South Dakota     ', 'name' =>'South Dakota');
-        $soptions[] = array('id' =>'Tennessee     ', 'name' =>'Tennessee');
-        $soptions[] = array('id' =>'Texas     ', 'name' =>'Texas');
-        $soptions[] = array('id' =>'Utah     ', 'name' =>'Utah');
-        $soptions[] = array('id' =>'Vermont     ', 'name' =>'Vermont');
-        $soptions[] = array('id' =>'Virginia     ', 'name' =>'Virginia');
-        $soptions[] = array('id' =>'Washington     ', 'name' =>'Washington');
-        $soptions[] = array('id' =>'West Virginia     ', 'name' =>'West Virginia');
-        $soptions[] = array('id' =>'Wisconsin     ', 'name' =>'Wisconsin');
-        $soptions[] = array('id' =>'Wyoming     ', 'name' =>'Wyoming');
-        $soptions[] = array('id' =>'Alberta     ', 'name' =>'Alberta');
-        $soptions[] = array('id' =>'British Columbia     ', 'name' =>'British Columbia');
-        $soptions[] = array('id' =>'Manitoba     ', 'name' =>'Manitoba');
-        $soptions[] = array('id' =>'New Brunswick     ', 'name' =>'New Brunswick');
+        $soptions[] = array('id' =>'Iowa', 'name' =>'Iowa');
+        $soptions[] = array('id' =>'Kansas', 'name' =>'Kansas');
+        $soptions[] = array('id' =>'Kentucky', 'name' =>'Kentucky');
+        $soptions[] = array('id' =>'Louisiana', 'name' =>'Louisiana');
+        $soptions[] = array('id' =>'Maine', 'name' =>'Maine');
+        $soptions[] = array('id' =>'Maryland', 'name' =>'Maryland');
+        $soptions[] = array('id' =>'Massachusetts', 'name' =>'Massachusetts');
+        $soptions[] = array('id' =>'Michigan', 'name' =>'Michigan');
+        $soptions[] = array('id' =>'Minnesota', 'name' =>'Minnesota');
+        $soptions[] = array('id' =>'Mississippi', 'name' =>'Mississippi');
+        $soptions[] = array('id' =>'Missouri', 'name' =>'Missouri');
+        $soptions[] = array('id' =>'Montana', 'name' =>'Montana');
+        $soptions[] = array('id' =>'Nebraska', 'name' =>'Nebraska');
+        $soptions[] = array('id' =>'Nevada', 'name' =>'Nevada');
+        $soptions[] = array('id' =>'New Hampshire', 'name' =>'New Hampshire');
+        $soptions[] = array('id' =>'New Jersey', 'name' =>'New Jersey');
+        $soptions[] = array('id' =>'New Mexico', 'name' =>'New Mexico');
+        $soptions[] = array('id' =>'New York', 'name' =>'New York');
+        $soptions[] = array('id' =>'North Carolina', 'name' =>'North Carolina');
+        $soptions[] = array('id' =>'North Dakota', 'name' =>'North Dakota');
+        $soptions[] = array('id' =>'Ohio', 'name' =>'Ohio');
+        $soptions[] = array('id' =>'Oklahoma', 'name' =>'Oklahoma');
+        $soptions[] = array('id' =>'Oregon', 'name' =>'Oregon');
+        $soptions[] = array('id' =>'Pennsylvania', 'name' =>'Pennsylvania');
+        $soptions[] = array('id' =>'Rhode Island', 'name' =>'Rhode Island');
+        $soptions[] = array('id' =>'South Carolina', 'name' =>'South Carolina');
+        $soptions[] = array('id' =>'South Dakota', 'name' =>'South Dakota');
+        $soptions[] = array('id' =>'Tennessee', 'name' =>'Tennessee');
+        $soptions[] = array('id' =>'Texas', 'name' =>'Texas');
+        $soptions[] = array('id' =>'Utah', 'name' =>'Utah');
+        $soptions[] = array('id' =>'Vermont', 'name' =>'Vermont');
+        $soptions[] = array('id' =>'Virginia', 'name' =>'Virginia');
+        $soptions[] = array('id' =>'Washington', 'name' =>'Washington');
+        $soptions[] = array('id' =>'West Virginia', 'name' =>'West Virginia');
+        $soptions[] = array('id' =>'Wisconsin', 'name' =>'Wisconsin');
+        $soptions[] = array('id' =>'Wyoming', 'name' =>'Wyoming');
+        $soptions[] = array('id' =>'Alberta', 'name' =>'Alberta');
+        $soptions[] = array('id' =>'British Columbia', 'name' =>'British Columbia');
+        $soptions[] = array('id' =>'Manitoba', 'name' =>'Manitoba');
+        $soptions[] = array('id' =>'New Brunswick', 'name' =>'New Brunswick');
         $soptions[] = array('id' =>'Newfoundland and Labrador', 'name' =>'Newfoundland and Labrador');
-        $soptions[] = array('id' =>'Northwest Territories     ', 'name' =>'Northwest Territories');
-        $soptions[] = array('id' =>'Nova Scotia     ', 'name' =>'Nova Scotia');
-        $soptions[] = array('id' =>'Nunavut     ', 'name' =>'Nunavut');
-        $soptions[] = array('id' =>'Ontario     ', 'name' =>'Ontario');
-        $soptions[] = array('id' =>'Prince Edward Island     ', 'name' =>'Prince Edward Island');
-        $soptions[] = array('id' =>'Quebec     ', 'name' =>'Quebec');
-        $soptions[] = array('id' =>'Saskatchewan     ', 'name' =>'Saskatchewan');
-        $soptions[] = array('id' =>'Yukon Territory     ', 'name' =>'Yukon Territory');
-        $soptions[] = array('id' =>'Australian Capital Territory     ', 'name' =>'Australian Capital Territory');
-        $soptions[] = array('id' =>'New South Wales     ', 'name' =>'New South Wales');
-        $soptions[] = array('id' =>'Northern Territory     ', 'name' =>'Northern Territory');
-        $soptions[] = array('id' =>'Queensland     ', 'name' =>'Queensland');
-        $soptions[] = array('id' =>'South Australia     ', 'name' =>'South Australia');
-        $soptions[] = array('id' =>'Tasmania     ', 'name' =>'Tasmania');
-        $soptions[] = array('id' =>'Victoria     ', 'name' =>'Victoria');
+        $soptions[] = array('id' =>'Northwest Territories', 'name' =>'Northwest Territories');
+        $soptions[] = array('id' =>'Nova Scotia', 'name' =>'Nova Scotia');
+        $soptions[] = array('id' =>'Nunavut', 'name' =>'Nunavut');
+        $soptions[] = array('id' =>'Ontario', 'name' =>'Ontario');
+        $soptions[] = array('id' =>'Prince Edward Island', 'name' =>'Prince Edward Island');
+        $soptions[] = array('id' =>'Quebec', 'name' =>'Quebec');
+        $soptions[] = array('id' =>'Saskatchewan', 'name' =>'Saskatchewan');
+        $soptions[] = array('id' =>'Yukon Territory', 'name' =>'Yukon Territory');
+        $soptions[] = array('id' =>'Australian Capital Territory', 'name' =>'Australian Capital Territory');
+        $soptions[] = array('id' =>'New South Wales', 'name' =>'New South Wales');
+        $soptions[] = array('id' =>'Northern Territory', 'name' =>'Northern Territory');
+        $soptions[] = array('id' =>'Queensland', 'name' =>'Queensland');
+        $soptions[] = array('id' =>'South Australia', 'name' =>'South Australia');
+        $soptions[] = array('id' =>'Tasmania', 'name' =>'Tasmania');
+        $soptions[] = array('id' =>'Victoria', 'name' =>'Victoria');
         $soptions[] = array('id' =>'Western Australia', 'name' =>'Western Australia');
         $soptions[] = array('id' =>'Other', 'name' =>'Other');
-        /*for($i=0; isset($soptions[$i]); $i++) {
-            $out .= '<option';
-            $out .= ' value="'.$soptions[$i]['name'].'"';
-            if ($value == $soptions[$i]['name']) {
-                $out .= ' selected="selected">'.$soptions[$i]['name'].'</option>';
-            } else {
-                $out .= '>'.$soptions[$i]['name'].'</option>';
-            }
-        }*/
-
-        /*$out .= '</select>' .
-               (!empty($this->invalid) ? ' <span class="xar-error">'.xarML('Invalid #(1)', $this->invalid) .'</span>' : '');
-        */
 
         $data['soptions'] = $soptions;
         $data['invalid']  = !empty($this->invalid) ? xarML('Invalid #(1)', $this->invalid) : '';
@@ -209,18 +189,14 @@ class Dynamic_StateList_Property extends Dynamic_Select_Property
                               'label'      => 'State Dropdown',
                               'format'     => '43',
                               'validation' => '',
-                            'source'     => '',
-                            'dependancies' => '',
-                            'requiresmodule' => '',
-                            'aliases'        => '',
-                            'args'           => serialize($args)
+                              'source'     => '',
+                              'dependancies' => '',
+                              'requiresmodule' => '',
+                              'aliases'        => '',
+                              'args'           => serialize($args)
                             // ...
                            );
         return $baseInfo;
      }
-
-
-
 }
-
 ?>
