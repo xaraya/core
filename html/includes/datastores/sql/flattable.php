57839ca04999879875acb47d096cd2692500d176
johnny <johnny@localmomentum.net>
2006-10-05 18:09:15 +0000

    plucked 	* html/includes/datastores/sql/flattable.php: ... applied changes from b8182301b60a46fa196231df63cb58633410f71b              through ddaa23e5b74ea3165b6312327649d4f57d52ed94

--- html/includes/datastores/sql/flattable.php
+++ html/includes/datastores/sql/flattable.php
@@ -338,7 +338,7 @@ if (empty($itemidfield)) {
         } else {
             // Note: Oracle doesn't like having the same field in a sub-query twice,
             //       so we use an alias for the primary field here
-            $query = "SELECT $itemidfield AS ddprimaryid, " . join(', ', $fieldlist) . "
+            $query = "SELECT DISTINCT $itemidfield AS ddprimaryid, " . join(', ', $fieldlist)
                         FROM " . join(', ', $tables) . $more . " ";
         }
 
