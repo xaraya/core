c1dd6cdb417f9283bcfe49b5533bbd90a0ff1351
mfl <mfl@netspan.ch>
2006-11-19 23:23:24 +0000

    Move the object descriptor to where all good classes get to go.

--- html/modules/dynamicdata/class/objects/master.php
+++ html/modules/dynamicdata/class/objects/master.php
@@ -8,48 +8,9 @@
  * @todo make this a real factory
  * @todo scoping of variables
 **/
+sys::import('structures.descriptor');
 sys::import('modules.dynamicdata.class.datastores');
 sys::import('modules.dynamicdata.class.properties');
-
-class ObjectDescriptor extends Object
-{
-    protected $args;
-
-    function __construct(array $args)
-    {
-        $this->setArgs($args);
-    }
-
-    public function getArgs()
-    {
-        return $this->args;
-    }
-
-    public function getPublicProperties(Object $object)
-    {
-        $o = $object->getClass();
-        $objectname = $o->getName();
-        $reflection = new ReflectionClass($objectname);
-        $properties = array();
-        foreach($reflection->getProperties() as $p) {
-            $prop = new ReflectionProperty($objectname,$p->name);
-            if ($prop->isPublic()) $properties[$p->name] = $prop->getValue($object);
-        }
-        return $properties;
-    }
-
-    public function refresh(Object $object)
-    {
-        $publicproperties = $this->getPublicProperties($object);
-        foreach ($this->args as $key => $value) if (in_array($key,$publicproperties)) $object->$key = $value;
-        else echo $key ."<br />";  // temporary for debugging
-    }
-
-    public function setArgs(array $args)
-    {
-        $this->args = $args;
-    }
-}
 
 class DataObjectDescriptor extends ObjectDescriptor
 {
