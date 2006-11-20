9773755ae4ed4b2bdc84c1f19230f523586e1c23
mfl <mfl@netspan.ch>
2006-11-20 10:59:29 +0000

    actually add the file in question

---
+++ html/modules/dynamicdata/class/properties/interfaces.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Interfaces for Dynamic Properties:
+ *
+ */
+
+interface iDataProperty
+{
+    public function checkInput($name = '', $value = null);
+    public function fetchValue($name = '');
+    public function getModule();
+    public static function getRegistrationInfo();
+    public function getTemplate();
+    public function getValue();
+    public function parseValidation($validation = '');
+    public function setValue($value);
+    public function showInput(Array $args = array());
+    public function showOutput(Array $args = array());
+    public function showValidation(Array $args = array());
+    public function updateValidation(Array $args = array());
+    public function validateValue($value = null);
+}
+
+?>
