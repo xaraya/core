60163a48c1b52c9770aff7381c4007a23ee97527
mfl <mfl@netspan.ch>
2006-12-28 13:36:30 +0000

    clean up nomenclature

--- html/modules/dynamicdata/xaradminapi/getmenulinks.php
+++ html/modules/dynamicdata/xaradminapi/getmenulinks.php
@@ -19,24 +19,6 @@
  */
 function dynamicdata_adminapi_getmenulinks()
 {
-    /*
-    This menu gets its data from the adminmenu.php file in the module's xardataapi folder.
-    You can add or change menu items by changing the data there.
-    Or you can create your own menu items here. They should have the form of this example:
-
-    $menulinks = array();
-    .....
-    if (xarSecurityCheck('EditRole',0)) {
-        $menulinks[] = array('url'   => xarModURL('roles',
-                                                  'admin',
-                                                  'viewroles'),
-                              'title' => xarML('View and edit the groups on the system'),
-                              'label' => xarML('View All Groups'));
-    }
-    .....
-    return $menulinks;
-    */
-
     // No special menu. Just return a standard array
     return xarModAPIFunc('base','admin','menuarray',array('module' => 'dynamicdata'));
 }
