3464aa8a8ab064676ffe1e09c29323ed855a0297
mfl <mfl@netspan.ch>
2007-03-22 12:59:30 +0000

    update Xaraya_Classic bl vers.

--- html/themes/Xaraya_Classic/xartheme.php
+++ html/themes/Xaraya_Classic/xartheme.php
@@ -5,13 +5,13 @@ $themeinfo['directory'] = 'Xaraya_Classic';
 $themeinfo['author'] = 'Doug Daulton, rev 2 by AndyV';
 $themeinfo['homepage'] = 'http://www.dougdaulton.com';
 $themeinfo['email'] = 'apakuni@xaraya.com';
-$themeinfo['description'] = 'Default Theme for Xaraya CMS';
+$themeinfo['description'] = 'Default Theme for Xaraya Framework';
 $themeinfo['contact_info'] = 'Doug Daulton, apakuni@xaraya.com';
 $themeinfo['publish_date'] = '01/15/2003';
 $themeinfo['license'] = 'GPL';
 $themeinfo['version'] = '2.0.0';
 $themeinfo['xar_version'] = '1.0';
-$themeinfo['bl_version'] = '1.0';
+$themeinfo['bl_version'] = '2.0';
 $themeinfo['class'] = '2';
 
 $themevars[] = array('name' => 'index', 'value' => '1', 'description' => 'turn on right blocks');
