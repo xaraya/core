8703e84361ded5295686e615128c5771c93ef873
mfl <mfl@netspan.ch>
2007-03-29 10:06:56 +0000

    admin menu change

--- html/modules/dynamicdata/xardataapi/adminmenu.php
+++ html/modules/dynamicdata/xardataapi/adminmenu.php
@@ -4,8 +4,9 @@
     {
         return array(
                 array('includes' => array('main','overview'), 'target' => 'overview', 'label' => xarML('DynamicData Overview')),
-                array('mask' => 'EditDynamicData', 'includes' => array('view','new','modify','modifyprop'), 'target' => 'view', 'title' => xarML('View module objects using dynamic data'), 'label' => xarML('Manage DataObjects')),
-                array('mask' => 'AdminDynamicData', 'includes' => 'modifyconfig', 'target' => 'modifyconfig', 'title' => xarML('Configure the default property types'), 'label' => xarML('List DataProperty Types')),
+                array('mask' => 'EditDynamicData', 'includes' => array('view','new','modify','modifyprop'), 'target' => 'view', 'title' => xarML('View dataobjects using dynamic data'), 'label' => xarML('DataObjects')),
+                array('mask' => 'AdminDynamicData', 'includes' => 'modifyconfig', 'target' => 'modifyconfig', 'title' => xarML('Configure the default dataproperty types'), 'label' => xarML('DataProperty Types')),
+//                array('mask' => 'AdminDynamicData', 'includes' => 'modifyconfig', 'target' => 'modifyconfig', 'title' => xarML('Configure relationships'), 'label' => xarML('Relationships')),
                 array('mask' => 'AdminDynamicData', 'includes' => array('utilities','query','util'), 'target' => 'utilities', 'title' => xarML('Import/export and other utilities'), 'label' => xarML('Utilities')),
         );
     }
