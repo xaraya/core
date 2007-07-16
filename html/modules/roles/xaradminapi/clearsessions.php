3504f9a1692d6e2699e75b067be666e410f572b9
johnny <johnny@localmomentum.net>
2007-07-16 06:04:35 +0000

    make session clearing usable again sessid -> id

--- html/modules/roles/xaradminapi/clearsessions.php
+++ html/modules/roles/xaradminapi/clearsessions.php
@@ -1,22 +1,21 @@
 <?php
 /**
- * Delete a group & info
- *
  * @package modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) 2002-2007 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
- * @subpackage Roles module
+ * @subpackage roles
  * @link http://xaraya.com/index.php/release/27.html
  */
 
-/* deletegroup - delete a group & info
- *
+/**
+ * Clear sessions 
+ * 
  * @author Marc Lutolf <marcinmilan@xaraya.com>
  * @param $args['id']
  * @return true on success, false otherwise
- * @todo Move this to sessions subssystem, doesnt belong here.
+ * @todo Move this to sessions subsystem, doesnt belong here.
  */
 function roles_adminapi_clearsessions($spared)
 {
@@ -26,11 +25,11 @@ function roles_adminapi_clearsessions($spared)
     $xartable = xarDB::getTables();
     $sessionstable = $xartable['session_info'];
 
-    $query = "SELECT sessid, role_id FROM $sessionstable";
+    $query = "SELECT id, role_id FROM $sessionstable";
     $result = $dbconn->executeQuery($query);
 
     // Prepare query outside the loop
-    $sql = "DELETE FROM $sessionstable WHERE sessid = ?";
+    $sql = "DELETE FROM $sessionstable WHERE id = ?";
     $stmt = $dbconn->prepareStatement($sql);
     try {
         $dbconn->begin();
