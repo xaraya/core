d1cd32956e9569e38ebbb42d3c83dcaaa3cc0c30
mfl <mfl@netspan.ch>
2007-07-30 01:00:27 +0000

    fix for filepicker and lost password routine; change validation property to a textarea

--- html/modules/roles/xaruser/lostpassword.php
+++ html/modules/roles/xaruser/lostpassword.php
@@ -79,14 +79,8 @@ function roles_user_lostpassword()
             //Update user password
 		    $role = xarRoles::get($user['id']);
 			$modifiedstatus = $role->setPass($user['pass']);
-			$modifiedrole = $role->updateItem();
-			if (!$modifiedrole) return;
-            // check for user and grab id if exists
-            /*
-            if (!xarModAPIFunc('roles','admin','update',$user)) {
-                throw new DataNotFoundException(array(),'Problem updating the user information');
-            }
-            */
+			if (!$role->updateItem()) return;
+
               // Send Reminder Email
             if (!xarModAPIFunc('roles', 'admin','senduseremail', array('id' => array($user['id'] => '1'), 'mailtype' => 'reminder', 'pass' => $user['pass']))) return;
 
