704de5970c465f561c1d14ef8073b4916254c8f0
mfl <mfl@netspan.ch>
2007-08-13 19:18:15 +0000

    First cut at running blocks created as classes in parallel to traditional blocks.

--- html/modules/blocks/xaruserapi/read_type_info.php
+++ html/modules/blocks/xaruserapi/read_type_info.php
@@ -1,5 +1,5 @@
 <?php
-/** 
+/**
  * Read a block's type info.
  *
  * @package modules
@@ -21,7 +21,7 @@
 function blocks_userapi_read_type_info($args)
 {
     extract($args);
-    
+
     if (empty($module) && empty($type)) {
         // No identifier provided.
         throw new EmptyParameterException('module or type');
@@ -33,19 +33,31 @@ function blocks_userapi_read_type_info($args)
     if (function_exists($infofunc)) {
         return $infofunc();
     }
-    
-    // Load and execute the info function of the block.
-    if (!xarModAPIFunc(
-        'blocks', 'admin', 'load',
-        array(
-            'modName' => $module,
-            'blockName' => $type,
-            'blockFunc' => 'info'
-        )
-    )) {return;}
+
+    $classpath = 'modules/' . $module . '/xarblocks/' . $type . '.php';
+    	// are we are using a block class?
+    if (!file_exists($classpath)) {
+		// Load and execute the info function of the block.
+		if (!xarModAPIFunc(
+			'blocks', 'admin', 'load',
+			array(
+				'modName' => $module,
+				'blockName' => $type,
+				'blockFunc' => 'info'
+			)
+		)) {return;}
+    }
 
     if (function_exists($infofunc)) {
         return $infofunc();
+    } elseif (file_exists($classpath)) {
+    	// we are using a block class
+        sys::import('modules.' . $module . '.xarblocks.' . $type);
+        sys::import('xaraya.structures.descriptor');
+        $name = ucfirst($type) . "Block";
+        $descriptor = new ObjectDescriptor(array());
+        $block = new $name($descriptor);
+        return $block->getInfo();
     } else {
         // No block info function found.
         return;
