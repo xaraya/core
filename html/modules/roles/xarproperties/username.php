78a513c6f541e5a01e4135d995740a88f6cc6fe3
mfl <mfl@netspan.ch>
2007-10-29 13:24:59 +0000

    rework username property to have more validation optiions and check for existence

--- html/modules/roles/xarproperties/username.php
+++ html/modules/roles/xarproperties/username.php
@@ -13,12 +13,20 @@
  * Handle Username Property
  * @author mikespub <mikespub@xaraya.com>
  */
-class UsernameProperty extends DataProperty
+sys::import('modules.base.xarproperties.textbox');
+
+class UsernameProperty extends TextBoxProperty
 {
     public $id         = 7;
     public $name       = 'username';
     public $desc       = 'Username';
     public $reqmodules = array('roles');
+
+    public $rawvalue   = null;
+
+    public $linkrule   = 1;
+    public $existrule  = 0;
+    public $validationargs   = array('min','max','regex','linkrule','existrule');
 
     function __construct(ObjectDescriptor $descriptor)
     {
@@ -31,12 +39,46 @@ class UsernameProperty extends DataProperty
 
     public function validateValue($value = null)
     {
+		// Save the incoming value until validation is successful
+		$this->rawvalue = $value;
+
+        // Validate as a text box
+        if (!parent::validateValue($value)) return false;
+
         if (!isset($value)) {
             $value = $this->value;
         }
+
+        /* CHECKME: was this good for anything?
         if (empty($value)) {
             $value = xarUserGetVar('id');
         }
+		*/
+
+        if (empty($value)) return true;
+
+        $role = xarRoles::ufindRole($value);
+
+        switch ((int)$this->existrule) {
+        	case 1:
+            if (!empty($role)) {
+	            $this->invalid = xarML('user #(1) already exists', $value);
+				return false;
+            }
+        	break;
+
+        	case 2:
+            if (empty($role)) {
+	            $this->invalid = xarML('user #(1) does not exist', $value);
+				return false;
+            }
+        	break;
+
+        	case 0:
+        	default:
+        }
+
+        /*
         // check that the user exists, but dont except
         if (is_numeric($value)) {
             try {
@@ -61,50 +103,134 @@ class UsernameProperty extends DataProperty
             $this->value = $value;
             return true;
         }
+        */
+		$this->value = empty($role) ? 0 : $role->getID();;
+        return true;
     }
 
     public function showInput(Array $data = array())
     {
         extract($data);
-        if (!isset($value)) $value = $this->value;
-        if (empty($value))  $value = xarUserGetVar('id');
+//        echo "X".$this->rawvalue."X";//exit;
+        if (isset($this->rawvalue)) {
+        	$data['value'] = $this->rawvalue;
+        	$data['user'] = $this->rawvalue;
+        } else {
+			if (!isset($value)) $value = $this->value;
+			if (empty($value))  {
+				$data['user'] = '';
+				$data['value']= 0;
+			} else {
+				try {
+					$user = xarUserGetVar('name', $value);
+					if (empty($user))
+						$user = xarUserGetVar('uname', $value);
+				} catch(NotFoundExceptions $e) {
+					$user = $value;
+				}
 
-        try {
-            $user = xarUserGetVar('name', $value);
-            if (empty($user))
-                $user = xarUserGetVar('uname', $value);
-        } catch(NotFoundExceptions $e) {
-            $user = $value;
+				$data['user'] = xarVarprepForDisplay($user);
+				$data['value']= $value;
+			}
         }
 
-        $data['user'] = xarVarprepForDisplay($user);
-        $data['value']= $value;
         return parent::showInput($data);
     }
 
     public function showOutput(Array $data = array())
     {
-        extract($data);
-        if (!isset($value)) $value = $this->value;
-        if (empty($value))  $value = xarUserGetVar('id');
-
-        try {
-            $user = xarUserGetVar('name', $value);
-            if (empty($user))
-                $user = xarUserGetVar('uname', $value);
-        } catch(NotFoundExceptions $e) {
-            $user = $value;
-        }
-
-        $data['user']  = xarVarPrepForDisplay($user);
-        $data['value'] = $value;
-
-        if ($this->validation) {
-            $data['linkurl'] = xarModURL('roles','user','display',array('id' => $value));
+        if (isset($this->rawvalue)) {
+        	$data['value'] = $this->rawvalue;
+        	$data['user'] = $this->rawvalue;
         } else {
-            $data['linkurl'] = "";
-        }
+			extract($data);
+			if (!isset($value)) $value = $this->value;
+			if (empty($value))  $value = xarUserGetVar('id');
+
+			try {
+				$user = xarUserGetVar('name', $value);
+				if (empty($user))
+					$user = xarUserGetVar('uname', $value);
+			} catch(NotFoundExceptions $e) {
+				$user = $value;
+			}
+
+			$data['user']  = xarVarPrepForDisplay($user);
+			$data['value'] = $value;
+
+			if ($this->validation) {
+				$data['linkurl'] = xarModURL('roles','user','display',array('id' => $value));
+			} else {
+				$data['linkurl'] = "";
+			}
+		}
         return parent::showOutput($data);
+    }
+
+    public function parseValidation($validation = '')
+    {
+        if (is_array($validation)) {
+            $fields = $validation;
+        } else {
+            $fields = unserialize($validation);
+        }
+        if (!empty($fields) && is_array($fields)) {
+            foreach ($this->validationargs as $validationarg) {
+                if (isset($fields[$validationarg])) {
+                    $this->$validationarg = $fields[$validationarg];
+                }
+            }
+        }
+    }
+
+    public function showValidation(Array $args = array())
+    {
+        extract($args);
+        $data = array();
+        $data['name']       = !empty($name) ? $name : 'dd_'.$this->id;
+        $data['id']         = !empty($id)   ? $id   : 'dd_'.$this->id;
+        $data['tabindex']   = !empty($tabindex) ? $tabindex : 0;
+        $data['size']       = !empty($size) ? $size : 50;
+        $data['invalid']    = !empty($this->invalid) ? xarML('Invalid #(1)', $this->invalid) :'';
+
+        if (isset($validation)) {
+            $this->validation = $validation;
+            $this->parseValidation($validation);
+        }
+        foreach ($this->validationargs as $validationarg) {
+        	$data[$validationarg] = $this->$validationarg;
+        }
+
+        // allow template override by child classes
+        $module    = empty($module)   ? $this->getModule()   : $module;
+        $template  = empty($template) ? $this->getTemplate() : $template;
+
+        return xarTplProperty($module, $template, 'validation', $data);
+    }
+
+    public function updateValidation(Array $args = array())
+    {
+        extract($args);
+
+        // in case we need to process additional input fields based on the name
+        $name = empty($name) ? 'dd_'.$this->id : $name;
+
+        // do something with the validation and save it in $this->validation
+        if (isset($validation)) {
+            if (is_array($validation)) {
+                $data = array();
+                foreach ($this->validationargs as $validationarg) {
+                    if (isset($validation[$validationarg])) {
+                        $data[$validationarg] = $validation[$validationarg];
+                    }
+                }
+                $this->validation = serialize($data);
+
+            } else {
+                $this->validation = $validation;
+            }
+        }
+        return true;
     }
 }
 ?>
