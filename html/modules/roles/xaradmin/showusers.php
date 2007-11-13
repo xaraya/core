af59a4be048dfc829aea7ad67e3ba135b63bff08
mfl <mfl@netspan.ch>
2007-11-13 15:10:01 +0000

    Minor adjustment to showusers function

--- html/modules/roles/xaradmin/showusers.php
+++ html/modules/roles/xaradmin/showusers.php
@@ -65,8 +65,10 @@ function roles_admin_showusers()
     if (empty($q) || isset($reload)) {
         $types = xarModAPIFunc('roles','user','getitemtypes');
         $basetypes = array();
+        // Show only roles based on the user type
         foreach ($types as $key => $value) {
-            $basetype = xarModAPIFunc('dynamicdata','user','getbaseancestor',array('itemtype' => $key, 'moduleid' => 27));
+            $basetype = xarModAPIFunc('dynamicdata','user','getbaseitemtype',array('itemtype' => $key, 'moduleid' => 27));
+            if ($basetype == ROLES_USERTYPE) $basetypes[] = $basetype;
         }
         $xartable = xarDB::getTables();
         $q = new xarQuery('SELECT');
