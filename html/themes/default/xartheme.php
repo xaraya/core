e73b5c2d5d9bc26f7b6e59c303fb3ef884b3b1bd
rolf <rolf@karu.nl>
2008-02-27 20:18:30 +0000

    Added default theme (a sketch)

---
+++ html/themes/default/xartheme.php
@@ -0,0 +1,16 @@
+<?php
+$themeinfo['name'] = 'default';
+$themeinfo['id'] = '11005';
+$themeinfo['directory'] = 'default';
+$themeinfo['author'] = 'Rolf Meijer';
+$themeinfo['homepage'] = 'http://karu.nl';
+$themeinfo['email'] = 'rolf@karu.nl';
+$themeinfo['description'] = 'Xaraya 2.x Default Theme';
+$themeinfo['contact_info'] = 'Rolf Meijer, rolf@karu.nl, karu.nl';
+$themeinfo['publish_date'] = '07/05/2007';
+$themeinfo['license'] = 'Public Domain';
+$themeinfo['version'] = '0.1.0';
+$themeinfo['xar_version'] = '2.0';
+$themeinfo['bl_version'] = '2.0';
+$themeinfo['class'] = '2';
+?>
