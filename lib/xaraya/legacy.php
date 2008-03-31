a0041edcc9ed14a4992d42d08740fd5815796c66
mfl <mfl@netspan.ch>
2008-03-31 16:51:40 +0000

    remove the Everybody entry from the rolemenbers table

--- lib/xaraya/legacy.php
+++ lib/xaraya/legacy.php
@@ -98,7 +98,9 @@ function xarModDelAllVars($modName)                  {   return xarModVars::dele
 function xarModGetUserVar($modName, $name, $id = NULL, $prep = NULL){   return xarModUserVars::get($modName, $name, $id, $prep);  }
 function xarModSetUserVar($modName, $name, $value, $id=NULL)        {   return xarModUserVars::set($modName, $name, $value, $id); }
 
-// This function no longer does anything
+// These functions no longer do anything
 function xarMakePrivilegeRoot($privilege)        {   return true; }
+function xarMakeRoleRoot($name) { return true; }
+
 
 ?>
