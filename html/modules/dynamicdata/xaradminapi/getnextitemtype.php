fa8c8003c223f9055fa6f986ac6ebb3370c9fa59
mfl <mfl@netspan.ch>
2008-05-25 15:43:32 +0000

    a solution ot get nextitemtype

--- html/modules/dynamicdata/xaradminapi/getnextitemtype.php
+++ html/modules/dynamicdata/xaradminapi/getnextitemtype.php
@@ -22,11 +22,12 @@
 function dynamicdata_adminapi_getnextitemtype($args = array())
 {
     extract($args);
-    if (empty($modid)) throw new EmptyParameterException('modid');
+    if (empty($modid)) $modid = 182;
     $types = DataObjectMaster::getModuleItemTypes(array('moduleid' => $modid));
     $ids = array_keys($types);
     sort($ids);
     $lastid = array_pop($ids);
+    return $lastid;
     // DD and DD-type modules go one way
     if ($modid == 182 || $modid == 27) return $lastid + 1;
     // other module go another
