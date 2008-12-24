45c2859f8c386408d32d61f8f4a86ec34400fc8e
mfl <mfl@netspan.ch>
2008-12-24 14:40:52 +0000

    remove DDF from the base module

--- html/modules/base/xaruserapi/getfavicon.php
+++ html/modules/base/xaruserapi/getfavicon.php
@@ -2,7 +2,7 @@
 /**
  * Return the favicon for a given url
  * @package modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) copyright-placeholder
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
--- html/modules/base/xarversionsapi/order.php
+++ html/modules/base/xarversionsapi/order.php
@@ -3,7 +3,7 @@
  * Base User Version management functions
  *
  * @package modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) copyright-placeholder
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
