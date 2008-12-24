3a7a569e7f7716396840f4dbd3a6191eb8977699
mfl <mfl@netspan.ch>
2008-12-24 14:41:49 +0000

    remove DDF from the blocks module

--- html/modules/blocks/xaradminapi/delete_cacheinstance.php
+++ html/modules/blocks/xaradminapi/delete_cacheinstance.php
@@ -3,7 +3,7 @@
  * Delete a cache block instance
  *
  * @package modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) 2002-2006 The copyright-placeholder
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
--- html/modules/blocks/xaradminapi/update_type_info.php
+++ html/modules/blocks/xaradminapi/update_type_info.php
@@ -2,7 +2,7 @@
 /**
  * Read the info details of a block type
  * @package modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) 2002-2006 The copyright-placeholder
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
