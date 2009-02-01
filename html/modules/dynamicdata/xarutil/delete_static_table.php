347497e24b96f0ce63ad714938181512b3641e99
mfl <mfl@netspan.ch>
2009-02-01 19:42:37 +0000

    add missing files for table mgmt support

---
+++ html/modules/dynamicdata/xarutil/delete_static_table.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Delete a table
+ *
+ */
+    sys::import('modules.dynamicdata.class.objects.master');
+    
+    function dynamicdata_util_delete_static_table()
+    {
+        if (!xarSecurityCheck('AdminDynamicData')) return;
+
+        if (!xarVarFetch('table',      'str:1',  $data['table'],    '',     XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('confirm',    'bool',   $data['confirm'], false,       XARVAR_NOT_REQUIRED)) return;
+
+        $data['object'] = DataObjectMaster::getObject(array('name' => 'dynamicdata_tablefields'));
+
+        $data['tplmodule'] = 'dynamicdata';
+
+        if ($data['confirm']) {
+        
+            $query = 'DROP TABLE ' .$data['table'];
+            $dbconn = xarDB::getConn();
+            $dbconn->Execute($query);
+
+            // Jump to the next page
+            xarResponseRedirect(xarModURL('dynamicdata','util','view_static'));
+            return true;
+        }
+        return $data;
+    }
+
+?>
---
+++ html/modules/dynamicdata/xarutil/rename_static_table.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Delete a table
+ *
+ */
+    sys::import('modules.dynamicdata.class.objects.master');
+    
+    function dynamicdata_util_rename_static_table()
+    {
+        if (!xarSecurityCheck('AdminDynamicData')) return;
+
+        if (!xarVarFetch('table',      'str:1',  $data['table'],    '',     XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('newtable',   'str:1',  $data['newtable'],    '',     XARVAR_NOT_REQUIRED)) return;
+        if (!xarVarFetch('confirm',    'bool',   $data['confirm'], false,       XARVAR_NOT_REQUIRED)) return;
+
+        $data['object'] = DataObjectMaster::getObject(array('name' => 'dynamicdata_tablefields'));
+
+        $data['tplmodule'] = 'dynamicdata';
+
+        if ($data['confirm']) {
+            if (empty($data['newtable'])) 
+                xarResponseRedirect(xarModURL('dynamicdata','util','view_static',array('table' => $data['table'])));
+            $query = 'RENAME TABLE ' . $data['table'] . ' TO ' . $data['newtable'];
+            $dbconn = xarDB::getConn();
+            $dbconn->Execute($query);
+
+            // Jump to the next page
+            xarResponseRedirect(xarModURL('dynamicdata','util','view_static',array('table' => $data['newtable'])));
+            return true;
+        }
+        return $data;
+    }
+
+?>
