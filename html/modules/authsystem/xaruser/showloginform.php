d56a89f640490192dda5eadfdea9cea196c33ab4
mfl <mfl@netspan.ch>
2009-03-01 08:01:46 +0000

    disapproval of revision 'a990449b68c68712c7800667a950a85c11ba5be4'

--- html/modules/authsystem/xaruser/showloginform.php
+++ html/modules/authsystem/xaruser/showloginform.php
@@ -3,7 +3,7 @@
  * Shows the user login form when login block is not active
  *
  * @package modules
- * @copyright (C) copyright-placeholder
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
