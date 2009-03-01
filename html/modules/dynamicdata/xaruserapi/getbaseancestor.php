1a26f8306062b015c113781880e5467b655831ea
mfl <mfl@netspan.ch>
2009-03-01 07:57:48 +0000

    disapproval of revision 'd9006b88451d1cbeeace56e732cf03df525393f7'

--- html/modules/dynamicdata/xaruserapi/getbaseancestor.php
+++ html/modules/dynamicdata/xaruserapi/getbaseancestor.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package modules
- * @copyright (C) copyright-placeholder
+ * @copyright (C) 2002-2006 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  * @subpackage dynamicdata
