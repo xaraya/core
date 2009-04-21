e5c7ac9c8c9e67036ac801d567b076ca6c115d76
mfl <mfl@netspan.ch>
2009-04-21 06:52:14 +0000

    Simplify the UI a bit: remove the new block group and new block type options from the menu. They are accessed through the corresponding view pages

--- html/modules/base/xaradminapi/menuarray.php
+++ html/modules/base/xaradminapi/menuarray.php
@@ -27,7 +27,15 @@ function base_adminapi_menuarray($args)
     $menuarray = xarModAPIFunc('base','admin','loadadminmenuarray',array('module' => $args['module']));
     if (!empty($menuarray)) {
         foreach ($menuarray as $menuitem) {
-            $url = isset($menuitem['target']) ? xarModURL($args['module'],'admin',$menuitem['target']) : xarServer::getBaseURL();
+            if (isset($menuitem['target'])) {
+                if ($menuitem['target'] == 'overview') {
+                    $url = xarModURL('base','admin',$menuitem['target'],array('overview' => $args['module']));
+                } else {
+                    $url = xarModURL($args['module'],'admin',$menuitem['target']);
+                }
+            } else {
+                $url = xarServer::getBaseURL();
+            }
             $link = array('url'   => $url,
                           'title' => $menuitem['title'],
                           'label' => $menuitem['label']
@@ -43,7 +51,15 @@ function base_adminapi_menuarray($args)
     } elseif (xarModAPIFunc($args['module'],'data','adminmenu',0)) {
         $tabs = xarModAPIFunc($args['module'],'data','adminmenu');
         foreach($tabs as $tab) {
-            $url = isset($tab['target']) ? xarModURL($args['module'],'admin',$tab['target']) : xarServer::getBaseURL();
+            if (isset($tab['target'])) {
+                if ($tab['target'] == 'overview') {
+                    $url = xarModURL('base','admin',$tab['target'],array('overview' => $args['module']));
+                } else {
+                    $url = xarModURL($args['module'],'admin',$tab['target']);
+                }
+            } else {
+                $url = xarServer::getBaseURL();
+            }
             $label = isset($tab['label']) ? $tab['label'] : xarML('Missing label');
             $title = isset($tab['title']) ? $tab['title'] : $label;
             $link = array('url'   => $url,
