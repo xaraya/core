7d2d22fab8da0d51166a5e8cee42ca35d246a0f5
mfl <mfl@netspan.ch>
2009-04-26 19:52:37 +0000

    merge debris

--- html/modules/dynamicdata/xarinit.php
+++ html/modules/dynamicdata/xarinit.php
@@ -150,7 +150,7 @@ function dynamicdata_init()
 # Create the object and property dataobjects
 #
 
-        $modid = xarMod::getRegID('dynamicdata');
+        $module_id = xarMod::getRegID('dynamicdata');
 
         // create default objects for dynamic data
         $sql = "INSERT INTO $dynamic_objects (
