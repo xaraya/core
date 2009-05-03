8d6a92df275de8bfd2e7db6ec51c8bb88263eed7
mfl <mfl@netspan.ch>
2009-05-03 09:50:21 +0000

    finish modules overview rewrite (for now)

--- html/modules/modules/xardataapi/adminmenu.php
+++ html/modules/modules/xardataapi/adminmenu.php
@@ -4,7 +4,7 @@
     {
         return array(
                 array('includes' => array('main','overview'), 'target' => 'main', 'label' => xarML('Modules Overview')),
-                array('mask' => 'AdminModules', 'includes' => 'list', 'target' => 'list', 'title' => xarML('View list of all installed modules on the system'), 'label' => xarML('View Available Modules')),
+                array('mask' => 'AdminModules', 'includes' => 'list', 'target' => 'list', 'title' => xarML('View list of all installed modules on the system'), 'label' => xarML('View Modules')),
                 array('mask' => 'AdminModules', 'includes' => 'hooks', 'target' => 'hooks', 'title' => xarML('Extend the functionality of your modules via hooks'), 'label' => xarML('Configure Hooks')),
                 array('mask' => 'AdminModules', 'includes' => 'modifyconfig', 'target' => 'modifyconfig', 'title' => xarML('Modify configuration parameters'), 'label' => xarML('Modify Configuration')),
         );
