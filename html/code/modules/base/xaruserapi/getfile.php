ac7e9c49c293b8d723e40f573c0133496de97c47
mfl <mfl@netspan.ch>
2009-10-20 22:14:02 +0000

    adjust copyright dates

--- html/code/modules/base/xaruserapi/getfile.php
+++ html/code/modules/base/xaruserapi/getfile.php
@@ -2,7 +2,7 @@
 /**
  * Get a file from the Internet
  * @package modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) 2002-2009 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
