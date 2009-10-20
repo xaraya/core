1cfcc85258f99987fe152e796d46609cc18672db
mfl <mfl@netspan.ch>
2009-10-20 22:21:49 +0000

    adjust copyright dates

--- html/code/modules/blocks/xaradminapi/create_group.php
+++ html/code/modules/blocks/xaradminapi/create_group.php
@@ -2,7 +2,7 @@
 /**
  * create a new group
  * @package modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) 2002-2009 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
--- html/code/modules/blocks/xaruserapi/getitemtypes.php
+++ html/code/modules/blocks/xaruserapi/getitemtypes.php
@@ -3,7 +3,7 @@
  * Utility function to retrieve the list of item types
  *
  * @package modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) 2002-2009 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
