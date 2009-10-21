55d17c9adaa08e2f427820d6087b27d5892da7ff
mfl <mfl@netspan.ch>
2009-10-21 12:12:14 +0000

    adjust copyright dates

--- html/code/modules/themes/xaradminapi/activate.php
+++ html/code/modules/themes/xaradminapi/activate.php
@@ -3,7 +3,7 @@
  * Activate a theme if it has an active function
  *
  * @package Xaraya eXtensible Management System
- * @copyright (C) 2005 The Digital Development Foundation
+ * @copyright (C) 2002-2009 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
