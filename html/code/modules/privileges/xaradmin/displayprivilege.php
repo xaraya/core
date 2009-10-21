c4163cabd89d37566e6cf6d795ff8c80bb7861f9
mfl <mfl@netspan.ch>
2009-10-21 12:00:24 +0000

    adjust copyright dates

--- html/code/modules/privileges/xaradmin/displayprivilege.php
+++ html/code/modules/privileges/xaradmin/displayprivilege.php
@@ -2,7 +2,7 @@
 /**
  * Displayprivilege - display privilege details
  * @package core modules
- * @copyright (C) 2002-2006 The Digital Development Foundation
+ * @copyright (C) 2002-2009 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
