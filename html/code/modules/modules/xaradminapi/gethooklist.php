a10231a90b8d62cef8fcb81ac2e08b097ecb185e
mfl <mfl@netspan.ch>
2009-10-21 11:56:58 +0000

    adjust copyright dates

--- html/code/modules/modules/xaradminapi/gethooklist.php
+++ html/code/modules/modules/xaradminapi/gethooklist.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Xaraya eXtensible Management System
- * @copyright (C) 2007 The Digital Development Foundation
+ * @copyright (C) 2002-2009 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
--- html/code/modules/modules/xaradminapi/updatehooks.php
+++ html/code/modules/modules/xaradminapi/updatehooks.php
@@ -2,7 +2,7 @@
 /**
  * Update hooks for a particular hook module
  * @package Xaraya eXtensible Management System
- * @copyright (C) 2005 The Digital Development Foundation
+ * @copyright (C) 2002-2009 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
