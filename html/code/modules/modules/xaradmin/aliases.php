3e1b6519ac36111871d356baa3eedcec90e6b0b1
mfl <mfl@netspan.ch>
2009-11-07 13:20:24 +0000

    add a page for managing module aliases

---
+++ html/code/modules/modules/xaradmin/aliases.php
@@ -0,0 +1,16 @@
+<?php
+    function modules_admin_aliases($args)
+    {
+        if (!xarVarFetch('name',   'str', $modname,     NULL, XARVAR_NOT_REQUIRED)) {return;}
+        if (!xarVarFetch('remove', 'str', $removealias, NULL, XARVAR_NOT_REQUIRED)) {return;}
+        if (!xarVarFetch('add',    'str', $addalias,    NULL, XARVAR_NOT_REQUIRED)) {return;}
+        if (!empty($removealias) && !empty($modname)) {
+            xarModAlias::delete($removealias, $modname);
+        } elseif (!empty($addalias) && !empty($modname)) {
+            xarModAlias::set($addalias, $modname);
+        }
+        $data['modname'] = $modname;
+        $data['aliasesMap'] = xarConfigVars::get(null,'System.ModuleAliases');
+        return $data;
+    }
+?>
