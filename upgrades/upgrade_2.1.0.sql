8c3962976ff50e774e778cb63ed6b05cfe414d35
mfl <mfl@netspan.ch>
2010-02-14 15:54:30 +0000

    change the name of a configuration setting

--- upgrades/upgrade_2.1.0.sql
+++ upgrades/upgrade_2.1.0.sql
@@ -6,3 +6,9 @@
 /* Merging the blockgroups and blocks tables */
 /* FIXME: add SQL to move blockgroup data from xar_block_groups to xar_block_instances*/
 DROP TABLE xar_block_groups;
+
+/* Changing the name of the installation_rows configuration to installation_addremove */
+UPDATE `xar_dynamic_configurations` SET `name` = REPLACE(name, "installation_rows", "installation_addremove");
+UPDATE `xar_dynamic_configurations` SET `description` = REPLACE(description, "Allow adding of rows", "Allow adding of items");
+UPDATE `xar_dynamic_configurations` SET `label` = REPLACE(label, "Add/Delete Rows", "Add/Delete Items");
+UPDATE `xar_dynamic_properties` SET `configuration` = REPLACE(configuration, "Add/Delete Rows", "Add/Delete Items");
