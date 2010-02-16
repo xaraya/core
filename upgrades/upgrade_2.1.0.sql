bd66f711dae03f3d2ed5f3e2620ee55e5365cc86
mfl <mfl@netspan.ch>
2010-02-16 14:37:16 +0000

    change the name of a configuration setting

--- upgrades/upgrade_2.1.0.sql
+++ upgrades/upgrade_2.1.0.sql
@@ -9,6 +9,6 @@ DROP TABLE xar_block_groups;
 
 /* Changing the name of the installation_rows configuration to installation_addremove */
 UPDATE `xar_dynamic_configurations` SET `name` = REPLACE(name, "installation_rows", "installation_addremove");
-UPDATE `xar_dynamic_configurations` SET `description` = REPLACE(description, "Allow adding of rows", "Allow adding of items");
-UPDATE `xar_dynamic_configurations` SET `label` = REPLACE(label, "Add/Delete Rows", "Add/Delete Items");
+UPDATE `xar_dynamic_configurations` SET `description` = REPLACE(description, "Allow adding of rows", "Allow adding/removing of items");
+UPDATE `xar_dynamic_configurations` SET `label` = REPLACE(label, "Add/Delete Rows", "Add/Remove Items");
 UPDATE `xar_dynamic_properties` SET `configuration` = REPLACE(configuration, 's:19:"initialization_rows";', 's:24:"initialization_addremove";');
