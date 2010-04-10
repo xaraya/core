9c3ab575205b09040599b941d8007ed8c7f074f2
mfl <mfl@netspan.ch>
2010-04-10 07:59:15 +0000

    move the template transforms to the legacy folder as a separate function call. do a fullset while were at it

---
+++ html/lib/xaraya/legacy/template_transforms.php
@@ -0,0 +1,134 @@
+<?php
+    function fixLegacy()
+    {
+        // Quick & dirty wrapper for missing xmlns:xar in old 1.x templates
+        if (!strpos($this->prepXml, ' xmlns:xar="') && !strpos($this->prepXml, '</xar:template>')) {
+            $this->prepXml = '<?xml version="1.0" encoding="utf-8"?>
+<xar:template xmlns:xar="http://xaraya.com/2004/blocklayout">' . $this->prepXml . '</xar:template>';
+
+            // Replace non-numeric entitites in old 1.x templates
+            $this->prepXml = str_replace(array_keys(entities()),entities(),$this->prepXml);
+
+            // Allow xar:set name="$var" in old 1.x templates
+            $this->prepXml = str_replace('<xar:set name="$','<xar:set name="',$this->prepXml);
+
+            // Stop eating </textarea> in old 1.x templates
+            $this->prepXml = str_replace('></textarea>','>&#160;</textarea>',$this->prepXml);
+
+            // Javascript include in old 1.x templates
+            $this->prepXml = str_replace('<xar:base-include-javascript','<xar:javascript',$this->prepXml);
+        }
+    }
+
+    function entities()
+    {
+        return array(
+                '&amp;'    =>      '&#38;',
+                '&lsquo;'    =>      '&#145;',
+                '&rsquo;'    =>      '&#146;',
+                '&ldquo;'    =>      '&#147;',
+                '&rdquo;'    =>      '&#148;',
+                '&bull'    =>      '&#149;',
+                '&en;'    =>      '&#150;',
+                '&em;'    =>      '&#151;',
+
+                '&nbsp;'    =>      '&#160;',
+                '&iexcl;'    =>      '&#161;',
+                '&cent;'    =>      '&#162;',
+                '&pound;'    =>      '&#163;',
+                '&curren;'    =>      '&#164;',
+                '&yen;'    =>      '&#165;',
+                '&brvbar;'    =>      '&#166;',
+                '&sect;'    =>      '&#167;',
+                '&uml;'    =>      '&#168;',
+                '&copy;'    =>      '&#169;',
+                '&ordf;'    =>      '&#170;',
+                '&laquo;'    =>      '&#171;',
+                '&not;'    =>      '&#172;',
+                '&shy;'    =>      '&#173;',
+                '&reg;'    =>      '&#174;',
+                '&macr;'    =>      '&#175;',
+                '&deg;'    =>      '&#176;',
+                '&plusmn;'    =>      '&#177;',
+                '&sup2;'    =>      '&#178;',
+                '&sup3;'    =>      '&#179;',
+                '&acute;'    =>      '&#180;',
+                '&micro;'    =>      '&#181;',
+                '&para;'    =>      '&#182;',
+                '&middot;'    =>      '&#183;',
+                '&cedil;'    =>      '&#184;',
+                '&sup1;'    =>      '&#185;',
+                '&ordm;'    =>      '&#186;',
+                '&raquo;'    =>      '&#187;',
+                '&frac14;'    =>      '&#188;',
+                '&frac12;'    =>      '&#189;',
+                '&frac34;'    =>      '&#190;',
+                '&iquest;'    =>      '&#191;',
+                '&Agrave;'    =>      '&#192;',
+                '&Aacute;'    =>      '&#193;',
+                '&Acirc;'    =>      '&#194;',
+                '&Atilde;'    =>      '&#195;',
+                '&Auml;'    =>      '&#196;',
+                '&Aring;'    =>      '&#197;',
+                '&AElig;'    =>      '&#198;',
+                '&Ccedil;'    =>      '&#199;',
+                '&Egrave;'    =>      '&#200;',
+                '&Eacute;'    =>      '&#201;',
+                '&Ecirc;'    =>      '&#202;',
+                '&Euml;'    =>      '&#203;',
+                '&Igrave;'    =>      '&#204;',
+                '&Iacute;'    =>      '&#205;',
+                '&Icirc;'    =>      '&#206;',
+                '&Iuml;'    =>      '&#207;',
+                '&ETH;'    =>      '&#208;',
+                '&Ntilde;'    =>      '&#209;',
+                '&Ograve;'    =>      '&#210;',
+                '&Oacute;'    =>      '&#211;',
+                '&Ocirc;'    =>      '&#212;',
+                '&Otilde;'    =>      '&#213;',
+                '&Ouml;'    =>      '&#214;',
+                '&times;'    =>      '&#215;',
+                '&Oslash;'    =>      '&#216;',
+                '&Ugrave;'    =>      '&#217;',
+                '&Uacute;'    =>      '&#218;',
+                '&Ucirc;'    =>      '&#219;',
+                '&Uuml;'    =>      '&#220;',
+                '&Yacute;'    =>      '&#221;',
+                '&THORN;'    =>      '&#222;',
+                '&szlig;'    =>      '&#223;',
+                '&agrave;'    =>      '&#224;',
+                '&aacute;'    =>      '&#225;',
+                '&acirc;'    =>      '&#226;',
+                '&atilde;'    =>      '&#227;',
+                '&auml;'    =>      '&#228;',
+                '&aring;'    =>      '&#229;',
+                '&aelig;'    =>      '&#230;',
+                '&ccedil;'    =>      '&#231;',
+                '&egrave;'    =>      '&#232;',
+                '&eacute;'    =>      '&#233;',
+                '&ecirc;'    =>      '&#234;',
+                '&euml;'    =>      '&#235;',
+                '&igrave;'    =>      '&#236;',
+                '&iacute;'    =>      '&#237;',
+                '&icirc;'    =>      '&#238;',
+                '&iuml;'    =>      '&#239;',
+                '&eth;'    =>      '&#240;',
+                '&ntilde;'    =>      '&#241;',
+                '&ograve;'    =>      '&#242;',
+                '&oacute;'    =>      '&#243;',
+                '&ocirc;'    =>      '&#244;',
+                '&otilde;'    =>      '&#245;',
+                '&ouml;'    =>      '&#246;',
+                '&divide;'    =>      '&#247;',
+                '&oslash;'    =>      '&#248;',
+                '&ugrave;'    =>      '&#249;',
+                '&uacute;'    =>      '&#250;',
+                '&ucirc;'    =>      '&#251;',
+                '&uuml;'    =>      '&#252;',
+                '&yacute;'    =>      '&#253;',
+                '&thorn;'    =>      '&#254;',
+                '&yuml;'    =>      '&#255;',
+                '&bull;'    =>      '&#8226;',
+                );
+    }
+?>
