79bb39d307904c2451462a6df0f7a6fe1d6a11da
mfl <mfl@netspan.ch>
2010-04-26 15:19:00 +0000

    break up the installer file and lose the bits we no longer need

---
+++ html/code/modules/installer/xaradmin/phase1.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Installer
+ *
+ * @package Installer
+ * @copyright see the html/credits.html file in this release
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ *
+ * @subpackage Installer
+ * @link http://xaraya.com/index.php/release/200.html
+ */
+
+/* Do not allow this script to run if the install script has been removed.
+ * This assumes the install.php and index.php are in the same directory.
+ * @author Paul Rosania
+ * @author Marcel van der Boom <marcel@hsdev.com>
+ */
+
+/**
+ * Phase 1: Welcome (Set Language and Locale) Page
+ *
+ * @access private
+ * @return data array of language values
+ */
+function installer_admin_phase1()
+{
+    if (!file_exists('install.php')) { throw new Exception('Already installed');}
+    xarVarFetch('install_language','str::',$install_language, 'en_US.utf-8', XARVAR_NOT_REQUIRED);
+
+    // Get the installed locales
+    $locales = xarMLSListSiteLocales();
+
+    // Construct the array for the selectbox (iso3code, string in own locale)
+    if(!empty($locales)) {
+        $languages = array();
+        foreach ($locales as $locale) {
+            // Get the isocode and the description
+            // Before we load the locale data, let's check if the locale is there
+
+            // <marco> This check is really not necessary since available locales are
+            // already determined from existing files. The relative code is in install.php
+            //$fileName = sys::varpath() . "/locales/$locale/locale.xml";
+            //if(file_exists($fileName)) {
+            $locale_data =& xarMLSLoadLocaleData($locale);
+            $languages[$locale] = $locale_data['/language/display'];
+            //}
+        }
+    }
+
+    $data['install_language'] = $install_language;
+    $data['languages'] = $languages;
+    $data['phase'] = 1;
+    $data['phase_label'] = xarML('Step One');
+
+    return $data;
+}
+?>
