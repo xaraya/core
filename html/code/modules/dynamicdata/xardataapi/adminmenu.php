11a73124be880926d4c6b5abb034921191a04879
mfl <mfl@netspan.ch>
2010-05-15 08:28:09 +0000

    align some dd file names with standard convention

--- html/code/modules/dynamicdata/xardataapi/adminmenu.php
+++ html/code/modules/dynamicdata/xardataapi/adminmenu.php
@@ -14,10 +14,10 @@
         return array(
                 array('includes' => array('main','overview'), 'target' => 'main', 'label' => xarML('Overview')),
                 array('mask' => 'EditDynamicData', 'includes' => array('view','new','modify','modifyprop'), 'target' => 'view', 'title' => xarML('View dataobjects using dynamic data'), 'label' => xarML('DataObjects')),
-                array('mask' => 'AdminDynamicData', 'includes' => 'modifyconfig', 'target' => 'modifyconfig', 'title' => xarML('Configure the default dataproperty types'), 'label' => xarML('DataProperty Types')),
+                array('mask' => 'AdminDynamicData', 'includes' => 'view_propertydefs', 'target' => 'view_propertydefs', 'title' => xarML('Configure the default dataproperty types'), 'label' => xarML('DataProperty Types')),
 //                array('mask' => 'AdminDynamicData', 'includes' => 'relations', 'target' => 'relations', 'title' => xarML('Configure relationships'), 'label' => xarML('Relationships')),
                 array('mask' => 'AdminDynamicData', 'includes' => array('utilities','query','util'), 'target' => 'utilities', 'title' => xarML('Import/export and other utilities'), 'label' => xarML('Utilities')),
-                array('mask' => 'AdminDynamicData', 'includes' => array('modifymoduleconfig'), 'target' => 'modifymoduleconfig', 'title' => xarML('Modify the configuration of this module'), 'label' => xarML('Modify Configuration')),
+                array('mask' => 'AdminDynamicData', 'includes' => array('modifyconfig'), 'target' => 'modifyconfig', 'title' => xarML('Modify the configuration of this module'), 'label' => xarML('Modify Configuration')),
         );
     }
 ?>
