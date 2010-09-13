d8c704916ee42e5cf54356369440e69861f313d7
mfl <mfl@netspan.ch>
2010-09-13 06:27:38 +0000

    change default key and hint

--- html/lib/xaraya/encryption.php
+++ html/lib/xaraya/encryption.php
@@ -1,7 +1,7 @@
 <?php
-    $encryption['cipher'] = 'cast-128';
+    $encryption['cipher'] = 'blowfish';
     $encryption['mode'] = 'ecb';
-    $encryption['key'] = '770105b98be2fdd6a6f096d29ca5632c';
-    $encryption['hint'] = 'dorkington';
+    $encryption['key'] = '33ee7e1eb504b6619c1b445ca1442c21';
+    $encryption['hint'] = 'war and ...';
     $encryption['initvector'] = 'xaraya2x';
 ?>
