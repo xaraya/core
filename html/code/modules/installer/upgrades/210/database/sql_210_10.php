88e640c723010b686fdc928d416857d34e729039
mfl <mfl@netspan.ch>
2010-11-05 11:37:58 +0000

    standardize file level docblocks of the installer module

--- html/code/modules/installer/upgrades/210/database/sql_210_10.php
+++ html/code/modules/installer/upgrades/210/database/sql_210_10.php
@@ -2,11 +2,12 @@
 /**
  * @package modules
  * @subpackage installer module
+ * @category Xaraya Web Applications Framework
+ * @version 2.2.0
  * @copyright see the html/credits.html file in this release
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
- *
- * @link http://xaraya.com/index.php/release/200.html
+ * @link http://xaraya.com/index.php/release/42.html
  */
 
 function sql_210_10()
