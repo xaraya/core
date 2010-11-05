ba8b99793fdb1957c8fa9675f712c62294794d68
mfl <mfl@netspan.ch>
2010-11-05 11:15:54 +0000

    standardize file level docblocks of the blocks module

--- html/code/modules/blocks/xaradmin/new_type.php
+++ html/code/modules/blocks/xaradmin/new_type.php
@@ -3,10 +3,11 @@
  * Register a new block type
  * @package modules
  * @subpackage blocks module
+ * @category Xaraya Web Applications Framework
+ * @version 2.2.0
  * @copyright see the html/credits.html file in this release
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
- *
  * @link http://xaraya.com/index.php/release/13.html
  */
 /**
--- html/code/modules/blocks/xaradminapi/unregister_block_type.php
+++ html/code/modules/blocks/xaradminapi/unregister_block_type.php
@@ -3,10 +3,11 @@
  * Unregister block types
  * @package modules
  * @subpackage blocks module
+ * @category Xaraya Web Applications Framework
+ * @version 2.2.0
  * @copyright see the html/credits.html file in this release
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
- *
  * @link http://xaraya.com/index.php/release/13.html
  */
 /**
