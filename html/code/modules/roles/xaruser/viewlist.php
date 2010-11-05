5e9fd452e21be95a92501e04d80cabeecc478fd2
mfl <mfl@netspan.ch>
2010-11-05 15:15:18 +0000

    standardize file level docblocks of the roles module

--- html/code/modules/roles/xaruser/viewlist.php
+++ html/code/modules/roles/xaruser/viewlist.php
@@ -4,11 +4,12 @@
  *
  * @package modules
  * @subpackage roles module
+ * @category Xaraya Web Applications Framework
+ * @version 2.2.0
  * @copyright see the html/credits.html file in this release
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
- *
- * 
+ * @link http://xaraya.com/index.php/release/27.html
  */
 /**
  * @author  Marc Lutolf <marcinmilan@xaraya.com>
