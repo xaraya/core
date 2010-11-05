4806f91c0ae119cb6f76aa24b288d7c588ba2b14
mfl <mfl@netspan.ch>
2010-11-05 12:17:16 +0000

    standardize file level docblocks of the dynamicdata module

--- html/code/modules/dynamicdata/xarutil/export.php
+++ html/code/modules/dynamicdata/xarutil/export.php
@@ -2,11 +2,13 @@
 /**
  * @package modules
  * @subpackage dynamicdata module
+ * @category Xaraya Web Applications Framework
+ * @version 2.2.0
  * @copyright see the html/credits.html file in this release
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
- *
  * @link http://xaraya.com/index.php/release/182.html
+ *
  * @author mikespub <mikespub@xaraya.com>
  * @todo move the xml generate code to a template based system.
  */
