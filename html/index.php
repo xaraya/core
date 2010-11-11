05edd73d977c835a960b79b1a1181fdfdb266685
mfl <mfl@netspan.ch>
2010-11-11 08:40:12 +0000

    the 2.1.0 release

--- html/index.php
+++ html/index.php
@@ -6,7 +6,7 @@
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  * @subpackage entrypoint
- * @author Marco Canini 
+ * @author Marco Canini
  */
 
 $GLOBALS["Xaraya_PageTime"] = microtime(true);
