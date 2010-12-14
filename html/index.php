96c21849c560be7b81feaae48601eba47843f766
mfl <mfl@netspan.ch>
2010-12-14 16:45:56 +0000

    logged in users with admin type go to the admin template

--- html/index.php
+++ html/index.php
@@ -123,7 +123,7 @@ function xarMain()
         xarEvents::trigger('ServerRequest');
 
         // Set page template
-        if ($modType == 'admin' && xarTplGetPageTemplateName() == 'default') {
+        if (xarUserIsLoggedIn() && $modType == 'admin' && xarTplGetPageTemplateName() == 'default') {
              // Use the admin-$modName.xt page if available when $modType is admin
             // falling back on admin.xt if the former isn't available
             if (!xarTplSetPageTemplateName('admin-'.$modName)) {
