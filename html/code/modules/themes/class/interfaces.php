6b68060228c7ae163e5a37a754fa2c6dc10e6232
mfl <mfl@netspan.ch>
2010-12-15 20:42:38 +0000

    interface file for themes init

--- html/code/modules/themes/class/interfaces.php
+++ html/code/modules/themes/class/interfaces.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Interfaces for Dynamic Properties
+ * Interfaces for Themes
  *
  * @package modules
  * @subpackage dynamicdata module
@@ -12,28 +12,8 @@
  * @link http://xaraya.com/index.php/release/182.html
  */
 
-interface iDataProperty
+interface iThemeInit
 {
-    public function __construct(ObjectDescriptor $descriptor);
-    public function checkInput($name = '', $value = null);
-    public function fetchValue($name = '');
-    public static function getRegistrationInfo();
-    public function getValue();
-    public function parseConfiguration($configuration = '');
-    public function showConfiguration(Array $data = array());
-    public function updateConfiguration(Array $data = array());
-    public function setValue($value=null);
-    public function showHidden(Array $args = array());
-    public function showInput(Array $args = array());
-    public function showLabel(Array $args = array());
-//    CHECKME: public  or what?
-//    public function _showPreset(Array $args = array());
-    public function showOutput(Array $args = array());
-    public function validateValue($value = null);
-}
-
-interface iDataPropertyInstall
-{
-    public function install(Array $data=array());
+    public function init(Array $data=array());
 }
 ?>
