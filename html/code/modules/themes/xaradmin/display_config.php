9d53ff00af8c90bb64d0149b09b1cf37f481b0d7
mfl <mfl@netspan.ch>
2010-12-25 13:57:49 +0000

    add modify and display pages for theme configurations

---
+++ html/code/modules/themes/xaradmin/display_config.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package modules
+ * @subpackage themes module
+ * @category Xaraya Web Applications Framework
+ * @version 2.2.0
+ * @copyright see the html/credits.html file in this release
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ * @link http://xaraya.com/index.php/release/70.html
+ */
+
+sys::import('modules.dynamicdata.class.objects.master');
+
+function themes_admin_display_config(Array $args=array())
+{
+    if (!xarVarFetch('itemid' ,    'int',    $data['itemid'] , 0 ,          XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('confirm',    'bool',   $data['confirm'], false,       XARVAR_NOT_REQUIRED)) return;
+
+    $data['object'] = DataObjectMaster::getObject(array('name' => 'themes_configurations'));
+
+    if (!isset($data['object'])) return;
+    if (!$data['object']->checkAccess('display'))
+        return xarResponse::Forbidden(xarML('Display #(1) is forbidden', $data['object']->label));
+
+    $data['object']->getItem(array('itemid' => $data['itemid']));
+    return $data;
+}
+
+?>
