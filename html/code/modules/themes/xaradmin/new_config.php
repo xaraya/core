9e85767c6a9a4abed702ecad650f455476eaba03
mfl <mfl@netspan.ch>
2010-12-25 14:20:33 +0000

    add delete and new pages for theme configurations

---
+++ html/code/modules/themes/xaradmin/new_config.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Create a new configuration option
+ *
+ * @package modules
+ * @subpackage themes module
+ * @category Xaraya Web Applications Framework
+ * @version 2.2.0
+ * @copyright see the html/credits.html file in this release
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
+ * @link http://xaraya.com/index.php/release/70.html
+ */
+sys::import('modules.dynamicdata.class.objects.master');
+
+function themes_admin_new_config()
+{
+    // Security
+//die("X");
+//    if(!xarSecurityCheck('AddThemes')) return;
+
+    if (!xarVarFetch('confirm',    'bool',   $data['confirm'], false,     XARVAR_NOT_REQUIRED)) return;
+
+    $data['object'] = DataObjectMaster::getObject(array('name' => 'themes_configurations'));
+    if ($data['confirm']) {
+    
+        // we only retrieve 'preview' from the input here - the rest is handled by checkInput()
+        if(!xarVarFetch('preview', 'str', $preview,  NULL, XARVAR_DONT_SET)) {return;}
+
+        // Check for a valid confirmation key
+        if(!xarSecConfirmAuthKey()) return;
+        
+        // Get the data from the form
+        $isvalid = $data['object']->checkInput();
+        
+        if (!$isvalid) {
+            // Bad data: redisplay the form with error messages
+            return xarTplModule('themes','admin','new_config', $data);        
+        } else {
+            // Good data: create the item
+            $itemid = $data['object']->createItem();
+            
+            // Jump to the next page
+            xarResponse::redirect(xarModURL('themes','admin','view_configs'));
+            return true;
+        }
+    }
+    return $data;
+}
+
+?>
