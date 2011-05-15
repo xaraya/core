e0b9b83c4dbb5498e6eac54ec4785198dff4791d
mfl <mfl@netspan.ch>
2011-05-15 16:26:37 +0000

    Split off this release scenario

--- html/code/modules/installer/upgrades/220/main.php
+++ html/code/modules/installer/upgrades/220/main.php
@@ -29,6 +29,7 @@ function main_upgrade_220()
                         'sql_220_11', // Add a configvar for the ssl port
                         'sql_220_12', // Redefine the config property of the objects object
                         'sql_220_13', // Refactor roles name as textbox property
+                        'sql_220_14', // Split dataobject's access off from its configuration
                         
                     );
     foreach ($upgrades as $upgrade) {
