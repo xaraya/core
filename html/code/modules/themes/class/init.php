b693774ac07f34584ce72c3b4d3e06d6fe2ae9c4
mfl <mfl@netspan.ch>
2011-06-04 12:24:28 +0000

    move the configuration file to the top level

--- html/code/modules/themes/class/init.php
+++ html/code/modules/themes/class/init.php
@@ -17,7 +17,7 @@ class ThemeInit implements iThemeInit
 {
     public function init(Array $data=array())
     {
-        $dat_file = 'themes/' . $data['name'] . '/data/themes_configurations-dat.xml';
+        $dat_file = 'themes/' . $data['name'] . '/configuration.xml';
         $data = array('file' => $dat_file);
         try {
             $objectid = xarMod::apiFunc('dynamicdata','util','import', $data);
