1a2e27e1f630d10d9e22d5e63540f70c36615654
mfl <mfl@netspan.ch>
2011-07-10 05:47:13 +0000

    add the initial part of the 230 upgrade routine

--- html/code/modules/installer/xaradmin/upgrade.php
+++ html/code/modules/installer/xaradmin/upgrade.php
@@ -51,6 +51,7 @@ function installer_admin_upgrade()
                                 '2.1.2',
                                 '2.1.3',
                                 '2.2.0',
+                                '2.3.0',
     );
     
         
