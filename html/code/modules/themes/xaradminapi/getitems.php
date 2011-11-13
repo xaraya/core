56846860ba3e215137546f730d402598a8b66127
crispcreations <crispcreations@googlemail.com>
2011-11-13 15:31:11 +0000

    Themes: align getlist, getthemelist with getitems api function

--- html/code/modules/themes/xaradminapi/getitems.php
+++ html/code/modules/themes/xaradminapi/getitems.php
@@ -63,13 +63,12 @@ function themes_adminapi_getitems(Array $args=array())
         $bindvars[] = $class;
     }  
 
-    $sorts = strpos($sort, ',') !== false ? explode(',', $sort) : array($sort);
-    foreach ($sorts as $pairs) {
-        $pair = explode(' ', $pairs);
-        $sortfield = trim($pair[0]);
-        if (!isset($select[$sortfield])) continue;
-        $sortorder = isset($pair[1]) ? trim(strtoupper($pair[1])) : 'ASC';
-        $orderby[] = $select[$sortfield] . ' ' . $sortorder;
+    if (!is_array($sort))
+        $sort = strpos($sort, ',') !== false ? array_map('trim', explode(',', $sort)) : array(trim($sort));
+    foreach ($sort as $pairs) {
+        list($sortfield, $sortorder) = array_map('trim', array_pad(explode(' ', $pairs), 2, 'ASC'));
+        if (!isset($select[$sortfield]) || isset($orderby[$sortfield])) continue;
+        $orderby[$sortfield] = $select[$sortfield] . ' ' . strtoupper($sortorder);
     }
 
     $query = "SELECT " . join(',', $select);
@@ -88,22 +87,79 @@ function themes_adminapi_getitems(Array $args=array())
     }
     $result = $stmt->executeQuery($bindvars);
 
-    $themes = array();
+    $items = array();
     while($result->next()) {
-        $theme = array();
-        list($theme['regid'], 
-            $theme['name'], 
-            $theme['directory'], 
-            $theme['state'], 
-            $theme['class']) = $result->fields;
-        $theme['osdirectory'] = xarVarPrepForOS($theme['directory']);
-        $info = xarTheme_getFileInfo($theme['osdirectory']);
-        if (!$info) continue;
-        $theme += $info;
-        $themes[] = $theme;
+        $item = array();
+        foreach (array_keys($select) as $field)
+            $item[$field] = array_shift($result->fields);
+
+        if (xarVarIsCached('Theme.Infos', $item['regid'])) {
+            // merge cached info with db info 
+            $item += xarVarGetCached('Theme.Infos', $item['regid']);
+        } else {
+            $item['displayname'] = $item['name'];
+            // Shortcut for os prepared directory 
+            $item['osdirectory'] = xarVarPrepForOS($item['directory']);
+            
+            xarVarSetCached('Theme.BaseInfos', $item['name'], $item);                   
+
+            $fileinfo = xarTheme_getFileInfo($item['osdirectory']);
+            if (isset($fileinfo)) {
+                $item = array_merge($fileinfo, $item);
+                xarVarSetCached('Theme.Infos', $item['regid'], $item);
+                switch ($item['state']) {
+                case XARTHEME_STATE_MISSING_FROM_UNINITIALISED:
+                    $item['state'] = XARTHEME_STATE_UNINITIALISED;
+                    break;
+                case XARTHEME_STATE_MISSING_FROM_INACTIVE:
+                    $item['state'] = XARTHEME_STATE_INACTIVE;
+                    break;
+                case XARTHEME_STATE_MISSING_FROM_ACTIVE:
+                    $item['state'] = XARTHEME_STATE_ACTIVE;
+                    break;
+                case XARTHEME_STATE_MISSING_FROM_UPGRADED:
+                    $item['state'] = XARTHEME_STATE_UPGRADED;
+                    break;
+                }
+            } else {
+                // There was an entry in the database which was not in the file system,
+                // @CHECKME: do we really want to do this here? 
+                // <chris/> I think not, this is handled by refresh and managed in list UI 
+                /*
+                // This functionality was present in getlist api function
+                // remove the entry from the database
+                xarMod::apiFunc('themes', 'admin', 'remove', array('regid' => $item['regid']));
+                continue;
+                */
+                // <chris/> instead, let's apply the correct state and pass through 
+                // This functionality was present in getthemelist api 
+                // Following changes were applied by <andyv> on 21st May 2003
+                // as per the patch by Garrett Hunter
+                // Credits: Garrett Hunter <Garrett.Hunter@Verizon.net>
+                switch ($item['state']) {
+                case XARTHEME_STATE_UNINITIALISED:
+                    $item['state'] = XARTHEME_STATE_MISSING_FROM_UNINITIALISED;
+                    break;
+                case XARTHEME_STATE_INACTIVE:
+                    $item['state'] = XARTHEME_STATE_MISSING_FROM_INACTIVE;
+                    break;
+                case XARTHEME_STATE_ACTIVE:
+                    $item['state'] = XARTHEME_STATE_MISSING_FROM_ACTIVE;
+                    break;
+                case XARTHEME_STATE_UPGRADED:
+                    $item['state'] = XARTHEME_STATE_MISSING_FROM_UPGRADED;
+                    break;
+                }
+                //$item['class'] = "";
+                $item['version'] = "&#160;";
+
+            }
+        }
+        
+        $items[] = $item;
     }
     $result->close();
     
-    return $themes;
+    return $items;
 }
 ?>
