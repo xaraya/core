b2c1dee57816a97cea414c8128cc44e03ce1600a
mfl <mfl@netspan.ch>
2011-12-03 11:04:09 +0000

    more work on property and block MLS

--- html/lib/xaraya/mls.php
+++ html/lib/xaraya/mls.php
@@ -524,19 +524,41 @@ function xarMLSLoadTranslations($path)
         return true;
     }
 
+    // If this is a core file, get the translations and bail
+    if (strpos($path, sys::lib()) === 0) {
+        $translations = xarMLS_loadTranslations(XARMLS_DNTYPE_CORE, 'xaraya', 'core:', 'core');
+        return $translations;        
+    }
+    
+    // Remove the parts of the path before the dnType marker ('xaraya', 'modules' etc.)
+    if (strpos($path, sys::code()) === 0) {
+        // This is a module, property or block file
+        $path = substr($path, strlen(sys::code()));
+    } elseif (strpos($path, 'themes') === 0) {
+        // This is a theme file
+    } else {
+        throw new Exception('Unknown MLS path: ' . $path);
+    }
+    
     // Get a structured representation of the path.
     $pathElements = explode("/",$path);
 
-    // Initialise some defaults
-    $dnType = XARMLS_DNTYPE_MODULE; $possibleOverride = false; $ctxType = 'modules';
-
     // Determine dnType
-    // Lets get core files out of the way
-    if($pathElements[0] == 'lib') return xarMLS_loadTranslations(XARMLS_DNTYPE_CORE, 'xaraya', 'core:', 'core');
 
-    // modules have a fixed place, so if it's not 'modules/blah/blah' it's themes, period.
-    // NOTE: $pathElements changes here!
-    if(array_shift($pathElements) != 'modules') {
+    $firstelement = array_shift($pathElements);
+    if ($firstelement == 'modules') {
+        $dnType = XARMLS_DNTYPE_MODULE;
+        $possibleOverride = false;
+        $ctxType= 'modules';
+    } elseif ($firstelement == 'properties') {
+        $dnType = XARMLS_DNTYPE_PROPERTY;
+        $possibleOverride = false;
+        $ctxType= 'properties';
+    } elseif ($firstelement == 'blocks') {
+        $dnType = XARMLS_DNTYPE_BLOCK;
+        $possibleOverride = false;
+        $ctxType= 'blocks';
+    } else {
         $dnType = XARMLS_DNTYPE_THEME;
         $possibleOverride = true;
         $ctxType= 'themes';
