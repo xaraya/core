bdbc3756bddf631eb41c86b4e532bce0e194ee32
mfl <mfl@netspan.ch>
2012-03-04 18:05:10 +0000

    improve template cascade for standalone properties

--- html/lib/xaraya/templates.php
+++ html/lib/xaraya/templates.php
@@ -741,20 +741,30 @@ class xarTpl extends Object
             return self::executeFromFile($sourceFileName, $tplData);
         }
 
-        $tplModule = DataPropertyMaster::getProperty(array('type' => $propertyName))->tplmodule; 
-        
-        if ($modName == 'auto') {
-            // standalone property called in standalone context 
-            $sourceFileName = self::getScopeFileName('property', $propertyName, $tplBase, $propertyName);
-        } else {
-            // property called in module context
-            if ($tplModule == 'auto') {
-                // standalone property (caller > owner) 
-                $sourceFileName = self::getScopeFileName('property', $propertyName, $tplBase, $propertyName, '', $modName);
-            } else {
-                // module property (caller > owner)
-                $sourceFileName = self::getScopeFileName('module', $tplModule, $tplBase, $propertyName, 'properties', $modName);
+        $sourceFileName = self::getScopeFileName('module', $modName, $tplBase, $propertyName, 'properties');
 
+        // Property fall-back to default template in the module the property belongs to
+        if (empty($sourceFileName)) {
+            $tplModule = DataPropertyMaster::getProperty(array('type' => $propertyName))->tplmodule; 
+            
+            if ($modName == 'auto') {
+                // standalone property called in standalone context 
+                $sourceFileName = self::getScopeFileName('property', $propertyName, $tplBase, $propertyName);
+                
+                if (empty($sourceFileName)) {
+                    // property inherits its template
+                    $sourceFileName = self::getScopeFileName('module', $tplModule, $tplBase, $propertyName, 'properties', $modName);
+                }
+            } else {
+                // property called in module context
+                if ($tplModule == 'auto') {
+                    // standalone property (caller > owner) 
+                    $sourceFileName = self::getScopeFileName('property', $propertyName, $tplBase, $propertyName, '', $modName);
+                } else {
+                    // module property (caller > owner)
+                    $sourceFileName = self::getScopeFileName('module', $tplModule, $tplBase, $propertyName, 'properties', $modName);
+    
+                }
             }
         }
 
