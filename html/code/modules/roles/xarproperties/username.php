17ba0a95a35ddc0c42ac8edcb44154c6bb05b906
mfl <mfl@netspan.ch>
2012-11-29 15:49:22 +0000

    username.validateValue to look for existing users with hte same name in ANY state

--- html/code/modules/roles/xarproperties/username.php
+++ html/code/modules/roles/xarproperties/username.php
@@ -54,7 +54,7 @@ class UsernameProperty extends TextBoxProperty
         // We allow the special value [All]
         
         if ($value != '[All]') {
-        $role = xarRoles::ufindRole($value);        
+            $role = xarRoles::ufindRole($value, xarRoles::ROLES_USERTYPE, xarRoles::ROLES_STATE_ALL);
             switch ((int)$this->validation_existrule) {
                 case 1:
                 if (!empty($role)) {
