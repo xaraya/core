39a3a2d466196150bbb08fed5f6e6d9ff9524f02
Marc Lutolf <mfl@netspan.ch>
2013-05-10 00:49:18 +0200

    Cosmetics: modinfonew -> modifno

--- html/code/modules/modules/xaradmin/modinfo.php
+++ html/code/modules/modules/xaradmin/modinfo.php
@@ -1,6 +1,5 @@
 <?php
 /**
- * View complete module information/details
  * @package modules
  * @subpackage modules module
  * @category Xaraya Web Applications Framework
@@ -13,7 +12,7 @@
 /**
  * View complete module information/details
  * function passes the data to the template
- *
+ * opens in new window when browser is javascript enabled
  * @author Xaraya Development Team
  * @access public
  * @return array data for the template display
@@ -22,18 +21,13 @@
 function modules_admin_modinfo()
 {
     // Security
-    if (!xarSecurityCheck('AdminModules')) return; 
-        
-    if (!xarSecConfirmAuthKey()) {
-        return xarTpl::module('privileges','user','errors',array('layout' => 'bad_author'));
-    }        
-
+    if (!xarSecurityCheck('ViewModules')) return; 
+    
     $data = array();
     
-    if (!xarVarFetch('id', 'int:1:', $id, 0, XARVAR_NOT_REQUIRED)) return; 
-    if (empty($id)) return xarResponse::notFound();
+    if (!xarVarFetch('id', 'notempty', $id)) {return;}
 
-    // obtain maximum information about a module
+    // obtain maximum information about module
     $modinfo = xarMod::getInfo($id);
     
     // data vars for template
@@ -43,36 +37,39 @@ function modules_admin_modinfo()
     $data['moddispname']        = xarVarPrepForDisplay($modinfo['displayname']);
     $data['moddispdesc']        = xarVarPrepForDisplay($modinfo['displaydescription']);
     $data['modlisturl']         = xarModURL('modules', 'admin', 'list');
-    // check for proper icon, if not found display default
-    // also displaying a generic icon now, if it was provided
-    // additionally showing a short message if the icon is missing..
-    $modicon = sys::code() . 'modules/'.$modinfo['directory'].'/xarimages/admin.gif';
-    $modicongeneric = sys::code() . 'modules/'.$modinfo['directory'].'/xarimages/admin_generic.gif';
-    if(file_exists($modicon)){
-        $data['modiconurl']     = xarVarPrepForDisplay($modicon);
-        $data['modiconmsg'] = xarVarPrepForDisplay(xarML('as provided by the author'));
-    }elseif(file_exists($modicongeneric)){
-        $data['modiconurl']     = xarVarPrepForDisplay($modicongeneric);
-        $data['modiconmsg'] = xarVarPrepForDisplay(xarML('Only generic icon has been provided'));
-    }else{
-        $data['modiconurl']     = xarVarPrepForDisplay('modules/modules/xarimages/admin_generic.gif');
-        $data['modiconmsg'] = xarVarPrepForDisplay(xarML('[Original icon is missing.. 
-                                please ask this module developer to provide one in accordance with MDG]'));
+
+    $aliasesMap = xarConfigVars::get(null,'System.ModuleAliases');
+    $aliases = array();
+    foreach ($aliasesMap as $key => $value) {
+        if ($value == $data['modname']) $aliases[] = $key;
     }
-    $data['moddir']             = xarVarPrepForDisplay($modinfo['directory']);
+    $data['aliases']            = !empty($aliases) ? implode(', ', $aliases) : xarML('None');
+    $data['moddir']             = sys::code(). 'modules/' . xarVarPrepForDisplay($modinfo['directory']);
     $data['modclass']           = xarVarPrepForDisplay($modinfo['class']);
     $data['modcat']             = xarVarPrepForDisplay($modinfo['category']);
     $data['modver']             = xarVarPrepForDisplay($modinfo['version']);
-    $data['modauthor']          = preg_replace('/,/', '<br />', xarVarPrepForDisplay($modinfo['author']));
-    $data['modcontact']         = preg_replace('/,/', '<br />',xarVarPrepForDisplay($modinfo['contact']));
-    if(!empty($modinfo['dependency'])){
-        $dependency             = xarML('Working on it...');
+    $data['modauthor']          = xarVarPrepForDisplay($modinfo['author']);
+    $data['modcontact']         = xarVarPrepForDisplay($modinfo['contact']);
+    if(!empty($modinfo['dependencyinfo'])){
+        $dependencies = array();
+        foreach ($modinfo['dependencyinfo'] as $key => $value) {
+            $dependencies[] = $value . ' (' . $key . ')';
+            $data['moddependencies'] = implode(', ', $dependencies);
+        }
     } else {
-        $dependency             = xarML('None');
+        $data['moddependencies']             = xarML('None');
     }
-    $data['moddependency']      = xarVarPrepForDisplay($dependency);
     
-    // Redirect
+    $modname = $modinfo['name'];
+    $subjects = array();
+    $observers = xarEvents::getObserverModules();
+    $hookobservers = xarHooks::getObserverModules($modname);
+    //$hooksubjects = xarHooks::getSubjectModules();
+    
+    if (!empty($hookobservers[$modname]['hooks'])) {
+        $data['hookobservers'] = $hookobservers[$modname]['hooks'];
+    }
+        
     return $data;
 }
 
