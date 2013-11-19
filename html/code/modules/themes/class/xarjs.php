bcf857bbed7a91ad80a2b6bf48c7722f4774c5b5
Marc Lutolf <mfl@netspan.ch>
2013-11-19 18:59:32 +0100

    Minor fixes to get remotes to show properly

--- html/code/modules/themes/class/xarjs.php
+++ html/code/modules/themes/class/xarjs.php
@@ -273,7 +273,7 @@ class xarJS extends Object
         // remove any missing libs
         foreach ($this->local_libs as $compare => $curlib) {
             if (!isset($libs[$compare]))
-                unset($this->$libs[$compare]);
+                unset($this->local_libs[$compare]);
         }
 
     }
@@ -816,14 +816,12 @@ class xarJS extends Object
 **/
     private function getLibInfo($lib, $vers='', $src='')
     {
-        if (empty($src)) return;
-
         // We will look at the local and remote libraries Xaraya is aware of
         // Ceteris paribus, remote libraries will be privileged
         
         $candidates = array();
 
-        if (isset ($this->local_libs[$lib])) {
+        if (isset($this->local_libs[$lib])) {
             $lib = $this->local_libs[$lib];
         
             // Start by taking the latest version
@@ -844,11 +842,9 @@ class xarJS extends Object
         }
 
         foreach ($this->remote_libs as $thislib) {
-            foreach ($dirarray as $file => $filearray) {
-                if (!empty($src) && ($thislib['src'] != $src)) continue;
-                if (!empty($vers) && ($thislib['version'] != $vers)) continue;
-                $candidates[] = $thislib;
-            }
+            if (!empty($src) && ($thislib['src'] != $src)) continue;
+            if (!empty($vers) && ($thislib['version'] != $vers)) continue;
+            $candidates[] = $thislib;
         }
         
         // Sort the libraries by descending version and origin
