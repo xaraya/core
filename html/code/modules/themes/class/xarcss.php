342e27a64ff2cbdc8f1355c2c00f18b043353392
Marc Lutolf <mfl@netspan.ch>
2013-12-09 22:08:49 +0100

    Support for local absolute links

--- html/code/modules/themes/class/xarcss.php
+++ html/code/modules/themes/class/xarcss.php
@@ -145,6 +145,15 @@ class xarCSS extends Object
             'alternatedir' => !empty($alternatedir) ? xarVarPrepForOS($alternatedir) : '',
         );
 
+        // Local absolute url, just include it and return
+        // We support this above all for testing third party stuff we may later integrate
+        $server = xarServer::getHost();
+        if (($tag['method'] == "link") && !empty($tag['source']) &&
+            preg_match("!://($server|localhost|127\.0\.0\.1)(:\d+|)/!",$tag['source'])) {
+            $tag['url'] = $tag['source'];       
+            return $this->queue($method, $scope, $tag['url'], $tag);
+        }
+
         // set additional params based on method
         switch ($method) {
             case 'embed':
