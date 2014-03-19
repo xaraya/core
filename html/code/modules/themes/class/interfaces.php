3c1ed042173db9141ce9eebe74a7b54b22514cbf
Marc Lutolf <mfl@netspan.ch>
2014-03-19 14:03:09 +0100

    Cosmetics

--- html/code/modules/themes/class/interfaces.php
+++ html/code/modules/themes/class/interfaces.php
@@ -3,7 +3,7 @@
  * Interfaces for Themes
  *
  * @package modules
- * @subpackage dynamicdata module
+ * @subpackage themes module
  * @category Xaraya Web Applications Framework
  * @version 2.2.0
  * @copyright see the html/credits.html file in this release
