f987f735ba480d8cbaf941cfb011677b869cdee9
Marc Lutolf <mfl@netspan.ch>
2014-05-24 11:18:18 +0200

    Allow mysqli option in anticipation of PDO

--- html/code/modules/installer/xaradmin/phase4.php
+++ html/code/modules/installer/xaradmin/phase4.php
@@ -40,7 +40,7 @@ function installer_admin_phase4()
 
     // Supported  Databases:
     $data['database_types']      = array('mysql'       => array('name' => 'MySQL'   , 'available' => extension_loaded('mysql')),
-                                         'mysqli'      => array('name' => 'MySQLi' , 'available' => 0 && extension_loaded('mysqli')),
+                                         'mysqli'      => array('name' => 'MySQLi' , 'available' => extension_loaded('mysqli')),
                                          'postgres'    => array('name' => 'Postgres', 'available' => extension_loaded('pgsql')),
                                          'sqlite'      => array('name' => 'SQLite'  , 'available' => extension_loaded('sqlite')),
                                          //'pdosqlite'   => array('name' => 'PDO SQLite'  , 'available' => extension_loaded('pdo_sqlite')),
