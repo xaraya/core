806ad23e32e0d090c42bc69cb64af4d6ef14f73a
Marc Lutolf <mfl@netspan.ch>
2014-11-25 15:33:28 +0100

    Support for the ancestor parameter

--- html/code/modules/roles/xaruserapi/getallgroups.php
+++ html/code/modules/roles/xaruserapi/getallgroups.php
@@ -28,78 +28,96 @@ function roles_userapi_getallgroups(Array $args=array())
     if(!xarSecurityCheck('ViewRoles')) return;
 
     sys::import('xaraya.structures.query');
-    $q = new Query('SELECT');
-    $q->addtable($xartable['roles'],'r');
-    $q->addtable($xartable['rolemembers'], 'rm');
-    $q->leftjoin('r.id','rm.role_id');
-    $q->addfields(array('r.id AS id','r.name AS name','r.users AS users','rm.parent_id AS parentid'));
 
-    $conditions = array();
-    // Restriction by group.
-    if (isset($group)) {
-        $groups = explode(',', $group);
-        foreach ($groups as $group) {
-            $conditions[] = $q->peq('r.name',trim($group));
-        }
-    }
-// Restriction by parent group.
-    if (isset($parent)) {
-        $groups = explode(',', $parent);
-        foreach ($groups as $group) {
-            $group = xarMod::apiFunc(
-                'roles', 'user', 'get',
-                array(
-                    (is_numeric($group) ? 'id' : 'name') => trim($group),
-                    'itemtype' => xarRoles::ROLES_GROUPTYPE
-                )
-            );
-            if (isset($group['id']) && is_numeric($group['id'])) {
-                $conditions[] = $q->peq('rm.parent_id',$group['id']);
+    if (isset($group) || isset($parent)) {
+
+        $q = new Query('SELECT');
+        $q->addtable($xartable['roles'],'r');
+        $q->addtable($xartable['rolemembers'], 'rm');
+        $q->leftjoin('r.id','rm.role_id');
+        $q->addfields(array('r.id AS id','r.name AS name','r.users AS users','rm.parent_id AS parentid'));
+
+        $conditions = array();
+// Restriction by group
+        if (isset($group)) {
+            $groups = explode(',', $group);
+            foreach ($groups as $group) {
+                $conditions[] = $q->peq('r.name',trim($group));
+            }
+
+// Restriction by parent group
+        } elseif (isset($parent)) {
+            $groups = explode(',', $parent);
+            foreach ($groups as $group) {
+                $group = xarMod::apiFunc(
+                    'roles', 'user', 'get',
+                    array(
+                        (is_numeric($group) ? 'id' : 'name') => trim($group),
+                        'itemtype' => xarRoles::ROLES_GROUPTYPE
+                    )
+                );
+                if (isset($group['id']) && is_numeric($group['id'])) {
+                    $conditions[] = $q->peq('rm.parent_id',$group['id']);
+                }
             }
         }
-    }
-// Restriction by ancestor group.
-    // FIXME: this is really broken
-    if (isset($ancestor) && 0 == 1) {
-        $q1 = new Query('SELECT',$xartable['roles']);
-        $q1->addfields(array('id','name'));
-        $q1->eq('itemtype',xarRoles::ROLES_GROUPTYPE);
-        $q1->run();
-        $allgroups = $q1->output();
-        $descendants = array();
+
+        if (count($conditions) != 0) $q->qor($conditions);
+        $q->eq('r.itemtype',xarRoles::ROLES_GROUPTYPE);
+        $q->ne('r.state',xarRoles::ROLES_STATE_DELETED);
+        $q->run();
+        return $q->output();
+
+// Restriction by ancestor group. This option supports group IDs or names.
+    } elseif (isset($ancestor)) {
+
+        // Get all the groups
+        $q = new Query('SELECT');
+        $q->addtable($xartable['roles'], 'r');
+        $q->addtable($xartable['rolemembers'], 'rm');
+        $q->leftjoin('r.id', 'rm.role_id');
+        $q->addfields(array('r.id','r.name','r.users','rm.parent_id'));
+        $q->eq('r.itemtype',xarRoles::ROLES_GROUPTYPE);
+        $q->run();
+        $allgroups = array();
+        $result = $q->output();
+        foreach ($result as $row) $allgroups[$row['name']] = $row;
+        
+        // Make sure we have IDs for all inputs
         $groups = explode(',', $ancestor);
-        foreach ($groups as $group) {
-            $descendants = array_merge($descendants,_getDescendants(trim($group),$allgroups));
-        }
         $ids = array();
-        foreach ($descendants as $descendant) {
-            if (!in_array($descendant[1],$ids)) {
-                $ids[] = $descendant[1];
-                $conditions[] = $q->peq('rm.role_id',$descendant[1]);
+        foreach ($groups as $group) {
+            $group = trim($group);
+            if (is_numeric($group)) {
+                $ids[] = $group;
+            } else {
+                if (isset($allgroups[$group])) $ids[] = (int)$allgroups[$group]['id'];
             }
         }
+        
+        // Run the recursion
+        $descendants = array();
+        foreach ($ids as $id) {
+            $subgroups = array_merge($descendants, recursive_getDescendants($id, $allgroups));
+            foreach($subgroups as $subgroup) $descendants[$subgroup['id']] = $subgroup;
+        }
+        return $descendants;
     }
-
-    if (count($conditions) != 0) $q->qor($conditions);
-    $q->eq('r.itemtype',xarRoles::ROLES_GROUPTYPE);
-    $q->ne('r.state',xarRoles::ROLES_STATE_DELETED);
-    $q->run();
-    return $q->output();
 }
 
-function _getDescendants($ancestor,$groups)
+function recursive_getDescendants($ancestor, &$allgroups)
 {
     $descendants = array();
-    foreach($groups as $group){
-        if($group['name'] == $ancestor)
-            $descendants[$group['id']] = array($group['name'],$group['id']);
+    foreach($allgroups as $group){
+        if($group['parent_id'] == $ancestor)
+            $descendants[$group['id']] = $group;
     }
+    $subgroups = array();
     foreach($descendants as $descendant){
-        $subgroups = _getDescendants($descendant[0],$groups);
-        foreach($subgroups as $subgroup){
-            $descendants[$subgroup['id']] = $subgroup['id'];
-        }
+        $subgroups = recursive_getDescendants((int)$descendant['id'], $allgroups);
+        foreach($subgroups as $subgroup) $subgroups[$subgroup['id']] = $subgroup;
     }
-    return $descendants ;
+    $descendants = array_merge($descendants, $subgroups);
+    return $descendants;
 }
 ?>
