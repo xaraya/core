9f83e363acd9ff0965c508c68be1085e61669d88
Marc Lutolf <mfl@netspan.ch>
2015-03-05 08:45:24 +0100

    Cosmetics

--- html/code/modules/base/xaradmin/main.php
+++ html/code/modules/base/xaradmin/main.php
@@ -29,7 +29,7 @@
 function base_admin_main()
 {
     // Security
-    if(!xarSecurityCheck('ViewBase')) return;
+    if(!xarSecurityCheck('EditBase')) return;
 
     $request = new xarRequest();
     $refererinfo = xarController::$request->getInfo(xarServer::getVar('HTTP_REFERER'));
