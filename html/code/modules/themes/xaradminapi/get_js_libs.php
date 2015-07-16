d79b2eec04a5aee603d8d0c4b796ff4c8b24a2e5
Marc Lutolf <mfl@netspan.ch>
2015-07-16 16:39:51 +0200

    Add file to get js libs

---
+++ html/code/modules/themes/xaradminapi/get_js_libs.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package modules
+ * @subpackage themes module
+ * @copyright see the html/credits.html file in this release
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com 
+ */
+
+function themes_adminapi_get_js_libs(Array $args=array())
+{
+    sys::import('modules.themes.class.xarjs');
+    $instance = xarJS::getInstance();
+    if (empty($args['scope']) || ($args['scope'] == 'local')) {
+        $args['scope'] = 'local_libs';
+    } else {
+        $args['scope'] = 'remote_libs';
+    }
+    
+    // Retrieve the libraries of the chosen scope: local or remote
+    $libs = $instance->$args['scope'];
+    
+    // If we have a specific lib we are looking for, then filter
+    if (!empty($args['lib'])) {
+        $result = array();
+        foreach ($libs as $lib) {
+            if ($lib['lib'] == $args['lib']) $result[] = $lib;
+        }
+    } else {
+        $result = $libs;
+    }
+        
+    return $result;
+}
+?>
