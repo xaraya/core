16670bc9c35d69ffa4ffc2cfbf5d0986db27409f
Marc Lutolf <mfl@netspan.ch>
2016-10-12 16:47:34 +0200

    More
    Signed-off-by: Marc Lutolf <mfl@netspan.ch>

--- html/lib/xaraya/datastores/sql/relational.php
+++ html/lib/xaraya/datastores/sql/relational.php
@@ -414,6 +414,9 @@ class RelationalDataStore extends SQLDataStore
         // Run the query
         if (!$q->run()) throw new Exception(xarML('Query failed'));
         $result = $q->output();
+    if (xarModVars::get('eventhub','debugmode') && in_array(xarUserGetVar('id'),xarConfigVars::get(null, 'Site.User.DebugAdmins'))) {
+        echo count($result);
+    }
         if (empty($result)) return;
 
         // Distribute the results to the appropriate properties
