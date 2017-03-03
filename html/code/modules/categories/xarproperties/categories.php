dd5747cebd62bc4620600e3a9f63602d4232158f
Marc Lutolf <mfl@netspan.ch>
2017-03-03 11:59:11 +0100

    Undo last change to validateValue

--- html/code/modules/categories/xarproperties/categories.php
+++ html/code/modules/categories/xarproperties/categories.php
@@ -148,8 +148,8 @@ class CategoriesProperty extends DataProperty
         foreach ($value as $baseid => $categories) {
             foreach ($categories as $category) {
                 $category_id = (int)$category;
-//                $this->categories[$category_id . "_" . (int)$this->basecategories[$baseid]] = $category_id;
-                $this->categories[(int)$this->basecategories[$baseid]-1][$category_id] = $category_id;
+                $this->categories[$category_id . "_" . (int)$this->basecategories[$baseid]] = $category_id;
+//                $this->categories[(int)$this->basecategories[$baseid]-1][$category_id] = $category_id;
             }
         }
 
