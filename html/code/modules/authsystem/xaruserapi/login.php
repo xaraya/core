31d7066141ba445bb4c2375acacbcde526870828
Marc Lutolf <mfl@netspan.ch>
2017-04-27 19:51:59 +0200

    Docs: rework the authsystem headers

--- html/code/modules/authsystem/xaruserapi/login.php
+++ html/code/modules/authsystem/xaruserapi/login.php
@@ -2,13 +2,11 @@
 /**
  * Log a user on the system
  *
- * @package modules
- * @subpackage authsystem module
+ * @package modules\authsystem
  * @category Xaraya Web Applications Framework
  * @version 2.4.0
  * @copyright see the html/credits.html file in this release
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
- * @link http://www.xaraya.info
  * @link http://xaraya.info/index.php/release/42.html
  *
  * @author Jan Schrage
