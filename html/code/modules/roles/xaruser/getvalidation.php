637afb1abcf52793ead84bfc50178cc05027a7b7
Marc Lutolf <mfl@netspan.ch>
2017-04-28 08:34:11 +0200

    Docs: rework the roles module headers

--- html/code/modules/roles/xaruser/getvalidation.php
+++ html/code/modules/roles/xaruser/getvalidation.php
@@ -2,13 +2,12 @@
 /**
  * Getvalidation validates a new user into the system
  *
- * @package modules
- * @subpackage roles module
+ * @package modules\roles
+ * @subpackage roles
  * @category Xaraya Web Applications Framework
  * @version 2.4.0
  * @copyright see the html/credits.html file in this release
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
- * @link http://www.xaraya.info
  * @link http://xaraya.info/index.php/release/27.html
  */
 /**
