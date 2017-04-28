b5eb8db6dda1b64c571571f8dccace9a9a0cdb29
Marc Lutolf <mfl@netspan.ch>
2017-04-28 11:31:58 +0200

    Docs: rework installer module headers

--- html/code/modules/installer/xaradminapi.php
+++ html/code/modules/installer/xaradminapi.php
@@ -1,13 +1,12 @@
 <?php
 /**
  * Modify the system configuration File
- * @package modules
- * @subpackage installer module
+ * @package modules\installer
+ * @subpackage installer
  * @category Xaraya Web Applications Framework
  * @version 2.4.0
  * @copyright see the html/credits.html file in this release
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
- * @link http://www.xaraya.info
  * @link http://xaraya.info/index.php/release/200.html
  */
 
--- html/code/modules/installer/xarconfigurations/community.conf.php
+++ html/code/modules/installer/xarconfigurations/community.conf.php
@@ -2,13 +2,12 @@
 /**
  * Community configuration
  *
- * @package modules
- * @subpackage installer module
+ * @package modules\installer
+ * @subpackage installer
  * @category Xaraya Web Applications Framework
  * @version 2.4.0
  * @copyright see the html/credits.html file in this release
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
- * @link http://www.xaraya.info
  * @link http://xaraya.info/index.php/release/200.html
  */
 /*
