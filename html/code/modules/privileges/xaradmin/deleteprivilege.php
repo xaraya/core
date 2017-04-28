4f48df9557984c8f73073d74f3e3624b7a122355
Marc Lutolf <mfl@netspan.ch>
2017-04-28 08:20:18 +0200

    Docs: rework the privileges module headers

--- html/code/modules/privileges/xaradmin/deleteprivilege.php
+++ html/code/modules/privileges/xaradmin/deleteprivilege.php
@@ -1,13 +1,12 @@
 <?php
 /**
  * DeletePrivilege - delete a privilege
- * @package modules
- * @subpackage privileges module
+ * @package modules\privileges
+ * @subpackage privileges
  * @category Xaraya Web Applications Framework
  * @version 2.4.0
  * @copyright see the html/credits.html file in this release
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
- * @link http://www.xaraya.info
  * @link http://xaraya.info/index.php/release/1098.html
  *
  * @author Marc Lutolf <marcinmilan@xaraya.com>
