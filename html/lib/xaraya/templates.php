bb7334fb043357de2647dc17ee254489a68dfa32
Marc Lutolf <mfl@netspan.ch>
2017-04-28 12:52:54 +0200

    More of same

--- html/lib/xaraya/templates.php
+++ html/lib/xaraya/templates.php
@@ -2,8 +2,8 @@
 /**
  * BlockLayout Template Engine
  *
- * @package core
- * @package templating
+ * @package core\templating
+ * @subpackage templating
  * @category Xaraya Web Applications Framework
  * @version 2.4.0
  * @copyright see the html/credits.html file in this release
