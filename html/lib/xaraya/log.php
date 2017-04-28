e5b250233c6b281839248c3181e689bb9a2bd41c
Marc Lutolf <mfl@netspan.ch>
2017-04-28 14:33:02 +0200

    Docs: exceptions

--- html/lib/xaraya/log.php
+++ html/lib/xaraya/log.php
@@ -22,6 +22,13 @@
 
 /**
  * Logging package defines
+ * @package core\exceptions
+ * @subpackage exceptions
+ * @category Xaraya Web Applications Framework
+ * @version 2.4.0
+ * @copyright see the html/credits.html file in this release
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.info
  */
 
 define('XARLOG_LEVEL_EMERGENCY', 1);
