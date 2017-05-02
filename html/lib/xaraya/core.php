3b755d857d2e5492e5f96641301b38b3270a0e5d
Marc Lutolf <mfl@netspan.ch>
2017-05-02 09:19:44 +0200

    Docs: core

--- html/lib/xaraya/core.php
+++ html/lib/xaraya/core.php
@@ -95,6 +95,17 @@ define('XARCORE_VERSION_REV', $rev);
  * --------------------------------------------------------- 
  **/
 
+/**
+ * @package core
+ * @subpackage core
+ * @category Xaraya Web Applications Framework
+ * @version 2.4.0
+ * @copyright see the html/credits.html file in this release
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.info
+ *
+ * @author Marc Lutolf <marc@luetolf-carroll.com>
+**/
 class xarConst
 {
     /**
