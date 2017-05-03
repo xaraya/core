d81e9d04dd075a609319fc23654befba526b863b
Marc Lutolf <mfl@netspan.ch>
2017-05-03 18:31:36 +0200

    Docs: varia

--- html/install.php
+++ html/install.php
@@ -97,6 +97,18 @@ define ('XARINSTALL_PHASE_SETTINGS_COLLECTION', '5');
  */
 define ('XARINSTALL_PHASE_BOOTSTRAP',           '6');
 
+/**
+ * Loads the files required for running an installation
+ *
+ * @package modules\installer
+ * @subpackage installer
+ * @category Xaraya Web Applications Framework
+ * @version 2.4.0
+ * @copyright see the html/credits.html file in this release
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.info
+ * @author Marc Lutolf <mfl@netspan.ch>
+ */
 function xarInstallLoader()
 {
 /**
