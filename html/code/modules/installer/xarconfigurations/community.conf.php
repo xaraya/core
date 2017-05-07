23d6f0f4f53eb2b4fd628c52a3c2aa4ddb2b3018
Marc Lutolf <mfl@netspan.ch>
2017-05-07 12:14:15 +0200

    Docs: modules\install

--- html/code/modules/installer/xarconfigurations/community.conf.php
+++ html/code/modules/installer/xarconfigurations/community.conf.php
@@ -7,6 +7,15 @@
  * @author Marc Lutolf
  */
 
+/**
+ * @package modules\installer\installer
+ * @subpackage installer
+ * @category Xaraya Web Applications Framework
+ * @version 2.4.0
+ * @copyright see the html/credits.html file in this release
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://xaraya.info/index.php/release/200.html
+ */
 $configuration_name = xarML('Community Site -- modules and privilege for semi-open access');
 
 /**
