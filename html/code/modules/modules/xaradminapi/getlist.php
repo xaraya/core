49eff1c4fe6ec5646fa1d23a38a55b2bed8e8b0c
Marc Lutolf <mfl@netspan.ch>
2017-05-17 20:10:46 +0200

    Cosmetics

--- html/code/modules/modules/xaradminapi/getlist.php
+++ html/code/modules/modules/xaradminapi/getlist.php
@@ -99,6 +99,7 @@ function modules_adminapi_getlist(Array $args=array())
         $get['state'] = XARMOD_STATE_ACTIVE;
     }
    
-    return xarMod::apiFunc('modules', 'admin', 'getitems', $get);
+    $result =& xarMod::apiFunc('modules', 'admin', 'getitems', $get);
+    return $result;
 }
 ?>
