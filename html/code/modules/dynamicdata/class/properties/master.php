b22c8893b8ae9dac891612f0eb30cf33812e236d
Marc Lutolf <mfl@netspan.ch>
2017-07-26 23:23:49 +0200

    Cosmetics

--- html/code/modules/dynamicdata/class/properties/master.php
+++ html/code/modules/dynamicdata/class/properties/master.php
@@ -125,6 +125,8 @@ class DataPropertyMaster extends Object
         if(!isset($objectref) || empty($args['name']) || empty($args['type']))
             return;
 
+        xarLog::message("DataPropertyMaster::addProperty: Adding a new property " . $args['name'], xarLog::LEVEL_INFO);
+        
         // "beautify" label based on name if not specified
         // TODO: this is a presentation issue, doesnt belong here.
         if(!isset($args['label']) && !empty($args['name']))
@@ -154,6 +156,8 @@ class DataPropertyMaster extends Object
 
         // if the property involves upload, tell its object
         if(isset($property->upload)) $objectref->upload = true;
+        
+        return true;
     }
 
     /**
@@ -193,7 +197,7 @@ class DataPropertyMaster extends Object
             $propertyInfo  = $proptypes[$args['type']];
             $propertyClass = $propertyInfo['class'];
 
-            xarLog::message("DataPropertyMaster::getProperty: Getting a new property $propertyClass", xarLog::LEVEL_INFO);
+            xarLog::message("DataPropertyMaster::getProperty: Getting a new property " . $propertyClass, xarLog::LEVEL_INFO);
 
             // If we don't have the class yet, get it now
             if (!class_exists($propertyClass)) {
@@ -207,7 +211,7 @@ class DataPropertyMaster extends Object
                 sys::import($dp);
                 
                 // Load the translations for this file
-                $loaded = xarMLSLoadTranslations($propertyfile);
+                $loaded = xarMLS::loadTranslations($propertyfile);
                 if (!$loaded) xarLog::message("Property translations for $propertyClass NOT loaded", xarLog::LEVEL_WARNING);
             }
 
@@ -224,7 +228,6 @@ class DataPropertyMaster extends Object
 
         return $property;
     }
-
     static function createProperty(Array $args=array())
     {
         $object = DataObjectMaster::getObject(
