b21481d790f36d9104428a974703a825bf4356fa
Marc Lutolf <mfl@netspan.ch>
2017-09-07 14:24:23 +0200

    Minor fix for commit and rollback

--- html/code/modules/modules/xaradminapi/remove.php
+++ html/code/modules/modules/xaradminapi/remove.php
@@ -27,8 +27,6 @@ function modules_adminapi_remove(Array $args=array())
     if(!xarSecurityCheck('AdminModules')) return;
 
     // Remove variables and module
-    $dbconn = xarDB::getConn();
-    $tables =& xarDB::getTables();
 
     // Get module information
     if (isset($name)) $regid = xarMod::getRegid($name, 'module');
@@ -36,72 +34,74 @@ function modules_adminapi_remove(Array $args=array())
 
     //TODO: Add check if there is any dependents
     // Make the whole thing atomic
-    try {
-        $dbconn->begin();
 
-        // If the files have been removed, the module will now also be removed from the db
-        if ($modinfo['state'] == XARMOD_STATE_MISSING_FROM_UNINITIALISED ||
-            $modinfo['state'] == XARMOD_STATE_MISSING_FROM_INACTIVE ||
-            $modinfo['state'] == XARMOD_STATE_MISSING_FROM_ACTIVE ||
-            $modinfo['state'] == XARMOD_STATE_MISSING_FROM_UPGRADED ) {
+    // If the files have been removed, the module will now also be removed from the db
+    if ($modinfo['state'] == XARMOD_STATE_MISSING_FROM_UNINITIALISED ||
+        $modinfo['state'] == XARMOD_STATE_MISSING_FROM_INACTIVE ||
+        $modinfo['state'] == XARMOD_STATE_MISSING_FROM_ACTIVE ||
+        $modinfo['state'] == XARMOD_STATE_MISSING_FROM_UPGRADED ) {
 
-            // All cleanup needs to happen before a module entry is removed
-            xarEvents::notify('ModRemove', $modinfo['name']);
-            // this is now handled by the modules module ModRemove event observer
-            //xarModVars::delete_all($modinfo['name']);
+        // All cleanup needs to happen before a module entry is removed
+        xarEvents::notify('ModRemove', $modinfo['name']);
+        // this is now handled by the modules module ModRemove event observer
+        //xarModVars::delete_all($modinfo['name']);
 
-            // Remove the module itself
-            $query = "DELETE FROM $tables[modules] WHERE regid = ?";
-            $dbconn->Execute($query,array($modinfo['regid']));
-        } else {
-            // Module deletion function
-            xarMod::apiFunc('modules', 'admin', 'executeinitfunction',
-                          array('regid' => $regid, 'function' => 'delete'));
-
-            // All cleanup needs to happen before a module entry is removed
-            xarEvents::notify('ModRemove', $modinfo['name']);
-            // this is now handled by the modules module ModRemove event observer
-            //xarModVars::delete_all($modinfo['name']);
-
-            // Update state of module
-            xarMod::apiFunc('modules', 'admin', 'setstate',
-                          array('regid' => $regid,'state' => XARMOD_STATE_UNINITIALISED));
+        // Remove the module itself
+        try {
+            $dbconn = xarDB::getConn();
+            $tables =& xarDB::getTables();
+            $dbconn->begin();
+                $query = "DELETE FROM $tables[modules] WHERE regid = ?";
+                $dbconn->Execute($query,array($modinfo['regid']));
+            $dbconn->commit();
+        } catch (Exception $e) {
+            $dbconn->rollback();
+            throw $e;
         }
+    } else {
+        // Module deletion function
+        xarMod::apiFunc('modules', 'admin', 'executeinitfunction',
+                      array('regid' => $regid, 'function' => 'delete'));
 
-        // Delete any masks still around
+        // All cleanup needs to happen before a module entry is removed
+        xarEvents::notify('ModRemove', $modinfo['name']);
         // this is now handled by the modules module ModRemove event observer
-        // xarRemoveMasks($modinfo['name']);
-        // this is now handled by the modules module ModRemove event observer
-        // xarModCallHooks('module','remove',$modinfo['name'],'',$modinfo['name']);
+        //xarModVars::delete_all($modinfo['name']);
 
-        //
-        // Delete block details for this module.
-        //
-        // Get block types.
-        // this is now handled by the modules module ModRemove event observer
-        /*
-        $blocktypes = xarMod::apiFunc('blocks', 'user', 'getallblocktypes',
-                                    array('module' => $modinfo['name']));
-
-        // Delete block types.
-        if (is_array($blocktypes) && !empty($blocktypes)) {
-            foreach($blocktypes as $blocktype) {
-                xarMod::apiFunc('blocks', 'admin', 'delete_type', $blocktype);
-            }
-        }
-        */
-        // this is now handled by the modules module ModRemove event observer
-        /*
-        $defaultmod = xarModVars::get('modules', 'defaultmodule');
-        if ($modinfo['name'] == $defaultmod) {
-            xarModVars::set('modules', 'defaultmodule','base');
-        }
-        */
-        $dbconn->commit();
-    } catch (Exception $e) {
-        $dbconn->rollback();
-        throw $e;
+        // Update state of module
+        xarMod::apiFunc('modules', 'admin', 'setstate',
+                      array('regid' => $regid,'state' => XARMOD_STATE_UNINITIALISED));
     }
+
+    // Delete any masks still around
+    // this is now handled by the modules module ModRemove event observer
+    // xarRemoveMasks($modinfo['name']);
+    // this is now handled by the modules module ModRemove event observer
+    // xarModCallHooks('module','remove',$modinfo['name'],'',$modinfo['name']);
+
+    //
+    // Delete block details for this module.
+    //
+    // Get block types.
+    // this is now handled by the modules module ModRemove event observer
+    /*
+    $blocktypes = xarMod::apiFunc('blocks', 'user', 'getallblocktypes',
+                                array('module' => $modinfo['name']));
+
+    // Delete block types.
+    if (is_array($blocktypes) && !empty($blocktypes)) {
+        foreach($blocktypes as $blocktype) {
+            xarMod::apiFunc('blocks', 'admin', 'delete_type', $blocktype);
+        }
+    }
+    */
+    // this is now handled by the modules module ModRemove event observer
+    /*
+    $defaultmod = xarModVars::get('modules', 'defaultmodule');
+    if ($modinfo['name'] == $defaultmod) {
+        xarModVars::set('modules', 'defaultmodule','base');
+    }
+    */
 
     return true;
 }
