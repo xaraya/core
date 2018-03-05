973b2b501eb73a088525d4b34a3062fb364ec966
Marc Lutolf <mfl@netspan.ch>
2018-03-05 16:49:00 +0100

    Adjusted tableddl file

--- html/lib/xaraya/tableddl.php
+++ html/lib/xaraya/tableddl.php
@@ -381,4 +381,56 @@ function xarDBDropIndex($tableName, $index, $databaseType = NULL)
     return $sql;
 }
 
+class xarXMLInstaller extends Object
+{
+    public $tableprefix = '';
+    
+    // No constructor yet. maybe later
+    
+    static private function transform($xmlFile, $xslAction='display', $dbName='mysql', $xslFile=null)
+    {
+        // Park this here for now
+        $tableprefix = xarDB::getPrefix();
+        
+        if (!isset($xmlFile))
+            throw new BadParameterException('No file to transform!');
+        if (!isset($xslFile))
+            $xslFile = sys::root() . '/lib/xaraya/xslt/xml2ddl-'. $dbName . '.xsl';
+        if (!file_exists($xslFile)) {
+            $msg = xarML('The file #(1) was not found', $xslFile);
+            throw new BadParameterException($msg);
+        }
+        sys::import('xaraya.xslprocessor');
+        $xslProc = new XarayaXSLProcessor($xslFile);
+        $xslProc->setParameter('', 'action', $xslAction);
+        $xslProc->setParameter('', 'tableprefix', $tableprefix);
+        $xslProc->xmlFile = $xmlFile;
+        return $xslProc->transform($xslProc->xmlFile);
+    }
+    
+    static public function createTable($table, $module)
+    {
+        if (empty($module)) $module = 'base';
+        if (empty($module))
+            throw new BadParameterException('Missing a module name to create');
+        if (empty($table))
+            throw new BadParameterException('Missing a table name to create');
+            
+        $fullName = sys::root() . '/html/modules/' . $module . '/schema.xml';
+        if (!file_exists($fullName)) {
+            $msg = xarML('Could not find the file #(1) to create a table from', $fullName);
+            throw new BadParameterException($msg);
+        }
+        $sqlCode = self::transform($fullName, 'create');
+        $queries = explode(';',$sqlCode);
+        array_pop($queries);
+        $dbconn = xarDB::getConn();
+        foreach ($queries as $q) {
+            xarLog::message(xarML('Executing SQL: #(1)', $q), );
+            $dbconn->Execute($q);
+        }
+        return true;
+    }
+}
+
 ?>
