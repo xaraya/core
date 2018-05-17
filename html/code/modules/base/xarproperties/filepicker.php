8e952d5173d443b0f27cf42422434306ec53ff02
Marc Lutolf <mfl@netspan.ch>
2018-05-17 12:01:46 +0200

    Undo last change

--- html/code/modules/base/xarproperties/filepicker.php
+++ html/code/modules/base/xarproperties/filepicker.php
@@ -47,7 +47,7 @@ class FilePickerProperty extends SelectProperty
         $this->filepath = 'modules/base/xarproperties';
         // keep things relative here if possible (cfr. basedir vs. baseurl issue for images et al.)
         if (empty($this->initialization_basedirectory)) {
-            $this->initialization_basedirectory = 'var/img';
+            $this->initialization_basedirectory = 'var';
         } else {
             // Cater to common Xaraya calls
             if ((strpos($this->initialization_basedirectory,'sys') === 0) || (strpos($this->initialization_basedirectory,'xar') === 0)) {
