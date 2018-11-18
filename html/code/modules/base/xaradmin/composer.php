fb09c46487085f4c04c58c2d528a0768ad314119
Marc Lutolf <mfl@netspan.ch>
2018-11-18 20:52:04 +0100

    More on composer

--- html/code/modules/base/xaradmin/composer.php
+++ html/code/modules/base/xaradmin/composer.php
@@ -21,9 +21,18 @@ function base_admin_composer()
     // Security
     if(!xarSecurityCheck('ManageBase')) return;
 
-    if (!xarVarFetch('setup', 'isset', $setup, NULL, XARVAR_NOT_REQUIRED)) return;
-    if (!xarVarFetch('install', 'isset', $install, NULL, XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('setup',       'isset', $setup,       NULL, XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('install',     'isset', $install,     NULL, XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('install_dir', 'str',   $data['install_dir'], sys::lib(), XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('package_dir', 'str',   $data['package_dir'], 'vendor', XARVAR_NOT_REQUIRED)) return;
+    if (!xarVarFetch('install_com', 'str',   $data['install_com'], 'php composer.phar install ', XARVAR_NOT_REQUIRED)) return;
     
+    // Check if the installer has already been installed
+    $data['installed'] = file_exists('composer') && file_exists('composer/composer.phar');
+
+    // Default message is none
+    $data['message'] = '';
+
     $composerdir = 'composer';
     $setup_path = $composerdir . '/composer-setup.php';
     $phar_path = $composerdir . '/composer.phar';
@@ -35,6 +44,9 @@ function base_admin_composer()
             umask($old_umask);
         }
         if (!file_exists($phar_path)) {
+            if (file_exists($setup_path)) {
+                $output = shell_exec('rm ' . $setup_path);
+            }
             $ch = curl_init();
             $fh = fopen($setup_path, 'x');
             curl_setopt_array($ch, array(
@@ -46,14 +58,31 @@ function base_admin_composer()
             $output = curl_exec($ch);
             curl_close($ch);
             fclose($fh);
-
-            $output = shell_exec('php ' . $setup_path . ' --install-dir=' . $composerdir . ' --quiet');
+            
+            // Check the signature of the file we got
+            $expected_signature = trim(file_get_contents('http://composer.github.io/installer.sig'));
+            $actual_signature = trim(hash_file('sha384', $setup_path));
+            if ($expected_signature == $actual_signature) {
+                // Good signature: run the installer
+                $output = shell_exec('php ' . $setup_path . ' --install-dir=' . $composerdir . ' --quiet');
+            }
+            // Remove the setup file
             $output = shell_exec('rm ' . $setup_path);
+            xarController::redirect(xarServer::getCurrentURL());
         }
-    } 
-    
-    // Check if the installer has already been installed
-    $data['installed'] = file_exists('composer') && file_exists('composer/composer.phar');
+    } elseif ($install) {
+        if (empty($data['install_com'])) {
+            $data['message'] = xarML('No install command entered');
+            return $data;
+        }
+            
+        // Install the package
+        $base_directory = getcwd();
+        chdir($composerdir);
+        $output = shell_exec($data['install_com']);
+        chdir($base_directory);
+        $data['message'] = 'success';
+    }
 
     return $data;
 }
