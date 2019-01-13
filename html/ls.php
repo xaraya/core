12d1f2710e725f350678e5f2c6a0bb43c9cb1ddc
Marc Lutolf <mfl@netspan.ch>
2019-01-13 18:31:09 +0100

    Work on ls.php

--- html/ls.php
+++ html/ls.php
@@ -89,28 +89,37 @@ function xarLocalServicesMain($argc, $argv)
     if(xarMod::isAvailable($handler)) {
         return xarMod::apiFunc($handler,'cli','process',array('argc'=>$argc, 'argv'=>$argv));
     } else {
-        fwrite(STDERR,"Usage for local services entry point:
-        php5 ./".basename(__FILE__)." <type> [-u <user>][-p <pass>] [args]
-
-        <type>   : required designator for request type (module name)
-                   Currently Supported:
-                   - 'mail'  : a mail message is supplied at stdin
-        -u <user>: optional username to pass in
-        -p <pass>: optional cleartext password to pass in
-        [args]   : arguments specific to the supplied <type>
-        NOTES:
-           - if PHP doesnt have REMOTE_ADDR available, it will assume 127.0.0.1.
-             if that is not correct, make sure that PHP can determine your ip address
-             (for example by setting REMOTE_ADDR in the environment)
-             \n");
-        return 1;
+        return usage();
     }
+}
+
+function usage()
+{
+    fwrite(STDERR,"Usage for local services entry point:
+    php5 ./".basename(__FILE__)." <type> [-u <user>][-p <pass>] [args]
+
+    <type>   : required designator for request type (module name)
+               Currently Supported:
+               - 'mail'  : a mail message is supplied at stdin
+    -u <user>: optional username to pass in
+    -p <pass>: optional cleartext password to pass in
+    [args]   : arguments specific to the supplied <type>
+    NOTES:
+       - if PHP doesnt have REMOTE_ADDR available, it will assume 127.0.0.1.
+         if that is not correct, make sure that PHP can determine your ip address
+         (for example by setting REMOTE_ADDR in the environment)
+         \n");
+    return 1;
 }
 
 /**
  * Set up for local services
  */
-xarLSLoader();
+try {
+    xarLSLoader();
+} catch (Exception $e) {
+    return usage();
+}
 /**
  * Process the local request and shut down
  */
