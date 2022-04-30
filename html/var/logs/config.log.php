520bc5136a6296c80e692217c2499b710c589826
Marc Lutolf <marc@luetolf-carroll.com>
2022-04-30 23:02:47 +0200

    First pass at config.log.php

--- html/var/logs/config.log.php
+++ html/var/logs/config.log.php
@@ -12,33 +12,31 @@
  */
 
 /*
- * These values let you define the configuration for Xaraya's loggers
+ * These values define the configuration for Xaraya's loggers
  * 
  * You can change these values to suit your needs, but all of them need to be included
  *
- * You can remove this file, at which point Xaraya will fall back to default values that correspond 
- * to the initial ones in this file, and attempt to recreate this file.
+ * Most of these settings can be updated dynamically in the modifyconfig page of the base module.
  *
- * The fileName setting can be overridden in the modifyconfig page of the base module.
+ * You should not remove this file!
  */
 
-$config[] = array(
-    'type' => 'simple',
-    'config' => array(
-    	'fileName' => "xarayalog.txt",  // The name of the logfile
-    	'maxFileSize'  => "5000000",    // The maximum size of a log file in bytes. Once this value is reached Xaraya creates a new log file
-    	'mode'  => "0644",              // Integer containing the logfile's permissions mode.
-	)
-);
+// Simple logger
+$systemConfiguration['Log.Simple.Filename'] = 'xarayalog.txt'; // The name of the logfile of this logger
+$systemConfiguration['Log.Simple.Level'] = 's:1:"1";';          // The levels of messages this logger tracks.
+$systemConfiguration['Log.Simple.MaxFileSize'] = '50000000';    // The maximum size of a log file in bytes. Once this value is reached Xaraya creates a new log file
+$systemConfiguration['Log.Simple.Mode'] = '';              // Integer containing the logfile's permissions mode.
+
+// Email logger
+$systemConfiguration['Log.Mail.Recipient'] = 'xarayalog.txt'; // The name of the recipient that this logger sends emailds to
+$systemConfiguration['Log.Mail.Level'] = 's:1:"1";';          // The levels of messages this logger tracks.
+
 // Add further loggers here
+// HTML logger
 /*
-$config[] = array(
-    'type' => 'html',
-    'config' => array(
-    	'fileName' => "xarayalog.txt",  // The name of the logfile
-    	'maxFileSize'  => "5000000",    // The maximum size of a log file in bytes. Once this value is reached Xaraya creates a new log file
-    	'mode'  => "0644",              // Integer containing the logfile's permissions mode.
-	)
+$configuration['Log.HTML.Filename'] = 'xarayalog.txt';   // The name of the logfile
+$configuration['Log.HTML.MaxFileSize'] = '5000000';      // The maximum size of a log file in bytes. Once this value is reached Xaraya creates a new log file
+$configuration['Log.HTML.Mode'] = '0644';                // Integer containing the logfile's permissions mode.
 );
 */
 ?>
