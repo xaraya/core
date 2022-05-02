94916a3a95ca2e254c0f3120b72e5e52ebc74ea3
Marc Lutolf <marc@luetolf-carroll.com>
2022-05-02 19:08:53 +0200

    Clean up this file, for what it’s worth

--- html/var/logs/config.log.php
+++ html/var/logs/config.log.php
@@ -21,44 +21,44 @@
  */
 
 // Mail logger
-$systemConfiguration['Log.Mail.Recipient'] = 'xarayalog.txt'; // The recipient that this logger sends emails to
-$systemConfiguration['Log.Mail.Sender'] = 'Xaraya Logger';          // The sender that this logger uses to send emails
-$systemConfiguration['Log.Mail.Subject'] = 'Log Message';          // The subject of the emails
-$systemConfiguration['Log.Mail.Level'] = '1,2,4,8';          // The levels of messages this logger tracks.
-$systemConfiguration['Log.Mail.Timeformat'] = '%Y/%m/%d %H:%M:%S';              // String containing the format for the log's timestamps.
+$systemConfiguration['Log.Mail.Level'] = '1,2,4,8';                        // The levels of messages this logger tracks.
+$systemConfiguration['Log.Mail.Timeformat'] = '%Y/%m/%d %H:%M:%S';         // String containing the format for the log's timestamps.
+$systemConfiguration['Log.Mail.Recipient'] = 'noone@no.comxarayalog.txt';  // The recipient that this logger sends emails to
+$systemConfiguration['Log.Mail.Sender'] = 'Xaraya Logger';                 // The sender that this logger uses to send emails
+$systemConfiguration['Log.Mail.Subject'] = 'Log Message';                  // The subject of the emails
 
 // HTML logger
-$systemConfiguration['Log.Html.Filename'] = 'xarayalog.txt'; // The name of the logfile of this logger
-$systemConfiguration['Log.Html.Level'] = '1,2,4,8';          // The levels of messages this logger tracks.
-$systemConfiguration['Log.Html.MaxFileSize'] = '50000000';    // The maximum size of a log file in bytes. Once this value is reached Xaraya creates a new log file
-$systemConfiguration['Log.Html.Mode'] = '';              // Integer containing the logfile's permissions mode.
-$systemConfiguration['Log.Html.Timeformat'] = '%Y/%m/%d %H:%M:%S';              // String containing the format for the log's timestamps.
+$systemConfiguration['Log.Html.Level'] = '1,2,4,8';                        // The levels of messages this logger tracks.
+$systemConfiguration['Log.Html.Timeformat'] = '%Y/%m/%d %H:%M:%S';         // String containing the format for the log's timestamps.
+$systemConfiguration['Log.Html.Filename'] = 'htmllog.html';                // The name of the logfile of this logger
+$systemConfiguration['Log.Html.MaxFileSize'] = '50000000';                 // The maximum size of a log file in bytes. Once this value is reached Xaraya creates a new log file
+$systemConfiguration['Log.Html.Mode'] = '644';                             // Integer containing the logfile's permissions mode.
 
 // Javascript logger
-$systemConfiguration['Log.Html.Level'] = '1,2,4,8';          // The levels of messages this logger tracks.
-$systemConfiguration['Log.Html.Timeformat'] = '%Y/%m/%d %H:%M:%S';              // String containing the format for the log's timestamps.
+$systemConfiguration['Log.Html.Level'] = '1,2,4,8';                        // The levels of messages this logger tracks.
+$systemConfiguration['Log.Html.Timeformat'] = '%Y/%m/%d %H:%M:%S';         // String containing the format for the log's timestamps.
 
 // Mozilla logger
-$systemConfiguration['Log.Html.Level'] = '1,2,4,8';          // The levels of messages this logger tracks.
-$systemConfiguration['Log.Html.Timeformat'] = '%Y/%m/%d %H:%M:%S';              // String containing the format for the log's timestamps.
+$systemConfiguration['Log.Html.Level'] = '1,2,4,8';                        // The levels of messages this logger tracks.
+$systemConfiguration['Log.Html.Timeformat'] = '%Y/%m/%d %H:%M:%S';         // String containing the format for the log's timestamps.
 
 // Simple logger
-$systemConfiguration['Log.Simple.Filename'] = 'simplelog.txt1'; // The name of the logfile of this logger
-$systemConfiguration['Log.Simple.Level'] = '2,4,8,32,64';          // The levels of messages this logger tracks.
-$systemConfiguration['Log.Simple.MaxFileSize'] = '50000000';    // The maximum size of a log file in bytes. Once this value is reached Xaraya creates a new log file
-$systemConfiguration['Log.Simple.Mode'] = '644';              // Integer containing the logfile's permissions mode.
+$systemConfiguration['Log.Simple.Filename'] = 'simplelog.txt1';            // The name of the logfile of this logger
+$systemConfiguration['Log.Simple.Level'] = '1,2,4,8';                      // The levels of messages this logger tracks.
+$systemConfiguration['Log.Simple.MaxFileSize'] = '50000000';               // The maximum size of a log file in bytes. Once this value is reached Xaraya creates a new log file
+$systemConfiguration['Log.Simple.Mode'] = '644';                           // Integer containing the logfile's permissions mode.
 $systemConfiguration['Log.Simple.Timeformat'] = '%Y/%m/%d %H:%M:%S';              // String containing the format for the log's timestamps.
 
 // SQL logger
-$systemConfiguration['Log.Sql.SQLTable'] = 'xarayalog.txt'; // The name of the SQL table of this logger
-$systemConfiguration['Log.Sql.Level'] = '1,2,4,8';          // The levels of messages this logger tracks.
-$systemConfiguration['Log.Sql.Timeformat'] = '%Y/%m/%d %H:%M:%S';              // String containing the format for the log's timestamps.
+$systemConfiguration['Log.Sql.Level'] = '1,2,4,8';                         // The levels of messages this logger tracks.
+$systemConfiguration['Log.Sql.Timeformat'] = '%Y/%m/%d %H:%M:%S';          // String containing the format for the log's timestamps.
+$systemConfiguration['Log.Sql.SQLTable'] = 'log_messages';                 // The name of the SQL table of this logger
 
 // Syslog logger
-$systemConfiguration['Log.Syslog.Facility'] = 'xarayalog.txt'; // The name of the logfile of this logger
-$systemConfiguration['Log.Syslog.Level'] = '1,2,4,8';          // The levels of messages this logger tracks.
-$systemConfiguration['Log.Syslog.Options'] = '50000000';    // The maximum size of a log file in bytes. Once this value is reached Xaraya creates a new log file
-$systemConfiguration['Log.Syslog.Timeformat'] = '%Y/%m/%d %H:%M:%S';              // String containing the format for the log's timestamps.
+$systemConfiguration['Log.Syslog.Level'] = '1,2,4,8';                      // The levels of messages this logger tracks.
+$systemConfiguration['Log.Syslog.Timeformat'] = '%Y/%m/%d %H:%M:%S';       // String containing the format for the log's timestamps.
+$systemConfiguration['Log.Syslog.Facility'] = 'syslog.txt';                // The name of the logfile of this logger
+$systemConfiguration['Log.Syslog.Options'] = '50000000';                   // The maximum size of a log file in bytes. Once this value is reached Xaraya creates a new log file
 
 
 ?>
