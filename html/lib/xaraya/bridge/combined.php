0fc3c558b1f02ea1ae8afee0822ba1eca2eff2cf
mikespub <github@mikespub.net>
2023-01-28 20:48:17 +0000

    combined fastroute + requesthandler

---
+++ html/lib/xaraya/bridge/combined.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * Make use of the FastRouteBridge in routing.php for an all-in-one PSR-15 middleware + requesthandler
+ *
+ * require dirname(__DIR__).'/vendor/autoload.php';
+ * sys::init();
+ * xarCache::init();
+ * xarCore::xarInit(xarCore::SYSTEM_USER);
+ *
+ * // use some PSR-7 factory
+ * use Nyholm\Psr7\Factory\Psr17Factory;
+ * use Nyholm\Psr7Server\ServerRequestCreator;
+ * // use Xaraya PSR-15 compatible request handler + middleware
+ * use Xaraya\Bridge\Middleware\DefaultMiddleware;
+ * use Xaraya\Bridge\Middleware\FastRouteHandler;
+ *
+ * // get server request from somewhere
+ * $psr17Factory = new Psr17Factory();
+ * $requestCreator = new ServerRequestCreator($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory);
+ * $request = $requestCreator->fromGlobals();
+ *
+ * // the Xaraya PSR-15 request handler + middleware here
+ * $fastrouted = new FastRouteHandler($psr17Factory);
+ *
+ * // handle the request directly, or use as middleware
+ * $response = $fastrouted->handle($request);
+ *
+ * //echo $response->getBody();
+ * DefaultMiddleware::emitResponse($response);
+ */
+
+namespace Xaraya\Bridge\Middleware;
+
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Exception;
+use sys;
+
+sys::import('xaraya.bridge.routing');
+use Xaraya\Bridge\Routing\FastRouteBridge;
+use Xaraya\Bridge\Routing\FastRouteApiBridge;
+// @checkme rename FastRoute dispatcher as router here to avoid confusion with PSR-15 naming
+use FastRoute\Dispatcher as FastRouter;
+use FastRoute\RouteCollector;
+
+use function FastRoute\simpleDispatcher;
+
+class FastRouteHandler implements MiddlewareInterface, RequestHandlerInterface
+{
+    use DefaultResponseTrait;
+
+    protected $router;
+
+    /**
+     * Initialize the middleware with response factory (or container, ...)
+     */
+    public function __construct(?ResponseFactoryInterface $responseFactory = null, ?FastRouter $router = null, array $options = [])
+    {
+        $this->setResponseFactory($responseFactory);
+        $this->options = $options;
+        if (empty($router)) {
+            $router = simpleDispatcher(function (RouteCollector $r) {
+                $r->addGroup('/api', function (RouteCollector $r) {
+                    FastRouteApiBridge::addRouteCollection($r);
+                });
+                FastRouteBridge::addRouteCollection($r);
+            });
+        }
+        $this->setRouter($router);
+    }
+
+    public function setRouter(FastRouter $router): void
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * Handle the server request for RequestHandlerInterface
+     */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return $this->execute($request, null);
+    }
+
+    /**
+     * Process the server request for MiddlewareInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $next): ResponseInterface
+    {
+        return $this->execute($request, $next);
+    }
+
+    /**
+     * Execute the server request - this will set request attributes based on path variables + handle the request
+     */
+    public function execute(ServerRequestInterface $request, ?RequestHandlerInterface $next = null): ResponseInterface
+    {
+        // Strip the base uri for the calling script from the request path and set 'baseUri' request attribute
+        $request = DefaultMiddleware::stripBaseUri($request);
+        $method = $request->getMethod();
+        // @checkme see https://github.com/middlewares/fast-route/blob/master/src/FastRoute.php on using rawurldecode() here
+        $path = $request->getUri()->getPath();
+
+        // Let FastRoute identify the right handler and match the path variables
+        $routeInfo = $this->router->dispatch($method, $path);
+        switch ($routeInfo[0]) {
+            case FastRouter::NOT_FOUND:
+                // ... 404 Not Found - pass along to the next handler or return 404 error here
+                if (!empty($next)) {
+                    return $next->handle($request);
+                }
+                $response = $this->getResponseFactory()->createResponse();
+                $response = $response->withStatusCode(404);
+                $response->getBody()->write('Nothing to see here at ' . htmlspecialchars($path));
+                return $response;
+                break;
+            case FastRouter::METHOD_NOT_ALLOWED:
+                $allowedMethods = $routeInfo[1];
+                // ... 405 Method Not Allowed
+                $response = $this->getResponseFactory()->createResponse();
+                $response = $response->withStatusCode(405)->withHeader('Allow', implode(', ', $allowedMethods));
+                $response->getBody()->write('Method ' . htmlspecialchars($method) . ' is not allowed for ' . htmlspecialchars($path));
+                return $response;
+                break;
+            case FastRouter::FOUND:
+                $handler = $routeInfo[1];
+                $vars = $routeInfo[2];
+                foreach ($vars as $key => $value) {
+                    $request = $request->withAttribute($key, $value);
+                }
+                //return $routeInfo[1]($request)
+                //return $next->handle($request);
+                // ... call $handler with $vars
+                try {
+                    $result = FastRouteBridge::callHandler($handler, $vars);
+                } catch (Exception $e) {
+                    return $this->createExceptionResponse($e);
+                }
+                if (is_string($result)) {
+                    return $this->createResponse($result);
+                }
+                return $this->createJsonResponse($result);
+                break;
+            default:
+                $result = "Unknown result from FastRoute Dispatcher: " . var_export($routeInfo, true);
+                return $this->createResponse($result);
+                break;
+        }
+    }
+}
