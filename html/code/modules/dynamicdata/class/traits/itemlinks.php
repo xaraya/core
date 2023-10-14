ebac4595d1adc3122e9a60efe95eaeb00edf431b
mikespub <github@mikespub.net>
2023-10-14 12:38:57 +0200

    add DD user api trait for re-use in other modules

---
+++ html/code/modules/dynamicdata/class/traits/itemlinks.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * @package modules\dynamicdata
+ * @subpackage dynamicdata
+ * @category Xaraya Web Applications Framework
+ * @version 2.4.1
+ * @copyright see the html/credits.html file in this release
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link https://github.com/mikespub/xaraya-modules
+ *
+ * @author mikespub <mikespub@xaraya.com>
+ **/
+
+namespace Xaraya\DataObject\Traits;
+
+use DataObjectDescriptor;
+use DataObjectMaster;
+use DataPropertyMaster;
+use xarServer;
+use xarVar;
+use sys;
+
+sys::import('modules.dynamicdata.class.objects.master');
+
+/**
+ * For documentation purposes only - available via ItemLinksTrait
+ */
+interface ItemLinksInterface
+{
+    /**
+     * Utility function to retrieve the DD objects of this module (if any).
+     * @return array<string, mixed>
+     */
+    public static function getModuleObjects(): array;
+
+    /**
+     * Utility function to retrieve the list of itemtypes of this module (if any).
+     * @param array<string, mixed> $args array of optional parameters
+     * @return array<mixed> the itemtypes of this module and their description
+     */
+    public static function getItemTypes(array $args = []): array;
+
+    /**
+     * Utility function to pass individual item links to whoever
+     * @param array<string, mixed> $args array of optional parameters
+     *        string   $args['itemtype'] item type (optional)
+     *        array    $args['itemids'] array of item ids to get
+     * @return array<mixed> containing the itemlink(s) for the item(s).
+     */
+    public static function getItemLinks(array $args = []): array;
+}
+
+/**
+ * Trait to handle getitemtypes and getitemlinks user api
+ * for modules with their own DD objects
+ */
+trait ItemLinksTrait
+{
+    protected static int $moduleId = 123456;
+    protected static string $moduleName = 'OVERRIDE';
+    /** @var array<string, mixed> */
+    protected static array $moduleObjects = [];
+
+    /**
+     * Utility function to retrieve the DD objects of this module (if any).
+     * @return array<string, array<string, mixed>>
+     */
+    public static function getModuleObjects(): array
+    {
+        if (!empty(static::$moduleObjects)) {
+            return static::$moduleObjects;
+        }
+        $objects = DataObjectMaster::getObjects();
+        static::$moduleObjects = [];
+        foreach ($objects as $objectid => $objectinfo) {
+            /** @var array<string, mixed> $objectinfo */
+            if (intval($objectinfo['moduleid']) !== static::$moduleid) {
+                continue;
+            }
+            static::$moduleObjects[$objectinfo['name']] = $objectinfo;
+        }
+        return static::$moduleObjects;
+    }
+
+    /**
+     * Utility function to retrieve the list of itemtypes of this module (if any).
+     * @param array<string, mixed> $args array of optional parameters
+     * @return array<mixed> the itemtypes of this module and their description
+     */
+    public static function getItemTypes(array $args = []): array
+    {
+        $objects = static::getModuleObjects();
+        $itemtypes = [];
+        foreach ($objects as $name => $objectinfo) {
+            $itemtypes[$objectinfo['itemtype']] = [
+                'label' => xarVar::prepForDisplay($objectinfo['label']),
+                'title' => xarVar::prepForDisplay(xarML('View #(1)', $objectinfo['label'])),
+                'url'   => xarServer::getObjectURL($objectinfo['name'], 'view'),
+            ];
+        }
+        return $itemtypes;
+    }
+
+    /**
+     * Utility function to pass individual item links to whoever
+     * @param array<string, mixed> $args array of optional parameters
+     *        string   $args['itemtype'] item type (optional)
+     *        array    $args['itemids'] array of item ids to get
+     * @return array<mixed> containing the itemlink(s) for the item(s).
+     */
+    public static function getItemLinks(array $args = []): array
+    {
+        extract($args);
+
+        $itemlinks = [];
+        if (empty($itemtype)) {
+            $itemtype = null;
+        }
+        if (empty($itemids)) {
+            $itemids = null;
+        }
+
+        // for items managed by library itself only
+        $args = DataObjectDescriptor::getObjectID(['moduleid'  => static::$moduleid,
+                                        'itemtype'  => $itemtype]);
+        if (empty($args['objectid'])) {
+            return $itemlinks;
+        }
+        $status = DataPropertyMaster::DD_DISPLAYSTATE_ACTIVE;
+        $object = DataObjectMaster::getObjectList(['objectid'  => $args['objectid'],
+                                            'itemids' => $itemids,
+                                            'status' => $status]);
+        if (!isset($object) || (empty($object->objectid) && empty($object->table))) {
+            return $itemlinks;
+        }
+        if (!$object->checkAccess('view')) {
+            return $itemlinks;
+        }
+
+        $object->getItems();
+
+        $properties = & $object->getProperties();
+        $items = & $object->items;
+        if (!isset($items) || !is_array($items) || count($items) == 0) {
+            return $itemlinks;
+        }
+
+        // TODO: make configurable
+        $titlefield = '';
+        foreach ($properties as $name => $property) {
+            // let's use the first textbox property we find for now...
+            if ($property->type == 2) {
+                $titlefield = $name;
+                break;
+            }
+        }
+
+        // if we didn't have a list of itemids, return all the items we found
+        if (empty($itemids)) {
+            $itemids = array_keys($items);
+        }
+
+        foreach ($itemids as $itemid) {
+            if (!empty($titlefield) && isset($items[$itemid][$titlefield])) {
+                $label = $items[$itemid][$titlefield];
+            } else {
+                $label = xarML('Item #(1)', $itemid);
+            }
+            // $object->getActionURL('display', $itemid)
+            $itemlinks[$itemid] = ['url'   => xarServer::getObjectURL($object->name, 'display', ['itemid' => $itemid]),
+                                   'title' => xarML('Display Item'),
+                                   'label' => $label];
+        }
+        return $itemlinks;
+    }
+}
---
+++ html/code/modules/dynamicdata/class/traits/userapi.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package modules\dynamicdata
+ * @subpackage dynamicdata
+ * @category Xaraya Web Applications Framework
+ * @version 2.4.1
+ * @copyright see the html/credits.html file in this release
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link https://github.com/mikespub/xaraya-modules
+ *
+ * @author mikespub <mikespub@xaraya.com>
+ **/
+
+namespace Xaraya\DataObject\Traits;
+
+/**
+ * For documentation purposes only - available via UserApiTrait
+ */
+interface UserApiInterface extends ItemLinksInterface {}
+
+/**
+ * Trait to handle generic user api functions
+ * for modules with their own DD objects
+ */
+trait UserApiTrait
+{
+    use ItemLinksTrait;
+}
