c2ad138279b702054b7ac565a814df72ad961331
Marc Lutolf <marc@luetolf-carroll.com>
2024-01-31 16:18:54 +0100

    Rework the database entry data. Remove the overwrite option for now.

--- html/code/modules/installer/xaradmin/phase4.php
+++ html/code/modules/installer/xaradmin/phase4.php
@@ -26,35 +26,68 @@
 function installer_admin_phase4()
 {
     if (!file_exists('install.php')) { throw new Exception('Already installed');}
-    xarVar::fetch('install_language','str::',$install_language, 'en_US.utf-8', xarVar::NOT_REQUIRED);
+    xarVar::fetch('install_language',            'str::', $install_language, 'en_US.utf-8', xarVar::NOT_REQUIRED);
+    xarVar::fetch('continue',            'isset', $continue, NULL, xarVar::NOT_REQUIRED);
+    
+    xarVar::fetch('install_database_host',       'str::', $data['database_host'],       xarSystemVars::get(sys::CONFIG, 'DB.Host'), xarVar::NOT_REQUIRED);
+    xarVar::fetch('install_database_middleware', 'str::', $data['database_middleware'], xarSystemVars::get(sys::CONFIG, 'DB.Middleware'), xarVar::NOT_REQUIRED);
+    xarVar::fetch('install_database_type',       'str::', $data['database_type'],       xarSystemVars::get(sys::CONFIG, 'DB.Type'), xarVar::NOT_REQUIRED);
+    xarVar::fetch('install_database_name',       'str::', $data['database_name'],       xarSystemVars::get(sys::CONFIG, 'DB.Name'), xarVar::NOT_REQUIRED);
+    xarVar::fetch('install_database_username',   'str::', $data['database_username'],   xarSystemVars::get(sys::CONFIG, 'DB.UserName'), xarVar::NOT_REQUIRED);
+    xarVar::fetch('install_database_password',   'str::', $data['database_password'],   '', xarVar::NOT_REQUIRED);
+    xarVar::fetch('install_database_prefix',     'str::', $data['database_prefix'],     xarSystemVars::get(sys::CONFIG, 'DB.TablePrefix'), xarVar::NOT_REQUIRED);
+    xarVar::fetch('install_database_charset',    'str::', $data['database_charset'],    xarSystemVars::get(sys::CONFIG, 'DB.Charset'), xarVar::NOT_REQUIRED);
 
-    // Get default values from config files
-    $data['database_host']       = xarSystemVars::get(sys::CONFIG, 'DB.Host');
-    $data['database_username']   = xarSystemVars::get(sys::CONFIG, 'DB.UserName');
-    $data['database_password']   = '';
-    $data['database_name']       = xarSystemVars::get(sys::CONFIG, 'DB.Name');
-    $data['database_prefix']     = xarSystemVars::get(sys::CONFIG, 'DB.TablePrefix');
-    $data['database_type']       = xarSystemVars::get(sys::CONFIG, 'DB.Type');
-    $data['database_charset']    = xarSystemVars::get(sys::CONFIG, 'DB.Charset');
-
-    // Supported  Databases:
-    $data['database_types']      = array('mysqli'      => array('name' => 'MySQL'   , 'available' => extension_loaded('mysqli')),
-                                         //'mysqli'      => array('name' => 'MySQLi' , 'available' => extension_loaded('mysqli')),
-                                         'postgres'    => array('name' => 'Postgres (not supported in this version)', 'available' => extension_loaded('pgsql')),
-                                         //'pdosqlite'   => array('name' => 'PDO SQLite'  , 'available' => extension_loaded('pdo_sqlite')),
-                                         // use portable version of OCI8 driver to support ? bind variables
-                                         'oci8po'      => array('name' => 'Oracle 9+ (not supported)'  , 'available' => extension_loaded('oci8')),
-                                         'mssql'       => array('name' => 'MS SQL Server (not supported)' , 'available' => extension_loaded('mssql')),
+    // Supported Middleware:
+    $data['database_middleware_packages']  = array('Creole' => array('name' => 'Creole', 'available' => true),
+                                                   'PDO'    => array('name' => 'PDO',    'available' => extension_loaded('pdo')),
+                                                   'DBAL'   => array('name' => 'DBAL',   'available' => false),
                                         );
-    if (version_compare(PHP_VERSION,'5.4.0','ge')) {
-        $data['database_types']['sqlite3'] = array('name' => 'SQLite3 (not supported in this version)'  , 'available' => extension_loaded('sqlite3'));
-    } else {
-        $data['database_types']['sqlite']  = array('name' => 'SQLite (not supported in this version)'  , 'available' => extension_loaded('sqlite'));
+    // Supported Databases:
+    switch ($data['database_middleware']) {
+    	case 'Creole':
+			$data['database_types']  = array('mysqli'      => array('name' => 'MySQL'   , 'available' => extension_loaded('mysqli')),
+											 'postgres'    => array('name' => 'Postgres (limited supported in this version)', 'available' => false),
+											 'sqlite3'     => array('name' => 'SQLite (not supported in this version)'  , 'available' => false),
+											 // use portable version of OCI8 driver to support ? bind variables
+											 'oci8po'      => array('name' => 'Oracle 9+ (not supported)'  , 'available' => extension_loaded('oci8')),
+											 'mssql'       => array('name' => 'MS SQL Server (not supported)' , 'available' => extension_loaded('mssql')),
+											);
+		break;
+    	case 'PDO':
+    	default:
+			$data['database_types']  = array('pdomysqli'   => array('name' => 'MySQL'   , 'available' => extension_loaded('pdo_mysql')),
+											 'pdopostgres' => array('name' => 'Postgres (limited supported in this version)', 'available' => false),
+											 'pdosqlite'   => array('name' => 'PDO SQLite (not supported in this version)'  , 'available' => false),
+											);
+			break;
+    	case 'DBAL':
+    		// Nothing yet
+			break;
     }
 
-    $data['language'] = $install_language;
+	// The Continue button was clicked
+	if (isset($continue)) {
+		// Save everything to the configuration file
+		$variables['DB.Middleware'] =  $data['database_middleware'];                   
+		$variables['DB.Type'] =        $data['database_type'];                   
+		$variables['DB.Host'] =        $data['database_host'];                   
+		$variables['DB.UserName'] =    $data['database_username'];                   
+		$variables['DB.Password'] =    $data['database_password'];                   
+		$variables['DB.Name'] =        $data['database_name'];                   
+		$variables['DB.TablePrefix'] = $data['database_prefix'];                   
+		$variables['DB.Charset'] =     $data['database_charset'];
+		xarInstall::apifunc('modifysystemvars', array('variables'=> $variables));
+		
+		// Jump to the next page
+        xarController::redirect(xarServer::getCurrentURL(array('install_phase' => 5)));
+        return true;
+	}
+	
+	$data['language'] = $install_language;
     $data['phase'] = 4;
     $data['phase_label'] = xarML('Step Four');
 
     return $data;
 }
+
